9f86076b0f21b1ba359dedb8a1bd914e40d59a8f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2008-01-12 02:47:02 +0000

    mingw fixes (patch from vadmium)

---
+++ config-example.mk
@@ -0,0 +1,38 @@
+# Configuration make file
+# The commented-out lines show the defaults
+
+# Debug or release mode
+#BUILD = release
+
+# Where to find zlib, JPEG library and Free Type
+# The pre-compiled libraries in subversion have MS Visual C dependencies
+#INC_DIR =
+#LIB_DIR =
+#LDLIBS = -lfreetype -ljpeg -lz
+
+# Use static libraries even if DLLs / shared libraries available
+#LDFLAGS = -static
+
+# Don't use Microsoft's strsafe library
+#CPPFLAGS = -D DISABLE_STRSAFE
+
+#CFLAGS =
+# Configuration make file
+# The commented-out lines show the defaults
+
+# Debug or release mode
+#BUILD = release
+
+# Where to find zlib, JPEG library and Free Type
+# The pre-compiled libraries in subversion have MS Visual C dependencies
+#INC_DIR =
+#LIB_DIR =
+#LDLIBS = -lfreetype -ljpeg -lz
+
+# Use static libraries even if DLLs / shared libraries available
+#LDFLAGS = -static
+
+# Don't use Microsoft's strsafe library
+#CPPFLAGS = -D DISABLE_STRSAFE
+
+#CFLAGS =
