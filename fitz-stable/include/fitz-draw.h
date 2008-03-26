55eb2feef0dff14a94ef49714e419ba489c22246
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2008-03-26 07:23:36 +0000

    imported latest fitz-stable from http://ccxvii.net/darcsweb.cgi?r=fitz-stable;a=commitdiff;h=20080325001951-86a4e-0e0ee25289b10d458465d7a7a9ea12d5919d6fd8.gz

---
+++ fitz-stable/include/fitz-draw.h
@@ -0,0 +1,20 @@
+/*
+ * Rasterizer
+ */
+
+#ifdef _FITZ_DRAW_H_
+#error "fitz-draw.h must only be included once"
+#endif
+#define _FITZ_DRAW_H_
+
+#ifndef _FITZ_BASE_H_
+#error "fitz-base.h must be included before fitz-draw.h"
+#endif
+
+#ifndef _FITZ_WORLD_H_
+#error "fitz-world.h must be included before fitz-draw.h"
+#endif
+
+#include "fitz/draw_path.h"
+#include "fitz/draw_misc.h"
+
