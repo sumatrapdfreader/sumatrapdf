357fa672634f7987a5c87cbb3a046e3b5c418b57
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2008-05-28 03:00:53 +0000

    bump version to 0.8.1

--- src/SumatraPDF.cpp
+++ src/SumatraPDF.cpp
@@ -41,7 +41,7 @@
 #include "client\windows\handler\exception_handler.h"
 #endif
 
-#define CURR_VERSION "0.8.0"
+#define CURR_VERSION "0.8.1"
 
 // this sucks but I don't know any other way
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
