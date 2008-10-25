730ec5b9ab1297f2d684df94e520a1fab0821914
William Blum <will@unknown.com>
2008-10-25 21:07:04 +0000

    Add missing SyncTex files

---
+++ src/synctex_parser_utils.c
@@ -0,0 +1,224 @@
+/* 
+Copyright (c) 2008 jerome DOT laurens AT u-bourgogne DOT fr
+
+This file is part of the SyncTeX package.
+
+Version: 1.5
+See synctex_parser_readme.txt for more details
+
+License:
+--------
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE
+
+Except as contained in this notice, the name of the copyright holder  
+shall not be used in advertising or otherwise to promote the sale,  
+use or other dealings in this Software without prior written  
+authorization from the copyright holder.
+
+*/
+
+/*  In this file, we find all the functions that may depend on the operating system. */
+
+#include <synctex_parser_utils.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include <ctype.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void *_synctex_malloc(size_t size) {
+	void * ptr = malloc(size);
+	if(ptr) {
+/*  In Visual C, bzero is not available */
+#ifdef _MSC_VER
+		memset(ptr,0, size);
+#else
+		bzero(ptr,size);
+#endif
+	}
+	return (void *)ptr;
+}
+
+int _synctex_error(char * reason,...) {
+	va_list arg;
+	int result;
+	va_start (arg, reason);
+#	ifdef _WIN32
+	{/*	This code is contributed by William Blum */
+		char *buff;
+		size_t len;
+		OutputDebugString("SyncTeX ERROR: ");
+		len = _vscprintf(reason, arg) + 1;
+		buff = (char*)malloc( len * sizeof(char) );
+		result = vsprintf(buff, reason, arg) +strlen("SyncTeX ERROR: ");
+		OutputDebugString(buff);
+		OutputDebugString("\n");
+		free(buff);
+	}
+#else
+	result = fprintf(stderr,"SyncTeX ERROR: ");
+	result += vfprintf(stderr, reason, arg);
+	result += fprintf(stderr,"\n");
+#endif
+	va_end (arg);
+	return result;
+}
+
+/*  strip the last extension of the given string, this string is modified! */
+void _synctex_strip_last_path_extension(char * string) {
+	if(NULL != string){
+		char * last_component = NULL;
+		char * last_extension = NULL;
+		char * next = NULL;
+		/*  first we find the last path component */
+		if(NULL == (last_component = strstr(string,"/"))){
+			last_component = string;
+		} else {
+			++last_component;
+			while((next = strstr(last_component,"/"))){
+				last_component = next+1;
+			}
+		}
+#ifdef	_WIN32
+		/*  On Windows, the '\' is also a path separator. */
+		while((next = strstr(last_component,"\\"))){
+			last_component = next+1;
+		}
+#endif
+		/*  then we find the last path extension */
+		if((last_extension = strstr(last_component,"."))){
+			++last_extension;
+			while((next = strstr(last_extension,"."))){
+				last_extension = next+1;
+			}
+			--last_extension;/*  back to the "." */
+			if(last_extension>last_component){/*  filter out paths like ....my/dir/.hidden"*/
+				last_extension[0] = '\0';
+			}
+		}
+	}
+}
+
+/*  Compare two file names, windows is sometimes case insensitive... */
+synctex_bool_t _synctex_is_equivalent_file_name(const char *lhs, const char *rhs) {
+#	if _WIN32
+    /*  On Windows, filename should be compared case insensitive.
+	 *  The characters '/' and '\' are both valid path separators.
+	 *  There will be a very serious problem concerning UTF8 because
+	 *  not all the characters must be toupper...
+	 *  I would like to have URL's instead of filenames. */
+next_character:
+	if(SYNCTEX_IS_PATH_SEPARATOR(*lhs)) {/*  lhs points to a path separator */
+		if(!SYNCTEX_IS_PATH_SEPARATOR(*rhs)) {/*  but not rhs */
+			return synctex_NO;
+		}
+	} else if(SYNCTEX_IS_PATH_SEPARATOR(*rhs)) {/*  rhs points to a path separator but not lhs */
+		return synctex_NO;
+	} else if(toupper(*lhs) != toupper(*rhs)){/*  uppercase do not match */
+		return synctex_NO;
+	} else if (!*lhs) {/*  lhs is at the end of the string */
+		return *rhs ? synctex_NO : synctex_YES;
+	} else if(!*rhs) {/*  rhs is at the end of the string but not lhs */
+		return synctex_NO;
+	}
+	++lhs;
+	++rhs;
+	goto next_character;
+#	else
+    return 0 == strcmp(lhs,rhs)?synctex_YES:synctex_NO;
+#	endif
+}
+
+synctex_bool_t _synctex_path_is_absolute(const char * name) {
+	if(!strlen(name)) {
+		return synctex_NO;
+	}
+#	if _WIN32
+	if(strlen(name)>2) {
+		return (name[1]==':' && SYNCTEX_IS_PATH_SEPARATOR(name[2]))?synctex_YES:synctex_NO;
+	}
+	return synctex_NO;
+#	else
+    return SYNCTEX_IS_PATH_SEPARATOR(name[0])?synctex_YES:synctex_NO;
+#	endif
+}
+
+char * _synctex_last_path_component(const char * name) {
+	const char * c = name+strlen(name);
+	if(c>name) {
+		if(!SYNCTEX_IS_PATH_SEPARATOR(*c)) {
+			do {
+				--c;
+				if(SYNCTEX_IS_PATH_SEPARATOR(*c)) {
+					break; /*  We found the last path separator */
+				}
+			} while(c>name);
+		}
+		return (char *)c+1;/* the last path component is the void string*/
+	}
+	return (char *)c;
+}
+
+int _synctex_copy_with_quoting_last_path_component(const char * src, char ** dest_ref, size_t size) {
+  char * lpc;
+  if(src && dest_ref) {
+#		define dest (*dest_ref)
+		dest = NULL;	/*	Default behavior: no change and sucess. */
+		lpc = _synctex_last_path_component(src);
+		if(strlen(lpc)) {
+			if(strchr(lpc,' ') && lpc[0]!='"' && lpc[strlen(lpc)-1]!='"') {
+				/*	We are in the situation where adding the quotes is allowed.	*/
+				/*	Time to add the quotes.	*/
+				/*  Consistency test: we must have dest+size>dest+strlen(dest)+2
+				 *	or equivalently: strlen(dest)+2<size (see below) */
+				if(strlen(src)<size) {
+					if((dest = (char *)malloc(size+2))) {
+						if(dest != strncpy(dest,src,size)) {
+							_synctex_error("!  _synctex_copy_with_quoting_last_path_component: Copy problem");
+							free(dest);
+							dest = NULL;/*  Don't forget to reinitialize. */
+							return -2;
+						}
+						lpc += dest - src;	/*	Now lpc is the last path component of dest.	*/
+						memmove(lpc+1,lpc,strlen(lpc)+1);	/*	Also move the null terminating character. */
+						lpc[0]='"';
+						lpc[strlen(lpc)+1]='\0';/*	Consistency test */
+						lpc[strlen(lpc)]='"';
+						return 0;	/*	Success. */
+					}
+					return -1;	/*	Memory allocation error.	*/
+				}
+				_synctex_error("!  _synctex_copy_with_quoting_last_path_component: Internal inconsistency");
+				return -3;
+			}
+			return 0;	/*	Success. */
+		}
+		return 0;	/*	No last path component. */
+	}
+	return 1; /*  Bad parameter, this value is subject to changes. */
+}
---
+++ src/synctex_parser_utils.h
@@ -0,0 +1,104 @@
+/* 
+Copyright (c) 2008 jerome DOT laurens AT u-bourgogne DOT fr
+
+This file is part of the SyncTeX package.
+
+Version: 1.5
+See synctex_parser_readme.txt for more details
+
+License:
+--------
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE
+
+Except as contained in this notice, the name of the copyright holder  
+shall not be used in advertising or otherwise to promote the sale,  
+use or other dealings in this Software without prior written  
+authorization from the copyright holder.
+
+*/
+
+/*  The utilities declared here are subject to conditional implementation.
+ *  All the operating system special stuff goes here. */
+
+#   define synctex_bool_t int
+#   define synctex_YES -1
+#   define synctex_NO 0
+
+#ifndef __SYNCTEX_PARSER_UTILS__
+#   define __SYNCTEX_PARSER_UTILS__
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#	if _WIN32
+#		define SYNCTEX_IS_PATH_SEPARATOR(c) ('/' == c || '\\' == c)
+#	else
+#		define SYNCTEX_IS_PATH_SEPARATOR(c) ('/' == c)
+#	endif
+
+/*  This custom malloc functions initializes to 0 the newly allocated memory.
+ *  There is no bzero function on windows. */
+void *_synctex_malloc(size_t size);
+
+/*  This is used to log some informational message to the standard error stream.
+ *  On Windows, the stderr stream is not exposed and another method is used.
+ *	The return value is the number of characters printed.	*/
+int _synctex_error(char * reason,...);
+
+/*  strip the last extension of the given string, this string is modified!
+ *  This function depends on the OS because the path separator may differ.
+ *  This should be discussed more precisely. */
+void _synctex_strip_last_path_extension(char * string);
+
+/*  Compare two file names, windows is sometimes case insensitive...
+ *  The given strings may differ stricto sensu, but represent the same file name.
+ *  It might not be the real way of doing things.
+ *  The return value is an undefined non 0 value when the two file names are equivalent.
+ *  It is 0 otherwise. */
+synctex_bool_t _synctex_is_equivalent_file_name(const char *lhs, const char *rhs);
+
+/*	Description forthcoming.*/
+synctex_bool_t _synctex_path_is_absolute(const char * name);
+
+/*	Description forthcoming...*/
+char * _synctex_last_path_component(const char * name);
+
+/*	If the core of the last path component of src is not already enclosed with double quotes ('"')
+ *  and contains a space character (' '), then a new buffer is created, the src is copied and quotes are added.
+ *	In all other cases, no destination buffer is created and the src is not copied.
+ *  0 on success, which means no error, something non 0 means error, mainly due to memory allocation failure, or bad parameter.
+ *  This is used to fix a bug in the first version of pdftex with synctex (1.40.9) for which names with spaces
+ *  were not managed in a standard way.
+ *  On success, the caller owns the buffer pointed to by dest_ref (is any) and
+ *  is responsible of freeing the memory when done.
+ *	The size argument is the size of the src buffer. On return the dest_ref points to a buffer sized size+2.*/
+int _synctex_copy_with_quoting_last_path_component(const char * src, char ** dest_ref, size_t size);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
