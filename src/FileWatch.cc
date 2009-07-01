sumatrapdfreader/sumatrapdf

commit 3bf934a1a5457ffa2ac3849f4cc5c791b9d6892d

TCHARs for FileHistory.cc and FileWatch.cc
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@1177 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/FileWatch.cc
@@ -1,4 +1,8 @@
 // By william blum, 2008
+
+#define UNICODE
+#define _UNICODE
+
 #include "SumatraPDF.h"
 #include "FileWatch.h"
 #include "file_util.h"
@@ -10,14 +14,14 @@
 #include "base_util.h"
 
 // Get the directory name from a full file path and copy it to pszDir
-bool GetDirectoryW(LPCWSTR pszFile, PWSTR pszDir, size_t cchDir)
+bool GetDirectory(LPCTSTR pszFile, PTSTR pszDir, size_t cchDir)
 {
-    LPCWSTR pszBaseName = FilePathW_GetBaseName(pszFile);
+    LPCTSTR pszBaseName = FilePath_GetBaseName(pszFile);
 
     if (0 == pszDir || 0 == pszFile) {
         return false;
     }
-    if (!wstr_copyn(pszDir, cchDir, pszFile, pszBaseName-pszFile)) {
+    if (!tstr_copyn(pszDir, cchDir, pszFile, pszBaseName-pszFile)) {
         return false;
     }
 
@@ -107,16 +111,16 @@ void FileWatcher::Init(LPCWSTR filefullpath)
     if (IsThreadRunning())
         SynchronousAbort();
 
-    wstr_copy(szFilepath, dimof(szFilepath), filefullpath);
-    pszFilename = FilePathW_GetBaseName(szFilepath);
-    GetDirectoryW(filefullpath, szDir, dimof(szDir));
+    tstr_copy(szFilepath, dimof(szFilepath), filefullpath);
+    pszFilename = FilePath_GetBaseName(szFilepath);
+    GetDirectory(filefullpath, szDir, dimof(szDir));
     
-    _wstat(filefullpath, &timestamp);
+    _tstat(filefullpath, &timestamp);
 
     callbackparam = 0;
     pCallback = NULL;
 
-    hDir = CreateFileW(
+    hDir = CreateFile(
         szDir, // pointer to the directory containing the tex files
         FILE_LIST_DIRECTORY,                // access (read-write) mode
         FILE_SHARE_READ|FILE_SHARE_DELETE|FILE_SHARE_WRITE,  // share mode
@@ -223,10 +227,10 @@ bool FileWatcher::ReadDir()
     FILE_NOTIFY_INFORMATION *pFileNotify;
     pFileNotify = (PFILE_NOTIFY_INFORMATION)&buffer[1-curBuffer];
     while (pFileNotify) {
-        pFileNotify->FileName[min(pFileNotify->FileNameLength/sizeof(WCHAR), _MAX_FNAME-1)] = 0;
+        pFileNotify->FileName[min(pFileNotify->FileNameLength/sizeof(TCHAR), _MAX_FNAME-1)] = 0;
 
         // is it the file that is being watched?
-        if (_wcsicmp(pFileNotify->FileName, pszFilename) == 0) {
+        if (_tcsicmp(pFileNotify->FileName, pszFilename) == 0) {
             // file modified?
             if (pFileNotify->Action == FILE_ACTION_MODIFIED) {
 #if 0

baseutils/file_util.c
@@ -21,9 +21,9 @@ char *FilePath_ConcatA(const char *path, const char *name)
         return str_cat3(path, DIR_SEP_STR, name);
 }
 
-const char *FilePath_GetBaseName(const char *path)
+const TCHAR *FilePath_GetBaseName(const TCHAR *path)
 {
-    const char *fileBaseName = path + strlen(path);
+    const TCHAR *fileBaseName = path + lstrlen(path);
     while (fileBaseName > path) {
         if (char_is_dir_sep(fileBaseName[-1])) {
             return fileBaseName;

baseutils/file_util.h
@@ -47,7 +47,7 @@ void            FileList_Delete(FileList *fl);
 int             FileList_Len(FileList *fl);
 FileInfo *      FileList_GetFileInfo(FileList *fl, int file_no);
 
-const char *    FilePath_GetBaseName(const char *path);
+const TCHAR *   FilePath_GetBaseName(const TCHAR *path);
 const WCHAR *   FilePathW_GetBaseName(const WCHAR *path);
 char *          FilePath_GetDir(const char *path);
 WCHAR *         FilePathW_GetDir(const WCHAR *path);

src/FileHistory.cc
@@ -1,7 +1,12 @@
 /* Copyright Krzysztof Kowalczyk 2006-2009
    License: GPLv2 */
+
+#define UNICODE
+#define _UNICODE
+
 #include "FileHistory.h"
-#include "wstr_util.h"
+#include "tstr_util.h"
+// TODO: Move DBG_OUT out of str_util.h
 #include "str_util.h"
 
 #include <string.h>
@@ -47,7 +52,7 @@ FileHistoryList *FileHistoryList_Node_Create(void)
     return node;
 }
 
-FileHistoryList *FileHistoryList_Node_CreateFromFilePath(const WCHAR *filePath)
+FileHistoryList *FileHistoryList_Node_CreateFromFilePath(const TCHAR *filePath)
 {
     FileHistoryList *node;
 
@@ -56,7 +61,7 @@ FileHistoryList *FileHistoryList_Node_CreateFromFilePath(const WCHAR *filePath)
     if (!node)
         return NULL;
 
-    node->state.filePath = (const WCHAR*)wstr_dup(filePath);
+    node->state.filePath = (const TCHAR*)tstr_dup(filePath);
     if (!node->state.filePath)
         goto Error;
     return node;
@@ -116,7 +121,7 @@ void FileHistoryList_Node_Append(FileHistoryList **root, FileHistoryList *node)
     curr->next = node;
 }
 
-FileHistoryList *FileHistoryList_Node_FindByFilePath(FileHistoryList **root, const WCHAR *filePath)
+FileHistoryList *FileHistoryList_Node_FindByFilePath(FileHistoryList **root, const TCHAR *filePath)
 {
     FileHistoryList *curr;
 
@@ -128,7 +133,7 @@ FileHistoryList *FileHistoryList_Node_FindByFilePath(FileHistoryList **root, con
     curr = *root;
     while (curr) {
         assert(curr->state.filePath);
-        if (wstr_ieq(filePath, curr->state.filePath))
+        if (tstr_ieq(filePath, curr->state.filePath))
             return curr;
         curr = curr->next;
     }
@@ -136,7 +141,7 @@ FileHistoryList *FileHistoryList_Node_FindByFilePath(FileHistoryList **root, con
     return NULL;
 }
 
-BOOL FileHistoryList_Node_RemoveByFilePath(FileHistoryList **root, const WCHAR *filePath)
+BOOL FileHistoryList_Node_RemoveByFilePath(FileHistoryList **root, const TCHAR *filePath)
 {
     FileHistoryList *node;
 

commit 116c47af3b2a99f43f16e0ad8cbcdaacfa37f7e0

fix issue 552: File watching destroyed starting from rev r1162 (actual regression in r1177)

src/FileWatch.cc
@@ -16,7 +16,8 @@
 // Get the directory name from a full file path and copy it to pszDir
 bool GetDirectory(LPCTSTR pszFile, PTSTR pszDir, size_t cchDir)
 {
-    LPCTSTR pszBaseName = FilePath_GetBaseName(pszFile);
+    // TODO: Compile file_util.c as UNICODE
+    LPCTSTR pszBaseName = FilePathW_GetBaseName(pszFile);
 
     if (0 == pszDir || 0 == pszFile) {
         return false;
@@ -112,7 +113,7 @@ void FileWatcher::Init(LPCWSTR filefullpath)
         SynchronousAbort();
 
     tstr_copy(szFilepath, dimof(szFilepath), filefullpath);
-    pszFilename = FilePath_GetBaseName(szFilepath);
+    pszFilename = FilePathW_GetBaseName(szFilepath);
     GetDirectory(filefullpath, szDir, dimof(szDir));
     
     _tstat(filefullpath, &timestamp);