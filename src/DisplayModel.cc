sumatrapdfreader/sumatrapdf

commit 91de67fa4a0e99ae96f62479ddbb08f391f8cc02

fix issue 33: Remember page and scroll y in page coordinates instead of absolute scroll y
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@1268 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/DisplayModel.cc
@@ -137,22 +137,16 @@ bool displayStateFromDisplayModel(DisplayState *ds, DisplayModel *dm)
     if (!ds->filePath)
         return FALSE;
     ds->displayMode = dm->displayMode();
-    ds->pageNo = dm->currentPageNo();
     ds->rotation = dm->rotation();
     ds->zoomVirtual = dm->zoomVirtual();
-    ds->scrollX = (int)dm->areaOffset.x;
-    if (displayModeContinuous(dm->displayMode())) {
-        /* TODO: should be offset of top page */
-        PdfPageInfo *pageInfo = dm->getPageInfo(ds->pageNo);
-        if (pageInfo)                
-            ds->scrollY = (int)(dm->areaOffset.y + PADDING_PAGE_BORDER_TOP - pageInfo->currPosY);
-        else
-            ds->scrollY = 0;
-
-    } else {
-        ds->scrollY = (int)dm->areaOffset.y;
-    }
     ds->showToc = dm->_showToc;
+
+    ScrollState ss;
+    dm->getScrollState(&ss);
+    ds->pageNo = ss.page;
+    ds->scrollX = (int)ss.x;
+    ds->scrollY = (int)ss.y;
+
     return TRUE;
 }
 
@@ -851,25 +845,12 @@ void DisplayModel::changeTotalDrawAreaSize(SizeD totalDrawAreaSize)
 {
     int     newPageNo;
     int     currPageNo;
+    ScrollState ss;
 
-    currPageNo = currentPageNo();
-
+    getScrollState(&ss);
     setTotalDrawAreaSize(totalDrawAreaSize);
-
     relayout(zoomVirtual(), rotation());
-    recalcVisibleParts();
-    recalcLinksCanvasPos();
-    renderVisibleParts();
-    setScrollbarsState();
-    newPageNo = currentPageNo();
-    if (newPageNo != currPageNo) {
-        // TODO: Try to even better keep the current position during resizing
-        if (currPageNo != -1)
-            goToPage(currPageNo, 0);
-        else
-            pageChanged();
-    }
-    repaintDisplay(true);
+    setScrollState(&ss);
 }
 
 void DisplayModel::goToPage(int pageNo, int scrollY, int scrollX)
@@ -1134,10 +1115,11 @@ void DisplayModel::scrollYByAreaDy(bool forward, bool changePage)
 
 void DisplayModel::zoomTo(double zoomVirtual)
 {
+    ScrollState ss;
+    getScrollState(&ss);
     //DBG_OUT("DisplayModel::zoomTo() zoomVirtual=%.6f\n", _zoomVirtual);
-    int currPageNo = currentPageNo();
     relayout(zoomVirtual, rotation());
-    goToPage(currPageNo, 0);
+    setScrollState(&ss);
 }
 
 void DisplayModel::zoomBy(double zoomFactor)
@@ -1713,3 +1695,34 @@ int DisplayModel::getLinkCount()
     return count;
 }
 
+bool DisplayModel::getScrollState(ScrollState *state)
+{
+    state->page = currentPageNo();
+    PdfPageInfo *pageInfo = getPageInfo(state->page);
+    state->x = max(pageInfo->screenX - pageInfo->bitmapX, 0);
+    state->y = max(pageInfo->screenY - pageInfo->bitmapY, 0);
+    
+    return cvtScreenToUser(&state->page, &state->x, &state->y);
+}
+
+void DisplayModel::setScrollState(ScrollState *state)
+{
+    // Update the internal metrics first
+    goToPage(state->page, 0);
+    // Bail out, if the page wasn't scrolled
+    if (0 == state->x && 0 == state->y)
+        return;
+
+    PdfPageInfo *pageInfo = getPageInfo(state->page);
+    double x = max(pageInfo->screenX - pageInfo->bitmapX, 0);
+    double y = max(pageInfo->screenY - pageInfo->bitmapY, 0);
+
+    double newX = state->x;
+    double newY = state->y;
+    cvtUserToScreen(state->page, &newX, &newY);
+    // TODO: why is this needed?
+    if (abs(newY - y) > 1)
+        newY += PADDING_PAGE_BORDER_TOP;
+
+    goToPage(state->page, newY - y, areaOffset.x + newX - x);
+}

src/DisplayModel.h
@@ -119,6 +119,13 @@ typedef struct SearchStateData {
     int             currPage; /* page for the last hit */
 } SearchStateData;
 
+/* The current scroll state (needed for saving/restoring the scroll position) */
+typedef struct ScrollState {
+    int page;
+    double x; /* in user space units (per page) */
+    double y; /* in user space units (per page) */
+} ScrollState;
+
 /* Information needed to drive the display of a given PDF document on a screen.
    You can think of it as a model in the MVC pardigm.
    All the display changes should be done through changing this model via
@@ -261,6 +268,9 @@ public:
 
     void            rebuildLinks();
     void            handleLink2(pdf_link* link);
+
+    bool            getScrollState(ScrollState *state);
+    void            setScrollState(ScrollState *state);
 
 protected:
 

src/DisplayState.h
@@ -32,8 +32,8 @@ enum DisplayMode {
 #define PAGE_NO_STR                 "Page"
 #define ZOOM_VIRTUAL_STR            "ZoomVirtual"
 #define ROTATION_STR                "Rotation"
-#define SCROLL_X_STR                "Scroll X"
-#define SCROLL_Y_STR                "Scroll Y"
+#define SCROLL_X_STR                "Scroll X2"
+#define SCROLL_Y_STR                "Scroll Y2"
 #define WINDOW_STATE_STR            "Window State"
 #define WINDOW_X_STR                "Window X"
 #define WINDOW_Y_STR                "Window Y"

src/SumatraPDF.cpp
@@ -2243,6 +2243,7 @@ static bool LoadPdfIntoWindow(
              UI properly */
     DisplayMode displayMode = gGlobalPrefs.m_defaultDisplayMode;
     int startPage = 1;
+    ScrollState ss = { 1, 0.0, 0.0 };
     int scrollbarYDx = 0;
     int scrollbarXDy = 0;
     bool showAsFullScreen = WIN_STATE_FULLSCREEN == gGlobalPrefs.m_windowState;
@@ -2293,15 +2294,17 @@ static bool LoadPdfIntoWindow(
 
     win->dm->setAppData((void*)win);
 
-    int offsetX = 0;
-    int offsetY = 0;
     double zoomVirtual = gGlobalPrefs.m_defaultZoom;
     int rotation = DEFAULT_ROTATION;
 
     win->state = WS_SHOWING_PDF;
     if (state) {
-        offsetX = state->scrollX;
-        offsetY = state->scrollY;
+        if (win->dm->validPageNo(startPage)) {
+            ss.page = startPage;
+            /* TODO: make sure scrollX isn't bogus */
+            ss.x = state->scrollX;
+            ss.y = state->scrollY;
+        }
         zoomVirtual = state->zoomVirtual;
         rotation = state->rotation;
         win->dm->_showToc = state->showToc;
@@ -2323,10 +2326,7 @@ static bool LoadPdfIntoWindow(
     ZoomMenuItemCheck(win->hMenu, menuId, TRUE);
 
     win->dm->relayout(zoomVirtual, rotation);
-    if (!win->dm->validPageNo(startPage))
-        startPage = 1;
-    /* TODO: make sure offsetX isn't bogus */
-    win->dm->goToPage(startPage, offsetY, offsetX);
+    win->dm->setScrollState(&ss);
 
     if (!is_new_window) {
         WindowInfo_RedrawAll(win);

commit 550bb68c0549cb35092ef7bc61b904d54b52e157

fix startup assertion introduced in r1268

src/DisplayModel.cc
@@ -843,14 +843,17 @@ void DisplayModel::renderVisibleParts(void)
 
 void DisplayModel::changeTotalDrawAreaSize(SizeD totalDrawAreaSize)
 {
-    int     newPageNo;
-    int     currPageNo;
     ScrollState ss;
 
-    getScrollState(&ss);
+    bool isDocLoaded = getScrollState(&ss);
     setTotalDrawAreaSize(totalDrawAreaSize);
     relayout(zoomVirtual(), rotation());
-    setScrollState(&ss);
+    if (isDocLoaded) {
+        setScrollState(&ss);
+    } else {
+        recalcVisibleParts();
+        setScrollbarsState();
+    }
 }
 
 void DisplayModel::goToPage(int pageNo, int scrollY, int scrollX)
@@ -1698,6 +1701,9 @@ int DisplayModel::getLinkCount()
 bool DisplayModel::getScrollState(ScrollState *state)
 {
     state->page = currentPageNo();
+    if (state->page <= 0)
+        return false;
+
     PdfPageInfo *pageInfo = getPageInfo(state->page);
     state->x = max(pageInfo->screenX - pageInfo->bitmapX, 0);
     state->y = max(pageInfo->screenY - pageInfo->bitmapY, 0);