1316f48b3ef625be0327311d6cab17bc53918104
Simon Bunzli <zeniko@gmail.com>
2009-08-02 01:42:19 +0000

    revert accidental checkin in r1347

--- src/DisplayModel.cc
+++ src/DisplayModel.cc
@@ -63,8 +63,6 @@ static int cacheMutexInitialized = 0;
 static BitmapCacheEntry *   gBitmapCache[MAX_BITMAPS_CACHED] = {0};
 static int                  gBitmapCacheCount = 0;
 
-static bool bookMode = true;
-
 DisplaySettings gDisplaySettings = {
   PADDING_PAGE_BORDER_TOP_DEF,
   PADDING_PAGE_BORDER_BOTTOM_DEF,
