130c9a541b015a9ff64555e34c02424d22a5866b
Simon Bunzli <zeniko@gmail.com>
2009-12-05 02:06:27 +0000

    fix issue 756: Colors drawn incorrectly

--- mupdf/mupdf/pdf_colorspace2.c
+++ mupdf/mupdf/pdf_colorspace2.c
@@ -135,6 +135,67 @@ void pdf_convpixmap(fz_colorspace *ss, fz_pixmap *sp, fz_colorspace *ds, fz_pixm
 	else fz_stdconvpixmap(ss, sp, ds, dp);
 }
 
+/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=756 */
+/* function adapted from Poppler's GfxState_helpers.h
+   Poppler is licensed under GPL, see http://poppler.freedesktop.org/ */
+static inline void cmykToRGBMatrixMultiplication(float *sv, float *dv)
+{
+	float c = sv[0], m = sv[1], y = sv[2], k = sv[3];
+	float c1 = 1 - c, m1 = 1 - m, y1 = 1 - y, k1 = 1 - k;
+	float r, g, b, x;
+
+	// this is a matrix multiplication, unrolled for performance
+	//                        C M Y K
+	x = c1 * m1 * y1 * k1; // 0 0 0 0
+	r = g = b = x;
+	x = c1 * m1 * y1 * k;  // 0 0 0 1
+	r += 0.1373 * x;
+	g += 0.1216 * x;
+	b += 0.1255 * x;
+	x = c1 * m1 * y  * k1; // 0 0 1 0
+	r += x;
+	g += 0.9490 * x;
+	x = c1 * m1 * y  * k;  // 0 0 1 1
+	r += 0.1098 * x;
+	g += 0.1020 * x;
+	x = c1 * m  * y1 * k1; // 0 1 0 0
+	r += 0.9255 * x;
+	b += 0.5490 * x;
+	x = c1 * m  * y1 * k;  // 0 1 0 1
+	r += 0.1412 * x;
+	x = c1 * m  * y  * k1; // 0 1 1 0
+	r += 0.9294 * x;
+	g += 0.1098 * x;
+	b += 0.1412 * x;
+	x = c1 * m  * y  * k;  // 0 1 1 1
+	r += 0.1333 * x;
+	x = c  * m1 * y1 * k1; // 1 0 0 0
+	g += 0.6784 * x;
+	b += 0.9373 * x;
+	x = c  * m1 * y1 * k;  // 1 0 0 1
+	g += 0.0588 * x;
+	b += 0.1412 * x;
+	x = c  * m1 * y  * k1; // 1 0 1 0
+	g += 0.6510 * x;
+	b += 0.3137 * x;
+	x = c  * m1 * y  * k;  // 1 0 1 1
+	g += 0.0745 * x;
+	x = c  * m  * y1 * k1; // 1 1 0 0
+	r += 0.1804 * x;
+	g += 0.1922 * x;
+	b += 0.5725 * x;
+	x = c  * m  * y1 * k;  // 1 1 0 1
+	b += 0.0078 * x;
+	x = c  * m  * y  * k1; // 1 1 1 0
+	r += 0.2118 * x;
+	g += 0.2119 * x;
+	b += 0.2235 * x;
+
+	dv[0] = MIN(MAX(r, 0), 1);
+	dv[1] = MIN(MAX(g, 0), 1);
+	dv[2] = MIN(MAX(b, 0), 1);
+}
+
 void pdf_convcolor(fz_colorspace *ss, float *sv, fz_colorspace *ds, float *dv)
 {
 
@@ -189,9 +250,7 @@ void pdf_convcolor(fz_colorspace *ss, float *sv, fz_colorspace *ds, float *dv)
 		}
 		else if (ds == pdf_devicergb)
 		{
-			dv[0] = 1.0 - MIN(sv[0] + sv[3], 1.0);
-			dv[1] = 1.0 - MIN(sv[1] + sv[3], 1.0);
-			dv[2] = 1.0 - MIN(sv[2] + sv[3], 1.0);
+			cmykToRGBMatrixMultiplication(sv, dv); /* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=756 */
 		}
 		else
 			fz_stdconvcolor(ss, sv, ds, dv);
