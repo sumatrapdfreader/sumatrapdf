1652086e11c2e970ed55ee7270174186376f267d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-05-21 07:28:09 +0000

    restore my hand-written Makefile from before it was overwritten by mupdf's version

--- mupdf/builddbg.sh
+++ mupdf/builddbg.sh
@@ -1,8 +1,8 @@
 EXT=../ext
-#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg6b JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=dbg64 -j2
-#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg6b JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=dbg -j2
+#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg-8a JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=dbg64 -j2 -f Makefile.kjk
+#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg-8a JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=dbg -j2 -f Makefile.kjk
 
-#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg6b JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=rel -j4
-#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg6b JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=dbg -j4
+#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg-8a JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=rel -j4 -f Makefile.kjk
+#make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg-8a JBIG2_DIR=$EXT/jbig2dec JASPER_DIR=$EXT/jasper/src/libjasper CFG=dbg -j4 -f Makefile.kjk
 
-make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg6b JBIG2_DIR=$EXT/jbig2dec OPENJPEG_DIR=$EXT/openjpeg-1.3/libopenjpeg CFG=dbg -j4
+make ZLIB_DIR=$EXT/zlib-1.2.3 JPEG_DIR=$EXT/jpeg-8a JBIG2_DIR=$EXT/jbig2dec OPENJPEG_DIR=$EXT/openjpeg-1.3/libopenjpeg CFG=dbg -j4 -f Makefile.kjk
