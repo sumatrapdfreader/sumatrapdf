a8b7d93b0e3a79bc2a906486dc83f87c652f5260
Simon Bunzli <zeniko@gmail.com>
2010-10-16 22:33:26 +0000

    speed up certain cases of pixmap color conversion

--- mupdf/fitz/res_colorspace.c
+++ mupdf/fitz/res_colorspace.c
@@ -402,6 +402,11 @@ fz_stdconvpixmap(fz_pixmap *src, fz_pixmap *dst)
 	unsigned char *s = src->samples;
 	unsigned char *d = dst->samples;
 
+	/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=691637 */
+	/* memoize fz_convertcolor, as it can be quite inefficient for PostScript functions */
+	fz_hashtable *colors = fz_newhash(509, src->n - 1);
+	unsigned char *c;
+
 	assert(src->w == dst->w && src->h == dst->h);
 	assert(src->n == ss->n + 1);
 	assert(dst->n == ds->n + 1);
@@ -410,6 +415,16 @@ fz_stdconvpixmap(fz_pixmap *src, fz_pixmap *dst)
 	{
 		for (x = 0; x < src->w; x++)
 		{
+			/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=691637 */
+			if ((c = fz_hashfind(colors, s)))
+			{
+				memcpy(d, c, dst->n - 1);
+				s += src->n - 1;
+				d += dst->n - 1;
+				*d++ = *s++;
+				continue;
+			}
+
 			for (k = 0; k < src->n - 1; k++)
 				srcv[k] = *s++ / 255.0f;
 
@@ -419,8 +434,12 @@ fz_stdconvpixmap(fz_pixmap *src, fz_pixmap *dst)
 				*d++ = dstv[k] * 255;
 
 			*d++ = *s++;
+
+			fz_hashinsert(colors, s - src->n, d - dst->n);
 		}
 	}
+
+	fz_freehash(colors);
 }
 
 void
