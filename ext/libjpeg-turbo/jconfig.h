3d56326ccbe9d73ac5f087bca2644c096b493383
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-12-07 06:10:00 +0000

    fix jpeg-turbo compilation (was picking up C files from libjpeg)

--- ext/libjpeg-turbo/jconfig.h
+++ ext/libjpeg-turbo/jconfig.h
@@ -1,29 +1,28 @@
 /* Version ID for the JPEG library.
  * Might be useful for tests like "#if JPEG_LIB_VERSION >= 60".
  */
-#define JPEG_LIB_VERSION  62	/* Version 6b */
+#define JPEG_LIB_VERSION  80
+ 
+#define C_ARITH_CODING_SUPPORTED  1
+#define D_ARITH_CODING_SUPPORTED  1
 
-#define C_ARITH_CODING_SUPPORTED
-#define D_ARITH_CODING_SUPPORTED
-
-#define HAVE_PROTOTYPES
-#define HAVE_UNSIGNED_CHAR
-#define HAVE_UNSIGNED_SHORT
+#define HAVE_PROTOTYPES           1
+#define HAVE_UNSIGNED_CHAR        1
+#define HAVE_UNSIGNED_SHORT       1
 #undef __CHAR_UNSIGNED__
-#define HAVE_STDDEF_H
-#define HAVE_STDLIB_H
+#define HAVE_STDDEF_H             1
+#define HAVE_STDLIB_H             1
+
 #undef NEED_BSD_STRINGS
 #undef NEED_SYS_TYPES_H
 #undef NEED_SHORT_EXTERNAL_NAMES
 
 #undef INCOMPLETE_TYPES_BROKEN
 
-#define WITH_SIMD
-
+#define WITH_SIMD                 1
 
 /* Define if shift is unsigned */
 #undef RIGHT_SHIFT_IS_UNSIGNED
-
 
 /* Define to empty if `const' does not conform to ANSI C. */
 #undef const
