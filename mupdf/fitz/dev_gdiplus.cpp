955113a611d5be0d7c1780023443e08ad56cb322
Simon Bunzli <zeniko@gmail.com>
2010-12-07 17:25:01 +0000

    GDI+ update

--- mupdf/fitz/dev_gdiplus.cpp
+++ mupdf/fitz/dev_gdiplus.cpp
@@ -231,23 +231,24 @@ public:
 			0.0f, 0.0f, 0.0f, 0.0f, 1.0f
 		};
 		imgAttrs.SetColorMatrix(&matrix, ColorMatrixFlagsDefault, ColorAdjustTypeBitmap);
+		// imgAttrs.SetWrapMode(WrapModeClamp);
 		
 		float scale = 2.0 * fz_matrixexpansion(ctm) / sqrtf(image->w * image->w + image->h * image->h);
 		if (scale < 1.0 && MIN(image->w, image->h) > 10)
 		{
-			float w = image->w * scale;
-			float h = image->h * scale;
+			int w = ceilf(image->w * scale);
+			int h = ceilf(image->h * scale);
 			Bitmap *scaled = new Bitmap(w, h);
 			
 			Graphics g2(scaled);
 			_setup(&g2);
-			g2.DrawImage(&PixmapBitmap(image), -0.5f, -0.5f, w, h);
+			g2.DrawImage(&PixmapBitmap(image), 0, 0, w, h);
 			
-			graphics->DrawImage(scaled, corners, 3, -0.5f, -0.5f, w, h, UnitPixel, &imgAttrs);
+			graphics->DrawImage(scaled, corners, 3, 0, 0, w, h, UnitPixel, &imgAttrs);
 			delete scaled;
 		}
 		else
-			graphics->DrawImage(&PixmapBitmap(image), corners, 3, -0.5f, -0.5f, image->w, image->h, UnitPixel, &imgAttrs);
+			graphics->DrawImage(&PixmapBitmap(image), corners, 3, 0., 0., image->w, image->h, UnitPixel, &imgAttrs);
 	}
 
 	float getAlpha(float alpha=1.0) { return stack->alpha * alpha; }
@@ -261,6 +262,7 @@ protected:
 		graphics->SetInterpolationMode(InterpolationModeHighQualityBicubic);
 		graphics->SetSmoothingMode(SmoothingModeHighQuality);
 		graphics->SetTextRenderingHint(TextRenderingHintAntiAlias);
+		graphics->SetPixelOffsetMode(PixelOffsetModeHighQuality);
 		
 		return graphics;
 	}
