44203e1266cff03f362636d9101c1b526f80cadd
Simon Bunzli <zeniko@gmail.com>
2011-02-04 08:43:11 +0000

    change version number to 1.4

--- src/Version.h
+++ src/Version.h
@@ -3,7 +3,7 @@
 
 // CURR_VERSION can be over-written externally (via makefile)
 #ifndef CURR_VERSION
-#define CURR_VERSION 1.3
+#define CURR_VERSION 1.4
 #endif
 
 // #define SVN_PRE_RELEASE_VER 2295
@@ -21,12 +21,12 @@
   // hack: adds " (dbg)" after the version
   #define CURR_VERSION_STR _T(QM3(CURR_VERSION, \x20, (dbg)))
  #endif
- #define VER_RESOURCE      1,3,0,0
+ #define VER_RESOURCE      1,4,0,0
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., 0)
  #define UPDATE_CHECK_VER  _T(QM(CURR_VERSION))
 #else
  #define CURR_VERSION_STR  _T(QM3(CURR_VERSION, ., SVN_PRE_RELEASE_VER))
- #define VER_RESOURCE      1,3,0,SVN_PRE_RELEASE_VER
+ #define VER_RESOURCE      1,4,0,SVN_PRE_RELEASE_VER
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., SVN_PRE_RELEASE_VER)
  #define UPDATE_CHECK_VER  _T(QM(SVN_PRE_RELEASE_VER))
 #endif
