0068ae7182a6dadd2180dba5906eaae4a4426e54
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-02-06 01:32:05 +0000

    remove redirects to NUL because it also silences python's error messages, which makes debugging scripts impossible

--- scripts/vc.bat
+++ scripts/vc.bat
@@ -10,22 +10,22 @@ REM otherwise fall back to VS 2005 or use any later version
 
 :TRY_VS9
 REM "%VS90COMNTOOLS%vsvars32.bat"
-"%ProgramFiles%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat" 2>NUL
+"%ProgramFiles%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 IF NOT ERRORLEVEL 1 EXIT /B
 
 :TRY_VS9_X86
-"%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat" 2>NUL
+"%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 IF NOT ERRORLEVEL 1 EXIT /B
 
 :TRY_VS8
 REM "%VS80COMNTOOLS%vsvars32.bat"
-"%ProgramFiles%\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat" 2>NUL
+"%ProgramFiles%\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat"
 IF NOT ERRORLEVEL 1 EXIT /B
 
 REM Note: Visual Studio 2010 doesn't support Windows 2000
 
 :TRY_VS10
-"%ProgramFiles%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat" 2>NUL
+"%ProgramFiles%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
 IF NOT ERRORLEVEL 1 EXIT /B
 
 REM Fail, if no Visual Studio installation has been found
