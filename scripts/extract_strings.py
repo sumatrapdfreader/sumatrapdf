dfd24959648c76685c83f8d3d180193164ff1c4d
Simon Bunzli <zeniko@gmail.com>
2011-03-06 23:39:19 +0000

    follow up to r2962

--- scripts/extract_strings.py
+++ scripts/extract_strings.py
@@ -10,7 +10,7 @@ about untranslated strings to stdout and adds untranslated strings as
 comments at the end of strings file for each language.
 """
 
-C_FILES_TO_PROCESS = ["SumatraPDF.cpp", "SumatraAbout.cpp", "SumatraProperties.cpp", "SumatraDialogs.cc", "CrashHandler.cpp", "AppTools.cc"]
+C_FILES_TO_PROCESS = ["SumatraPDF.cpp", "SumatraAbout.cpp", "SumatraProperties.cpp", "SumatraDialogs.cpp", "CrashHandler.cpp", "AppTools.cpp"]
 C_FILES_TO_PROCESS = [os.path.join("..", "src", f) for f in C_FILES_TO_PROCESS]
 STRINGS_PATH = os.path.join("..", "src", "strings")
 TRANSLATION_PATTERN = r'_TRN?\("(.*?)"\)'
