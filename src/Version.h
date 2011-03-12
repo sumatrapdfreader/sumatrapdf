597bce0f0a2a62613e8008fc313c2994c2acc843
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-03-12 23:14:43 +0000

    bump version to 1.5

--- src/Version.h
+++ src/Version.h
@@ -6,7 +6,7 @@
 
 // CURR_VERSION can be over-written externally (via makefile)
 #ifndef CURR_VERSION
-#define CURR_VERSION 1.4
+#define CURR_VERSION 1.5
 #endif
 
 // #define SVN_PRE_RELEASE_VER 2295
@@ -24,12 +24,12 @@
   // hack: adds " (dbg)" after the version
   #define CURR_VERSION_STR _T(QM3(CURR_VERSION, \x20, (dbg)))
  #endif
- #define VER_RESOURCE      1,4,0,0
+ #define VER_RESOURCE      1,5,0,0
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., 0)
  #define UPDATE_CHECK_VER  _T(QM(CURR_VERSION))
 #else
  #define CURR_VERSION_STR  _T(QM3(CURR_VERSION, ., SVN_PRE_RELEASE_VER))
- #define VER_RESOURCE      1,4,0,SVN_PRE_RELEASE_VER
+ #define VER_RESOURCE      1,5,0,SVN_PRE_RELEASE_VER
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., SVN_PRE_RELEASE_VER)
  #define UPDATE_CHECK_VER  _T(QM(SVN_PRE_RELEASE_VER))
 #endif
