sumatrapdfreader/sumatrapdf

commit f0e856590dbf2c1690c37a770111d70f52896aac

if an image is too big to load into memory, ignore the image instead of abort()ing in fz_calloc() (fixes issue 1278); fix leaking of samples in in pdf_loadimageimp() in error paths; don't abort() in fz_renderftglyph() if glyph's width or height is 0 (exhibited in dejpeg_techspecs.pdf from issue 1332)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@3157 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/fitz/base_memory.c
@@ -12,11 +12,10 @@ fz_malloc(int size)
 	return p;
 }
 
+/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
 void *
-fz_calloc(int count, int size)
+fz_calloc_no_abort(int count, int size)
 {
-	void *p;
-
 	if (count == 0 || size == 0)
 		return 0;
 
@@ -25,8 +24,13 @@ fz_calloc(int count, int size)
 		fprintf(stderr, "fatal error: out of memory (integer overflow)\n");
 		abort();
 	}
+	return calloc(count, size);
+}
 
-	p = malloc(count * size);
+void *
+fz_calloc(int count, int size)
+{
+	void *p = fz_calloc_no_abort(count, size);
 	if (!p)
 	{
 		fprintf(stderr, "fatal error: out of memory\n");

mupdf/fitz/fitz.h
@@ -147,6 +147,8 @@ char *fz_geterrorline(int n);
 /* memory allocation */
 void *fz_malloc(int size);
 void *fz_calloc(int count, int size);
+void *fz_calloc_no_abort(int count, int size); /* SumatraPDF */
+
 void *fz_realloc(void *p, int count, int size);
 void fz_free(void *p);
 char *fz_strdup(char *s);
@@ -687,7 +689,9 @@ struct fz_pixmap_s
 	int freesamples;
 };
 
-fz_pixmap *fz_newpixmapwithdata(fz_colorspace *colorspace, int x, int y, int w, int h, unsigned char *samples);
+/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
+fz_pixmap *fz_newpixmap_no_abort(fz_colorspace *, int x, int y, int w, int h);
+fz_pixmap *fz_newpixmapwithdata(fz_colorspace *colorspace, int x, int y, int w, int h, unsigned char *samples, int abortonfail);
 fz_pixmap *fz_newpixmapwithrect(fz_colorspace *, fz_bbox bbox);
 fz_pixmap *fz_newpixmap(fz_colorspace *, int x, int y, int w, int h);
 fz_pixmap *fz_keeppixmap(fz_pixmap *pix);

mupdf/fitz/res_font.c
@@ -332,7 +332,14 @@ fz_renderftglyph(fz_font *font, int gid, fz_matrix trm)
 		return nil;
 	}
 
-	glyph = fz_newpixmap(nil,
+	/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
+	if (face->glyph->bitmap.width == 0 || face->glyph->bitmap.rows == 0)
+	{
+		fz_warn("empty glyph (gid %d)", gid);
+		return nil;
+	}
+
+    glyph = fz_newpixmap(nil,
 		face->glyph->bitmap_left,
 		face->glyph->bitmap_top - face->glyph->bitmap.rows,
 		face->glyph->bitmap.width,

mupdf/fitz/res_pixmap.c
@@ -1,7 +1,8 @@
 #include "fitz.h"
 
+/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
 fz_pixmap *
-fz_newpixmapwithdata(fz_colorspace *colorspace, int x, int y, int w, int h, unsigned char *samples)
+fz_newpixmapwithdata(fz_colorspace *colorspace, int x, int y, int w, int h, unsigned char *samples, int abortonfail)
 {
 	fz_pixmap *pix;
 
@@ -31,7 +32,11 @@ fz_newpixmapwithdata(fz_colorspace *colorspace, int x, int y, int w, int h, unsi
 	{
 		/* SumatraPDF: abort on integer overflow */
 		if (pix->w > INT_MAX / pix->n) abort();
-		pix->samples = fz_calloc(pix->h, pix->w * pix->n);
+		/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
+		if (abortonfail)
+			pix->samples = fz_calloc(pix->h, pix->w * pix->n);
+		else
+			pix->samples = fz_calloc_no_abort(pix->h, pix->w * pix->n);
 		pix->freesamples = 1;
 	}
 
@@ -41,7 +46,21 @@ fz_newpixmapwithdata(fz_colorspace *colorspace, int x, int y, int w, int h, unsi
 fz_pixmap *
 fz_newpixmap(fz_colorspace *colorspace, int x, int y, int w, int h)
 {
-	return fz_newpixmapwithdata(colorspace, x, y, w, h, nil);
+	/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
+	return fz_newpixmapwithdata(colorspace, x, y, w, h, nil, 1);
+}
+
+/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
+fz_pixmap *
+fz_newpixmap_no_abort(fz_colorspace *colorspace, int x, int y, int w, int h)
+{
+	fz_pixmap *pixmap = fz_newpixmapwithdata(colorspace, x, y, w, h, nil, 0);
+	if (!pixmap->samples)
+	{
+		fz_free(pixmap);
+		return NULL;
+	}
+	return pixmap;
 }
 
 fz_pixmap *

mupdf/mupdf/pdf_image.c
@@ -157,7 +157,10 @@ pdf_loadimageimp(fz_pixmap **imgp, pdf_xref *xref, fz_obj *rdb, fz_obj *dict, fz
 	}
 
 	stride = (w * n * bpc + 7) / 8;
-	samples = fz_calloc(h, stride);
+	/* SumatraPDF: http://code.google.com/p/sumatrapdf/issues/detail?id=1332 */
+	samples = fz_calloc_no_abort(h, stride);
+	if (!samples)
+		return fz_throw("image too big to load");
 
 	if (cstm)
 	{
@@ -172,6 +175,7 @@ pdf_loadimageimp(fz_pixmap **imgp, pdf_xref *xref, fz_obj *rdb, fz_obj *dict, fz
 				fz_dropcolorspace(colorspace);
 			if (mask)
 				fz_droppixmap(mask);
+			fz_free(samples); /* SumatraPDF */
 			return fz_rethrow(error, "cannot open image data stream (%d 0 R)", fz_tonum(dict));
 		}
 	}
@@ -184,6 +188,7 @@ pdf_loadimageimp(fz_pixmap **imgp, pdf_xref *xref, fz_obj *rdb, fz_obj *dict, fz
 			fz_dropcolorspace(colorspace);
 		if (mask)
 			fz_droppixmap(mask);
+		fz_free(samples); /* SumatraPDF */
 		return fz_rethrow(len, "cannot read image data");
 	}
 
@@ -221,7 +226,12 @@ pdf_loadimageimp(fz_pixmap **imgp, pdf_xref *xref, fz_obj *rdb, fz_obj *dict, fz
 
 	/* Unpack samples into pixmap */
 
-	tile = fz_newpixmap(colorspace, 0, 0, w, h);
+	tile = fz_newpixmap_no_abort(colorspace, 0, 0, w, h);
+	if (!tile)
+	{
+		fz_free(samples);
+		return fz_throw("image too big to load");
+	}
 
 	scale = 1;
 	if (!indexed)

src/PdfEngine.cpp
@@ -69,7 +69,7 @@ fz_pixmap *fz_newpixmap_nullonoom(fz_colorspace *colorspace, int x, int y, int w
     if (!samples)
         return NULL;
 
-    fz_pixmap *image = fz_newpixmapwithdata(colorspace, x, y, w, h, samples);
+    fz_pixmap *image = fz_newpixmapwithdata(colorspace, x, y, w, h, samples, 1);
     // tell MuPDF to free the memory when discarding the pixmap
     image->freesamples = 1;
 

commit 3307338ca6bef799cebba4a3086c43cdbf1e23e4

don't abort when calling fz_calloc with either count==0 or size==0 (regression from r3157)

mupdf/fitz/base_memory.c
@@ -24,13 +24,19 @@ fz_calloc_no_abort(int count, int size)
 		fprintf(stderr, "fatal error: out of memory (integer overflow)\n");
 		return nil;
 	}
+
 	return calloc(count, size);
 }
 
 void *
 fz_calloc(int count, int size)
 {
-	void *p = fz_calloc_no_abort(count, size);
+	void *p;
+
+	if (count == 0 || size == 0)
+		return 0;
+
+	p = fz_calloc_no_abort(count, size);
 	if (!p)
 	{
 		fprintf(stderr, "fatal error: out of memory\n");