7c2d3682ac20bff7ef488ca36da0d038fdd4e143
Simon Bunzli <zeniko@gmail.com>
2011-05-03 11:44:01 +0000

    fix two more DjVu printing issues

--- src/DjVuEngine.cpp
+++ src/DjVuEngine.cpp
@@ -350,16 +350,18 @@ RenderedBitmap *CDjVuEngine::RenderBitmap(int pageNo, float zoom, int rotation, 
 bool CDjVuEngine::RenderPage(HDC hDC, int pageNo, RectI screenRect, float zoom, int rotation, RectD *pageRect, RenderTarget target)
 {
     bool success = true;
+    RectD mediabox = PageMediabox(pageNo);
     HRGN clip = CreateRectRgn(screenRect.x, screenRect.y, screenRect.x + screenRect.dx, screenRect.y + screenRect.dy);
     SelectClipRgn(hDC, clip);
 
     // render in 1 MB bands, as otherwise GDI can run out of memory
-    RectD rect = pageRect ? *pageRect : PageMediabox(pageNo);
+    RectD rect = pageRect ? *pageRect : mediabox;
     int bandDy = (int)((1 << 20) / (rect.dy * zoom));
-    PointI pt = Transform(rect.TL(), pageNo, zoom, rotation).Convert<int>();
+    PointI pt = Transform(rect, pageNo, zoom, rotation).TL().Convert<int>();
 
     for (int y = 0; y * bandDy < rect.dy; y++) {
         RectD pageBand(rect.x, y * bandDy, rect.dx, bandDy);
+        pageBand = pageBand.Intersect(mediabox);
         RectI screenBand = Transform(pageBand, pageNo, zoom, rotation).Round();
         screenBand.Offset(screenRect.x - pt.x, screenRect.y - pt.y);
 
