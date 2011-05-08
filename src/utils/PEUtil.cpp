2a4221fc4dc1cf0aaa5c701820ea1067276bc6ce
Simon Bunzli <zeniko@gmail.com>
2011-05-08 09:41:34 +0000

    add license header to PEUtil.*

--- src/utils/PEUtil.cpp
+++ src/utils/PEUtil.cpp
@@ -1,3 +1,6 @@
+/* Copyright 2006-2011 the SumatraPDF project authors (see AUTHORS file).
+   License: Simplified BSD (see COPYING) */
+
 #include "BaseUtil.h"
 #include "PEUtil.h"
 #include "FileUtil.h"
--- src/utils/PEUtil.h
+++ src/utils/PEUtil.h
@@ -1,3 +1,6 @@
+/* Copyright 2006-2011 the SumatraPDF project authors (see AUTHORS file).
+   License: Simplified BSD (see COPYING) */
+
 #ifndef PEUtil_h
 #define PEUtil_h
 
