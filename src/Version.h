7fb5d975827c9a2d982503ea0483c071a31aa9e8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-05-17 21:27:27 +0000

    bump version to 1.7

--- src/Version.h
+++ src/Version.h
@@ -6,7 +6,7 @@
 
 // CURR_VERSION can be over-written externally (via makefile)
 #ifndef CURR_VERSION
-#define CURR_VERSION 1.6
+#define CURR_VERSION 1.7
 #endif
 
 #define APP_NAME_STR       _T("SumatraPDF")
@@ -26,12 +26,12 @@
   // hack: adds " (dbg)" after the version
   #define CURR_VERSION_STR _T(QM3(CURR_VERSION, \x20, (dbg)))
  #endif
- #define VER_RESOURCE      1,6,0,0
+ #define VER_RESOURCE      1,7,0,0
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., 0)
  #define UPDATE_CHECK_VER  _T(QM(CURR_VERSION))
 #else
  #define CURR_VERSION_STR  _T(QM3(CURR_VERSION, ., SVN_PRE_RELEASE_VER))
- #define VER_RESOURCE      1,6,0,SVN_PRE_RELEASE_VER
+ #define VER_RESOURCE      1,7,0,SVN_PRE_RELEASE_VER
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., SVN_PRE_RELEASE_VER)
  #define UPDATE_CHECK_VER  _T(QM(SVN_PRE_RELEASE_VER))
 #endif
