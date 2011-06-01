3b8340558964379ed0c10d1928484ea6abf5558c
Simon Bunzli <zeniko@gmail.com>
2011-06-01 07:03:00 +0000

    fix another potential crash

--- mupdf/pdf/pdf_xref.c
+++ mupdf/pdf/pdf_xref.c
@@ -496,9 +496,15 @@ pdf_load_xref(pdf_xref *xref, char *buf, int bufsize)
 
 	/* broken pdfs where object offsets are out of range */
 	for (i = 0; i < xref->len; i++)
+	{
 		if (xref->table[i].type == 'n')
 			if (xref->table[i].ofs <= 0 || xref->table[i].ofs >= xref->file_size)
 				return fz_throw("object offset out of range: %d (%d 0 R)", xref->table[i].ofs, i);
+		/* SumatraPDF: ensure that streamed objects reside insided a known non-streamed object */
+		if (xref->table[i].type == 'o')
+			if (xref->table[i].ofs <= 0 || xref->table[i].ofs >= xref->len || xref->table[xref->table[i].ofs].type != 'n')
+				return fz_throw("invalid reference to non-object-stream: %d (%d 0 R)", xref->table[i].ofs, i);
+	}
 
 	return fz_okay;
 }
