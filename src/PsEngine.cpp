sumatrapdfreader/sumatrapdf

commit 8e0a3d72162048070133d914148bb2a63035b19d

also sniff PJL files for Postscript data
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@3879 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/PsEngine.cpp
@@ -251,9 +251,12 @@ bool PsEngine::IsSupportedFile(const TCHAR *fileName, bool sniff)
         return false;
 
     if (sniff) {
-        char header[2] = { 0 };
+        char header[1024];
+        ZeroMemory(header, sizeof(header));
         file::ReadAll(fileName, header, sizeof(header));
-        return str::EqN(header, "%!", sizeof(header));
+        return str::EqN(header, "%!", sizeof(header)) ||
+               // also sniff PJL (Printer Job Language) files containing Postscript data
+               str::StartsWith(header, "\x1B%-12345X@PJL") && str::Find(header, "\n%!PS-Adobe-");
     }
 
     return str::EndsWithI(fileName, _T(".ps"));

commit 09234f046a374282568e23c7812e7e8c9ab65d53

fix PsEngine sniffing regression introduced with r3879

src/PsEngine.cpp
@@ -89,6 +89,11 @@ static TCHAR *GetTempFileName(const TCHAR *prefix=_T("PsE"))
 
 static PdfEngine *ps2pdf(const TCHAR *fileName)
 {
+    // Ghostscript produces a document with a single empty page
+    // for invalid paths instead of failing
+    if (!file::Exists(fileName))
+        return NULL;
+
     // TODO: read from gswin32c's stdout instead of using a TEMP file
     ScopedMem<TCHAR> tmpFile(GetTempFileName());
     ScopedFile tmpFileScope(tmpFile);
@@ -254,12 +259,12 @@ bool PsEngine::IsSupportedFile(const TCHAR *fileName, bool sniff)
         char header[1024];
         ZeroMemory(header, sizeof(header));
         file::ReadAll(fileName, header, sizeof(header));
-        return str::EqN(header, "%!", sizeof(header)) ||
+        return str::StartsWith(header, "%!") ||
                // also sniff PJL (Printer Job Language) files containing Postscript data
                str::StartsWith(header, "\x1B%-12345X@PJL") && str::Find(header, "\n%!PS-Adobe-");
     }
 
-    return str::EndsWithI(fileName, _T(".ps")) && file::Exists(fileName);
+    return str::EndsWithI(fileName, _T(".ps"));
 }
 
 PsEngine *PsEngine::CreateFromFileName(const TCHAR *fileName)