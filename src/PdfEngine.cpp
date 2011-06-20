sumatrapdfreader/sumatrapdf

commit 3b29ac6ae28a167442dcbefbd7258561efd9006e

use page labels instead of page numbers when available (fixes issue 501)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@3927 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/PdfEngine.cpp
@@ -533,6 +533,90 @@ pdf_outline *pdf_loadattachments(pdf_xref *xref)
     return root.next;
 }
 
+struct PageLabelInfo {
+    int startAt, countFrom;
+    const char *type;
+    fz_obj *prefix;
+};
+
+int cmpPageLabelInfo(const void *a, const void *b)
+{
+    return ((PageLabelInfo *)a)->startAt - ((PageLabelInfo *)b)->startAt;
+}
+
+TCHAR *FormatPageLabel(const char *type, int pageNo, const TCHAR *prefix)
+{
+    if (str::Eq(type, "D"))
+        return str::Format(_T("%s%d"), prefix, pageNo);
+    if (str::EqI(type, "R")) {
+        // roman numbering style
+        ScopedMem<TCHAR> number(str::FormatRomanNumeral(pageNo));
+        if (*type == 'r')
+            str::ToLower(number.Get());
+        return str::Format(_T("%s%s"), prefix, number);
+    }
+    if (str::EqI(type, "A")) {
+        // alphabetic numbering style (A..Z, AA..ZZ, AAA..ZZZ, ...)
+        str::Str<TCHAR> number;
+        number.Append('A' + (pageNo - 1) % 26);
+        for (int i = 1; i < (pageNo - 1) / 26; i++)
+            number.Append(number[0]);
+        if (*type == 'a')
+            str::ToLower(number.Get());
+        return str::Format(_T("%s%s"), prefix, number);
+    }
+    return str::Dup(prefix);
+}
+
+void BuildPageLabelRec(fz_obj *node, int pageCount, Vec<PageLabelInfo>& data)
+{
+    fz_obj *obj;
+    if ((obj = fz_dict_gets(node, "Kids"))) {
+        for (int i = 0; i < fz_array_len(obj); i++)
+            BuildPageLabelRec(fz_array_get(obj, i), pageCount, data);
+    }
+    else if ((obj = fz_dict_gets(node, "Nums"))) {
+        for (int i = 0; i < fz_array_len(obj); i += 2) {
+            fz_obj *info = fz_array_get(obj, i + 1);
+            PageLabelInfo pli;
+            pli.startAt = fz_to_int(fz_array_get(obj, i)) + 1;
+            if (pli.startAt < 1)
+                continue;
+            pli.type = fz_to_name(fz_dict_gets(info, "S"));
+            pli.prefix = fz_dict_gets(info, "P");
+            pli.countFrom = fz_to_int(fz_dict_gets(info, "St"));
+            if (pli.countFrom < 1)
+                pli.countFrom = 1;
+            data.Append(pli);
+        }
+    }
+}
+
+StrVec *BuildPageLabelVec(fz_obj *root, int pageCount)
+{
+    Vec<PageLabelInfo> data;
+    BuildPageLabelRec(root, pageCount, data);
+    data.Sort(cmpPageLabelInfo);
+
+    StrVec *labels = new StrVec();
+    labels->MakeSpaceAt(0, pageCount);
+
+    for (size_t i = 0; i < data.Count(); i++) {
+        int secLen = (i < data.Count() - 1 ? data[i + 1].startAt : pageCount + 1) - data[i].startAt;
+        ScopedMem<TCHAR> prefix(str::conv::FromPdf(data[i].prefix));
+        for (int j = 0; j < secLen; j++) {
+            free(labels->At(data[i].startAt + j - 1));
+            labels->At(data[i].startAt + j - 1) = FormatPageLabel(data[i].type, data[i].countFrom + j, prefix);
+        }
+    }
+
+    for (int i = 0; i < pageCount; i++)
+        if (!labels->At(i))
+            labels->At(i) = str::Dup(_T(""));
+
+    return labels;
+}
+
 ///// Above are extensions to Fitz and MuPDF, now follows PdfEngine /////
 
 struct PdfPageRun {
@@ -597,6 +681,10 @@ public:
     }
     virtual DocToCItem *GetToCTree();
 
+    virtual bool HasPageLabels() { return _pagelabels != NULL; }
+    virtual TCHAR *GetPageLabel(int pageNo);
+    virtual int GetPageByLabel(const TCHAR *label);
+
     virtual char *GetDecryptionKey() const;
     virtual void RunGC();
 
@@ -645,6 +733,7 @@ protected:
     pdf_outline   * _attachments;
     fz_obj        * _info;
     fz_glyph_cache* _drawcache;
+    StrVec        * _pagelabels;
 };
 
 class PdfLink : public PageElement, public PageDestination {
@@ -711,7 +800,7 @@ public:
 
 CPdfEngine::CPdfEngine() : _fileName(NULL), _xref(NULL), _mediaboxes(NULL),
     _outline(NULL), _attachments(NULL), _info(NULL), _pages(NULL),
-    _drawcache(NULL), _decryptionKey(NULL)
+    _drawcache(NULL), _pagelabels(NULL), _decryptionKey(NULL)
 {
     InitializeCriticalSection(&_pagesAccess);
     InitializeCriticalSection(&_xrefAccess);
@@ -731,8 +820,6 @@ CPdfEngine::~CPdfEngine()
         free(_pages);
     }
 
-    if (_mediaboxes)
-        delete[] _mediaboxes;
     if (_outline)
         pdf_free_outline(_outline);
     if (_attachments)
@@ -751,6 +838,9 @@ CPdfEngine::~CPdfEngine()
         assert(_runCache[0]->refs == 1);
         dropPageRun(_runCache[0], true);
     }
+
+    delete[] _mediaboxes;
+    delete _pagelabels;
     free((void*)_fileName);
     free(_decryptionKey);
 
@@ -950,6 +1040,9 @@ bool CPdfEngine::finishLoading()
     _info = fz_dict_gets(_xref->trailer, "Info");
     if (_info)
         _info = fz_copy_dict(pdf_resolve_indirect(_info));
+    fz_obj *pagelabels = fz_dict_gets(fz_dict_gets(_xref->trailer, "Root"), "PageLabels");
+    if (pagelabels)
+        _pagelabels = BuildPageLabelVec(pagelabels, PageCount());
     LeaveCriticalSection(&_xrefAccess);
 
     return true;
@@ -1584,6 +1677,23 @@ bool CPdfEngine::IsImagePage(int pageNo)
     dropPageRun(run);
 
     return hasSingleImage;
+}
+
+TCHAR *CPdfEngine::GetPageLabel(int pageNo)
+{
+    if (!_pagelabels || pageNo < 1 || PageCount() < pageNo)
+        return BaseEngine::GetPageLabel(pageNo);
+
+    return str::Dup(_pagelabels->At(pageNo - 1));
+}
+
+int CPdfEngine::GetPageByLabel(const TCHAR *label)
+{
+    int pageNo = _pagelabels ? _pagelabels->Find(label) + 1 : 0;
+    if (!pageNo)
+        return BaseEngine::GetPageByLabel(label);
+
+    return pageNo;
 }
 
 void CPdfEngine::RunGC()

src/BaseEngine.h
@@ -266,6 +266,15 @@ public:
     // caller must delete the result (when no longer needed)
     virtual DocToCItem *GetToCTree() { return NULL; }
 
+    // checks whether this document has explicit labels for pages (such as
+    // roman numerals) instead of the default plain arabic numbering
+    virtual bool HasPageLabels() { return false; }
+    // returns a label to be displayed instead of the page number
+    // caller must free() the result
+    virtual TCHAR *GetPageLabel(int pageNo) { return str::Format(_T("%d"), pageNo); }
+    // reverts GetPageLabel by returning the first page number having the given label
+    virtual int GetPageByLabel(const TCHAR *label) { return _ttoi(label); }
+
     // returns a string to remember when the user wants to save a document's password
     // (don't implement for document types that don't support password protection)
     // caller must free() the result

src/SumatraDialogs.cpp
@@ -94,15 +94,15 @@ TCHAR *Dialog_GetPassword(HWND hwndParent, const TCHAR *fileName, bool *remember
 
 /* For passing data to/from GoToPage dialog */
 struct Dialog_GoToPage_Data {
-    int     currPageNo;      /* currently shown page number */
-    int     pageCount;       /* total number of pages */
-    int     pageEnteredOut;  /* page number entered by user */
+    const TCHAR *   currPageLabel;  // currently shown page label
+    int             pageCount;      // total number of pages
+    bool            onlyNumeric;    // whether the page label must be numeric
+    TCHAR *         newPageLabel;   // page number entered by user
 };
 
 static INT_PTR CALLBACK Dialog_GoToPage_Proc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     HWND                    editPageNo;
-    TCHAR *                 newPageNoTxt;
     Dialog_GoToPage_Data *  data;
 
     if (WM_INITDIALOG == message)
@@ -110,17 +110,22 @@ static INT_PTR CALLBACK Dialog_GoToPage_Proc(HWND hDlg, UINT message, WPARAM wPa
         data = (Dialog_GoToPage_Data*)lParam;
         assert(data);
         SetWindowLongPtr(hDlg, GWLP_USERDATA, (LONG_PTR)data);
-        assert(1 <= data->currPageNo && data->currPageNo <= data->pageCount);
         win::SetText(hDlg, _TR("Go to page"));
 
-        newPageNoTxt = str::Format(_T("%d"), data->currPageNo);
-        SetDlgItemText(hDlg, IDC_GOTO_PAGE_EDIT, newPageNoTxt);
-        free(newPageNoTxt);
-        newPageNoTxt = str::Format(_TR("(of %d)"), data->pageCount);
-        SetDlgItemText(hDlg, IDC_GOTO_PAGE_LABEL_OF, newPageNoTxt);
-        free(newPageNoTxt);
-
         editPageNo = GetDlgItem(hDlg, IDC_GOTO_PAGE_EDIT);
+        if (!data->onlyNumeric)
+            SetWindowLong(editPageNo, GWL_STYLE, GetWindowLong(editPageNo, GWL_STYLE) & ~ES_NUMBER);
+        if (data->currPageLabel) {
+            assert(!data->onlyNumeric);
+            SetDlgItemText(hDlg, IDC_GOTO_PAGE_EDIT, data->currPageLabel);
+        }
+        else {
+            ScopedMem<TCHAR> pageNo(str::Format(_T("%d")));
+            SetDlgItemText(hDlg, IDC_GOTO_PAGE_EDIT, pageNo);
+        }
+        ScopedMem<TCHAR> totalCount(str::Format(_TR("(of %d)"), data->pageCount));
+        SetDlgItemText(hDlg, IDC_GOTO_PAGE_LABEL_OF, totalCount);
+
         Edit_SelectAll(editPageNo);
         SetDlgItemText(hDlg, IDC_STATIC, _TR("&Go to page:"));
         SetDlgItemText(hDlg, IDOK, _TR("Go to page"));
@@ -139,13 +144,8 @@ static INT_PTR CALLBACK Dialog_GoToPage_Proc(HWND hDlg, UINT message, WPARAM wPa
                 case IDOK:
                     data = (Dialog_GoToPage_Data*)GetWindowLongPtr(hDlg, GWLP_USERDATA);
                     assert(data);
-                    data->pageEnteredOut = 0;
                     editPageNo = GetDlgItem(hDlg, IDC_GOTO_PAGE_EDIT);
-                    newPageNoTxt = win::GetText(editPageNo);
-                    if (newPageNoTxt) {
-                        data->pageEnteredOut = _ttoi(newPageNoTxt);
-                        free(newPageNoTxt);
-                    }
+                    data->newPageLabel = win::GetText(editPageNo);
                     EndDialog(hDlg, IDOK);
                     return TRUE;
 
@@ -158,18 +158,19 @@ static INT_PTR CALLBACK Dialog_GoToPage_Proc(HWND hDlg, UINT message, WPARAM wPa
     return FALSE;
 }
 
-/* Shows a 'go to page' dialog and returns a page number entered by the user
-   or an invalid page number if user clicked "cancel" button or there was an error. */
-int Dialog_GoToPage(HWND hwnd, int currentPageNo, int pageCount)
+/* Shows a 'go to page' dialog and returns the page label entered by the user
+   or NULL if user clicked the "cancel" button or there was an error.
+   The caller must free() the result. */
+TCHAR *Dialog_GoToPage(HWND hwnd, const TCHAR *currentPageLabel, int pageCount, bool onlyNumeric)
 {
-    Dialog_GoToPage_Data    data;
-    
-    data.currPageNo = currentPageNo;
+    Dialog_GoToPage_Data data;
+    data.currPageLabel = currentPageLabel;
     data.pageCount = pageCount;
-    INT_PTR dialogResult = DialogBoxParam(NULL, MAKEINTRESOURCE(IDD_DIALOG_GOTO_PAGE), hwnd, Dialog_GoToPage_Proc, (LPARAM)&data);
-    if (IDOK == dialogResult)
-        return data.pageEnteredOut;
-    return 0;
+    data.onlyNumeric = onlyNumeric;
+    data.newPageLabel = NULL;
+
+    DialogBoxParam(NULL, MAKEINTRESOURCE(IDD_DIALOG_GOTO_PAGE), hwnd, Dialog_GoToPage_Proc, (LPARAM)&data);
+    return data.newPageLabel;
 }
 
 /* For passing data to/from Find dialog */

src/SumatraDialogs.h
@@ -6,7 +6,7 @@
 
 #include "AppPrefs.h"
 
-int     Dialog_GoToPage(HWND hwnd, int currentPageNo, int pageCount);
+TCHAR * Dialog_GoToPage(HWND hwnd, const TCHAR *currentPageLabel, int pageCount, bool onlyNumeric=true);
 TCHAR * Dialog_Find(HWND hwnd, const TCHAR *previousSearch, bool *matchCase);
 TCHAR * Dialog_GetPassword(HWND hwnd, const TCHAR *fileName, bool *rememberPassword);
 INT_PTR Dialog_PdfAssociate(HWND hwnd, bool *dontAskAgainOut);

src/SumatraPDF.cpp
@@ -1598,6 +1598,13 @@ Error:
         UpdateWindow(win.hwndFrame);
     }
     if (win.IsDocLoaded()) {
+        DWORD style = GetWindowLong(win.hwndPageBox, GWL_STYLE);
+        if (win.dm->engine && win.dm->engine->HasPageLabels())
+            style &= ~ES_NUMBER;
+        else
+            style |= ES_NUMBER;
+        SetWindowLong(win.hwndPageBox, GWL_STYLE, style);
+
         win.dm->SetScrollState(ss);
         win.UpdateToolbarState();
     }
@@ -1724,9 +1731,11 @@ void WindowInfo::PageNoChanged(int pageNo)
         return;
 
     if (INVALID_PAGE_NO != pageNo) {
-        ScopedMem<TCHAR> buf(str::Format(_T("%d"), pageNo));
+        ScopedMem<TCHAR> buf(dm->engine->GetPageLabel(pageNo));
         win::SetText(hwndPageBox, buf);
         ToolbarUpdateStateForWindow(*this);
+        if (dm->engine && dm->engine->HasPageLabels())
+            UpdateToolbarPageText(*this, dm->pageCount());
     }
     if (pageNo != currPageNo) {
         UpdateTocSelection(pageNo);
@@ -4132,7 +4141,13 @@ static void OnMenuGoToPage(WindowInfo& win)
         return;
     }
 
-    int newPageNo = Dialog_GoToPage(win.hwndFrame, win.dm->currentPageNo(), win.dm->pageCount());
+    ScopedMem<TCHAR> label(win.dm->engine->GetPageLabel(win.dm->currentPageNo()));
+    ScopedMem<TCHAR> newPageLabel(Dialog_GoToPage(win.hwndFrame, label, win.dm->pageCount(),
+                                                  !win.dm->engine->HasPageLabels()));
+    if (!newPageLabel)
+        return;
+
+    int newPageNo = win.dm->engine->GetPageByLabel(newPageLabel);
     if (win.dm->validPageNo(newPageNo))
         win.dm->goToPage(newPageNo, 0, true);
 }
@@ -5097,7 +5112,7 @@ static LRESULT CALLBACK WndProcPageBox(HWND hwnd, UINT message, WPARAM wParam, L
         switch (wParam) {
         case VK_RETURN: {
             ScopedMem<TCHAR> buf(win::GetText(win->hwndPageBox));
-            int newPageNo = _ttoi(buf);
+            int newPageNo = win->dm->engine->GetPageByLabel(buf);
             if (win->dm->validPageNo(newPageNo)) {
                 win->dm->goToPage(newPageNo, 0, true);
                 SetFocus(win->hwndFrame);
@@ -5146,15 +5161,22 @@ void UpdateToolbarPageText(WindowInfo& win, int pageCount)
     int pos_y = (r.bottom - pageWndRect.dy) / 2;
 
     TCHAR *buf;
+    SIZE size2 = { 0 };
     if (-1 == pageCount)
         buf = win::GetText(win.hwndPageTotal);
-    else if (0 != pageCount)
-        buf = str::Format(_T(" / %d"), pageCount);
-    else
+    else if (!pageCount)
         buf = str::Dup(_T(""));
+    else if (!win.dm || !win.dm->engine || !win.dm->engine->HasPageLabels())
+        buf = str::Format(_T(" / %d"), pageCount);
+    else {
+        buf = str::Format(_T(" (%d / %d)"), win.dm->currentPageNo(), pageCount);
+        ScopedMem<TCHAR> buf2(str::Format(_T(" (%d / %d)"), pageCount, pageCount));
+        size2 = TextSizeInHwnd(win.hwndPageTotal, buf2);
+    }
 
     win::SetText(win.hwndPageTotal, buf);
-    SIZE size2 = TextSizeInHwnd(win.hwndPageTotal, buf);
+    if (0 == size2.cx)
+        size2 = TextSizeInHwnd(win.hwndPageTotal, buf);
     size2.cx += 6;
     free(buf);
 

src/utils/StrUtil.cpp
@@ -466,6 +466,35 @@ TCHAR *FormatFloatWithThousandSep(double number, const TCHAR *unit)
     return unit ? str::Format(_T("%s %s"), buf, unit) : str::Dup(buf);
 }
 
+// cf. http://rosettacode.org/wiki/Roman_numerals/Encode#C.2B.2B
+TCHAR *FormatRomanNumeral(int number)
+{
+    if (number < 1)
+        return NULL;
+
+    static struct {
+        int value;
+        const TCHAR *numeral;
+    } romandata[] = {
+        { 1000, _T("M") }, { 900, _T("CM") }, { 500, _T("D") }, { 400, _T("CD") },
+        {  100, _T("C") }, {  90, _T("XC") }, {  50, _T("L") }, {  40, _T("XL") },
+        {   10, _T("X") }, {   9, _T("IX") }, {   5, _T("V") }, {   4, _T("IV") }, { 1, _T("I") }
+    };
+
+    size_t len = 0;
+    for (int num = number, i = 0; i < dimof(romandata); i++)
+        for (; num >= romandata[i].value; num -= romandata[i].value)
+            len += romandata[i].numeral[1] ? 2 : 1;
+    assert(len > 0);
+
+    TCHAR *roman = SAZA(TCHAR, len + 1), *c = roman;
+    for (int num = number, i = 0; i < dimof(romandata); i++)
+        for (; num >= romandata[i].value; num -= romandata[i].value)
+            c += str::BufSet(c, romandata[i].numeral[1] ? 3 : 2, romandata[i].numeral);
+
+    return roman;
+}
+
 /* compares two strings "naturally" by sorting numbers within a string
    numerically instead of by pure ASCII order; we imitate Windows Explorer
    by sorting special characters before alphanumeric characters

src/utils/StrUtil.h
@@ -95,6 +95,7 @@ bool    HexToMem(const char *s, unsigned char *buf, int bufLen);
 
 TCHAR * FormatFloatWithThousandSep(double number, const TCHAR *unit=NULL);
 TCHAR * FormatNumWithThousandSep(size_t num, const TCHAR *sep=NULL);
+TCHAR * FormatRomanNumeral(int number);
 
 int     CmpNatural(const TCHAR *a, const TCHAR *b);
 

src/utils/UtilTests.cpp
@@ -260,6 +260,29 @@ static void TStrTest()
         str::ToLower(wstr);
         assert(str::Eq(wstr, L"aabbcc... 1-9"));
     }
+
+    struct {
+        int number;
+        const TCHAR *result;
+    } formatRomanData[] = {
+        { 1,    _T("I") },
+        { 3,    _T("III") },
+        { 6,    _T("VI") },
+        { 14,   _T("XIV") },
+        { 49,   _T("XLIX") },
+        { 176,  _T("CLXXVI") },
+        { 499,  _T("CDXCIX") },
+        { 1666, _T("MDCLXVI") },
+        { 2011, _T("MMXI") },
+        { 12345,_T("MMMMMMMMMMMMCCCXLV") },
+        { 0,    NULL },
+        { -133, NULL },
+    };
+
+    for (int i = 0; i < dimof(formatNumData); i++) {
+        ScopedMem<TCHAR> tmp(str::FormatRomanNumeral(formatRomanData[i].number));
+        assert(str::Eq(tmp, formatRomanData[i].result));
+    }
 }
 
 static void FileUtilTest()

commit 1fa7d52f47ed92b3e3c22a520b4f72d4293851a7

fix crash introduced with r3927

src/PdfEngine.cpp
@@ -559,11 +559,11 @@ TCHAR *FormatPageLabel(const char *type, int pageNo, const TCHAR *prefix)
         // alphabetic numbering style (A..Z, AA..ZZ, AAA..ZZZ, ...)
         str::Str<TCHAR> number;
         number.Append('A' + (pageNo - 1) % 26);
-        for (int i = 1; i < (pageNo - 1) / 26; i++)
+        for (int i = 0; i < (pageNo - 1) / 26; i++)
             number.Append(number[0]);
         if (*type == 'a')
             str::ToLower(number.Get());
-        return str::Format(_T("%s%s"), prefix, number);
+        return str::Format(_T("%s%s"), prefix, number.Get());
     }
     return str::Dup(prefix);
 }