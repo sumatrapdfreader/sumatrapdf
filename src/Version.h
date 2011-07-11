b762c87b90b3d336813af03387f06c78b3755966
Simon Bunzli <zeniko@gmail.com>
2011-07-11 08:03:17 +0000

    increase version number

--- src/Version.h
+++ src/Version.h
@@ -6,7 +6,10 @@
 
 // CURR_VERSION can be over-written externally (via makefile)
 #ifndef CURR_VERSION
-#define CURR_VERSION 1.7
+#define CURR_VERSION 1.8
+#endif
+#ifndef CURR_VERSION_COMMA
+#define CURR_VERSION_COMMA 1,8,0
 #endif
 
 #define APP_NAME_STR       _T("SumatraPDF")
@@ -26,12 +29,12 @@
   // hack: adds " (dbg)" after the version
   #define CURR_VERSION_STR _T(QM3(CURR_VERSION, \x20, (dbg)))
  #endif
- #define VER_RESOURCE      1,7,0,0
+ #define VER_RESOURCE      CURR_VERSION_COMMA,0
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., 0)
  #define UPDATE_CHECK_VER  _T(QM(CURR_VERSION))
 #else
  #define CURR_VERSION_STR  _T(QM3(CURR_VERSION, ., SVN_PRE_RELEASE_VER))
- #define VER_RESOURCE      1,7,0,SVN_PRE_RELEASE_VER
+ #define VER_RESOURCE      CURR_VERSION_COMMA,SVN_PRE_RELEASE_VER
  #define VER_RESOURCE_STR  QM3(CURR_VERSION, .0., SVN_PRE_RELEASE_VER)
  #define UPDATE_CHECK_VER  _T(QM(SVN_PRE_RELEASE_VER))
 #endif
