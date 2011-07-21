e0895b8198e9f080902a5d7bd624472e88aa5ef6
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-07-21 03:48:04 +0000

    remove pugixml

--- src/ChmEngine.cpp
+++ src/ChmEngine.cpp
@@ -1,7 +1,6 @@
 /* Copyright 2006-2011 the SumatraPDF project authors (see AUTHORS file).
    License: GPLv3 */
 
-//#include "pugixml.hpp"
 #include "ChmEngine.h"
 #include "StrUtil.h"
 #include "FileUtil.h"
