d150cdee38fc89148568de0930e21a44dc35c6e0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-07-22 08:55:40 +0000

    start on HtmlWindow.[h|cpp]

---
+++ src/utils/HtmlWindow.h
@@ -0,0 +1,11 @@
+/* Copyright 2006-2011 the SumatraPDF project authors (see AUTHORS file).
+   License: Simplified BSD (see COPYING) */
+#ifndef HtmlWindow_h
+#define HtmlWindow_h
+
+class HtmlWindow {
+public:
+    HWND hwnd;
+};
+
+#endif
