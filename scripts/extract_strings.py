49a4d6c7f63bd0d9bf04e764afcc7a39bb687200
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-07-25 07:24:28 +0000

    extract strings to translate from Menu.cpp as well

--- scripts/extract_strings.py
+++ scripts/extract_strings.py
@@ -15,7 +15,7 @@ C_FILES_TO_PROCESS = [
   "SumatraPDF.cpp", "SumatraAbout.cpp", "SumatraProperties.cpp", 
   "SumatraDialogs.cpp", "CrashHandler.cpp", "AppTools.cpp", "Favorites.cpp",
   "TableOfContents.cpp", "Toolbar.cpp", "Print.cpp", "Search.cpp",
-  "Selection.cpp"
+  "Selection.cpp", "Menu.cpp"
 ]
 C_FILES_TO_PROCESS = [os.path.join("..", "src", f) for f in C_FILES_TO_PROCESS]
 STRINGS_PATH = os.path.join("..", "strings")
