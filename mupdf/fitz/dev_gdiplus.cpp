sumatrapdfreader/sumatrapdf

commit 33640d892d9656515982dee3a30e9813b302f2d8

support non-square pens in dev_gdiplus (fixes issue 1612)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4392 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/fitz/dev_gdiplus.cpp
@@ -743,9 +743,12 @@ gdiplus_get_path(fz_path *path, fz_matrix ctm, int evenodd=1)
 static Pen *
 gdiplus_get_pen(Brush *brush, fz_matrix ctm, fz_stroke_state *stroke)
 {
-	Pen *pen = new Pen(brush, stroke->linewidth * fz_matrix_expansion(ctm));
+	// TODO: pens are too narrow at low zoom levels
+	float me = fz_matrix_expansion(ctm);
+	Pen *pen = new Pen(brush, stroke->linewidth * me);
+	pen->SetTransform(&Matrix(ctm.a / me, ctm.b / me, ctm.c / me, ctm.d / me, 0, 0));
 	
-	pen->SetMiterLimit(stroke->miterlimit);
+	pen->SetMiterLimit(stroke->miterlimit * me);
 	pen->SetLineCap(stroke->start_cap == 1 ? LineCapRound : stroke->start_cap == 2 ? LineCapSquare : LineCapFlat,
 		stroke->end_cap == 1 ? LineCapRound : stroke->end_cap == 2 ? LineCapSquare : LineCapFlat,
 		stroke->dash_cap == 1 ? DashCapRound : DashCapFlat);

commit 3c3232924da7a4b89abbb3c49f78e7abc3f05378

fix minor regression from r4392

mupdf/fitz/dev_gdiplus.cpp
@@ -748,7 +748,7 @@ gdiplus_get_pen(Brush *brush, fz_matrix ctm, fz_stroke_state *stroke)
 	Pen *pen = new Pen(brush, stroke->linewidth * me);
 	pen->SetTransform(&Matrix(ctm.a / me, ctm.b / me, ctm.c / me, ctm.d / me, 0, 0));
 	
-	pen->SetMiterLimit(stroke->miterlimit * me);
+	pen->SetMiterLimit(stroke->miterlimit);
 	pen->SetLineCap(stroke->start_cap == 1 ? LineCapRound : stroke->start_cap == 2 ? LineCapSquare : LineCapFlat,
 		stroke->end_cap == 1 ? LineCapRound : stroke->end_cap == 2 ? LineCapSquare : LineCapFlat,
 		stroke->dash_cap == 1 ? DashCapRound : DashCapFlat);