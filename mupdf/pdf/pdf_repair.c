sumatrapdfreader/sumatrapdf

commit e3d4c91318c75491f857dea24d9d1e86efdc345e

tweak pdf_repair_obj
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4363 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/pdf/pdf_repair.c
@@ -65,6 +65,7 @@ pdf_repair_obj(fz_stream *file, char *buf, int cap, int *stmofsp, int *stmlenp, 
 		fz_drop_obj(dict);
 	}
 
+	/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692424
 	while ( tok != PDF_TOK_STREAM &&
 		tok != PDF_TOK_ENDOBJ &&
 		tok != PDF_TOK_ERROR &&
@@ -74,7 +75,16 @@ pdf_repair_obj(fz_stream *file, char *buf, int cap, int *stmofsp, int *stmlenp, 
 		if (error)
 			return fz_rethrow(error, "cannot scan for endobj or stream token");
 	}
-
+	*/
+	error = pdf_lex(&tok, file, buf, cap, &len);
+	if (error)
+		return fz_rethrow(error, "cannot scan for endobj or stream token");
+	if (tok != PDF_TOK_STREAM && tok != PDF_TOK_ENDOBJ && tok != PDF_TOK_ERROR && tok != PDF_TOK_EOF)
+	{
+		while (len-- > 0)
+			fz_unread_byte(file);
+	}
+	else
 	if (tok == PDF_TOK_STREAM)
 	{
 		int c = fz_read_byte(file);

commit 32f996214ac4917fe1acb601bb05402ff146f4ff

fix a potential infinite loop (regression from r4363)

mupdf/pdf/pdf_repair.c
@@ -76,6 +76,7 @@ pdf_repair_obj(fz_stream *file, char *buf, int cap, int *stmofsp, int *stmlenp, 
 			return fz_rethrow(error, "cannot scan for endobj or stream token");
 	}
 	*/
+	len = 0;
 	error = pdf_lex(&tok, file, buf, cap, &len);
 	if (error)
 		return fz_rethrow(error, "cannot scan for endobj or stream token");