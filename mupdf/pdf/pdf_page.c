be1e7208b851aec94feeb914e6eeb0f19ff4e30c
Simon Bunzli <zeniko@gmail.com>
2011-09-23 21:11:54 +0000

    don't just drop damaged pages (cf. issue 1643)

--- mupdf/pdf/pdf_page.c
+++ mupdf/pdf/pdf_page.c
@@ -68,7 +68,7 @@ pdf_load_page_tree_node(pdf_xref *xref, fz_obj *node, struct info info)
 		fz_dict_dels(node, ".seen");
 	}
 	/* SumatraPDF: fix a potential NULL pointer dereference */
-	else if (fz_is_dict(node))
+	else if (fz_is_indirect(node) || fz_is_dict(node))
 	{
 		dict = fz_resolve_indirect(node);
 
