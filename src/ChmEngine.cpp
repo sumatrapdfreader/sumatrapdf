sumatrapdfreader/sumatrapdf

commit bbb8801a6a51426f85672c09b3d6870604ad86ed

trying to fix chm focus issues by subclassing browser control window and handling WM_SETFOCUS there. Not enabled because CallWindowProc(win->wndProcBrowser, ...) in WndProcBrowser crashes for mysterious reasons
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4643 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/utils/HtmlWindow.cpp
@@ -13,6 +13,9 @@
 #include "WinUtil.h"
 #include "Scopes.h"
 
+//TODO: not working yet
+//#define HOOK_BROWSER_CONTROL 1
+
 // Info about implementing web browser control
 // http://www.codeproject.com/KB/COM/cwebpage.aspx
 
@@ -332,9 +335,8 @@ static HWND GetBrowserControlHwnd(HWND hwndControlParent)
     return w3;
 }
 
-// This is a wndproc of the window that is a parent hwnd of embedded browser
-// control.
-static LRESULT CALLBACK WndProcHtml(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+// WndProc of the window that is a parent hwnd of embedded browser control.
+static LRESULT CALLBACK WndProcParent(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     HtmlWindow *win = (HtmlWindow*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
     if (!win)
@@ -363,16 +365,39 @@ static LRESULT CALLBACK WndProcHtml(HWND hwnd, UINT msg, WPARAM wParam, LPARAM l
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-void SubclassHtmlHwnd(HWND hwnd, HtmlWindow *htmlWin)
+// WndProc of the embedded web browser control.
+static LRESULT CALLBACK WndProcBrowser(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    // Note: assuming hwnd is plain hwnd, with no special hwnd proc
-    SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)WndProcHtml);
-    SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)htmlWin);
+    HtmlWindow *win = (HtmlWindow*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+    if (!win)
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    if (msg == WM_SETFOCUS) {
+        win->OnLButtonDown();
+    }
+    if (win->wndProcBrowserPrev) {
+        // TODO: why do we crash here? (if HOOK_BROWSER_CONTROL is defined)
+        return CallWindowProc(win->wndProcBrowserPrev, hwnd, msg, wParam, lParam);
+    }
+    else
+        return DefWindowProc(hwnd, msg, wParam, lParam);    
 }
 
-void UnsubclassHtmlHwnd(HWND hwnd)
+void HtmlWindow::SubclassHwnd()
+{
+    SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)WndProcParent);
+    SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)this);
+#ifdef HOOK_BROWSER_CONTROL
+    HWND hwndBrowser = GetBrowserControlHwnd(hwnd);
+    wndProcBrowserPrev = (WNDPROC)SetWindowLongPtr(hwndBrowser, GWLP_WNDPROC, (LONG_PTR)WndProcBrowser);
+    SetWindowLongPtr(hwndBrowser, GWLP_USERDATA, (LONG_PTR)this);
+#endif
+}
+
+void HtmlWindow::UnsubclassHwnd()
 {
     SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)0);
+    HWND hwndBrowser = GetBrowserControlHwnd(hwnd);
+    SetWindowLongPtr(hwndBrowser, GWLP_USERDATA, (LONG_PTR)0);
 }
 
 HtmlWindow::HtmlWindow(HWND hwnd, HtmlWindowCallback *cb) :
@@ -380,10 +405,10 @@ HtmlWindow::HtmlWindow(HWND hwnd, HtmlWindowCallback *cb) :
     oleInPlaceObject(NULL), viewObject(NULL),
     connectionPoint(NULL), oleObjectHwnd(NULL),
     adviseCookie(0), aboutBlankShown(false), htmlWinCb(cb),
+    wndProcBrowserPrev(NULL),
     documentLoaded(false), canGoBack(false), canGoForward(false)
 {
     assert(hwnd);
-    SubclassHtmlHwnd(hwnd, this);
     CreateBrowser();
 }
 
@@ -459,11 +484,14 @@ void HtmlWindow::CreateBrowser()
 
     webBrowser->put_RegisterAsBrowser(VARIANT_TRUE);
     webBrowser->put_RegisterAsDropTarget(VARIANT_TRUE);
+    EnsureAboutBlankShown();
+    WaitUntilLoaded(3*1000);
+    SubclassHwnd();
 }
 
 HtmlWindow::~HtmlWindow()
 {
-    UnsubclassHtmlHwnd(hwnd);
+    UnsubclassHwnd();
     if (oleInPlaceObject) {
         oleInPlaceObject->InPlaceDeactivate();
         oleInPlaceObject->UIDeactivate();

src/utils/HtmlWindow.h
@@ -41,7 +41,13 @@ protected:
     void EnsureAboutBlankShown();
     void CreateBrowser();
 
+
+    void SubclassHwnd();
+    void UnsubclassHwnd();
+
 public:
+    WNDPROC wndProcBrowserPrev;
+
     HtmlWindow(HWND hwnd, HtmlWindowCallback *cb);
     ~HtmlWindow();
 

commit 0773d4e79e8dcbf494fdb7433ef00aa1d1c32c09

don't let loading about:blank cause WaitUntilLoaded to return too early, causing blank thumbnails to be created (regression from r4643)

src/ChmEngine.cpp
@@ -226,7 +226,7 @@ void CChmEngine::DisplayPage(const TCHAR *pageUrl)
     // TODO: unfortunately we display at standard zoom level first
     // and them zoom in, which is visible to the user. No idea how
     // to fix it (simply)
-    htmlWindow->WaitUntilLoaded(3*1000);
+    htmlWindow->WaitUntilLoaded(3 * 1000, url);
     //htmlWindow->DisplayHtml(_T("<html><body>Hello!</body></html>"));
 }
 

src/utils/HtmlWindow.cpp
@@ -406,9 +406,9 @@ HtmlWindow::HtmlWindow(HWND hwndParent, HtmlWindowCallback *cb) :
     hwndParent(hwndParent), webBrowser(NULL), oleObject(NULL),
     oleInPlaceObject(NULL), viewObject(NULL),
     connectionPoint(NULL), oleObjectHwnd(NULL),
-    adviseCookie(0), aboutBlankShown(false), htmlWinCb(cb),
+    adviseCookie(0), htmlWinCb(cb),
     wndProcBrowserPrev(NULL),
-    documentLoaded(false), canGoBack(false), canGoForward(false)
+    canGoBack(false), canGoForward(false)
 {
     assert(hwndParent);
     CreateBrowser();
@@ -486,8 +486,8 @@ void HtmlWindow::CreateBrowser()
 
     webBrowser->put_RegisterAsBrowser(VARIANT_TRUE);
     webBrowser->put_RegisterAsDropTarget(VARIANT_TRUE);
+
     EnsureAboutBlankShown();
-    WaitUntilLoaded(3*1000);
     SubclassHwnd();
 }
 
@@ -556,13 +556,13 @@ void HtmlWindow::NavigateToUrl(const TCHAR *urlStr)
 #endif
     if (!url.bstrVal)
         return;
+    currentURL.Set(NULL);
     webBrowser->Navigate2(&url, 0, 0, 0, 0);
     VariantClear(&url);
 }
 
 void HtmlWindow::GoBack()
 {
-    aboutBlankShown = false; // TODO: is this necessary?
     if (webBrowser)
         webBrowser->GoBack();
 }
@@ -603,20 +603,8 @@ void HtmlWindow::FindInCurrentPage()
 
 void HtmlWindow::EnsureAboutBlankShown()
 {
-    if (aboutBlankShown)
-        return;
-    aboutBlankShown = true;
-
     NavigateToUrl(_T("about:blank"));
-    ScopedComQIPtr<IHTMLDocument2> doc;
-    // wait until shown
-    while (!doc) {
-        Sleep(0);
-        ScopedComPtr<IDispatch> docDispatch;
-        HRESULT hr = webBrowser->get_Document(&docDispatch);
-        if (SUCCEEDED(hr) && docDispatch)
-            doc = docDispatch;
-    }
+    WaitUntilLoaded(INFINITE, _T("about:blank"));
 }
 
 void HtmlWindow::DisplayHtml(const TCHAR *html)
@@ -704,7 +692,7 @@ HBITMAP HtmlWindow::TakeScreenshot(RectI area, SizeI finalSize)
 // the navigation.
 bool HtmlWindow::OnBeforeNavigate(const TCHAR *url, bool newWindow)
 {
-    documentLoaded = false;
+    currentURL.Set(NULL);
     if (htmlWinCb)
         return htmlWinCb->OnBeforeNavigate(url, newWindow);
     return true;
@@ -712,7 +700,7 @@ bool HtmlWindow::OnBeforeNavigate(const TCHAR *url, bool newWindow)
 
 void HtmlWindow::OnDocumentComplete(const TCHAR *url)
 {
-    documentLoaded = true;
+    currentURL.Set(str::Dup(url));
 }
 
 static void PumpRemainingMessages()
@@ -734,17 +722,15 @@ void HtmlWindow::SendMsg(UINT msg, WPARAM wp, LPARAM lp)
     SendMessage(hwndBrowser, msg, wp, lp);
 }
 
-// Probably could check IHTMLDocument2::::get_readyState() but checking
-// documentLoaded works
-bool HtmlWindow::WaitUntilLoaded(DWORD maxWaitMs)
+bool HtmlWindow::WaitUntilLoaded(DWORD maxWaitMs, const TCHAR *url)
 {
     MillisecondTimer timer(true);
-    const DWORD sleepTimeMs = 100; // 0.1 sec
-    while (!documentLoaded && timer.GetCurrTimeInMs() < maxWaitMs) {
+    // wait for either the url or any url (if url == NULL) being loaded
+    while (str::Eq(currentURL, url) == !url && timer.GetCurrTimeInMs() < maxWaitMs) {
         PumpRemainingMessages();
-        Sleep(sleepTimeMs);
+        Sleep(100);
     }
-    return documentLoaded;
+    return currentURL != NULL;
 }
 
 FrameSite::FrameSite(HtmlWindow * win)

src/utils/HtmlWindow.h
@@ -31,15 +31,12 @@ protected:
 
     DWORD               adviseCookie;
 
-    bool                aboutBlankShown;
-
-    bool                documentLoaded;
+    ScopedMem<TCHAR>    currentURL;
 
     HtmlWindowCallback *htmlWinCb;
 
     void EnsureAboutBlankShown();
     void CreateBrowser();
-
 
     void SubclassHwnd();
     void UnsubclassHwnd();
@@ -60,7 +57,7 @@ public:
     void SetZoomPercent(int zoom);
     int  GetZoomPercent();
     void FindInCurrentPage();
-    bool WaitUntilLoaded(DWORD maxWaitMs);
+    bool WaitUntilLoaded(DWORD maxWaitMs, const TCHAR *url=NULL);
     void SendMsg(UINT msg, WPARAM wp, LPARAM lp);
     void OnLButtonDown() const;
 