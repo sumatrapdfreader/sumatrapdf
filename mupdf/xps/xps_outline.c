f60ddbd3f22bc909a134fb91f077c2d352f6b5a5
Simon Bunzli <zeniko@gmail.com>
2011-11-28 21:11:33 +0000

    improve detection of XPS document properties

--- mupdf/xps/xps_outline.c
+++ mupdf/xps/xps_outline.c
@@ -191,19 +191,17 @@ xps_open_and_parse(xps_context *ctx, char *path, xml_element **rootp)
 {
 	xps_part *part = xps_read_part(ctx, path);
 	if (!part)
-		return fz_rethrow(-1, "cannot read zip part '%s'", path);
+		return fz_rethrow(-1, "cannot read part '%s'", path);
 
 	*rootp = xml_parse_document(part->data, part->size);
 	xps_free_part(ctx, part);
 
 	if (!*rootp)
-		return fz_rethrow(-1, "cannot parse metadata for part '%s'", path);
+		return fz_rethrow(-1, "cannot parse part '%s'", path);
 	return fz_okay;
 }
 
 static inline int iswhite(c) { return c == ' ' || c == '\t' || c == '\n' || c == '\r'; }
-
-// <cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><dc:title>XML Paper Specification</dc:title><dc:subject>XPS Specification and Reference Guide</dc:subject><dc:creator>Jesse McGatha</dc:creator><dcterms:created xsi:type="dcterms:W3CDTF">2006-10-19T01:21:08Z</dcterms:created><dcterms:modified xsi:type="dcterms:W3CDTF">2006-10-19T01:21:08Z</dcterms:modified></cp:coreProperties>
 
 static void
 xps_hacky_get_prop(char *data, fz_obj *dict, char *name, char *tag_name)
@@ -227,34 +225,33 @@ xps_hacky_get_prop(char *data, fz_obj *dict, char *name, char *tag_name)
 	fz_drop_obj(value);
 }
 
-#define CONTENT_TYPE_CORE_PROPS \
-	"application/vnd.openxmlformats-package.core-properties+xml"
+#define REL_CORE_PROPERTIES \
+	"http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties"
 
 static int
 xps_find_doc_props_path(xps_context *ctx, char path[1024])
 {
 	xml_element *root;
 
-	int code = xps_open_and_parse(ctx, "/[Content_Types].xml", &root);
+	int code = xps_open_and_parse(ctx, "/_rels/.rels", &root);
 	if (code != fz_okay)
 		return code;
 
 	*path = '\0';
-	if (root && !strcmp(xml_tag(root), "Types"))
+	if (!strcmp(xml_tag(root), "Relationships"))
 	{
 		xml_element *item;
 		for (item = xml_down(root); item; item = xml_next(item))
 		{
-			if (!strcmp(xml_tag(item), "Override") && xml_att(item, "ContentType") &&
-				!strcmp(xml_att(item, "ContentType"), CONTENT_TYPE_CORE_PROPS) &&
-				xml_att(item, "PartName"))
+			if (!strcmp(xml_tag(item), "Relationship") && xml_att(item, "Type") &&
+				!strcmp(xml_att(item, "Type"), REL_CORE_PROPERTIES) && xml_att(item, "Target"))
 			{
-				fz_strlcpy(path, xml_att(item, "PartName"), 1024);
+				xps_absolute_path(path, "", xml_att(item, "Target"), 1024);
 			}
 		}
 	}
 	else
-		code = fz_throw("couldn't parse part '[Content_Types].xml'");
+		code = fz_throw("couldn't parse part '/_rels/.rels'");
 
 	xml_free_element(root);
 
@@ -268,10 +265,7 @@ fz_obj *xps_extract_doc_props(xps_context *ctx)
 	fz_obj *dict;
 
 	if (xps_find_doc_props_path(ctx, path) != fz_okay)
-	{
-		fz_catch(-1, "couldn't find the exact part name for /docProps/core.xml");
-		fz_strlcpy(path, "/docProps/core.xml", sizeof(path));
-	}
+		fz_catch(-1, "ignore broken part '/_rels/.rels'");
 	if (!*path)
 		return NULL;
 
