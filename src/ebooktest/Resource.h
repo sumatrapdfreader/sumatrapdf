e61c9c22e86a39ce3ea3568bb5d3573a423a6521
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-12-06 08:21:06 +0000

    start on ebooktest app

---
+++ src/ebooktest/Resource.h
@@ -0,0 +1,2 @@
+#define IDI_SUMATRAPDF                  1
+
---
+++ src/ebooktest/et.cpp
@@ -0,0 +1,536 @@
+/* Copyright 2010-2011 the SumatraPDF project authors (see AUTHORS file).
+   License: GPLv3 */
+
+#include <windows.h>
+#include <GdiPlus.h>
+#include <shlobj.h>
+#include <Tlhelp32.h>
+#include <Shlwapi.h>
+#include <objidl.h>
+#include <io.h>
+
+#include "Resource.h"
+#include "BaseUtil.h"
+#include "StrUtil.h"
+#include "FileUtil.h"
+#include "WinUtil.h"
+#include "Version.h"
+#include "Vec.h"
+#include "CmdLineParser.h"
+#include "Transactions.h"
+#include "Scopes.h"
+
+using namespace Gdiplus;
+
+// define to 1 to enable shadow effect, to 0 to disable
+#define DRAW_TEXT_SHADOW 1
+#define DRAW_MSG_TEXT_SHADOW 0
+
+#define ET_FRAME_CLASS_NAME    _T("ET_FRAME")
+
+#define WIN_DX    420
+#define WIN_DY    340
+
+#define ID_BUTTON_EXIT                11
+
+#define PUSH_BUTTON_DY  dpiAdjust(22)
+#define WINDOW_MARGIN   8
+
+static HINSTANCE        ghinst;
+static HWND             gHwndFrame = NULL;
+static HWND             gHwndButtonExit = NULL;
+
+static HFONT            gFontDefault;
+
+Color gCol1(196, 64, 50); Color gCol1Shadow(134, 48, 39);
+Color gCol2(227, 107, 35); Color gCol2Shadow(155, 77, 31);
+Color gCol3(93,  160, 40); Color gCol3Shadow(51, 87, 39);
+Color gCol4(69, 132, 190); Color gCol4Shadow(47, 89, 127);
+Color gCol5(112, 115, 207); Color gCol5Shadow(66, 71, 118);
+
+
+#define TEN_SECONDS_IN_MS 10*1000
+
+static HFONT CreateDefaultGuiFont()
+{
+    HDC hdc = GetDC(NULL);
+    HFONT font = GetSimpleFont(hdc, _T("MS Shell Dlg"), 14);
+    ReleaseDC(NULL, hdc);
+    return font;
+}
+
+static float gUiDPIFactor = 1.0f;
+
+inline int dpiAdjust(int value)
+{
+    return (int)(value * gUiDPIFactor);
+}
+
+static void InvalidateFrame()
+{
+    ClientRect rc(gHwndFrame);
+    InvalidateRect(gHwndFrame, &rc.ToRECT(), FALSE);
+}
+static HWND CreateDefaultButton(HWND hwndParent, const TCHAR *label, int width, int id=IDOK)
+{
+    RectI rc(0, 0, dpiAdjust(width), PUSH_BUTTON_DY);
+
+    // TODO: determine the sizes of buttons by measuring their real size
+    // and adjust size of the window appropriately
+    ClientRect r(hwndParent);
+    rc.x = r.dx - rc.dx - WINDOW_MARGIN;
+    rc.y = r.dy - rc.dy - WINDOW_MARGIN;
+    HWND button = CreateWindow(WC_BUTTON, label,
+                        BS_DEFPUSHBUTTON | WS_CHILD | WS_VISIBLE | WS_TABSTOP,
+                        rc.x, rc.y, rc.dx, rc.dy, hwndParent,
+                        (HMENU)id, ghinst, NULL);
+    SetWindowFont(button, gFontDefault, TRUE);
+
+    return button;
+}
+
+static void CreateButtonExit(HWND hwndParent)
+{
+    gHwndButtonExit = CreateDefaultButton(hwndParent, _T("Close"), 80, ID_BUTTON_EXIT);
+}
+
+static void OnButtonExit()
+{
+    SendMessage(gHwndFrame, WM_CLOSE, 0, 0);
+}
+
+inline void EnableAndShow(HWND hwnd, bool enable)
+{
+    ShowWindow(hwnd, enable ? SW_SHOW : SW_HIDE);
+    EnableWindow(hwnd, enable);
+}
+
+// This display is inspired by http://letteringjs.com/
+typedef struct {
+    // part that doesn't change
+    char c;
+    Color col, colShadow;
+    REAL rotation;
+    REAL dyOff; // displacement
+
+    // part calculated during layout
+    REAL dx, dy;
+    REAL x;
+} LetterInfo;
+
+LetterInfo gLetters[] = {
+    { 'S', gCol1, gCol1Shadow, -3.f,     0, 0, 0 },
+    { 'U', gCol2, gCol2Shadow,  0.f,     0, 0, 0 },
+    { 'M', gCol3, gCol3Shadow,  2.f,  -2.f, 0, 0 },
+    { 'A', gCol4, gCol4Shadow,  0.f, -2.4f, 0, 0 },
+    { 'T', gCol5, gCol5Shadow,  0.f,     0, 0, 0 },
+    { 'R', gCol5, gCol5Shadow, 2.3f, -1.4f, 0, 0 },
+    { 'A', gCol4, gCol4Shadow,  0.f,     0, 0, 0 },
+    { 'P', gCol3, gCol3Shadow,  0.f, -2.3f, 0, 0 },
+    { 'D', gCol2, gCol2Shadow,  0.f,   3.f, 0, 0 },
+    { 'F', gCol1, gCol1Shadow,  0.f,     0, 0, 0 }
+};
+
+#define SUMATRA_LETTERS_COUNT (dimof(gLetters))
+
+static char RandUppercaseLetter()
+{
+    // TODO: clearly, not random but seem to work ok anyway
+    static char l = 'A' - 1;
+    l++;
+    if (l > 'Z')
+        l = 'A';
+    return l;
+}
+
+static void RandomizeLetters()
+{
+    for (int i = 0; i < dimof(gLetters); i++) {
+        gLetters[i].c = RandUppercaseLetter();
+    }
+}
+
+static void SetLettersSumatraUpTo(int n)
+{
+    char *s = "SUMATRAPDF";
+    for (int i = 0; i < dimof(gLetters); i++) {
+        if (i < n) {
+            gLetters[i].c = s[i];
+        } else {
+            gLetters[i].c = ' ';
+        }
+    }
+}
+
+static void SetLettersSumatra()
+{
+    SetLettersSumatraUpTo(SUMATRA_LETTERS_COUNT);
+}
+
+class FrameTimeoutCalculator {
+
+    LARGE_INTEGER   timeStart;
+    LARGE_INTEGER   timeLast;
+    LONGLONG        ticksPerFrame;
+    LONGLONG        ticsPerMs;
+    LARGE_INTEGER   timeFreq;
+
+public:
+    FrameTimeoutCalculator(int framesPerSecond) {
+        QueryPerformanceFrequency(&timeFreq); // number of ticks per second
+        ticsPerMs = timeFreq.QuadPart / 1000;
+        ticksPerFrame = timeFreq.QuadPart / framesPerSecond;
+        QueryPerformanceCounter(&timeStart);
+        timeLast = timeStart;
+    }
+
+    // in seconds, as a double
+    double ElapsedTotal() {
+        LARGE_INTEGER timeCurr;
+        QueryPerformanceCounter(&timeCurr);
+        LONGLONG elapsedTicks =  timeCurr.QuadPart - timeStart.QuadPart;
+        double res = (double)elapsedTicks / (double)timeFreq.QuadPart;
+        return res;
+    }
+
+    DWORD GetTimeoutInMilliseconds() {
+        LARGE_INTEGER timeCurr;
+        LONGLONG elapsedTicks;
+        QueryPerformanceCounter(&timeCurr);
+        elapsedTicks = timeCurr.QuadPart - timeLast.QuadPart;
+        if (elapsedTicks > ticksPerFrame) {
+            return 0;
+        } else {
+            LONGLONG timeoutMs = (ticksPerFrame - elapsedTicks) / ticsPerMs;
+            return (DWORD)timeoutMs;
+        }
+    }
+
+    void Step() {
+        timeLast.QuadPart += ticksPerFrame;
+    }
+};
+
+// an animation that reveals letters one by one
+
+// how long the animation lasts, in seconds
+#define REVEALING_ANIM_DUR double(2)
+
+static FrameTimeoutCalculator *gRevealingLettersAnim = NULL;
+
+int gRevealingLettersAnimLettersToShow;
+
+static void RevealingLettersAnimStart()
+{
+    int framesPerSec = (int)(double(SUMATRA_LETTERS_COUNT) / REVEALING_ANIM_DUR);
+    gRevealingLettersAnim = new FrameTimeoutCalculator(framesPerSec);
+    gRevealingLettersAnimLettersToShow = 0;
+    SetLettersSumatraUpTo(0);
+}
+
+static void RevealingLettersAnimStop()
+{
+    delete gRevealingLettersAnim;
+    gRevealingLettersAnim = NULL;
+    SetLettersSumatra();
+    InvalidateFrame();
+}
+
+static void RevealingLettersAnim()
+{
+    if (gRevealingLettersAnim->ElapsedTotal() > REVEALING_ANIM_DUR) {
+        RevealingLettersAnimStop();
+        return;
+    }
+    DWORD timeOut = gRevealingLettersAnim->GetTimeoutInMilliseconds();
+    if (timeOut != 0)
+        return;
+    SetLettersSumatraUpTo(++gRevealingLettersAnimLettersToShow);
+    gRevealingLettersAnim->Step();
+    InvalidateFrame();
+}
+
+static void AnimStep()
+{
+    if (gRevealingLettersAnim)
+        RevealingLettersAnim();
+}
+
+static void CalcLettersLayout(Graphics& g, Font *f, int dx)
+{
+    static BOOL didLayout = FALSE;
+    if (didLayout) return;
+
+    LetterInfo *li;
+    StringFormat sfmt;
+    const REAL letterSpacing = -12.f;
+    REAL totalDx = -letterSpacing; // counter last iteration of the loop
+    WCHAR s[2] = { 0 };
+    Gdiplus::PointF origin(0.f, 0.f);
+    Gdiplus::RectF bbox;
+    for (int i = 0; i < dimof(gLetters); i++) {
+        li = &gLetters[i];
+        s[0] = li->c;
+        g.MeasureString(s, 1, f, origin, &sfmt, &bbox);
+        li->dx = bbox.Width;
+        li->dy = bbox.Height;
+        totalDx += li->dx;
+        totalDx += letterSpacing;
+    }
+
+    REAL x = ((REAL)dx - totalDx) / 2.f;
+    for (int i = 0; i < dimof(gLetters); i++) {
+        li = &gLetters[i];
+        li->x = x;
+        x += li->dx;
+        x += letterSpacing;
+    }
+    RevealingLettersAnimStart();
+    didLayout = TRUE;
+}
+
+static REAL DrawMessage(Graphics &g, TCHAR *msg, REAL y, REAL dx, Color color)
+{
+    ScopedMem<WCHAR> s(str::conv::ToWStr(msg));
+
+    Font f(L"Impact", 16, FontStyleRegular);
+    Gdiplus::RectF maxbox(0, y, dx, 0);
+    Gdiplus::RectF bbox;
+    g.MeasureString(s, -1, &f, maxbox, &bbox);
+
+    bbox.X += (dx - bbox.Width) / 2.f;
+    StringFormat sft;
+    sft.SetAlignment(StringAlignmentCenter);
+#if DRAW_MSG_TEXT_SHADOW
+    {
+        bbox.X--; bbox.Y++;
+        SolidBrush b(Color(0xff, 0xff, 0xff));
+        g.DrawString(s, -1, &f, bbox, &sft, &b);
+        bbox.X++; bbox.Y--;
+    }
+#endif
+    SolidBrush b(color);
+    g.DrawString(s, -1, &f, bbox, &sft, &b);
+
+    return bbox.Height;
+}
+
+static void DrawSumatraLetters(Graphics &g, Font *f, Font *fVer, REAL y)
+{
+    LetterInfo *li;
+    WCHAR s[2] = { 0 };
+    for (int i = 0; i < dimof(gLetters); i++) {
+        li = &gLetters[i];
+        s[0] = li->c;
+        if (s[0] == ' ')
+            return;
+
+        g.RotateTransform(li->rotation, MatrixOrderAppend);
+#if DRAW_TEXT_SHADOW
+        // draw shadow first
+        SolidBrush b2(li->colShadow);
+        Gdiplus::PointF o2(li->x - 3.f, y + 4.f + li->dyOff);
+        g.DrawString(s, 1, f, o2, &b2);
+#endif
+
+        SolidBrush b1(li->col);
+        Gdiplus::PointF o1(li->x, y + li->dyOff);
+        g.DrawString(s, 1, f, o1, &b1);
+        g.RotateTransform(li->rotation, MatrixOrderAppend);
+        g.ResetTransform();
+    }
+
+    // draw version number
+    REAL x = gLetters[dimof(gLetters)-1].x;
+    g.TranslateTransform(x, y);
+    g.RotateTransform(45.f);
+    REAL x2 = 15; REAL y2 = -34;
+
+    ScopedMem<WCHAR> ver_s(str::conv::ToWStr(_T("v") CURR_VERSION_STR));
+#if DRAW_TEXT_SHADOW
+    SolidBrush b1(Color(0, 0, 0));
+    g.DrawString(ver_s, -1, fVer, Gdiplus::PointF(x2 - 2, y2 - 1), &b1);
+#endif
+    SolidBrush b2(Color(0xff, 0xff, 0xff));
+    g.DrawString(ver_s, -1, fVer, Gdiplus::PointF(x2, y2), &b2);
+    g.ResetTransform();
+}
+
+static void DrawFrame2(Graphics &g, RectI r)
+{
+    g.SetCompositingQuality(CompositingQualityHighQuality);
+    g.SetSmoothingMode(SmoothingModeAntiAlias);
+    g.SetPageUnit(UnitPixel);
+
+    Font f(L"Impact", 40, FontStyleRegular);
+    CalcLettersLayout(g, &f, r.dx);
+
+    SolidBrush bgBrush(Color(0xff, 0xf2, 0));
+    Gdiplus::Rect r2(r.y - 1, r.x - 1, r.dx + 1, r.dy + 1);
+    g.FillRectangle(&bgBrush, r2);
+
+    Font f2(L"Impact", 16, FontStyleRegular);
+    DrawSumatraLetters(g, &f, &f2, 18.f);
+}
+
+static void DrawFrame(HWND hwnd, HDC dc, PAINTSTRUCT *ps)
+{
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+    RECT rcTmp;
+    if (IntersectRect(&rcTmp, &rc, &ps->rcPaint)) {
+        HBRUSH brushNativeBg = CreateSolidBrush(GetSysColor(COLOR_BTNFACE));
+        FillRect(dc, &rc, brushNativeBg);
+        DeleteObject(brushNativeBg);
+    }
+
+    // TODO: cache bmp object?
+    Graphics g(dc);
+    ClientRect rc2(hwnd);
+    Bitmap bmp(rc2.dx, rc2.dy, &g);
+    Graphics g2((Image*)&bmp);
+    DrawFrame2(g2, rc2);
+    g.DrawImage(&bmp, 0, 0);
+}
+
+static void OnPaintFrame(HWND hwnd)
+{
+    PAINTSTRUCT ps;
+    HDC dc = BeginPaint(hwnd, &ps);
+    DrawFrame(hwnd, dc, &ps);
+    EndPaint(hwnd, &ps);
+}
+
+static void OnCreateWindow(HWND hwnd)
+{
+    gHwndButtonExit = CreateDefaultButton(hwnd, _T("Exit"), 150);
+}
+
+static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_CREATE:
+            OnCreateWindow(hwnd);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        case WM_ERASEBKGND:
+            return TRUE;
+
+        case WM_PAINT:
+            OnPaintFrame(hwnd);
+            break;
+
+        case WM_COMMAND:
+            switch (LOWORD(wParam))
+            {
+                case ID_BUTTON_EXIT:
+                case IDCANCEL:
+                    OnButtonExit();
+                    break;
+
+                default:
+                    return DefWindowProc(hwnd, message, wParam, lParam);
+            }
+            break;
+
+        default:
+            return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+static BOOL RegisterWinClass(HINSTANCE hInstance)
+{
+    WNDCLASSEX  wcex;
+
+    FillWndClassEx(wcex, hInstance);
+    wcex.lpszClassName  = ET_FRAME_CLASS_NAME;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SUMATRAPDF));
+    wcex.lpfnWndProc    = WndProcFrame;
+
+    ATOM atom = RegisterClassEx(&wcex);
+    return atom != NULL;
+}
+
+static BOOL InstanceInit(HINSTANCE hInstance, int nCmdShow)
+{
+    ghinst = hInstance;
+    gFontDefault = CreateDefaultGuiFont();
+    win::GetHwndDpi(NULL, &gUiDPIFactor);
+
+    gHwndFrame = CreateWindow(
+            ET_FRAME_CLASS_NAME, _T("Ebook Test ") CURR_VERSION_STR,
+            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            dpiAdjust(WIN_DX), dpiAdjust(WIN_DY),
+            NULL, NULL,
+            ghinst, NULL);
+
+    if (!gHwndFrame)
+        return FALSE;
+
+    CenterDialog(gHwndFrame);
+    ShowWindow(gHwndFrame, SW_SHOW);
+
+    return TRUE;
+}
+
+// inspired by http://engineering.imvu.com/2010/11/24/how-to-write-an-interactive-60-hz-desktop-application/
+static int RunApp()
+{
+    MSG msg;
+    FrameTimeoutCalculator ftc(60);
+    MillisecondTimer t;
+    t.Start();
+    for (;;) {
+        const DWORD timeout = ftc.GetTimeoutInMilliseconds();
+        DWORD res = WAIT_TIMEOUT;
+        if (timeout > 0) {
+            res = MsgWaitForMultipleObjects(0, 0, TRUE, timeout, QS_ALLEVENTS);
+        }
+        if (res == WAIT_TIMEOUT) {
+            AnimStep();
+            ftc.Step();
+        }
+
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                return (int)msg.wParam;
+            }
+            if (!IsDialogMessage(gHwndFrame, &msg)) {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+    }
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    int ret = 1;
+
+    SetErrorMode(SEM_NOOPENFILEERRORBOX | SEM_FAILCRITICALERRORS);
+
+    ScopedCom com;
+    InitAllCommonControls();
+    ScopedGdiPlus gdi;
+
+    //ParseCommandLine(GetCommandLine());
+
+    if (!RegisterWinClass(hInstance))
+        goto Exit;
+
+    if (!InstanceInit(hInstance, nCmdShow))
+        goto Exit;
+
+    ret = RunApp();
+
+Exit:
+    return ret;
+}
