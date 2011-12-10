1fcf9d91baf2c1724e3dda7d8da78d6dd19cf509
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-12-10 06:41:30 +0000

    import _ftol2_sse() from msvcrt.dll; stub implementations of _alldiv() and WinMainCRTStartup() (release et.exe links but crashes in gdi+

--- ext/minicrt/minicrt.cc
+++ ext/minicrt/minicrt.cc
@@ -22,8 +22,12 @@ More info:
 * http://kobyk.wordpress.com/2007/07/20/dynamically-linking-with-msvcrtdll-using-visual-c-2005/
 * http://adrianhenke.wordpress.com/2008/12/05/create-lib-file-from-dll/ - info on how to create
   .lib file from .def file
+ * http://www.ibsensoftware.com/download.html wcrt is another small C runtime library, not
+   open source
 
 TODO:
+* release build of et.exe links but crashes in gdiplus, possibly because
+  I'm not calling C++ constructors
 * msvcrt.dll (c:\windows\system32\msvcrt.dll) might contain
   more functionality in later versions of windows. We have
   to make sure we don't use function that are not present in XP
@@ -31,10 +35,8 @@ TODO:
   dumpbin and not using anything that is not there)
 */
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
 
+#if 0
 #ifdef UNICODE
 #undef UNICODE
 #endif
@@ -42,7 +44,11 @@ TODO:
 #ifdef _UNICODE
 #undef _UNICODE
 #endif
+#endif
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -76,4 +82,66 @@ FILE * __cdecl __iob_func(void) {
 	return __p__iob();
 }
 
+LONGLONG _alldiv(LONGLONG a, LONGLONG b) {
+	// TODO: clearly, this is not a correct implementation
+	return a;
+}
+
+}
+
+extern "C" {
+int _fltused = 0x9875;	
+}
+
+extern "C" void __cdecl WinMainCRTStartup() {
+    int mainret;
+    char *lpszCommandLine;
+    STARTUPINFO StartupInfo;
+
+    lpszCommandLine = GetCommandLineA();
+
+    // Skip past program name (first token in command line).
+
+    if (*lpszCommandLine == '"')  // Check for and handle quoted program name
+    {
+        lpszCommandLine++;  // Get past the first quote
+
+        // Now, scan, and skip over, subsequent characters until  another
+        // double-quote or a null is encountered
+        while (*lpszCommandLine && (*lpszCommandLine != '"'))
+            lpszCommandLine++;
+
+        // If we stopped on a double-quote (usual case), skip over it.
+
+        if (*lpszCommandLine == '"')
+            lpszCommandLine++;
+    }
+    else    // First token wasn't a quote
+    {
+        while (*lpszCommandLine > ' ')
+            lpszCommandLine++;
+    }
+
+    // Skip past any white space preceeding the second token.
+
+    while (*lpszCommandLine && (*lpszCommandLine <= ' '))
+        lpszCommandLine++;
+
+    StartupInfo.dwFlags = 0;
+    GetStartupInfo(&StartupInfo);
+
+    // set up our minimal cheezy atexit table
+    //_atexit_init();
+
+    // Call C++ constructors
+    //_initterm( __xc_a, __xc_z );
+
+    mainret = WinMain(GetModuleHandle(NULL),
+                      NULL,
+                      lpszCommandLine,
+                      StartupInfo.dwFlags & STARTF_USESHOWWINDOW
+                            ? StartupInfo.wShowWindow : SW_SHOWDEFAULT );
+
+    //_DoExit();
+    ExitProcess(mainret);
 }
