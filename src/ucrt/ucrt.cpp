8636db4de9b9891b4f10d2b7e749c4383b72a6e7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-12-11 00:15:00 +0000

    spoke too soon, compiler doesn't know the definition of _ftol()

--- src/ucrt/ucrt.cpp
+++ src/ucrt/ucrt.cpp
@@ -126,12 +126,14 @@ void __cdecl _wassert(const wchar_t *msg, const wchar_t *file, unsigned line)
     crash_me();
 }
 
+#if 0
 // TODO: msvcrt.dll in XP doesn't have _ftol2_sse, so we match it
 // just _ftol(), which might be slower. Could find an optimized version.
 void __declspec(naked) _ftol2_sse()
 {
     _ftol();
 }
+#endif
 
 #if 0
 // TODO: can it be made just an alias to __p_iob ?
