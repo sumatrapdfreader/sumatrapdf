10733a38c9e1d7a2bedcfb07b6cbecd0ee91367d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-12-17 03:32:46 +0000

    ebook: first crack at simple text layout, far from being correct

--- src/ebooktest/et.cpp
+++ src/ebooktest/et.cpp
@@ -148,17 +148,225 @@ static REAL DrawMessage(Graphics &g, TCHAR *msg, REAL y, REAL dx, Color color)
 #endif
 }
 
+// A sample text we're laying out
+const char *gTxt = "ClearType is dependent on the orientation and ordering of the LCD stripes. Currently,\nClearType is implemented only for vertical stripes that are ordered RGB. This might be a concern if you are\n using a tablet PC, where the display can be oriented in any direction, or if you are using a screen that\ncan be turned from landscape to portrait.\n\nThe following example draws text with two different quality settings:";
+
+struct StringPos {
+    StringPos() : s(NULL), len(0), x(0), y(0) {
+    }
+    StringPos(const char *s, size_t len, REAL x, REAL y) : s(s), len(len), x(x), y(y) {
+    }
+    const char *s;
+    size_t len;
+    REAL x,y;
+};
+
+class Page {
+public:
+    Page() : dx(0), dy(0), strings(NULL) {
+    }
+    Page(int dx, int dy) : dx(dx), dy(dy) {
+        strings = new Vec<StringPos>();
+    }
+    ~Page() {
+        delete strings;
+    }
+    int dx, dy; // used during layout
+    Vec<StringPos> *strings;
+};
+
+Vec<Page*> *gPages;
+
+static void Utf8ToWchar(const char *s, size_t sLen, WCHAR *bufOut, size_t bufOutMax)
+{
+    // TODO: clearly, write proper utf8 => wchar, check we don't exceed bufOutMax
+    for (size_t i = 0; i < sLen; i++) {
+        bufOut[i] = *s++;
+    }
+    bufOut[sLen] = 0;
+}
+
+struct WordInfo {
+    const char *s;
+    size_t len;
+    bool IsNewline() {
+        return ((len == 1) && (s[0] == '\n'));
+    }
+};
+
+class WordsIter {
+public:
+    WordsIter(const char *s) : s(s) {
+        Reset();
+    }
+
+    void Reset() {
+        curr = s;
+        len = strlen(s);
+        left = len;
+    }
+
+    WordInfo *Next();
+
+private:
+    WordInfo wi;
+
+    static const char *NewLineStr;
+    const char *s;
+    size_t len;
+
+    const char *curr;
+    size_t left;
+};
+
+const char *WordsIter::NewLineStr = "\n";
+
+static void SkipCharInStr(const char *& s, size_t& left, char c)
+{
+    while ((left > 0) && (*s == c)) {
+        ++s; --left;
+    }
+}
+
+static bool IsWordBreak(char c)
+{
+    return (c == ' ') || (c == '\n') || (c == '\r');
+}
+
+static void SkipNonWordBreak(const char *& s, size_t& left)
+{
+    while ((left > 0) && !IsWordBreak(*s)) {
+        ++s; --left;
+    }
+}
+
+// return true if s points to "\n", "\n\r", "\r" or "\r\n"
+// and advance s/left to skip it
+// We don't want to collapse multiple consequitive newlines into
+// one as we want to be able to detect paragraph breaks (i.e. empty
+// newlines i.e. a newline following another newline)
+static bool IsNewlineSkip(const char *& s, size_t& left)
+{
+    if (0 == left)
+        return false;
+    if ('\r' == *s) {
+        --left; ++s;
+        if ((left > 0) && ('\n' == *s)) {
+            --left; ++s;
+        }
+        return true;
+    } else if ('\n' == *s) {
+        --left; ++s;
+        if ((left > 0) && ('\r' == *s)) {
+            --left; ++s;
+        }
+        return true;
+    }
+    return false;
+}
+
+// iterates words in a string e.g. "foo bar\n" returns "foo", "bar" and "\n"
+// also unifies line endings i.e. "\r" an "\n\r" are turned into a single "\n"
+// returning NULL means end of iterations
+WordInfo *WordsIter::Next()
+{
+    SkipCharInStr(curr, left, ' ');
+    if (0 == left)
+        return NULL;
+    assert(*curr != 0);
+    if (IsNewlineSkip(curr, left)) {
+        wi.len = 0;
+        wi.s = NewLineStr;
+        return &wi;
+    }
+    wi.s = curr;
+    SkipNonWordBreak(curr, left);
+    wi.len = curr - wi.s;
+    assert(wi.len > 0);
+    return &wi;
+}
+
+static Vec<Page*> *LayoutText(Graphics *g, Font *f, int pageDx, int pageDy, const char *s)
+{
+    Vec<Page*> *pages = new Vec<Page*>();
+    WCHAR buf[512];
+    PointF pointZero(0, 0);
+    RectF bb;
+    g->MeasureString(L" ", 1, f, pointZero, &bb);
+    REAL spaceDx = bb.Width;
+    //FontFamily ff;
+    //f->GetFamily(&ff);
+    //REAL lineSpacing = (REAL)ff.GetLineSpacing(f->GetStyle()); // TODO: why does it return ridiculusly big numbe?
+    REAL lineSpacing = f->GetHeight(g);
+    WordsIter iter(s);
+    WordInfo *wi;
+    for (;;) {
+        // layout a single page
+        Page *page = new Page(pageDx, pageDy);
+        REAL dyLeft = (REAL)pageDy;
+        REAL y = 0;
+        while (dyLeft > lineSpacing) {
+            // layout a single line
+            REAL x = 0;
+            for (;;) {
+                wi = iter.Next();
+                if (NULL == wi)
+                    break;
+                Utf8ToWchar(wi->s, wi->len, buf, dimof(buf));
+                g->MeasureString(buf, wi->len, f, pointZero, &bb);
+                StringPos sp(wi->s, wi->len, x, y);
+                x += bb.Width;
+                page->strings->Append(sp);
+                x += spaceDx;
+                if (x > pageDx)
+                    break;
+            }
+            if (NULL == wi)
+                break;
+            dyLeft -= lineSpacing;
+            y += lineSpacing;
+        }
+        pages->Append(page);
+        if (NULL == wi)
+            break;
+    }
+    return pages;
+}
+
+static void DrawPage(Graphics *g, Font *f, int pageNo, REAL offX, REAL offY)
+{
+    SolidBrush br(Color(0,0,0)); // black
+    Page *p = gPages->At(pageNo);
+    size_t n = p->strings->Count();
+    WCHAR buf[512];
+    for (size_t i = 0; i < n; i++) {
+        StringPos sp = p->strings->At(i);
+        PointF o(sp.x + offX, sp.y + offY);
+        Utf8ToWchar(sp.s, sp.len, buf, dimof(buf));
+        g->DrawString(buf, sp.len, f, o, NULL, &br);
+    }
+}
+
 static void DrawFrame2(Graphics &g, RectI r)
 {
     g.SetCompositingQuality(CompositingQualityHighQuality);
     g.SetSmoothingMode(SmoothingModeAntiAlias);
     g.SetPageUnit(UnitPixel);
 
+    const int pageBorderX = 10;
+    const int pageBorderY = 20;
+    if (!gPages) {
+        int pageDx = r.dx - (pageBorderX * 2);
+        int pageDy = r.dy - (pageBorderY * 2);
+        gPages = LayoutText(&g, gFont, pageDx, pageDy , gTxt);
+    }
+
     //SolidBrush bgBrush(gColBg);
     Gdiplus::Rect r2(r.y - 1, r.x - 1, r.dx + 1, r.dy + 1);
     LinearGradientBrush bgBrush(RectF(0, 0, (REAL)r.dx, (REAL)r.dy), Color(0xd0,0xd0,0xd0), Color(0xff,0xff,0xff), LinearGradientModeVertical);
     g.FillRectangle(&bgBrush, r2);
 
+    DrawPage(&g, gFont, 0, (REAL)pageBorderX, (REAL)pageBorderY);
 }
 
 static bool BitmapSizeEquals(Bitmap *bmp, int dx, int dy)
@@ -330,6 +538,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
     ret = RunApp();
 
+    DeleteVecMembers<Page*>(*gPages);
+    delete gPages;
     ::delete gFont;
     ::delete frameBmp;
 
