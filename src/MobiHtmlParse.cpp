e3cd0b9d61d87ca43be1f550880276997108980d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-01-07 08:05:30 +0000

    mobi: categorize html tags in mobi files

--- src/MobiHtmlParse.cpp
+++ src/MobiHtmlParse.cpp
@@ -160,6 +160,108 @@ Next:
     return &currToken;
 }
 
+
+// enum names match the order of tags in an array
+enum TagEnum {
+    TagNotFound = -1,
+    Tag_A = 0,
+    Tag_B = 1,
+    Tag_Blockquote = 2,
+    Tag_Body = 3,
+    Tag_Br = 4,
+    Tag_Div = 5,
+    Tag_Font = 6,
+    Tag_Guide = 7,
+    Tag_H2 = 8,
+    Tag_Head = 9,
+    Tag_Html = 10,
+    Tag_I = 11,
+    Tag_Img = 12,
+    Tag_Li = 13,
+    Tag_Mbp_Pagebreak = 14,
+    Tag_Ol = 15,
+    Tag_P = 16,
+    Tag_Reference = 17,
+    Tag_Span = 18,
+    Tag_Sup = 19,
+    Tag_Table = 20,
+    Tag_Td = 21,
+    Tag_Tr = 22,
+    Tag_U = 23,
+    Tag_Ul = 24
+};
+
+char *gTags = "a\0b\0blockquote\0body\0br\0div\0font\0guide\0h2\0head\0html\0i\0img\0li\0mbp:pagebreak\0ol\0p\0reference\0span\0sup\0table\0td\0tr\0u\0ul\0";
+
+TagEnum FindTag(char *tag, size_t len)
+{
+    char *curr = gTags;
+    char *end = tag + len;
+    char firstTagChar = *tag;
+    int n = 0;
+    for (;;) {
+        // we're at the start of the next tag
+        char c = *curr;
+        if ((0 == c) || (c > firstTagChar)) {
+            // gTags are sorted alphabetically, so we
+            // can quit if current tag is > tag
+            return TagNotFound;
+        }
+        char *s = tag;
+        while (*curr && (s < end)) {
+            if (*s++ != *curr++)
+                goto NextTag;
+        }
+        if ((s == end) && (0 == *curr))
+            return (TagEnum)n;
+NextTag:
+        while (*curr) {
+            ++curr;
+        }
+        ++curr;
+        ++n;
+    }
+    return TagNotFound;
+}
+
+#if 0
+void DumpTag(HtmlToken *t)
+{
+    char buf[1024];
+    if (t->sLen + 3 > dimof(buf))
+        return;
+    memcpy(buf, t->s, t->sLen);
+    char *end = buf + t->sLen;
+    char *tmp = buf;
+    while ((tmp < end) && (*tmp != ' ')) {
+        ++tmp;
+    }
+    *tmp++ = '\n';
+    *tmp = 0;
+    printf(buf);
+}
+#endif
+
+static int IsName(int c)
+{
+    return c == '.' || c == '-' || c == '_' || c == ':' ||
+        (c >= '0' && c <= '9') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= 'a' && c <= 'z');
+}
+
+static size_t GetTagLen(uint8_t *s, size_t len)
+{
+    uint8_t *end = s + len;
+    uint8_t *curr = s;
+    while (curr < end) {
+        if (!IsName(*curr))
+            return curr - s;
+        ++curr;
+    }
+    return len;
+}
+
 // convert mobi html to a format optimized for layout/display
 // returns NULL in case of an error
 // caller has to free() the result
@@ -176,6 +278,18 @@ uint8_t *MobiHtmlToDisplay(uint8_t *s, size_t sLen, size_t& lenOut)
         if (t->IsError())
             return NULL;
         // TODO: interpret the tag
+        if (t->IsTag()) {
+            // HtmlToken string includes potential attributes,
+            // get the length of just the tagb
+            size_t tagLen = GetTagLen(t->s, t->sLen);
+            TagEnum tag = FindTag((char*)t->s, tagLen);
+            assert(tag != TagNotFound);
+        }
+#if 0
+        if (t->IsTag()) {
+            DumpTag(t);
+        }
+#endif
     }
     lenOut = res.Size();
     uint8_t *resData = res.StealData();
