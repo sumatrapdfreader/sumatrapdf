c328f0deda4faa7374ece6ea83a2a84cf0a3440e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-01-21 01:11:14 +0000

    add bit::Set(), bit::Clear(), bit::IsSet() for easier bit setting/clearing/testing

---
+++ src/utils/BitManip.h
@@ -0,0 +1,34 @@
+/* Copyright 2012 the SumatraPDF project authors (see AUTHORS file).
+   License: Simplified BSD (see COPYING.BSD) */
+
+#ifndef BitManip_h
+#define BitManip_h
+
+/* Simple functions to make it easier to set/clear/test for bits in integers */
+
+namespace bit {
+
+template <typename T>
+void Set(T& v, int bitNo)
+{
+    T mask = 1 << bitNo;
+    v |= mask;
+}
+
+template <typename T>
+void Clear(T& v, int bitNo)
+{
+    T mask = 1 << bitNo;
+    v &= ~mask;
+}
+
+template <typename T>
+bool IsSet(T v, int bitNo)
+{
+    T mask = 1 << bitNo;
+    return (v & mask) != 0;
+}
+
+}
+
+#endif
