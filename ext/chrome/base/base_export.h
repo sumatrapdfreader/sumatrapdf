22b0bc4fb167224c72d243f1e49b81ba82ac8e34
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-01-28 06:40:35 +0000

    there is no COMPONENT_BUILD

--- ext/chrome/base/base_export.h
+++ ext/chrome/base/base_export.h
@@ -6,21 +6,6 @@
 #define BASE_BASE_EXPORT_H_
 #pragma once
 
-#if defined(COMPONENT_BUILD)
-#if defined(WIN32)
-
-#if defined(BASE_IMPLEMENTATION)
-#define BASE_EXPORT __declspec(dllexport)
-#else
-#define BASE_EXPORT __declspec(dllimport)
-#endif  // defined(BASE_IMPLEMENTATION)
-
-#else  // defined(WIN32)
-#define BASE_EXPORT __attribute__((visibility("default")))
-#endif
-
-#else  // defined(COMPONENT_BUILD)
 #define BASE_EXPORT
-#endif
 
 #endif  // BASE_BASE_EXPORT_H_
