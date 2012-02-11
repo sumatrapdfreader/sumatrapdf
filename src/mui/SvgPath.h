8c72bd475596e9c360023260546c4c35277b23d7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-02-11 09:24:23 +0000

    forgot to add SvgPath.h

---
+++ src/mui/SvgPath.h
@@ -0,0 +1,16 @@
+/* Copyright 2012 the SumatraPDF project authors (see AUTHORS file).
+   License: Simplified BSD (see COPYING.BSD) */
+
+#ifndef SvgPath_h
+#define SvgPath_h
+
+namespace svg {
+
+using namespace Gdiplus;
+#include "GdiPlusUtil.h"
+
+GraphicsPath *GraphicsPathFromPathData(const char *s);
+
+}
+
+#endif
