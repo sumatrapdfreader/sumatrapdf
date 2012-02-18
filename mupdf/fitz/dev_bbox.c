4f6165a502e645d6210dd365a011e9a96e854c42
Simon Bunzli <zeniko@gmail.com>
2012-02-18 00:26:54 +0000

    improve dev_bbox handling of masks and tiling

--- mupdf/fitz/dev_bbox.c
+++ mupdf/fitz/dev_bbox.c
@@ -9,6 +9,7 @@ typedef struct fz_bbox_data_s
 	fz_bbox *result;
 	int top;
 	fz_rect stack[STACK_SIZE];
+	int ignore;
 } fz_bbox_data;
 
 static void
@@ -18,7 +19,7 @@ fz_bbox_add_rect(fz_device *dev, fz_rect rect, int clip)
 
 	if (0 < data->top && data->top <= STACK_SIZE)
 		rect = fz_intersect_rect(rect, data->stack[data->top-1]);
-	if (!clip && data->top <= STACK_SIZE)
+	if (!clip && data->top <= STACK_SIZE && !data->ignore)
 		*data->result = fz_union_bbox(*data->result, fz_round_rect(rect));
 	if (clip && ++data->top <= STACK_SIZE)
 		data->stack[data->top-1] = rect;
@@ -115,7 +116,17 @@ fz_bbox_pop_clip(fz_device *dev)
 static void
 fz_bbox_begin_mask(fz_device *dev, fz_rect rect, int luminosity, fz_colorspace *colorspace, float *color)
 {
+	fz_bbox_data *data = dev->user;
 	fz_bbox_add_rect(dev, rect, 1);
+	data->ignore++;
+}
+
+static void
+fz_bbox_end_mask(fz_device *dev)
+{
+	fz_bbox_data *data = dev->user;
+	assert(data->ignore > 0);
+	data->ignore--;
 }
 
 static void
@@ -128,6 +139,22 @@ static void
 fz_bbox_end_group(fz_device *dev)
 {
 	fz_bbox_pop_clip(dev);
+}
+
+static void
+fz_bbox_begin_tile(fz_device *dev, fz_rect area, fz_rect view, float xstep, float ystep, fz_matrix ctm)
+{
+	fz_bbox_data *data = dev->user;
+	fz_bbox_add_rect(dev, area, 0);
+	data->ignore++;
+}
+
+static void
+fz_bbox_end_tile(fz_device *dev)
+{
+	fz_bbox_data *data = dev->user;
+	assert(data->ignore > 0);
+	data->ignore--;
 }
 
 static void
@@ -145,6 +172,7 @@ fz_new_bbox_device(fz_context *ctx, fz_bbox *result)
 	fz_bbox_data *user = fz_malloc_struct(ctx, fz_bbox_data);
 	user->result = result;
 	user->top = 0;
+	user->ignore = 0;
 	dev = fz_new_device(ctx, user);
 	dev->free_user = fz_bbox_free_user;
 
@@ -166,8 +194,12 @@ fz_new_bbox_device(fz_context *ctx, fz_bbox *result)
 	dev->pop_clip = fz_bbox_pop_clip;
 
 	dev->begin_mask = fz_bbox_begin_mask;
+	dev->end_mask = fz_bbox_end_mask;
 	dev->begin_group = fz_bbox_begin_group;
 	dev->end_group = fz_bbox_end_group;
+
+	dev->begin_tile = fz_bbox_begin_tile;
+	dev->end_tile = fz_bbox_end_tile;
 
 	*result = fz_empty_bbox;
 
