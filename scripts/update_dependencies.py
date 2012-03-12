sumatrapdfreader/sumatrapdf

commit e193baae70ace65f7603ca4d10b0a72af08dcedd

generalize properties window to support MobiWindow
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5865 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/SumatraPDF.cpp
@@ -4,7 +4,6 @@
 #include "SumatraPDF.h"
 #include <shlobj.h>
 #include <wininet.h>
-#include <locale.h>
 
 #include "AppPrefs.h"
 #include "AppTools.h"
@@ -306,7 +305,6 @@ WindowInfo *FindWindowInfoByHwnd(HWND hwnd)
     for (size_t i = 0; i < gWindows.Count(); i++) {
         WindowInfo *win = gWindows.At(i);
         if (hwnd == win->hwndFrame      ||
-            hwnd == win->hwndProperties ||
             // canvas, toolbar, rebar, tocbox, splitters
             parent == win->hwndFrame    ||
             // infotips, message windows
@@ -1145,16 +1143,7 @@ WindowInfo *CreateAndShowWindowInfo()
 
 static void DeleteWindowInfo(WindowInfo *win)
 {
-    assert(win);
-    if (!win) return;
-
-    // must DestroyWindow(win->hwndProperties) before removing win from
-    // the list of properties beacuse WM_DESTROY handler needs to find
-    // WindowInfo for its HWND
-    if (win->hwndProperties) {
-        DestroyWindow(win->hwndProperties);
-        assert(NULL == win->hwndProperties);
-    }
+    DeletePropertiesWindow(win->hwndFrame);
     gWindows.Remove(win);
 
     ImageList_Destroy((HIMAGELIST)SendMessage(win->hwndToolbar, TB_GETIMAGELIST, 0, 0));
@@ -2312,10 +2301,7 @@ void CloseDocumentInWindow(WindowInfo *win)
     win->notifications->RemoveAllInGroup(NG_RESPONSE_TO_ACTION);
     win->notifications->RemoveAllInGroup(NG_PAGE_INFO_HELPER);
 
-    if (win->hwndProperties) {
-        DestroyWindow(win->hwndProperties);
-        assert(!win->hwndProperties);
-    }
+    DeletePropertiesWindow(win->hwndFrame);
     UpdateToolbarPageText(win, 0);
     UpdateToolbarFindText(win);
     if (wasChm) {
@@ -4451,7 +4437,7 @@ static LRESULT FrameOnCommand(WindowInfo *win, HWND hwnd, UINT msg, WPARAM wPara
             break;
 
         case IDM_PROPERTIES:
-            OnMenuProperties(*win);
+            OnMenuProperties(MakeSumatraWindow(win));
             break;
 
         case IDM_MOVE_FRAME_FOCUS:
@@ -4475,8 +4461,8 @@ static LRESULT FrameOnCommand(WindowInfo *win, HWND hwnd, UINT msg, WPARAM wPara
             // Don't break the shortcut for text boxes
             if (win->hwndFindBox == GetFocus() || win->hwndPageBox == GetFocus())
                 SendMessage(GetFocus(), WM_COPY, 0, 0);
-            else if (GetForegroundWindow() == win->hwndProperties)
-                CopyPropertiesToClipboard(win->hwndProperties);
+            else if (CopyPropertiesToClipboard(win->hwndFrame))
+                break;
             else if (!HasPermission(Perm_CopySelection))
                 break;
             else if (win->IsChm())

src/SumatraProperties.cpp
@@ -1,18 +1,49 @@
 /* Copyright 2006-2012 the SumatraPDF project authors (see AUTHORS file).
    License: GPLv3 */
 
+#include "BaseUtil.h"
+#include "FileUtil.h"
+#include "MobiWindow.h"
+#include "Scoped.h"
 #include "SumatraPDF.h"
-#include "WindowInfo.h"
 #include "SumatraProperties.h"
 #include "Translations.h"
+#include "WindowInfo.h"
 #include "WinUtil.h"
-#include "FileUtil.h"
-#include "Scoped.h"
 
 #define PROPERTIES_LEFT_RIGHT_SPACE_DX 8
 #define PROPERTIES_RECT_PADDING     8
 #define PROPERTIES_TXT_DY_PADDING 2
 #define PROPERTIES_WIN_TITLE    _TR("Document Properties")
+
+static Vec<PropertiesLayout*> gPropertiesWindows;
+
+static PropertiesLayout* FindPropertyWindowByParent(HWND hwndParent)
+{
+    for (size_t i = 0; i < gPropertiesWindows.Count(); i++) {
+        PropertiesLayout *pl = gPropertiesWindows.At(i);
+        if (pl->hwndParent == hwndParent)
+            return pl;
+    }
+    return NULL;
+}
+
+static PropertiesLayout* FindPropertyWindowByHwnd(HWND hwnd)
+{
+    for (size_t i = 0; i < gPropertiesWindows.Count(); i++) {
+        PropertiesLayout *pl = gPropertiesWindows.At(i);
+        if (pl->hwnd == hwnd)
+            return pl;
+    }
+    return NULL;
+}
+
+void DeletePropertiesWindow(HWND hwndParent)
+{
+    PropertiesLayout *pl = FindPropertyWindowByParent(hwndParent);
+    if (pl)
+        DestroyWindow(pl->hwnd);
+}
 
 // See: http://www.verypdf.com/pdfinfoeditor/pdf-date-format.htm
 // Format:  "D:YYYYMMDDHHMMSSxxxxxxx"
@@ -170,9 +201,8 @@ bool PropertiesLayout::HasProperty(const TCHAR *key)
     return false;
 }
 
-static void UpdatePropertiesLayout(HWND hwnd, HDC hdc, RectI *rect)
+static void UpdatePropertiesLayout(PropertiesLayout *layoutData, HDC hdc, RectI *rect)
 {
-    PropertiesLayout *layoutData = (PropertiesLayout *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
     ScopedFont fontLeftTxt(GetSimpleFont(hdc, LEFT_TXT_FONT, LEFT_TXT_FONT_SIZE));
     ScopedFont fontRightTxt(GetSimpleFont(hdc, RIGHT_TXT_FONT, RIGHT_TXT_FONT_SIZE));
     HGDIOBJ origFont = SelectObject(hdc, fontLeftTxt);
@@ -232,39 +262,40 @@ static void UpdatePropertiesLayout(HWND hwnd, HDC hdc, RectI *rect)
     SelectObject(hdc, origFont);
 }
 
-static HWND CreatePropertiesWindow(HWND hParent, PropertiesLayout& layoutData)
+static bool CreatePropertiesWindow(HWND hParent, PropertiesLayout* layoutData)
 {
-    HWND hwndProperties = CreateWindow(
+    CrashIf(layoutData->hwnd);
+    HWND hwnd = CreateWindow(
            PROPERTIES_CLASS_NAME, PROPERTIES_WIN_TITLE,
            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
            CW_USEDEFAULT, CW_USEDEFAULT,
            CW_USEDEFAULT, CW_USEDEFAULT,
            NULL, NULL,
            ghinst, NULL);
-    if (!hwndProperties)
-        return NULL;
+    if (!hwnd)
+        return false;
 
-    assert(!GetWindowLongPtr(hwndProperties, GWLP_USERDATA));
-    SetWindowLongPtr(hwndProperties, GWLP_USERDATA, (LONG_PTR)&layoutData);
-    ToggleWindowStyle(hwndProperties, WS_EX_LAYOUTRTL | WS_EX_NOINHERITLAYOUT, IsUIRightToLeft(), GWL_EXSTYLE);
+    layoutData->hwnd = hwnd;
+    layoutData->hwndParent = hParent;
+    ToggleWindowStyle(hwnd, WS_EX_LAYOUTRTL | WS_EX_NOINHERITLAYOUT, IsUIRightToLeft(), GWL_EXSTYLE);
 
     // get the dimensions required for the about box's content
     RectI rc;
     PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(hwndProperties, &ps);
-    UpdatePropertiesLayout(hwndProperties, hdc, &rc);
-    EndPaint(hwndProperties, &ps);
+    HDC hdc = BeginPaint(hwnd, &ps);
+    UpdatePropertiesLayout(layoutData, hdc, &rc);
+    EndPaint(hwnd, &ps);
 
     // resize the new window to just match these dimensions
-    WindowRect wRc(hwndProperties);
-    ClientRect cRc(hwndProperties);
+    WindowRect wRc(hwnd);
+    ClientRect cRc(hwnd);
     wRc.dx += rc.dx - cRc.dx;
     wRc.dy += rc.dy - cRc.dy;
-    MoveWindow(hwndProperties, wRc.x, wRc.y, wRc.dx, wRc.dy, FALSE);
-    CenterDialog(hwndProperties, hParent);
+    MoveWindow(hwnd, wRc.x, wRc.y, wRc.dx, wRc.dy, FALSE);
+    CenterDialog(hwnd, hParent);
 
-    ShowWindow(hwndProperties, SW_SHOW);
-    return hwndProperties;
+    ShowWindow(hwnd, SW_SHOW);
+    return true;
 }
 
 /*
@@ -284,33 +315,8 @@ Example xref->info ("Info") object:
 >>
 */
 
-void OnMenuProperties(WindowInfo& win, bool extended)
+static void GetProps(BaseEngine *engine, EngineType engineType, PropertiesLayout *layoutData)
 {
-    if (win.hwndProperties) {
-#if defined(DEBUG) || defined(ENABLE_EXTENDED_PROPERTIES)
-        // make a repeated Ctrl+D display some extended properties
-        // TODO: expose this through a UI button or similar
-        if (GetForegroundWindow() == win.hwndProperties) {
-            PropertiesLayout *data = (PropertiesLayout *)GetWindowLongPtr(win.hwndProperties, GWLP_USERDATA);
-            if (!data || !data->HasProperty(_TR("Fonts:"))) {
-                DestroyWindow(win.hwndProperties);
-                win.hwndProperties = NULL;
-                OnMenuProperties(win, true);
-            }
-        }
-#endif
-        SetActiveWindow(win.hwndProperties);
-        return;
-    }
-
-    if (!win.IsDocLoaded())
-        return;
-    BaseEngine *engine = win.dm->engine;
-
-    PropertiesLayout *layoutData = new PropertiesLayout();
-    if (!layoutData)
-        return;
-
     TCHAR *str = str::Dup(gPluginMode ? gPluginURL : engine->FileName());
     layoutData->AddProperty(_TR("File:"), str);
 
@@ -327,20 +333,20 @@ void OnMenuProperties(WindowInfo& win, bool extended)
     layoutData->AddProperty(_TR("Copyright:"), str);
 
     str = engine->GetProperty("CreationDate");
-    if (Engine_PDF == win.dm->engineType)
+    if (Engine_PDF == engineType)
         ConvDateToDisplay(&str, PdfDateParse);
-    else if (Engine_XPS == win.dm->engineType)
+    else if (Engine_XPS == engineType)
         ConvDateToDisplay(&str, IsoDateParse);
-    else if (Engine_Epub == win.dm->engineType)
+    else if (Engine_Epub == engineType)
         ConvDateToDisplay(&str, IsoDateParse);
     layoutData->AddProperty(_TR("Created:"), str);
 
     str = engine->GetProperty("ModDate");
-    if (Engine_PDF == win.dm->engineType)
+    if (Engine_PDF == engineType)
         ConvDateToDisplay(&str, PdfDateParse);
-    else if (Engine_XPS == win.dm->engineType)
+    else if (Engine_XPS == engineType)
         ConvDateToDisplay(&str, IsoDateParse);
-    else if (Engine_Epub == win.dm->engineType)
+    else if (Engine_Epub == engineType)
         ConvDateToDisplay(&str, IsoDateParse);
     layoutData->AddProperty(_TR("Modified:"), str);
 
@@ -365,6 +371,34 @@ void OnMenuProperties(WindowInfo& win, bool extended)
 
     str = str::Format(_T("%d"), engine->PageCount());
     layoutData->AddProperty(_TR("Number of Pages:"), str);
+}
+
+static void ShowProperties(WindowInfo& win, bool extended)
+{
+    TCHAR *str;
+    PropertiesLayout *layoutData = FindPropertyWindowByParent(win.hwndFrame);
+
+    if (layoutData) {
+#if defined(DEBUG) || defined(ENABLE_EXTENDED_PROPERTIES)
+        // make a repeated Ctrl+D display some extended properties
+        // TODO: expose this through a UI button or similar
+        if (GetForegroundWindow() == layoutData->hwndParent) {
+            if (!layoutData->HasProperty(_TR("Fonts:"))) {
+                DestroyWindow(layoutData->hwnd);
+                ShowProperties(win, true);
+            }
+        }
+#endif
+        SetActiveWindow(layoutData->hwnd);
+        return;
+    }
+
+    if (!win.IsDocLoaded())
+        return;
+    BaseEngine *engine = win.dm->engine;
+    layoutData = new PropertiesLayout();
+    gPropertiesWindows.Append(layoutData);
+    GetProps(engine, win.dm->engineType, layoutData);
 
     if (!win.IsChm()) {
         str = FormatPageSize(engine, win.dm->CurrentPageNo(), win.dm->Rotation());
@@ -404,12 +438,31 @@ void OnMenuProperties(WindowInfo& win, bool extended)
     }
 #endif
 
-    win.hwndProperties = CreatePropertiesWindow(win.hwndFrame, *layoutData);
+    if (!CreatePropertiesWindow(win.hwndFrame, layoutData))
+        delete layoutData;
+}
+
+static void ShowProperties(MobiWindow *win)
+{
+    PropertiesLayout *pl = FindPropertyWindowByParent(win->hwndFrame);
+    if (pl) {
+        SetActiveWindow(pl->hwnd);
+        return;
+    }
+    // TODO: show properties
+}
+
+void OnMenuProperties(SumatraWindow& win)
+{
+    if (win.AsWindowInfo())
+        ShowProperties(*win.AsWindowInfo(), false);
+    else if (win.AsMobiWindow())
+        ShowProperties(win.AsMobiWindow());
 }
 
 static void DrawProperties(HWND hwnd, HDC hdc)
 {
-    PropertiesLayout *layoutData = (PropertiesLayout *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+    PropertiesLayout *layoutData = FindPropertyWindowByHwnd(hwnd);
 #if 0
     HPEN penBorder = CreatePen(PS_SOLID, ABOUT_LINE_OUTER_SIZE, COL_BLACK);
 #endif
@@ -460,16 +513,18 @@ static void OnPaintProperties(HWND hwnd)
     PAINTSTRUCT ps;
     RectI rc;
     HDC hdc = BeginPaint(hwnd, &ps);
-    UpdatePropertiesLayout(hwnd, hdc, &rc);
+    UpdatePropertiesLayout(FindPropertyWindowByHwnd(hwnd), hdc, &rc);
     DrawProperties(hwnd, hdc);
     EndPaint(hwnd, &ps);
 }
 
-void CopyPropertiesToClipboard(HWND hwnd)
+// returns true if there is a properties window for a given parent window
+bool CopyPropertiesToClipboard(HWND hwndParent)
 {
     // concatenate all the properties into a multi-line string
-    PropertiesLayout *layoutData = (PropertiesLayout *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-    CrashIf(!layoutData);
+    PropertiesLayout *layoutData = FindPropertyWindowByParent(hwndParent);
+    if (!layoutData)
+        return false;
 
     str::Str<TCHAR> lines(256);
     for (size_t i = 0; i < layoutData->Count(); i++) {
@@ -478,11 +533,12 @@ void CopyPropertiesToClipboard(HWND hwnd)
     }
 
     CopyTextToClipboard(lines.LendData());
+    return true;
 }
 
 LRESULT CALLBACK WndProcProperties(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    WindowInfo * win = FindWindowInfoByHwnd(hwnd);
+    PropertiesLayout *pl;
 
     switch (message)
     {
@@ -503,11 +559,10 @@ LRESULT CALLBACK WndProcProperties(HWND hwnd, UINT message, WPARAM wParam, LPARA
             break;
 
         case WM_DESTROY:
-            delete (PropertiesLayout *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-            if (win) {
-                assert(win->hwndProperties);
-                win->hwndProperties = NULL;
-            }
+            pl = FindPropertyWindowByHwnd(hwnd);
+            CrashIf(!pl);
+            gPropertiesWindows.Remove(pl);
+            delete pl;
             break;
 
         /* TODO: handle mouse move/down/up so that links work (?) */

src/SumatraProperties.h
@@ -31,13 +31,18 @@ public:
 
 class PropertiesLayout : public Vec<PropertyEl *> {
 public:
+    PropertiesLayout() : hwnd(NULL), hwndParent(NULL) { }
     ~PropertiesLayout() { DeleteVecMembers(*this); }
     void AddProperty(const TCHAR *key, TCHAR *value);
     bool HasProperty(const TCHAR *key);
+
+    HWND    hwnd;
+    HWND    hwndParent;
 };
 
-void OnMenuProperties(WindowInfo& win, bool extended=false);
-void CopyPropertiesToClipboard(HWND hwnd);
+void OnMenuProperties(SumatraWindow& win);
+bool CopyPropertiesToClipboard(HWND hwndParent);
+void DeletePropertiesWindow(HWND hwndParent);
 LRESULT CALLBACK WndProcProperties(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 #endif

src/MobiWindow.cpp
@@ -9,6 +9,7 @@
 #include "MobiDoc.h"
 #include "PageLayout.h"
 #include "Resource.h"
+#include "SumatraProperties.h"
 #include "SumatraAbout.h"
 #include "SumatraPDF.h"
 #include "Touch.h"
@@ -146,6 +147,7 @@ void DeleteMobiWindow(MobiWindow *win, bool forceDelete)
     if (gPluginMode && !forceDelete)
         return;
 
+    DeletePropertiesWindow(win->hwndFrame);
     delete win->ebookController;
     DestroyEbookControls(win->ebookControls);
     gMobiWindows.Remove(win);

src/WindowInfo.cpp
@@ -24,7 +24,7 @@ WindowInfo::WindowInfo(HWND hwnd) :
     hwndPageText(NULL), hwndPageBox(NULL), hwndPageBg(NULL), hwndPageTotal(NULL),
     hwndTocBox(NULL), hwndTocTree(NULL),
     hwndSidebarSplitter(NULL), hwndFavSplitter(NULL),
-    hwndInfotip(NULL), infotipVisible(false), hwndProperties(NULL),
+    hwndInfotip(NULL), infotipVisible(false),
     findThread(NULL), findCanceled(false), printThread(NULL), printCanceled(false),
     showSelection(false), mouseAction(MA_IDLE), dragStartPending(false),
     prevZoomVirtual(INVALID_ZOOM), prevDisplayMode(DM_AUTOMATIC),

src/WindowInfo.h
@@ -104,7 +104,6 @@ public:
     HWND            hwndFavSplitter;
 
     HWND            hwndInfotip;
-    HWND            hwndProperties;
 
     bool            infotipVisible;
     HMENU           menu;

src/memtrace/MemTraceDll.cpp
@@ -79,13 +79,11 @@ static BOOL ProcessDetach()
 
 static BOOL ThreadAttach()
 {
-    lf("ThreadAttach()");
     return TRUE;
 }
 
 static BOOL ThreadDetach()
 {
-    lf("ThreadDetach()");
     return TRUE;
 }
 

src/mui/test/MuiTest.cpp
@@ -1,7 +1,6 @@
 /* Copyright 2012 the SumatraPDF project authors (see AUTHORS file).
    License: BSD */
 
-#include <locale.h>
 #include "BaseUtil.h"
 #include "Resource.h"
 

commit 4ef60e4ed196f05754abac68404fc838bd93838e

fix Ctrl+C/Ctrl+D shortcuts for Properties windows (regression from r5865)

makefile.deps
@@ -23,38 +23,6 @@ $(ODLL)\PdfPreview.obj: src\utils\Vec.h src\utils\WinUtil.h
 $(ODLL)\PdfPreviewDll.obj: src\BaseEngine.h src\previewer\PdfPreview.h src\utils\Allocator.h
 $(ODLL)\PdfPreviewDll.obj: src\utils\BaseUtil.h src\utils\GeomUtil.h src\utils\Scoped.h
 $(ODLL)\PdfPreviewDll.obj: src\utils\StrUtil.h src\utils\Vec.h src\utils\WinUtil.h
-$(OEB)\EbookController.obj: src\ebooktest\EbookController.h src\ebooktest\EbookControls.h src\ebooktest\UiMsgEbook.h
-$(OEB)\EbookController.obj: src\MobiDoc.h src\mui\Mui.h src\mui\MuiBase.h
-$(OEB)\EbookController.obj: src\mui\MuiButton.h src\mui\MuiControl.h src\mui\MuiCss.h
-$(OEB)\EbookController.obj: src\mui\MuiEventMgr.h src\mui\MuiHwndWrapper.h src\mui\MuiLayout.h
-$(OEB)\EbookController.obj: src\mui\MuiPainter.h src\mui\MuiScrollBar.h src\PageLayout.h
-$(OEB)\EbookController.obj: src\utils\Allocator.h src\utils\BaseUtil.h src\utils\BitManip.h
-$(OEB)\EbookController.obj: src\utils\DebugLog.h src\utils\FileUtil.h src\utils\GeomUtil.h
-$(OEB)\EbookController.obj: src\utils\HtmlPullParser.h src\utils\Scoped.h src\utils\StrUtil.h
-$(OEB)\EbookController.obj: src\utils\ThreadUtil.h src\utils\Timer.h src\utils\UiMsg.h
-$(OEB)\EbookController.obj: src\utils\Vec.h
-$(OEB)\EbookControls.obj: src\ebooktest\EbookControls.h src\MobiDoc.h src\mui\Mui.h
-$(OEB)\EbookControls.obj: src\mui\MuiBase.h src\mui\MuiButton.h src\mui\MuiControl.h
-$(OEB)\EbookControls.obj: src\mui\MuiCss.h src\mui\MuiEventMgr.h src\mui\MuiHwndWrapper.h
-$(OEB)\EbookControls.obj: src\mui\MuiLayout.h src\mui\MuiPainter.h src\mui\MuiScrollBar.h
-$(OEB)\EbookControls.obj: src\mui\SvgPath.h src\PageLayout.h src\utils\Allocator.h
-$(OEB)\EbookControls.obj: src\utils\BaseUtil.h src\utils\BitManip.h src\utils\GdiPlusUtil.h
-$(OEB)\EbookControls.obj: src\utils\GeomUtil.h src\utils\HtmlPullParser.h src\utils\Scoped.h
-$(OEB)\EbookControls.obj: src\utils\StrUtil.h src\utils\Vec.h
-$(OEB)\EbookTest.obj: src\ebooktest\EbookController.h src\ebooktest\EbookControls.h src\ebooktest\EbookTestMenu.h
-$(OEB)\EbookTest.obj: src\ebooktest\Resource.h src\ebooktest\UiMsgEbook.h src\MobiDoc.h
-$(OEB)\EbookTest.obj: src\mui\Mui.h src\mui\MuiBase.h src\mui\MuiButton.h
-$(OEB)\EbookTest.obj: src\mui\MuiControl.h src\mui\MuiCss.h src\mui\MuiEventMgr.h
-$(OEB)\EbookTest.obj: src\mui\MuiHwndWrapper.h src\mui\MuiLayout.h src\mui\MuiPainter.h
-$(OEB)\EbookTest.obj: src\mui\MuiScrollBar.h src\mui\SvgPath.h src\PageLayout.h
-$(OEB)\EbookTest.obj: src\utils\Allocator.h src\utils\BaseUtil.h src\utils\BitManip.h
-$(OEB)\EbookTest.obj: src\utils\DebugLog.h src\utils\FileUtil.h src\utils\FrameTimeoutCalculator.h
-$(OEB)\EbookTest.obj: src\utils\GdiPlusUtil.h src\utils\GeomUtil.h src\utils\HtmlPullParser.h
-$(OEB)\EbookTest.obj: src\utils\Scoped.h src\utils\StrUtil.h src\utils\ThreadUtil.h
-$(OEB)\EbookTest.obj: src\utils\Timer.h src\utils\UiMsg.h src\utils\Vec.h
-$(OEB)\EbookTest.obj: src\utils\WinUtil.h src\Version.h
-$(OEB)\EbookTestMenu.obj: src\ebooktest\EbookTestMenu.h src\ebooktest\Resource.h src\utils\BaseUtil.h
-$(OEB)\EbookTestMenu.obj: src\utils\Scoped.h src\utils\StrUtil.h
 $(OM)\MemTraceDll.obj: src\memtrace\MemTraceDll.h src\utils\BaseUtil.h src\utils\DebugLog.h
 $(OM)\MemTraceDll.obj: src\utils\StrUtil.h
 $(OMUI)\MiniMui.obj: src\mui\MiniMui.h src\utils\Allocator.h src\utils\BaseUtil.h
@@ -262,12 +230,12 @@ $(OS)\MobiWindow.obj: src\mui\MuiEventMgr.h src\mui\MuiHwndWrapper.h src\mui\Mui
 $(OS)\MobiWindow.obj: src\mui\MuiPainter.h src\mui\MuiScrollBar.h src\PageLayout.h
 $(OS)\MobiWindow.obj: src\PdfEngine.h src\PsEngine.h src\resource.h
 $(OS)\MobiWindow.obj: src\Resource.h src\SumatraAbout.h src\SumatraPDF.h
-$(OS)\MobiWindow.obj: src\SumatraWindow.h src\TextSearch.h src\TextSelection.h
-$(OS)\MobiWindow.obj: src\Translations.h src\utils\Allocator.h src\utils\BaseUtil.h
-$(OS)\MobiWindow.obj: src\utils\BitManip.h src\utils\FileUtil.h src\utils\GeomUtil.h
-$(OS)\MobiWindow.obj: src\utils\HtmlPullParser.h src\utils\Scoped.h src\utils\StrUtil.h
-$(OS)\MobiWindow.obj: src\utils\ThreadUtil.h src\utils\Touch.h src\utils\Vec.h
-$(OS)\MobiWindow.obj: src\utils\WinUtil.h src\WindowInfo.h
+$(OS)\MobiWindow.obj: src\SumatraProperties.h src\SumatraWindow.h src\TextSearch.h
+$(OS)\MobiWindow.obj: src\TextSelection.h src\Translations.h src\utils\Allocator.h
+$(OS)\MobiWindow.obj: src\utils\BaseUtil.h src\utils\BitManip.h src\utils\FileUtil.h
+$(OS)\MobiWindow.obj: src\utils\GeomUtil.h src\utils\HtmlPullParser.h src\utils\Scoped.h
+$(OS)\MobiWindow.obj: src\utils\StrUtil.h src\utils\ThreadUtil.h src\utils\Touch.h
+$(OS)\MobiWindow.obj: src\utils\Vec.h src\utils\WinUtil.h src\WindowInfo.h
 $(OS)\MuPDF_Exports.obj: mupdf\fitz\fitz.h
 $(OS)\Notifications.obj: src\AppPrefs.h src\BaseEngine.h src\ChmEngine.h
 $(OS)\Notifications.obj: src\DisplayModel.h src\DisplayState.h src\DjVuEngine.h
@@ -394,13 +362,19 @@ $(OS)\SumatraPDF.obj: src\utils\Vec.h src\utils\WinUtil.h src\Version.h
 $(OS)\SumatraPDF.obj: src\WindowInfo.h
 $(OS)\SumatraProperties.obj: src\AppPrefs.h src\BaseEngine.h src\ChmEngine.h
 $(OS)\SumatraProperties.obj: src\DisplayModel.h src\DisplayState.h src\DjVuEngine.h
-$(OS)\SumatraProperties.obj: src\EngineManager.h src\EpubEngine.h src\ImagesEngine.h
-$(OS)\SumatraProperties.obj: src\PdfEngine.h src\PsEngine.h src\SumatraPDF.h
-$(OS)\SumatraProperties.obj: src\SumatraProperties.h src\SumatraWindow.h src\TextSearch.h
-$(OS)\SumatraProperties.obj: src\TextSelection.h src\Translations.h src\utils\Allocator.h
-$(OS)\SumatraProperties.obj: src\utils\BaseUtil.h src\utils\FileUtil.h src\utils\GeomUtil.h
-$(OS)\SumatraProperties.obj: src\utils\Scoped.h src\utils\StrUtil.h src\utils\Vec.h
-$(OS)\SumatraProperties.obj: src\utils\WinUtil.h src\WindowInfo.h
+$(OS)\SumatraProperties.obj: src\EbookController.h src\EngineManager.h src\EpubEngine.h
+$(OS)\SumatraProperties.obj: src\ImagesEngine.h src\MobiDoc.h src\MobiWindow.h
+$(OS)\SumatraProperties.obj: src\mui\Mui.h src\mui\MuiBase.h src\mui\MuiButton.h
+$(OS)\SumatraProperties.obj: src\mui\MuiControl.h src\mui\MuiCss.h src\mui\MuiEventMgr.h
+$(OS)\SumatraProperties.obj: src\mui\MuiHwndWrapper.h src\mui\MuiLayout.h src\mui\MuiPainter.h
+$(OS)\SumatraProperties.obj: src\mui\MuiScrollBar.h src\PageLayout.h src\PdfEngine.h
+$(OS)\SumatraProperties.obj: src\PsEngine.h src\SumatraPDF.h src\SumatraProperties.h
+$(OS)\SumatraProperties.obj: src\SumatraWindow.h src\TextSearch.h src\TextSelection.h
+$(OS)\SumatraProperties.obj: src\Translations.h src\utils\Allocator.h src\utils\BaseUtil.h
+$(OS)\SumatraProperties.obj: src\utils\BitManip.h src\utils\FileUtil.h src\utils\GeomUtil.h
+$(OS)\SumatraProperties.obj: src\utils\HtmlPullParser.h src\utils\Scoped.h src\utils\StrUtil.h
+$(OS)\SumatraProperties.obj: src\utils\ThreadUtil.h src\utils\Vec.h src\utils\WinUtil.h
+$(OS)\SumatraProperties.obj: src\WindowInfo.h
 $(OS)\TableOfContents.obj: src\AppPrefs.h src\AppTools.h src\BaseEngine.h
 $(OS)\TableOfContents.obj: src\ChmEngine.h src\DisplayModel.h src\DisplayState.h
 $(OS)\TableOfContents.obj: src\DjVuEngine.h src\EngineManager.h src\EpubEngine.h

scripts/update_dependencies.py
@@ -8,9 +8,9 @@ import os, re, fnmatch
 from util import verify_started_in_right_directory, group, uniquify
 pjoin = os.path.join
 
-DIRS = ["src", pjoin("src", "utils"), pjoin("src", "installer"), pjoin("src", "ifilter"), pjoin("src", "browserplugin"), pjoin("src", "previewer"), pjoin("src", "ebooktest"), pjoin("src", "mui"), pjoin("src", "memtrace")]
+DIRS = ["src", pjoin("src", "utils"), pjoin("src", "installer"), pjoin("src", "ifilter"), pjoin("src", "browserplugin"), pjoin("src", "previewer"), pjoin("src", "mui"), pjoin("src", "memtrace")]
 INCLUDE_DIRS = DIRS + [pjoin("mupdf", "fitz"), pjoin("mupdf", "pdf"), pjoin("mupdf", "xps")]
-OBJECT_DIRS = { "src\\utils": "$(OU)", "src\\browserplugin": "$(ODLL)", "src\\ifilter": "$(ODLL)", "src\\previewer": "$(ODLL)", "src\\ebooktest": "$(OEB)", "src\\mui": "$(OMUI)", "src\\memtrace": "$(OM)" } # default: "$(OS)"
+OBJECT_DIRS = { "src\\utils": "$(OU)", "src\\browserplugin": "$(ODLL)", "src\\ifilter": "$(ODLL)", "src\\previewer": "$(ODLL)", "src\\mui": "$(OMUI)", "src\\memtrace": "$(OM)" } # default: "$(OS)"
 MAKEFILE = "makefile.deps"
 DEPENDENCIES_PER_LINE = 3
 

src/SumatraPDF.cpp
@@ -314,7 +314,9 @@ WindowInfo *FindWindowInfoByHwnd(HWND hwnd)
             // ToC tree, sidebar title and close button
             parent == win->hwndTocBox   ||
             // Favorites tree, title, and close button
-            parent == win->hwndFavBox)
+            parent == win->hwndFavBox   ||
+            // Properties window (required for shortcut handling)
+            FindPropertyWindowByParent(win->hwndFrame))
         {
             return win;
         }

src/SumatraProperties.cpp
@@ -20,7 +20,7 @@
 
 static Vec<PropertiesLayout*> gPropertiesWindows;
 
-static PropertiesLayout* FindPropertyWindowByParent(HWND hwndParent)
+PropertiesLayout* FindPropertyWindowByParent(HWND hwndParent)
 {
     for (size_t i = 0; i < gPropertiesWindows.Count(); i++) {
         PropertiesLayout *pl = gPropertiesWindows.At(i);
@@ -383,7 +383,7 @@ static void ShowProperties(WindowInfo& win, bool extended)
 #if defined(DEBUG) || defined(ENABLE_EXTENDED_PROPERTIES)
         // make a repeated Ctrl+D display some extended properties
         // TODO: expose this through a UI button or similar
-        if (GetForegroundWindow() == layoutData->hwndParent) {
+        if (GetForegroundWindow() == layoutData->hwnd) {
             if (!layoutData->HasProperty(_TR("Fonts:"))) {
                 DestroyWindow(layoutData->hwnd);
                 ShowProperties(win, true);
@@ -539,14 +539,16 @@ static void OnPaintProperties(HWND hwnd)
     EndPaint(hwnd, &ps);
 }
 
-// returns true if there is a properties window for a given parent window
+// returns true if properties have been copied to the clipboard
 bool CopyPropertiesToClipboard(HWND hwndParent)
 {
-    // concatenate all the properties into a multi-line string
     PropertiesLayout *layoutData = FindPropertyWindowByParent(hwndParent);
     if (!layoutData)
         return false;
+    if (GetForegroundWindow() != layoutData->hwnd)
+        return false;
 
+    // concatenate all the properties into a multi-line string
     str::Str<TCHAR> lines(256);
     for (size_t i = 0; i < layoutData->Count(); i++) {
         PropertyEl *el = layoutData->At(i);

src/SumatraProperties.h
@@ -42,6 +42,7 @@ public:
 
 void OnMenuProperties(SumatraWindow& win);
 bool CopyPropertiesToClipboard(HWND hwndParent);
+PropertiesLayout *FindPropertyWindowByParent(HWND hwndParent);
 void DeletePropertiesWindow(HWND hwndParent);
 LRESULT CALLBACK WndProcProperties(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 