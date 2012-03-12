87aec913ff0a3c4a161d988b0a8158ab3ef57b1f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-03-12 04:36:09 +0000

    remove ebooktest

--- src/ebooktest/Resource.h
+++
@@ -1,6 +0,0 @@
-#define IDI_SUMATRAPDF                  1
-
-#define IDM_OPEN                        400
-#define IDM_CLOSE                       401
-#define IDM_EXIT                        402
-#define IDM_TOGGLE_BBOX                 403
