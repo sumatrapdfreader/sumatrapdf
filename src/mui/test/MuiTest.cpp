ea99a510feb5042344d0ad06420891806be28dd5
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-03-12 04:32:29 +0000

    start on mui test application (just a skeleton, doesn't even compile)

---
+++ src/mui/test/MuiTest.cpp
@@ -0,0 +1,292 @@
+/* Copyright 2012 the SumatraPDF project authors (see AUTHORS file).
+   License: BSD */
+
+#include <locale.h>
+#include "BaseUtil.h"
+#include "Resource.h"
+
+using namespace mui;
+
+#define FRAME_CLASS_NAME    _T("MUI_TEST_FRAME")
+
+#define WIN_DX    720
+#define WIN_DY    640
+
+static HINSTANCE            ghinst = NULL;
+static HWND                 gHwndFrame = NULL;
+
+static bool gShowTextBoundingBoxes = false;
+
+#define TEN_SECONDS_IN_MS 10*1000
+
+static float gUiDPIFactor = 1.0f;
+inline int dpiAdjust(int value)
+{
+    return (int)(value * gUiDPIFactor);
+}
+
+static void OnExit()
+{
+    SendMessage(gHwndFrame, WM_CLOSE, 0, 0);
+}
+
+static inline void EnableAndShow(HWND hwnd, bool enable)
+{
+    ShowWindow(hwnd, enable ? SW_SHOW : SW_HIDE);
+    EnableWindow(hwnd, enable);
+}
+
+#if 0
+static void OnOpen(HWND hwnd)
+{
+    OPENFILENAME ofn = { 0 };
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hwnd;
+
+    str::Str<TCHAR> fileFilter;
+    fileFilter.Append(_T("All supported documents"));
+
+    ofn.lpstrFilter = _T("All supported documents\0;*.mobi;*.awz;\0\0");
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY |
+                OFN_ALLOWMULTISELECT | OFN_EXPLORER;
+    ofn.nMaxFile = MAX_PATH * 100;
+    ScopedMem<TCHAR> file(SAZA(TCHAR, ofn.nMaxFile));
+    ofn.lpstrFile = file;
+
+    if (!GetOpenFileName(&ofn))
+        return;
+
+    TCHAR *fileName = ofn.lpstrFile + ofn.nFileOffset;
+    if (*(fileName - 1)) {
+        // special case: single filename without NULL separator
+        gEbookController->LoadMobi(ofn.lpstrFile);
+        return;
+    }
+    // this is just a test app, no need to support multiple files
+    CrashIf(true);
+}
+#endif
+
+static void OnToggleBbox(HWND hwnd)
+{
+    gShowTextBoundingBoxes = !gShowTextBoundingBoxes;
+    SetDebugPaint(gShowTextBoundingBoxes);
+    InvalidateRect(hwnd, NULL, FALSE);
+}
+
+static LRESULT OnCommand(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    int wmId = LOWORD(wParam);
+
+    if ((IDM_EXIT == wmId) || (IDCANCEL == wmId)) {
+        OnExit();
+        return 0;
+    }
+
+    if (IDM_OPEN == wmId) {
+        OnOpen(hwnd);
+        return 0;
+    }
+
+    if (IDM_TOGGLE_BBOX == wmId) {
+        OnToggleBbox(hwnd);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+static LRESULT OnKeyDown(HWND hwnd, UINT msg, WPARAM key, LPARAM lParam)
+{
+    switch (key) {
+    case VK_LEFT: case VK_PRIOR: case 'P':
+        break;
+    case VK_RIGHT: case VK_NEXT: case 'N':
+        break;
+    case VK_SPACE:
+        break;
+    case VK_F1:
+        OnToggleBbox(hwnd);
+        break;
+    case VK_HOME:
+        break;
+    case VK_END:
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, key, lParam);
+    }
+    return 0;
+}
+
+static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    static bool seenWmPaint = false;
+
+    if (gMainWnd) {
+        bool wasHandled;
+        LRESULT res = gMainWnd->evtMgr->OnMessage(msg, wParam, lParam, wasHandled);
+        if (wasHandled)
+            return res;
+    }
+
+    switch (msg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        // if we return 0, during WM_PAINT we can check
+        // PAINTSTRUCT.fErase to see if WM_ERASEBKGND
+        // was sent before WM_PAINT
+        case WM_ERASEBKGND:
+            return 0;
+
+        case WM_PAINT:
+            gMainWnd->OnPaint(hwnd);
+            break;
+
+        case WM_KEYDOWN:
+            return OnKeyDown(hwnd, msg, wParam, lParam);
+
+        case WM_COMMAND:
+            OnCommand(hwnd, msg, wParam, lParam);
+            break;
+
+        case WM_TIMER:
+            OnTimer(hwnd, wParam);
+            break;
+
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+static BOOL RegisterWinClass(HINSTANCE hInstance)
+{
+    WNDCLASSEX  wcex;
+
+    FillWndClassEx(wcex, hInstance);
+    // clear out CS_HREDRAW | CS_VREDRAW style so that resizing doesn't
+    // cause the whole window redraw
+    wcex.style = 0;
+    wcex.lpszClassName  = ET_FRAME_CLASS_NAME;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SUMATRAPDF));
+    wcex.hbrBackground  = CreateSolidBrush(GetSysColor(COLOR_BTNFACE));
+
+    wcex.lpfnWndProc    = WndProcFrame;
+
+    ATOM atom = RegisterClassEx(&wcex);
+    return atom != NULL;
+}
+
+static BOOL InstanceInit(HINSTANCE hInstance, int nCmdShow)
+{
+    ghinst = hInstance;
+    win::GetHwndDpi(NULL, &gUiDPIFactor);
+
+    gHwndFrame = CreateWindow(
+            FRAME_CLASS_NAME, _T("Mui Test"),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            dpiAdjust(WIN_DX), dpiAdjust(WIN_DY),
+            NULL, NULL,
+            ghinst, NULL);
+
+    if (!gHwndFrame)
+        return FALSE;
+
+    SetMenu(gHwndFrame, BuildMenu());
+    ShowWindow(gHwndFrame, SW_SHOW);
+    return TRUE;
+}
+
+// inspired by http://engineering.imvu.com/2010/11/24/how-to-write-an-interactive-60-hz-desktop-application/
+static int RunApp()
+{
+    MSG msg;
+    FrameTimeoutCalculator ftc(60);
+    for (;;) {
+        DWORD timeout = ftc.GetTimeoutInMilliseconds();
+        DWORD res = WAIT_TIMEOUT;
+        HANDLE handles[MAXIMUM_WAIT_OBJECTS];
+        DWORD handleCount = 0;
+        handles[handleCount++] = uimsg::GetQueueEvent();
+        CrashIf(handleCount >= MAXIMUM_WAIT_OBJECTS);
+        if ((timeout > 0) || (handleCount > 0)) {
+            if (0 == timeout)
+                timeout = INFINITE;
+            res = MsgWaitForMultipleObjects(handleCount, handles, FALSE, timeout, QS_ALLINPUT);
+        }
+
+        if (res == WAIT_OBJECT_0) {
+            DispatchUiMessages();
+        }
+
+#if 0
+         if (res == WAIT_TIMEOUT) {
+            ftc.Step();
+        }
+#endif
+
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                return (int)msg.wParam;
+            }
+            if (!IsDialogMessage(gHwndFrame, &msg)) {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        DispatchUiMessages();
+    }
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    int ret = 1;
+    LogProcessRunningTime();
+
+#ifdef DEBUG
+    // report memory leaks on DbgOut
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    SetErrorMode(SEM_NOOPENFILEERRORBOX | SEM_FAILCRITICALERRORS);
+    // ensure that C functions behave consistently under all OS locales
+    // (use Win32 functions where localized input or output is desired)
+    setlocale(LC_ALL, "C");
+
+#ifdef DEBUG
+    extern void SvgPath_UnitTests();
+    SvgPath_UnitTests();
+    extern void TrivialHtmlParser_UnitTests();
+    TrivialHtmlParser_UnitTests();
+    extern void BaseUtils_UnitTests();
+    BaseUtils_UnitTests();
+    extern void HtmlPullParser_UnitTests();
+    HtmlPullParser_UnitTests();
+#endif
+    ScopedCom com;
+    InitAllCommonControls();
+    ScopedGdiPlus gdi;
+
+    mui::Initialize();
+
+    if (!RegisterWinClass(hInstance))
+        goto Exit;
+
+    if (!InstanceInit(hInstance, nCmdShow))
+        goto Exit;
+
+    ret = RunApp();
+
+Exit:
+    mui::Destroy();
+    return ret;
+}
