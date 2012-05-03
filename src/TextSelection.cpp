d9347caa2a0b85b165c6efbcfaa0d76baccb83de
Simon Bunzli <zeniko@gmail.com>
2012-05-03 22:58:20 +0000

    fix potential read AV in FillResultRects

--- src/TextSelection.cpp
+++ src/TextSelection.cpp
@@ -120,8 +120,9 @@ int TextSelection::FindClosestGlyph(int pageNo, double x, double y)
 
 void TextSelection::FillResultRects(int pageNo, int glyph, int length, StrVec *lines)
 {
+    int len;
     RectI *coords;
-    const TCHAR *text = textCache->GetData(pageNo, NULL, &coords);
+    const TCHAR *text = textCache->GetData(pageNo, &len, &coords);
     RectI mediabox = engine->PageMediabox(pageNo).Round();
     RectI *c = &coords[glyph], *end = c + length;
     for (; c < end; c++) {
@@ -144,7 +145,7 @@ void TextSelection::FillResultRects(int pageNo, int glyph, int length, StrVec *l
         }
 
         // cut the right edge, if it overlaps the next character
-        if ((c[1].x || c[1].dx) && bbox.x < c[1].x && bbox.x + bbox.dx > c[1].x)
+        if (c + 1 < coords + len && (c[1].x || c[1].dx) && bbox.x < c[1].x && bbox.x + bbox.dx > c[1].x)
             bbox.dx = c[1].x - bbox.x;
 
         result.len++;
