141c07c8802cf3ba8a51b7161ac2cca599784af9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-05-06 22:27:25 +0000

    document how to add new regression test

--- src/regress/Regress.cpp
+++ src/regress/Regress.cpp
@@ -8,6 +8,11 @@ on presence of shared test files.
 
 Note: because it can be run as both release and debug, we can't use
 assert() or CrashIf() but CrashAlwaysIf().
+
+To write new regression test:
+- add a file src/regress/Regress${NN}.cpp with Regress${NN} function
+- #include "Regress${NN}.cpp" right before RunTest() function
+- call Regress${NN} function from RunTests()
 */
 
 #include "BaseUtil.h"
@@ -66,39 +71,7 @@ void VerifyFileExists(const TCHAR *filePath)
     }
 }
 
-// http://code.google.com/p/sumatrapdf/issues/detail?id=1926
-static void Regress00()
-{
-    TCHAR *filePath = path::Join(TestFilesDir(), _T("epub\\widget-figure-gallery-20120405.epub"));
-    VerifyFileExists(filePath);
-    Doc doc(Doc::CreateFromFile(filePath));
-    CrashAlwaysIf(doc.LoadingFailed());
-    CrashAlwaysIf(Doc_Epub != doc.GetDocType());
-
-    PoolAllocator   textAllocator;
-    HtmlFormatterArgs *args = CreateFormatterArgsDoc(doc, 820, 920, &textAllocator);
-    HtmlPage *pages[3];
-    HtmlFormatter *formatter = CreateFormatter(doc, args);
-    int page = 0;
-    for (HtmlPage *pd = formatter->Next(); pd; pd = formatter->Next()) {
-        pages[page++] = pd;
-        if (page == dimof(pages))
-            break;
-    }
-    delete formatter;
-    delete args;
-    CrashAlwaysIf(page != 3);
-
-    args = CreateFormatterArgsDoc(doc, 820, 920, &textAllocator);
-    args->reparseIdx = pages[2]->reparseIdx;
-    formatter = CreateFormatter(doc, args);
-    // if bug is present, this will crash in formatter->Next()
-    for (HtmlPage *pd = formatter->Next(); pd; pd = formatter->Next()) {
-        delete pd;
-    }
-    delete formatter;
-    delete args;
-}
+#include "Regress00.cpp"
 
 static void RunTests()
 {
