81f25485ece3da83ea83248344af40e8f651274d
Simon Bunzli <zeniko@gmail.com>
2012-09-20 13:14:47 +0000

    MuPDF: support OpenType font extraction

--- mupdf/apps/mupdfextract.c
+++ mupdf/apps/mupdfextract.c
@@ -96,6 +96,9 @@ static void savefont(pdf_obj *dict, int num)
 			ext = "cff";
 		else if (!strcmp(subtype, "CIDFontType0C"))
 			ext = "cid";
+		/* SumatraPDF: extract OpenType fonts */
+		else if (!strcmp(subtype, "OpenType"))
+			ext = "otf";
 		else
 			fz_throw(ctx, "Unhandled font type '%s'", subtype);
 	}
