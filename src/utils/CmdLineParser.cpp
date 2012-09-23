796485418aaf382d69d834c5e7d42e3ebee8375a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-23 21:57:26 +0000

    don't use assert() in CmdLineParser.cpp - the string comes from external sources so it can just as well be malformed; use str::IsWs() instead of _istspace()

--- src/utils/CmdLineParser.cpp
+++ src/utils/CmdLineParser.cpp
@@ -7,7 +7,6 @@
 /* returns the next character in '*txt' that isn't a backslash */
 static const TCHAR SkipBackslashs(const TCHAR *txt)
 {
-    assert(txt && '\\' == *txt);
     while ('\\' == *++txt);
     return *txt;
 }
@@ -15,7 +14,6 @@ static const TCHAR SkipBackslashs(const TCHAR *txt)
 /* appends the next quoted argument and returns the position after it */
 static const TCHAR *ParseQuoted(const TCHAR *arg, StrVec *out)
 {
-    assert(arg && '"' == *arg);
     arg++;
 
     str::Str<TCHAR> txt(str::Len(arg) / 2);
@@ -37,13 +35,11 @@ static const TCHAR *ParseQuoted(const TCHAR *arg, StrVec *out)
 /* appends the next unquoted argument and returns the position after it */
 static const TCHAR *ParseUnquoted(const TCHAR *arg, StrVec *out)
 {
-    assert(arg && *arg && '"' != *arg && !_istspace(*arg));
-
     const TCHAR *next;
     // contrary to http://msdn.microsoft.com/en-us/library/17w5ykft.aspx
     // we don't treat quotation marks or backslashes in non-quoted
     // arguments in any special way
-    for (next = arg; *next && !_istspace(*next); next++);
+    for (next = arg; *next && !str::IsWs(*next); next++);
     out->Append(str::DupN(arg, next - arg));
     return next;
 }
@@ -57,7 +53,7 @@ void ParseCmdLine(const TCHAR *cmdLine, StrVec& out)
 {
     while (cmdLine) {
         // skip whitespace
-        while (_istspace(*cmdLine))
+        while (str::IsWs(*cmdLine))
             cmdLine++;
         if ('"' == *cmdLine)
             cmdLine = ParseQuoted(cmdLine, &out);
