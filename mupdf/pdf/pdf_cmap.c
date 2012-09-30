7556eee48d61c142100e581cb21242bfa5903cba
Simon Bunzli <zeniko@gmail.com>
2012-09-30 15:22:12 +0000

    follow-up to r6710

--- mupdf/pdf/pdf_cmap.c
+++ mupdf/pdf/pdf_cmap.c
@@ -205,8 +205,8 @@ static void
 add_range(fz_context *ctx, pdf_cmap *cmap, int low, int high, int flag, int offset)
 {
 	/* SumatraPDF: range check (should be unsigned short instead of int) */
-	if ((low & ~0xffff) || (high & ~0xffff))
-		fz_throw(ctx, "range limits out of range");
+	if ((low & ~0xffff) || (high & ~0xffff) || low > high)
+		fz_throw(ctx, "range limits out of range (%d/%d)", low, high);
 	/* If the range is too large to be represented, split it */
 	if (high - low > 0x3fff)
 	{
