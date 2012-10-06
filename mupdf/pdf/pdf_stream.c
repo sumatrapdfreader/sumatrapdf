1ec8125559cd6bd0ffcadf839c8c91057a1a832f
Simon Bunzli <zeniko@gmail.com>
2012-10-06 14:12:04 +0000

    PDF: prevent use-after-free

--- mupdf/pdf/pdf_stream.c
+++ mupdf/pdf/pdf_stream.c
@@ -518,6 +518,8 @@ pdf_load_compressed_stream(pdf_document *xref, int num, int gen)
 	fz_catch(ctx)
 	{
 		fz_free(ctx, bc);
+		/* SumatraPDF: prevent use-after-free */
+		fz_rethrow(ctx);
 	}
 	return bc;
 }
