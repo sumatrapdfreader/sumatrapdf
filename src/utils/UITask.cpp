b8aa2328d1c78ba04ac66bd2a41ae754bc79ba5d
Simon Bunzli <zeniko@gmail.com>
2012-11-16 22:11:54 +0000

    'Only use RegisterWindowMessage when more than one application must process the same message.'

--- src/utils/UITask.cpp
+++ src/utils/UITask.cpp
@@ -11,14 +11,14 @@
 namespace uitask {
 
 static HWND  gTaskDispatchHwnd;
-static UINT  gTaskMsg;
 
-#define WND_CLASS _T("UITask_Wnd_Class")
+#define UITASK_CLASS_NAME   _T("UITask_Wnd_Class")
+#define WM_QUEUE_TASK       (WM_USER + 1)
 
 static LRESULT CALLBACK WndProcTaskDispatch(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     UITask *task;
-    if (msg == gTaskMsg) {
+    if (WM_QUEUE_TASK == msg) {
         task = (UITask*)lParam;
         lf("executing %s", task->name);
         task->Execute();
@@ -30,19 +30,17 @@ static LRESULT CALLBACK WndProcTaskDispatch(HWND hwnd, UINT msg, WPARAM wParam, 
 
 void Initialize()
 {
-    gTaskMsg = RegisterWindowMessageA("UITask_Process_Msg");
-
     WNDCLASSEX  wcex = { 0 };
     HINSTANCE hinst = GetModuleHandle(NULL);
     FillWndClassEx(wcex, hinst);
     wcex.lpfnWndProc    = WndProcTaskDispatch;
-    wcex.lpszClassName  = WND_CLASS;
+    wcex.lpszClassName  = UITASK_CLASS_NAME;
     wcex.hIcon          = NULL;
     wcex.hIconSm        = NULL;
     RegisterClassEx(&wcex);
 
     gTaskDispatchHwnd = CreateWindow(
-            WND_CLASS, _T("UITask Dispatch Window"),
+            UITASK_CLASS_NAME, _T("UITask Dispatch Window"),
             WS_OVERLAPPED,
             0, 0, 0, 0,
             NULL, NULL,
@@ -57,8 +55,7 @@ void Post(UITask *task)
 {
     CrashIf(!task);
     lf("posting %s", task->name);
-    PostMessage(gTaskDispatchHwnd, gTaskMsg, 0, (LPARAM)task);
+    PostMessage(gTaskDispatchHwnd, WM_QUEUE_TASK, 0, (LPARAM)task);
 }
 
 }
-
