42fb997b8f0447872e3a8c4ac1cd6246e943dc7c
Simon Bunzli <zeniko@gmail.com>
2013-01-14 08:21:58 +0000

    follow-up to r7243

--- src/utils/TgaReader.cpp
+++ src/utils/TgaReader.cpp
@@ -392,10 +392,10 @@ unsigned char *SerializeBitmap(HBITMAP hbmp, size_t *bmpBytesOut)
                 tgaData.Append(line + i * 3, 3);
             } else {
                 // determine the length of a run of different pixels
-                while (i + j < w && j < 128 && !memeq3(line + (i + j - 1) * 3, line + (i + j) * 3)) {
+                while (i + j < w && j <= 128 && !memeq3(line + (i + j - 1) * 3, line + (i + j) * 3)) {
                     j++;
                 }
-                if (i + j < w && j < 128)
+                if (i + j < w || j > 128)
                     j--;
                 tgaData.Append(j - 1);
                 tgaData.Append(line + i * 3, j * 3);
