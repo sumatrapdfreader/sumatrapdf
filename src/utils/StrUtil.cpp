3725503e9581fe7c9044d95244e71b2bdf7c577d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2013-04-01 22:27:04 +0000

    improve UnknownToUtf8()

--- src/utils/StrUtil.cpp
+++ src/utils/StrUtil.cpp
@@ -1143,6 +1143,103 @@ WCHAR *ToPlainUrl(const WCHAR *url)
     return plainUrl;
 }
 
+// --- copyright for utf8 code below
+
+/*
+ * Copyright 2001-2004 Unicode, Inc.
+ * 
+ * Disclaimer
+ * 
+ * This source code is provided as is by Unicode, Inc. No claims are
+ * made as to fitness for any particular purpose. No warranties of any
+ * kind are expressed or implied. The recipient agrees to determine
+ * applicability of information provided. If this file has been
+ * purchased on magnetic or optical media from Unicode, Inc., the
+ * sole remedy for any claim will be exchange of defective media
+ * within 90 days of receipt.
+ * 
+ * Limitations on Rights to Redistribute This Code
+ * 
+ * Unicode, Inc. hereby grants the right to freely use the information
+ * supplied in this file in the creation of products supporting the
+ * Unicode Standard, and to make copies of this file in any form
+ * for internal or external distribution as long as this notice
+ * remains attached.
+ */
+
+typedef unsigned char	UTF8;	/* typically 8 bits */
+
+/*
+ * Index into the table below with the first byte of a UTF-8 sequence to
+ * get the number of trailing bytes that are supposed to follow it.
+ * Note that *legal* UTF-8 values can't have 4 or 5-bytes. The table is
+ * left as-is for anyone who may want to do such conversion, which was
+ * allowed in earlier algorithms.
+ */
+static const char trailingBytesForUTF8[256] = {
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,4,4,4,4,5,5,5,5
+};
+
+/*
+ * Utility routine to tell whether a sequence of bytes is legal UTF-8.
+ * This must be called with the length pre-determined by the first byte.
+ * If not calling this from ConvertUTF8to*, then the length can be set by:
+ *  length = trailingBytesForUTF8[*source]+1;
+ * and the sequence is illegal right away if there aren't that many bytes
+ * available.
+ * If presented with a length > 4, this returns false.  The Unicode
+ * definition of UTF-8 goes up to 4-byte sequences.
+ */
+
+static bool isLegalUTF8(const UTF8 *source, int length) {
+    UTF8 a;
+    const UTF8 *srcptr = source+length;
+
+    switch (length) {
+    default: return false;
+    /* Everything else falls through when "true"... */
+    case 4: if ((a = (*--srcptr)) < 0x80 || a > 0xBF) return false;
+    case 3: if ((a = (*--srcptr)) < 0x80 || a > 0xBF) return false;
+    case 2: if ((a = (*--srcptr)) > 0xBF) return false;
+
+        switch (*source) {
+            /* no fall-through in this inner switch */
+            case 0xE0: if (a < 0xA0) return false; break;
+            case 0xED: if (a > 0x9F) return false; break;
+            case 0xF0: if (a < 0x90) return false; break;
+            case 0xF4: if (a > 0x8F) return false; break;
+            default:   if (a < 0x80) return false;
+        }
+
+    case 1: if (*source >= 0x80 && *source < 0xC2) return false;
+    }
+
+    if (*source > 0xF4) return false;
+
+    return true;
+}
+
+/* --------------------------------------------------------------------- */
+
+/*
+ * Exported function to return whether a UTF-8 sequence is legal or not.
+ * This is not used here; it's just exported.
+ */
+bool isLegalUTF8Sequence(const UTF8 *source, const UTF8 *sourceEnd) {
+    int length = trailingBytesForUTF8[*source]+1;
+    if (source+length > sourceEnd) return false;
+    return isLegalUTF8(source, length);
+}
+
+// --- end of Unicode, Inc. utf8 code
+
 namespace conv {
 
 // tries to convert a string in unknown encoding to utf8, as best
@@ -1150,7 +1247,7 @@ namespace conv {
 // As an optimization, can return src if the string already is
 // valid utf8. Otherwise returns a copy of the string and the
 // caller has to free() it
-char * UnknownToUtf8(const char *s, size_t len)
+char *UnknownToUtf8(const char *s, size_t len)
 {
     if (0 == len)
         len = str::Len(s);
@@ -1169,15 +1266,12 @@ char * UnknownToUtf8(const char *s, size_t len)
         return str::conv::ToUtf8((const WCHAR *)s, cchLen);
     }
 
-    WCHAR *uni = NULL;
-    // if is valid utf8, leave it alone
-    // TODO: could be faster by just checking the characters
-    uni = str::conv::FromUtf8(s, len);
-    if (!uni)
-        uni = str::conv::FromAnsi(s, len);
-    char *res = str::conv::ToUtf8(uni);
-    free(uni);
-    return res;
+    // if s is valid utf8, leave it alone
+    if (isLegalUTF8Sequence((UTF8*)s, (UTF8*)(s + len)))
+        return (char*)s;
+
+    ScopedMem<WCHAR> uni(str::conv::FromAnsi(s, len));
+    return str::conv::ToUtf8(uni.Get());
 }
 
 size_t ToCodePageBuf(char *buf, int cbBufSize, const WCHAR *s, UINT cp)
