d71a0e9f68461be1f825992fbc6d15ef5cdd1414
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2013-05-23 06:55:54 +0000

    forgot to checkin tools/tests; removed gyp directory

--- gyp/gengyp.bat
+++
@@ -1,8 +0,0 @@
-@rem some versions of cygwin add cygwin's bin directory to front of %PATH%
-@rem For gyp, I need to run Window's Python.
-@rem this is a work-around (as long as python is installed to c:\Python27)
-@rem TODO: could be more intelligent and detect Python path
-@set PP=c:\Python27
-%PP%\python gyp/gengyp.py
-%PP%\python gyp/fix-vsproj-dirs.py vs/gyp/sizer-vs2010.vcxproj.filters
-%PP%\python gyp/fix-vsproj-dirs.py vs/gyp/sizer-vs2012.vcxproj.filters
---
+++ tools/tests/unit_main.cpp
@@ -0,0 +1,11 @@
+#include "BaseUtil.h"
+
+// in UnitTests.cpp
+extern void BaseUtils_UnitTests();
+
+// TODO: move this into UnitTests.cpp ?
+int main(int argc, char **argv)
+{
+    BaseUtils_UnitTests();
+    return 0;
+}
