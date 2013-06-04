sumatrapdfreader/sumatrapdf

commit c30b1ffa7059d516bc990baa994c7c844a069dfb

merge latest MuPDF update
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@8080 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/pdf/pdf_font.c
@@ -5,7 +5,7 @@
 #include FT_FREETYPE_H
 #include FT_XFREE86_H
 
-static void pdf_load_font_descriptor(pdf_font_desc *fontdesc, pdf_document *xref, pdf_obj *dict, char *collection, char *basefont, int has_encoding);
+static void pdf_load_font_descriptor(pdf_font_desc *fontdesc, pdf_document *xref, pdf_obj *dict, char *collection, char *basefont, int iscidfont, int has_encoding);
 
 static char *base_font_names[][10] =
 {
@@ -530,7 +530,7 @@ pdf_load_simple_font(pdf_document *xref, pdf_obj *dict)
 		fz_try(ctx)
 		{
 		if (descriptor)
-			pdf_load_font_descriptor(fontdesc, xref, descriptor, NULL, basefont, pdf_dict_gets(dict, "Encoding") != NULL);
+			pdf_load_font_descriptor(fontdesc, xref, descriptor, NULL, basefont, 0, pdf_dict_gets(dict, "Encoding") != NULL);
 		else
 			pdf_load_builtin_font(ctx, fontdesc, basefont);
 		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=691690 */
@@ -572,7 +572,7 @@ pdf_load_simple_font(pdf_document *xref, pdf_obj *dict)
 				pdf_drop_font(ctx, fontdesc);
 				fontdesc = NULL;
 				fontdesc = pdf_new_font_desc(ctx);
-				pdf_load_font_descriptor(fontdesc, xref, descriptor, "Adobe-GB1", cp936fonts[i+1], 1);
+				pdf_load_font_descriptor(fontdesc, xref, descriptor, "Adobe-GB1", cp936fonts[i+1], 0, 1);
 				fontdesc->encoding = pdf_load_system_cmap(ctx, "GBK-EUC-H");
 				fontdesc->to_unicode = pdf_load_system_cmap(ctx, "Adobe-GB1-UCS2");
 				fontdesc->to_ttf_cmap = pdf_load_system_cmap(ctx, "Adobe-GB1-UCS2");
@@ -938,7 +938,7 @@ load_cid_font(pdf_document *xref, pdf_obj *dict, pdf_obj *encoding, pdf_obj *to_
 		descriptor = pdf_dict_gets(dict, "FontDescriptor");
 		if (!descriptor)
 			fz_throw(ctx, "syntaxerror: missing font descriptor");
-		pdf_load_font_descriptor(fontdesc, xref, descriptor, collection, basefont, 1);
+		pdf_load_font_descriptor(fontdesc, xref, descriptor, collection, basefont, 1, 1);
 
 		face = fontdesc->font->ft_face;
 		kind = ft_kind(face);
@@ -1022,6 +1022,17 @@ load_cid_font(pdf_document *xref, pdf_obj *dict, pdf_obj *encoding, pdf_obj *to_
 		fz_catch(ctx)
 		{
 			fz_warn(ctx, "cannot load ToUnicode CMap");
+		}
+
+		/* If we have an identity encoding, we're supposed to use the glyph ids directly.
+		 * If we only have a substitute font, that won't work.
+		 * Make a last ditch attempt by using
+		 * the ToUnicode table if it exists to map via the substitute font's cmap. */
+		if (strstr(fontdesc->encoding->cmap_name, "Identity-") && fontdesc->font->ft_substitute)
+		{
+			fz_warn(ctx, "non-embedded font using identity encoding: %s", basefont);
+			if (fontdesc->to_unicode && !fontdesc->to_ttf_cmap)
+				fontdesc->to_ttf_cmap = pdf_keep_cmap(ctx, fontdesc->to_unicode);
 		}
 
 		/* Horizontal */
@@ -1160,7 +1171,7 @@ pdf_load_type0_font(pdf_document *xref, pdf_obj *dict)
  */
 
 static void
-pdf_load_font_descriptor(pdf_font_desc *fontdesc, pdf_document *xref, pdf_obj *dict, char *collection, char *basefont, int has_encoding)
+pdf_load_font_descriptor(pdf_font_desc *fontdesc, pdf_document *xref, pdf_obj *dict, char *collection, char *basefont, int iscidfont, int has_encoding)
 {
 	pdf_obj *obj1, *obj2, *obj3, *obj;
 	char *fontname, *origname;
@@ -1203,7 +1214,7 @@ pdf_load_font_descriptor(pdf_font_desc *fontdesc, pdf_document *xref, pdf_obj *d
 		fz_catch(ctx)
 		{
 			fz_warn(ctx, "ignored error when loading embedded font; attempting to load system font");
-			if (origname != fontname)
+			if (origname != fontname && !iscidfont)
 				pdf_load_builtin_font(ctx, fontdesc, fontname);
 			else
 				pdf_load_system_font(ctx, fontdesc, fontname, collection, has_encoding);
@@ -1211,7 +1222,7 @@ pdf_load_font_descriptor(pdf_font_desc *fontdesc, pdf_document *xref, pdf_obj *d
 	}
 	else
 	{
-		if (origname != fontname)
+		if (origname != fontname && !iscidfont)
 			pdf_load_builtin_font(ctx, fontdesc, fontname);
 		else
 			pdf_load_system_font(ctx, fontdesc, fontname, collection, has_encoding);

mupdf/Makefile
@@ -12,7 +12,7 @@ default: all
 # Do not specify CFLAGS or LIBS on the make invocation line - specify
 # XCFLAGS or XLIBS instead. Make ignores any lines in the makefile that
 # set a variable that was set on the command line.
-CFLAGS += $(XCFLAGS) -Ifitz -Ipdf -Ixps -Icbz -Iscripts
+CFLAGS += $(XCFLAGS) -Ifitz -Ipdf -Ixps -Icbz -Iimage -Iscripts
 LIBS += $(XLIBS) -lfreetype -ljbig2dec -ljpeg -lopenjpeg -lz -lm
 LIBS_V8 = $(LIBS) $(V8LIBS)
 
@@ -47,6 +47,7 @@ FITZ_HDR := fitz/fitz.h fitz/fitz-internal.h
 MUPDF_HDR := $(FITZ_HDR) pdf/mupdf.h pdf/mupdf-internal.h
 MUXPS_HDR := $(FITZ_HDR) xps/muxps.h xps/muxps-internal.h
 MUCBZ_HDR := $(FITZ_HDR) cbz/mucbz.h
+MUIMAGE_HDR := $(FITZ_HDR) image/muimage.h
 
 $(OUT) $(GEN) :
 	$(MKDIR_CMD)
@@ -70,7 +71,9 @@ $(OUT)/%.o : xps/%.c $(MUXPS_HDR) | $(OUT)
 	$(CC_CMD)
 $(OUT)/%.o : cbz/%.c $(MUCBZ_HDR) | $(OUT)
 	$(CC_CMD)
-$(OUT)/%.o : apps/%.c fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h | $(OUT)
+$(OUT)/%.o : image/%.c $(MUCBZ_HDR) | $(OUT)
+	$(CC_CMD)
+$(OUT)/%.o : apps/%.c fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h image/muimage.h | $(OUT)
 	$(CC_CMD)
 $(OUT)/%.o : scripts/%.c | $(OUT)
 	$(CC_CMD)
@@ -90,17 +93,20 @@ MUPDF_V8_SRC := $(filter-out pdf_js_none.c, $(MUPDF_ALL_SRC))
 MUPDF_V8_CPP_SRC := $(notdir $(wildcard pdf/*.cpp))
 MUXPS_SRC := $(notdir $(wildcard xps/*.c))
 MUCBZ_SRC := $(notdir $(wildcard cbz/*.c))
+MUIMAGE_SRC := $(notdir $(wildcard image/*.c))
 
 $(FITZ_LIB) : $(addprefix $(OUT)/, $(FITZ_SRC:%.c=%.o))
 $(FITZ_LIB) : $(addprefix $(OUT)/, $(MUPDF_SRC:%.c=%.o))
 $(FITZ_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
 $(FITZ_LIB) : $(addprefix $(OUT)/, $(MUCBZ_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUIMAGE_SRC:%.c=%.o))
 
 $(FITZ_V8_LIB) : $(addprefix $(OUT)/, $(FITZ_SRC:%.c=%.o))
 $(FITZ_V8_LIB) : $(addprefix $(OUT)/, $(MUPDF_V8_SRC:%.c=%.o))
 $(FITZ_V8_LIB) : $(addprefix $(OUT)/, $(MUPDF_V8_CPP_SRC:%.cpp=%.o))
 $(FITZ_V8_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
 $(FITZ_V8_LIB) : $(addprefix $(OUT)/, $(MUCBZ_SRC:%.c=%.o))
+$(FITZ_V8_LIB) : $(addprefix $(OUT)/, $(MUIMAGE_SRC:%.c=%.o))
 
 libs: $(FITZ_LIB) $(THIRD_LIBS)
 libs_v8: libs $(FITZ_V8_LIB)
@@ -211,7 +217,7 @@ mandir ?= $(prefix)/share/man
 install: $(FITZ_LIB) $(MUVIEW) $(MUDRAW) $(MUTOOL)
 	install -d $(DESTDIR)$(bindir) $(DESTDIR)$(libdir) $(DESTDIR)$(incdir) $(DESTDIR)$(mandir)/man1
 	install $(FITZ_LIB) $(DESTDIR)$(libdir)
-	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h $(DESTDIR)$(incdir)
+	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h image/muimage.h $(DESTDIR)$(incdir)
 	install $(MUVIEW) $(MUDRAW) $(MUTOOL) $(DESTDIR)$(bindir)
 	install $(wildcard apps/man/*.1) $(DESTDIR)$(mandir)/man1
 

mupdf/apps/mudraw.c
@@ -14,6 +14,28 @@
 
 enum { TEXT_PLAIN = 1, TEXT_HTML = 2, TEXT_XML = 3 };
 
+enum { OUT_PNG, OUT_PPM, OUT_PNM, OUT_PAM, OUT_PGM, OUT_PBM, OUT_TGA, OUT_BMP };
+
+typedef struct
+{
+	char *suffix;
+	int format;
+} suffix_t;
+
+static const suffix_t suffix_table[] =
+{
+	{ ".png", OUT_PNG },
+	{ ".pgm", OUT_PGM },
+	{ ".ppm", OUT_PPM },
+	{ ".pnm", OUT_PNM },
+	{ ".pam", OUT_PAM },
+	{ ".pbm", OUT_PBM },
+	{ ".tga", OUT_TGA }, /* SumatraPDF: support TGA as output format */
+#ifdef GDI_PLUS_BMP_RENDERER
+	{ ".bmp", OUT_BMP },
+#endif
+};
+
 /*
 	A useful bit of bash script to call this to generate mjs files:
 	for f in tests_private/pdf/forms/v1.3/ *.pdf ; do g=${f%.*} ; echo $g ; ../mupdf.git/win32/debug/mudraw.exe -j $g.mjs $g.pdf ; done
@@ -84,6 +106,7 @@ static int height = 0;
 static int fit = 0;
 static int errored = 0;
 static int ignore_errors = 0;
+static int output_format;
 
 static fz_text_sheet *sheet = NULL;
 static fz_colorspace *colorspace;
@@ -204,7 +227,6 @@ static void drawbmp(fz_context *ctx, fz_document *doc, fz_page *page, fz_display
 	BITMAPINFO bmi = { 0 };
 	int bmp_data_len;
 	unsigned char *bmp_data;
-	int as_tga = !strstr(output, ".bmp");
 
 	fz_bound_page(doc, page, &bounds);
 	zoom = resolution / 72;
@@ -262,12 +284,12 @@ static void drawbmp(fz_context *ctx, fz_document *doc, fz_page *page, fz_display
 
 	bmi.bmiHeader.biSize = sizeof(bmi.bmiHeader);
 	bmi.bmiHeader.biWidth = w;
-	bmi.bmiHeader.biHeight = as_tga ? -h : h;
+	bmi.bmiHeader.biHeight = output_format == OUT_TGA ? -h : h;
 	bmi.bmiHeader.biPlanes = 1;
-	bmi.bmiHeader.biBitCount = as_tga ? 32 : 24;
+	bmi.bmiHeader.biBitCount = output_format == OUT_TGA ? 32 : 24;
 	bmi.bmiHeader.biCompression = BI_RGB;
 
-	bmp_data_len = as_tga ? w * h * 4 : ((w * 3 + 3) / 4) * 4 * h;
+	bmp_data_len = output_format == OUT_TGA ? w * h * 4 : ((w * 3 + 3) / 4) * 4 * h;
 	bmp_data = fz_malloc(ctx, bmp_data_len);
 	if (!GetDIBits(dc, hbmp, 0, h, bmp_data, &bmi, DIB_RGB_COLORS))
 		fz_throw(ctx, "cannot draw page %d in PDF file '%s'", pagenum, filename);
@@ -286,7 +308,7 @@ static void drawbmp(fz_context *ctx, fz_document *doc, fz_page *page, fz_display
 		if (!f)
 			fz_throw(ctx, "could not create raster file '%s'", buf);
 
-		if (as_tga)
+		if (output_format == OUT_TGA)
 		{
 			fz_pixmap *pix = fz_new_pixmap_with_data(ctx, fz_device_bgr, w, h, bmp_data);
 			fz_write_tga(ctx, pix, buf, 0);
@@ -555,7 +577,7 @@ static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
 
 #ifdef GDI_PLUS_BMP_RENDERER
 	// hack: use -G0 to "enable GDI+" when saving as TGA
-	if (output && (strstr(output, ".bmp") || strstr(output, ".tga") && !gamma_value))
+	if (output_format == OUT_BMP || output_format == OUT_TGA && !gamma_value)
 		drawbmp(ctx, doc, page, list, pagenum, &cookie);
 	else
 #endif
@@ -660,19 +682,19 @@ static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
 			{
 				char buf[512];
 				sprintf(buf, output, pagenum);
-				if (strstr(output, ".pgm") || strstr(output, ".ppm") || strstr(output, ".pnm"))
+				if (output_format == OUT_PGM || output_format == OUT_PPM)
 					fz_write_pnm(ctx, pix, buf);
-				else if (strstr(output, ".pam"))
+				else if (output_format == OUT_PAM)
 					fz_write_pam(ctx, pix, buf, savealpha);
-				else if (strstr(output, ".png"))
+				else if (output_format == OUT_PNG)
 					fz_write_png(ctx, pix, buf, savealpha);
-				else if (strstr(output, ".pbm")) {
+				else if (output_format == OUT_PBM) {
 					fz_bitmap *bit = fz_halftone_pixmap(ctx, pix, NULL);
 					fz_write_pbm(ctx, bit, buf);
 					fz_drop_bitmap(ctx, bit);
 				}
 				/* SumatraPDF: support TGA as output format */
-				else if (strstr(output, ".tga"))
+				else if (output_format == OUT_TGA)
 					fz_write_tga(ctx, pix, buf, savealpha);
 			}
 
@@ -878,14 +900,28 @@ int main(int argc, char **argv)
 
 	fz_set_aa_level(ctx, alphabits);
 
+	/* Determine output type */
+	output_format = OUT_PNG;
+	if (output)
+	{
+		char *suffix = output;
+		int i;
+
+		for (i = 0; i < nelem(suffix_table); i++)
+		{
+			char *s = strstr(suffix, suffix_table[i].suffix);
+
+			if (s != NULL)
+			{
+				suffix = s+1;
+				output_format = suffix_table[i].format;
+				i = 0;
+			}
+		}
+	}
+
 	colorspace = fz_device_rgb;
-	if (output && strstr(output, ".pgm"))
-		colorspace = fz_device_gray;
-	if (output && strstr(output, ".ppm"))
-		colorspace = fz_device_rgb;
-	if (output && strstr(output, ".pbm"))
-		colorspace = fz_device_gray;
-	if (grayscale)
+	if (grayscale || output_format == OUT_PGM || output_format == OUT_PBM)
 		colorspace = fz_device_gray;
 
 	timing.count = 0;

mupdf/apps/win_main.c
@@ -186,7 +186,7 @@ int winfilename(wchar_t *buf, int len)
 	ofn.nMaxFile = len;
 	ofn.lpstrInitialDir = NULL;
 	ofn.lpstrTitle = L"MuPDF: Open PDF file";
-	ofn.lpstrFilter = L"Documents (*.pdf;*.xps;*.cbz;*.zip)\0*.zip;*.cbz;*.xps;*.pdf\0PDF Files (*.pdf)\0*.pdf\0XPS Files (*.xps)\0*.xps\0CBZ Files (*.cbz;*.zip)\0*.zip;*.cbz\0All Files\0*\0\0";
+	ofn.lpstrFilter = L"Documents (*.pdf;*.xps;*.cbz;*.zip;*.png;*.jpg;*.tif)\0*.zip;*.cbz;*.xps;*.pdf;*.jpe;*.jpg;*.jpeg;*.jfif;*.tif;*.tiff\0PDF Files (*.pdf)\0*.pdf\0XPS Files (*.xps)\0*.xps\0CBZ Files (*.cbz;*.zip)\0*.zip;*.cbz\0Image Files (*.png;*.jpe;*.tif)\0*.png;*.jpg;*.jpe;*.jpeg;*.jfif;*.tif;*.tiff\0All Files\0*\0\0";
 	ofn.Flags = OFN_FILEMUSTEXIST|OFN_HIDEREADONLY;
 	return GetOpenFileNameW(&ofn);
 }
@@ -204,7 +204,7 @@ int wingetsavepath(pdfapp_t *app, char *buf, int len)
 	ofn.nMaxFile = PATH_MAX;
 	ofn.lpstrInitialDir = NULL;
 	ofn.lpstrTitle = L"MuPDF: Save PDF file";
-	ofn.lpstrFilter = L"Documents (*.pdf;*.xps;*.cbz;*.zip)\0*.zip;*.cbz;*.xps;*.pdf\0PDF Files (*.pdf)\0*.pdf\0XPS Files (*.xps)\0*.xps\0CBZ Files (*.cbz;*.zip)\0*.zip;*.cbz\0All Files\0*\0\0";
+	ofn.lpstrFilter = L"Documents (*.pdf;*.xps;*.cbz;*.zip;*.png;*.jpg;*.tif)\0*.zip;*.cbz;*.xps;*.pdf;*.jpe;*.jpg;*.jpeg;*.jfif;*.tif;*.tiff\0PDF Files (*.pdf)\0*.pdf\0XPS Files (*.xps)\0*.xps\0CBZ Files (*.cbz;*.zip)\0*.zip;*.cbz\0Image Files (*.png;*.jpe;*.tif)\0*.png;*.jpg;*.jpe;*.jpeg;*.jfif;*.tif;*.tiff\0All Files\0*\0\0";
 	ofn.Flags = OFN_HIDEREADONLY;
 	if (GetSaveFileName(&ofn))
 	{

mupdf/cbz/mucbz.c
@@ -364,7 +364,7 @@ cbz_load_page(cbz_document *doc, int number)
 
 		data = cbz_read_zip_entry(doc, doc->entry[number].offset, &size);
 
-		page->image = fz_new_image_from_buffer(ctx, data, size);
+		page->image = fz_new_image_from_data(ctx, data, size);
 	}
 	fz_catch(ctx)
 	{

mupdf/draw/draw_mesh.c
@@ -1,326 +1,156 @@
 #include "fitz-internal.h"
 
-/*
- * polygon clipping
- */
-
-enum { IN, OUT, ENTER, LEAVE };
-enum { MAXV = 3 + 4 };
 enum { MAXN = 2 + FZ_MAX_COLORS };
 
-static inline void copy_vert(float *dst, const float *src, int n)
+static void paint_scan(fz_pixmap *restrict pix, int y, int fx0, int fx1, int cx0, int cx1, const int *restrict v0, const int *restrict v1, int n)
 {
-	while (n--)
-		*dst++ = *src++;
-}
+	unsigned char *p;
+	int c[MAXN], dc[MAXN];
+	int k, w;
+	float div, mul;
+	int x0, x1;
 
-static int clipx(float val, int ismax, const float *v1, const float *v2, int n, float *dst)
-{
-	float t;
-	int i;
-
-	int v1o = ismax ? v1[0] > val : v1[0] < val;
-	int v2o = ismax ? v2[0] > val : v2[0] < val;
-
-	if (v1o + v2o == 0)
+	/* Ensure that fx0 is left edge, and fx1 is right */
+	if (fx0 > fx1)
 	{
-		/* Both ends of the line are valid - keep v2, v1 will be kept by another call */
-		copy_vert(dst, v2, n);
-		return 1;
+		const int *v;
+		int t = fx0; fx0 = fx1; fx1 = t;
+		v = v0; v0 = v1; v1 = v;
 	}
+	else if (fx0 == fx1)
+		return;
 
-	if (v1o + v2o == 2)
-		/* Both ends are invalid - drop both */
-		return 0;
+	/* Clip fx0, fx1 to range */
+	if (fx0 >= cx1)
+		return;
+	if (fx1 <= cx0)
+		return;
+	x0 = (fx0 > cx0 ? fx0 : cx0);
+	x1 = (fx1 < cx1 ? fx1 : cx1);
 
-	/* It is tempting to spot that the second intersection finding
-	 * operation below can be reversed so that it's in the same form as
-	 * the first one. Do not do this, as the vagaries of floating point
-	 * maths means we get nasty rounding problems. */
-	if (v2o)
-	{
-		/* v1 valid, v2 invalid - Keep intersection */
-		t = (val - v1[0]) / (v2[0] - v1[0]);
-		dst[0] = val;
-		dst[1] = v1[1] + t * (v2[1] - v1[1]);
-		for (i = 2; i < n; i++)
-			dst[i] = v1[i] + t * (v2[i] - v1[i]);
-		return 1;
-	}
-	else
-	{
-		/* v2 valid, v1 invalid - Keep intersection, plus v2 */
-		t = (val - v2[0]) / (v1[0] - v2[0]);
-		dst[0] = val;
-		dst[1] = v2[1] + t * (v1[1] - v2[1]);
-		dst[MAXN] = v2[0];
-		dst[MAXN+1] = v2[1];
-		for (i = 2; i < n; i++)
-		{
-			dst[i] = v2[i] + t * (v1[i] - v2[i]);
-			dst[i+MAXN] = v2[i];
-		}
-		return 2;
-	}
-}
-
-static int clipy(float val, int ismax, const float *v1, const float *v2, int n, float *dst)
-{
-	float t;
-	int i;
-
-	int v1o = ismax ? v1[1] > val : v1[1] < val;
-	int v2o = ismax ? v2[1] > val : v2[1] < val;
-
-	if (v1o + v2o == 0)
-	{
-		/* Both ends of the line are valid - keep v2, v1 will be kept by another call */
-		copy_vert(dst, v2, n);
-		return 1;
-	}
-
-	if (v1o + v2o == 2)
-		/* Both ends are invalid - drop both */
-		return 0;
-
-	/* It is tempting to spot that the second intersection finding
-	 * operation below can be reversed so that it's in the same form as
-	 * the first one. Do not do this, as the vagaries of floating point
-	 * maths means we get nasty rounding problems. */
-	if (v2o)
-	{
-		/* v1 valid, v2 invalid - Keep intersection */
-		t = (val - v1[1]) / (v2[1] - v1[1]);
-		dst[0] = v1[0] + t * (v2[0] - v1[0]);
-		dst[1] = val;
-		for (i = 2; i < n; i++)
-			dst[i] = v1[i] + t * (v2[i] - v1[i]);
-		return 1;
-	}
-	else
-	{
-		/* v2 valid, v1 invalid - Keep intersection, plus v2 */
-		t = (val - v2[1]) / (v1[1] - v2[1]);
-		dst[0] = v2[0] + t * (v1[0] - v2[0]);
-		dst[1] = val;
-		dst[MAXN] = v2[0];
-		dst[MAXN+1] = v2[1];
-		for (i = 2; i < n; i++)
-		{
-			dst[i] = v2[i] + t * (v1[i] - v2[i]);
-			dst[i+MAXN] = v2[i];
-		}
-		return 2;
-	}
-}
-
-static int clip_poly(float src[MAXV][MAXN],
-	float dst[MAXV][MAXN], int len, int n,
-	float val, int isy, int ismax)
-{
-	int v1, v2, cp;
-
-	v1 = len - 1;
-	cp = 0;
-
-	if (isy)
-	{
-		for (v2 = 0; v2 < len; v2++)
-		{
-			cp += clipy(val, ismax, src[v1], src[v2], n, dst[cp]);
-			v1 = v2;
-		}
-	}
-	else
-	{
-		for (v2 = 0; v2 < len; v2++)
-		{
-			cp += clipx(val, ismax, src[v1], src[v2], n, dst[cp]);
-			v1 = v2;
-		}
-	}
-
-	return cp;
-}
-
-/*
- * gouraud shaded polygon scan conversion
- */
-
-static void paint_scan(fz_pixmap *pix, int y, int x1, int x2, int *v1, int *v2, int n)
-{
-	unsigned char *p = pix->samples + (unsigned int)(((y - pix->y) * pix->w + (x1 - pix->x)) * pix->n);
-	int v[FZ_MAX_COLORS];
-	int dv[FZ_MAX_COLORS];
-	int w = x2 - x1;
-	int k;
-
-	assert(w >= 0);
-	assert(y >= pix->y);
-	assert(y < pix->y + pix->h);
-	assert(x1 >= pix->x);
-	assert(x2 <= pix->x + pix->w);
-
+	w = x1 - x0;
 	if (w == 0)
 		return;
 
+	div = 1.0f / (fx1 - fx0);
+	mul = (x0 - fx0);
 	for (k = 0; k < n; k++)
 	{
-		v[k] = v1[k];
-		dv[k] = (v2[k] - v1[k]) / w;
+		dc[k] = (v1[k] - v0[k]) * div;
+		c[k] = v0[k] + dc[k] * mul;
 	}
 
+	p = pix->samples + ((x0 - pix->x) + (y - pix->y) * pix->w) * pix->n;
 	while (w--)
 	{
 		for (k = 0; k < n; k++)
 		{
-			*p++ = v[k] >> 16;
-			v[k] += dv[k];
+			*p++ = c[k]>>16;
+			c[k] += dc[k];
 		}
 		*p++ = 255;
 	}
 }
 
-static int find_next(int gel[MAXV][MAXN], int len, int a, int *s, int *e, int d)
+typedef struct edge_data_s edge_data;
+
+struct edge_data_s
 {
-	int b;
+	float x;
+	float dx;
+	int v[2*MAXN];
+};
 
-	while (1)
+static inline void prepare_edge(const float *restrict vtop, const float *restrict vbot, edge_data *restrict edge, float y, int n)
+{
+	float r = 1.0f / (vbot[1] - vtop[1]);
+	float t = (y - vtop[1]) * r;
+	float diff = vbot[0] - vtop[0];
+	int i;
+
+	edge->x = vtop[0] + diff * t;
+	edge->dx = diff * r;
+
+	for (i = 0; i < n; i++)
 	{
-		b = a + d;
-		if (b == len)
-			b = 0;
-		if (b == -1)
-			b = len - 1;
-
-		if (gel[b][1] == gel[a][1])
-		{
-			a = b;
-			continue;
-		}
-
-		if (gel[b][1] > gel[a][1])
-		{
-			*s = a;
-			*e = b;
-			return 0;
-		}
-
-		return 1;
+		diff = vbot[i+2] - vtop[i+2];
+		edge->v[i] = (int)(65536.0f * (vtop[i+2] + diff * t));
+		edge->v[i+MAXN] = (int)(65536.0f * diff * r);
 	}
 }
 
-static void load_edge(int gel[MAXV][MAXN], int s, int e, int *ael, int *del, int n)
+static inline void step_edge(edge_data *edge, int n)
 {
-	int swp, k, dy;
+	int i;
 
-	if (gel[s][1] > gel[e][1])
+	edge->x += edge->dx;
+
+	for (i = 0; i < n; i++)
 	{
-		swp = s; s = e; e = swp;
+		edge->v[i] += edge->v[i + MAXN];
 	}
-
-	dy = gel[e][1] - gel[s][1];
-
-	ael[0] = gel[s][0];
-	del[0] = (gel[e][0] - gel[s][0]) / dy;
-	for (k = 2; k < n; k++)
-	{
-		ael[k] = gel[s][k];
-		del[k] = (gel[e][k] - gel[s][k]) / dy;
-	}
-}
-
-static inline void step_edge(int *ael, int *del, int n)
-{
-	int k;
-	ael[0] += del[0];
-	for (k = 2; k < n; k++)
-		ael[k] += del[k];
 }
 
 static void
-fz_paint_triangle(fz_pixmap *pix, float poly_in[3][MAXN], int n, const fz_irect *bbox)
+fz_paint_triangle(fz_pixmap *pix, float v[3][MAXN], int n, const fz_irect *bbox)
 {
-	float poly[MAXV][MAXN];
-	float temp[MAXV][MAXN];
-	float cx0 = bbox->x0;
-	float cy0 = bbox->y0;
-	float cx1 = bbox->x1;
-	float cy1 = bbox->y1;
-
-	int gel[MAXV][MAXN];
-	int ael[2][MAXN];
-	int del[2][MAXN];
-	int y, s0, s1, e0, e1;
-	int top, bot, len;
-
-	int i, k;
-
-	len = clip_poly(poly_in, temp, 3, n, cx0, 0, 0);
-	len = clip_poly(temp, poly, len, n, cx1, 0, 1);
-	len = clip_poly(poly, temp, len, n, cy0, 1, 0);
-	len = clip_poly(temp, poly, len, n, cy1, 1, 1);
-
-	if (len < 3)
-		return;
-
-	for (i = 0; i < len; i++)
-	{
-		gel[i][0] = floorf(poly[i][0] + 0.5f) * 65536; /* trunc and fix */
-		gel[i][1] = floorf(poly[i][1] + 0.5f); /* y is not fixpoint */
-		for (k = 2; k < n; k++)
-			gel[i][k] = poly[i][k] * 65536; /* fix with precision */
-	}
+	edge_data e0, e1;
+	int top, mid, bot;
+	float y, y1;
+	int minx, maxx;
 
 	top = bot = 0;
-	for (i = 0; i < len; i++)
+	if (v[1][1] < v[0][1]) top = 1; else bot = 1;
+	if (v[2][1] < v[top][1]) top = 2;
+	else if (v[2][1] > v[bot][1]) bot = 2;
+	if (v[top][1] == v[bot][1]) return;
+
+	/* Test if the triangle is completely outside the scissor rect */
+	if (v[bot][1] < bbox->y0) return;
+	if (v[top][1] > bbox->y1) return;
+
+	/* Magic! Ensure that mid/top/bot are all different */
+	mid = 3^top^bot;
+
+	assert(top != bot && top != mid && mid != bot);
+
+	minx = fz_maxi(bbox->x0, pix->x);
+	maxx = fz_mini(bbox->x1, pix->x + pix->w);
+
+	y = ceilf(fz_max(bbox->y0, v[top][1]));
+	y1 = ceilf(fz_min(bbox->y1, v[mid][1]));
+
+	n -= 2;
+	prepare_edge(v[top], v[bot], &e0, y, n);
+	if (y < y1)
 	{
-		if (gel[i][1] < gel[top][1])
-			top = i;
-		if (gel[i][1] > gel[bot][1])
-			bot = i;
+		prepare_edge(v[top], v[mid], &e1, y, n);
+
+		do
+		{
+			paint_scan(pix, y, (int)e0.x, (int)e1.x, minx, maxx, &e0.v[0], &e1.v[0], n);
+			step_edge(&e0, n);
+			step_edge(&e1, n);
+			y ++;
+		}
+		while (y < y1);
 	}
 
-	if (gel[bot][1] - gel[top][1] == 0)
-		return;
-
-	y = gel[top][1];
-
-	if (find_next(gel, len, top, &s0, &e0, 1))
-		return;
-	if (find_next(gel, len, top, &s1, &e1, -1))
-		return;
-
-	load_edge(gel, s0, e0, ael[0], del[0], n);
-	load_edge(gel, s1, e1, ael[1], del[1], n);
-
-	while (1)
+	y1 = ceilf(fz_min(bbox->y1, v[bot][1]));
+	if (y < y1)
 	{
-		int x0 = ael[0][0] >> 16;
-		int x1 = ael[1][0] >> 16;
+		prepare_edge(v[mid], v[bot], &e1, y, n);
 
-		if (ael[0][0] < ael[1][0])
-			paint_scan(pix, y, x0, x1, ael[0]+2, ael[1]+2, n-2);
-		else
-			paint_scan(pix, y, x1, x0, ael[1]+2, ael[0]+2, n-2);
-
-		step_edge(ael[0], del[0], n);
-		step_edge(ael[1], del[1], n);
-		y ++;
-
-		if (y >= gel[e0][1])
+		do
 		{
-			if (find_next(gel, len, e0, &s0, &e0, 1))
-				return;
-			load_edge(gel, s0, e0, ael[0], del[0], n);
+			paint_scan(pix, y, (int)e0.x, (int)e1.x, minx, maxx, &e0.v[0], &e1.v[0], n);
+			y ++;
+			if (y >= y1)
+				break;
+			step_edge(&e0, n);
+			step_edge(&e1, n);
 		}
-
-		if (y >= gel[e1][1])
-		{
-			if (find_next(gel, len, e1, &s1, &e1, -1))
-				return;
-			load_edge(gel, s1, e1, ael[1], del[1], n);
-		}
+		while (1);
 	}
 }
 

mupdf/fitz/doc_document.c
@@ -4,10 +4,12 @@
 extern struct pdf_document *pdf_open_document(fz_context *ctx, const char *filename);
 extern struct xps_document *xps_open_document(fz_context *ctx, const char *filename);
 extern struct cbz_document *cbz_open_document(fz_context *ctx, const char *filename);
+extern struct image_document *image_open_document(fz_context *ctx, const char *filename);
 
 extern struct pdf_document *pdf_open_document_with_stream(fz_context *ctx, fz_stream *file);
 extern struct xps_document *xps_open_document_with_stream(fz_context *ctx, fz_stream *file);
 extern struct cbz_document *cbz_open_document_with_stream(fz_context *ctx, fz_stream *file);
+extern struct image_document *image_open_document_with_stream(fz_context *ctx, fz_stream *file);
 
 extern int pdf_js_supported(void);
 
@@ -42,6 +44,11 @@ fz_open_document_with_stream(fz_context *ctx, const char *magic, fz_stream *stre
 			return (fz_document*) cbz_open_document_with_stream(ctx, stream);
 		if (!fz_strcasecmp(ext, ".pdf"))
 			return (fz_document*) pdf_open_document_with_stream(ctx, stream);
+		if (!fz_strcasecmp(ext, ".png") || !fz_strcasecmp(ext, ".jpg") ||
+			!fz_strcasecmp(ext, ".jpeg") || !fz_strcasecmp(ext, ".jfif") ||
+			!fz_strcasecmp(ext, ".jfif-tbnl") || !fz_strcasecmp(ext, ".jpe") ||
+			!fz_strcasecmp(ext, ".tif") || !fz_strcasecmp(ext, ".tiff"))
+			return (fz_document*) image_open_document_with_stream(ctx, stream);
 	}
 
 	if (!strcmp(magic, "cbz") || !strcmp(magic, "application/x-cbz"))
@@ -50,6 +57,13 @@ fz_open_document_with_stream(fz_context *ctx, const char *magic, fz_stream *stre
 		return (fz_document*) xps_open_document_with_stream(ctx, stream);
 	if (!strcmp(magic, "pdf") || !strcmp(magic, "application/pdf"))
 		return (fz_document*) pdf_open_document_with_stream(ctx, stream);
+	if (!strcmp(magic, "png") || !strcmp(magic, "image/png") ||
+		!strcmp(magic, "jpg") || !strcmp(magic, "image/jpeg") ||
+		!strcmp(magic, "jpeg") || !strcmp(magic, "image/pjpeg") ||
+		!strcmp(magic, "jpe") || !strcmp(magic, "jfif") ||
+		!strcmp(magic, "tif") || !strcmp(magic, "image/tiff") ||
+		!strcmp(magic, "tiff") || !strcmp(magic, "image/x-tiff"))
+		return (fz_document*) image_open_document_with_stream(ctx, stream);
 
 	/* last guess: pdf */
 	return (fz_document*) pdf_open_document_with_stream(ctx, stream);
@@ -68,6 +82,11 @@ fz_open_document(fz_context *ctx, const char *filename)
 			return (fz_document*) cbz_open_document(ctx, filename);
 		if (!fz_strcasecmp(ext, ".pdf"))
 			return (fz_document*) pdf_open_document(ctx, filename);
+		if (!fz_strcasecmp(ext, ".png") || !fz_strcasecmp(ext, ".jpg") ||
+			!fz_strcasecmp(ext, ".jpeg") || !fz_strcasecmp(ext, ".jpe") ||
+			!fz_strcasecmp(ext, ".jfif") || !fz_strcasecmp(ext, ".jfif-tbnl") ||
+			!fz_strcasecmp(ext, ".tif") || !fz_strcasecmp(ext, ".tiff"))
+			return (fz_document*) image_open_document(ctx, filename);
 	}
 
 	/* last guess: pdf */

mupdf/fitz/fitz-internal.h
@@ -992,7 +992,8 @@ void fz_free_compressed_buffer(fz_context *ctx, fz_compressed_buffer *buf);
 
 fz_image *fz_new_image(fz_context *ctx, int w, int h, int bpc, fz_colorspace *colorspace, int xres, int yres, int interpolate, int imagemask, float *decode, int *colorkey, fz_compressed_buffer *buffer, fz_image *mask);
 fz_image *fz_new_image_from_pixmap(fz_context *ctx, fz_pixmap *pixmap, fz_image *mask);
-fz_image *fz_new_image_from_buffer(fz_context *ctx, unsigned char *buf, int len);
+fz_image *fz_new_image_from_data(fz_context *ctx, unsigned char *data, int len);
+fz_image *fz_new_image_from_buffer(fz_context *ctx, fz_buffer *buffer);
 fz_pixmap *fz_image_get_pixmap(fz_context *ctx, fz_image *image, int w, int h);
 void fz_free_image(fz_context *ctx, fz_storable *image);
 fz_pixmap *fz_decomp_image_from_stream(fz_context *ctx, fz_stream *stm, fz_image *image, int in_line, int indexed, int l2factor, int native_l2factor);
@@ -1242,7 +1243,6 @@ void fz_print_path(fz_context *ctx, FILE *out, fz_path *, int indent);
 void fz_new_glyph_cache_context(fz_context *ctx);
 fz_glyph_cache *fz_keep_glyph_cache(fz_context *ctx);
 void fz_drop_glyph_cache_context(fz_context *ctx);
-void fz_purge_glyph_cache(fz_context *ctx);
 
 fz_path *fz_outline_ft_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *trm);
 fz_path *fz_outline_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm);

commit 2041817d6c47be00d4b407bdfb6feb83f17e4f53

PDF: fix font decoding regression from r8080 (fixes issue 2318)

mupdf/pdf/pdf_font.c
@@ -1019,6 +1019,9 @@ load_cid_font(pdf_document *xref, pdf_obj *dict, pdf_obj *encoding, pdf_obj *to_
 					fontdesc->to_ttf_cmap = pdf_load_system_cmap(ctx, "Adobe-Japan2-UCS2");
 				else if (!strcmp(collection, "Adobe-Korea1"))
 					fontdesc->to_ttf_cmap = pdf_load_system_cmap(ctx, "Adobe-Korea1-UCS2");
+				/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=2318 */
+				else if (!strcmp(collection, "Adobe-Identity") && fontdesc->font->ft_file)
+					fontdesc->font->ft_substitute = 0;
 			}
 		}
 