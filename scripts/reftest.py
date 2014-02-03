6633b013a6f5c3b7225a89ca3ef810ca25aad277
Simon Bunzli <zeniko@gmail.com>
2014-02-03 01:18:32 +0000

    revert r8596 for scripts where PEP8 isn't desired

--- scripts/reftest.py
+++ scripts/reftest.py
@@ -9,193 +9,175 @@ reftest.py /dir/to/test -refdir /dir/for/references
 reftest.py /dir/one /dir/two /dir/three
 """
 
-import os
-import sys
-import struct
-import fnmatch
+import os, sys, struct, fnmatch
 from subprocess import Popen, PIPE
 pjoin = os.path.join
 
-
 def EngineDump(EngineDumpExe, file, tgaPath):
-    proc = Popen([EngineDumpExe, file, "-full", "-render", tgaPath],
-                 stdout=PIPE)
-    xmlDump, _ = proc.communicate()
-    return xmlDump
-
+	proc = Popen([EngineDumpExe, file, "-full", "-render", tgaPath], stdout=PIPE)
+	xmlDump, _ = proc.communicate()
+	return xmlDump
 
 def TgaRleUnpack(data):
-    # unpacks data from a type 2 TGA file (24-bit uncompressed)
-    # or a type 10 TGA file (24-bit run-length encoded)
-    if len(data) >= 18 and struct.unpack("B", data[2]) == 2:
-        return data[18:]
-    exp, i = [], 18
-    while i + 4 < len(data):
-        bit = struct.unpack("B", data[i])[0] + 1
-        i += 1
-        if bit <= 128:
-            exp.append(data[i:i + bit * 3])
-            i += bit * 3
-        else:
-            exp.append(data[i:i + 3] * (bit - 128))
-            i += 3
-    return "".join(exp)
-
+	# unpacks data from a type 2 TGA file (24-bit uncompressed)
+	# or a type 10 TGA file (24-bit run-length encoded)
+	if len(data) >= 18 and struct.unpack("B", data[2]) == 2:
+		return data[18:]
+	exp, i = [], 18
+	while i + 4 < len(data):
+		bit = struct.unpack("B", data[i])[0] + 1
+		i += 1
+		if bit <= 128:
+			exp.append(data[i:i + bit * 3])
+			i += bit * 3
+		else:
+			exp.append(data[i:i + 3] * (bit - 128))
+			i += 3
+	return "".join(exp)
 
 def TgaCmpColor(col1, col2):
-    # returns 0 for identical colors, 1 for similar colors and 2 for different
-    # colors
-    if col1 == col2:
-        return 0
-    rgb1, rgb2 = struct.unpack("<BBB", col1), struct.unpack("<BBB", col2)
-    if (rgb1[0] - rgb2[0]) ** 2 + (rgb1[1] - rgb2[1]) ** 2 + (rgb1[2] - rgb2[2]) ** 2 < 75:
-        return 1
-    return 2
-
+	# returns 0 for identical colors, 1 for similar colors and 2 for different colors
+	if col1 == col2:
+		return 0
+	rgb1, rgb2 = struct.unpack("<BBB", col1), struct.unpack("<BBB", col2)
+	if (rgb1[0] - rgb2[0]) ** 2 + (rgb1[1] - rgb2[1]) ** 2 + (rgb1[2] - rgb2[2]) ** 2 < 75:
+		return 1
+	return 2
 
 def BitmapDiff(tgaRef, tgaCmp, tgaDiff):
-    # ensure that the comparison bitmap exists
-    if not os.path.isfile(tgaCmp):
-        open(tgaCmp, "wb").write("")
-    ref = open(tgaRef, "rb").read()
-    cmp = open(tgaCmp, "rb").read()
-
-    # bail if the files are either identical or the reference is broken
-    if ref == cmp:
-        return False
-    if len(ref) < 18:
-        return True
-
-    # determine bitmap dimensions
-    width, height = struct.unpack("<HH", ref[12:16])
-
-    # unpack the run-length encoded data
-    refData = TgaRleUnpack(ref)
-    if len(refData) < width * height * 3:
-        refData += "\x00" * (width * height * 3 - len(refData))
-    cmpData = TgaRleUnpack(cmp)
-    if len(cmpData) < width * height * 3:
-        cmpData += "\xFF" * (width * height * 3 - len(cmpData))
-
-    # bail if the files are just differently compressed
-    if refData == cmpData:
-        return False
-
-    # write a black pixel for identical, a dark red pixel for similar and a
-    # bright red pixel for different color values (packed as a type 9 TGA file,
-    # 8-bit indexed run-length encoded)
-    diff = [struct.pack(
-        "<BBBHHBHHHHBB", 0, 1, 9, 0, 3, 24, 0, 0, width, height, 8, 0),
-        "\x00\x00\x00", "\x00\x00\x80", "\x00\x00\xFF"]
-    for i in range(height):
-        refLine = refData[i * width * 3:(i + 1) * width * 3]
-        cmpLine = cmpData[i * width * 3:(i + 1) * width * 3]
-        j = 0
-        # reencode the difference file line-by-line
-        while j < width:
-            color = TgaCmpColor(
-                refLine[j * 3:j * 3 + 3], cmpLine[j * 3:j * 3 + 3])
-            k = j + 1
-            while k < width and k - j < 128 and color == TgaCmpColor(refLine[k * 3:k * 3 + 3], cmpLine[k * 3:k * 3 + 3]):
-                k += 1
-            diff.append(struct.pack("BB", k - j - 1 + 128, color))
-            j = k
-
-    # add the file footer
-    diff.append("\x00" * 8 + "TRUEVISION-XFILE.\x00")
-
-    # write the red-on-black difference file to disk
-    open(tgaDiff, "wb").write("".join(diff))
-    return True
-
+	# ensure that the comparison bitmap exists
+	if not os.path.isfile(tgaCmp):
+		open(tgaCmp, "wb").write("")
+	ref = open(tgaRef, "rb").read()
+	cmp = open(tgaCmp, "rb").read()
+	
+	# bail if the files are either identical or the reference is broken
+	if ref == cmp:
+		return False
+	if len(ref) < 18:
+		return True
+	
+	# determine bitmap dimensions
+	width, height = struct.unpack("<HH", ref[12:16])
+	
+	# unpack the run-length encoded data
+	refData = TgaRleUnpack(ref)
+	if len(refData) < width * height * 3:
+		refData += "\x00" * (width * height * 3 - len(refData))
+	cmpData = TgaRleUnpack(cmp)
+	if len(cmpData) < width * height * 3:
+		cmpData += "\xFF" * (width * height * 3 - len(cmpData))
+	
+	# bail if the files are just differently compressed
+	if refData == cmpData:
+		return False
+	
+	# write a black pixel for identical, a dark red pixel for similar and a
+	# bright red pixel for different color values (packed as a type 9 TGA file,
+	# 8-bit indexed run-length encoded)
+	diff = [struct.pack("<BBBHHBHHHHBB", 0, 1, 9, 0, 3, 24, 0, 0, width, height, 8, 0), "\x00\x00\x00", "\x00\x00\x80", "\x00\x00\xFF"]
+	for i in range(height):
+		refLine = refData[i * width * 3:(i + 1) * width * 3]
+		cmpLine = cmpData[i * width * 3:(i + 1) * width * 3]
+		j = 0
+		# reencode the difference file line-by-line
+		while j < width:
+			color = TgaCmpColor(refLine[j*3:j*3+3], cmpLine[j*3:j*3+3])
+			k = j + 1
+			while k < width and k - j < 128 and color == TgaCmpColor(refLine[k*3:k*3+3], cmpLine[k*3:k*3+3]):
+				k += 1
+			diff.append(struct.pack("BB", k - j - 1 + 128, color))
+			j = k
+	
+	# add the file footer
+	diff.append("\x00" * 8 + "TRUEVISION-XFILE.\x00")
+	
+	# write the red-on-black difference file to disk
+	open(tgaDiff, "wb").write("".join(diff))
+	return True
 
 def RefTestFile(EngineDumpExe, file, refdir):
-    # create an XML dump and bitmap renderings of all pages
-    base = os.path.splitext(os.path.split(file)[1])[0]
-    tgaPath = pjoin(refdir, base + "-%d.cmp.tga")
-    xmlDump = EngineDump(EngineDumpExe, file, tgaPath)
-
-    # compare the XML dumps (remove the dump if it's the same as the reference)
-    xmlRefPath = pjoin(refdir, base + ".ref.xml")
-    xmlCmpPath = pjoin(refdir, base + ".cmp.xml")
-    if not os.path.isfile(xmlRefPath):
-        open(xmlRefPath, "wb").write(xmlDump)
-    elif open(xmlRefPath, "rb").read() != xmlDump:
-        open(xmlCmpPath, "wb").write(xmlDump)
-        print "  FAIL!", xmlCmpPath
-    elif os.path.isfile(xmlCmpPath):
-        os.remove(xmlCmpPath)
-
-    # compare all bitmap renderings (and create diff bitmaps where needed)
-    for file in fnmatch.filter(os.listdir(refdir), base + "-[0-9]*.ref.tga"):
-        tgaRefPath = pjoin(refdir, file)
-        tgaCmpPath, tgaDiffPath = tgaRefPath[:-8] + \
-            ".cmp.tga", tgaRefPath[:-8] + ".diff.tga"
-        if BitmapDiff(tgaRefPath, tgaCmpPath, tgaDiffPath):
-            print "  FAIL!", tgaCmpPath
-        else:
-            os.remove(tgaCmpPath)
-            if os.path.isfile(tgaDiffPath):
-                os.remove(tgaDiffPath)
-    for file in fnmatch.filter(os.listdir(refdir), base + "-[0-9]*.cmp.tga"):
-        tgaCmpPath = pjoin(refdir, file)
-        tgaRefPath = tgaCmpPath[:-8] + ".ref.tga"
-        if not os.path.isfile(tgaRefPath):
-            os.rename(tgaCmpPath, tgaRefPath)
-
+	# create an XML dump and bitmap renderings of all pages
+	base = os.path.splitext(os.path.split(file)[1])[0]
+	tgaPath = pjoin(refdir, base + "-%d.cmp.tga")
+	xmlDump = EngineDump(EngineDumpExe, file, tgaPath)
+	
+	# compare the XML dumps (remove the dump if it's the same as the reference)
+	xmlRefPath = pjoin(refdir, base + ".ref.xml")
+	xmlCmpPath = pjoin(refdir, base + ".cmp.xml")
+	if not os.path.isfile(xmlRefPath):
+		open(xmlRefPath, "wb").write(xmlDump)
+	elif open(xmlRefPath, "rb").read() != xmlDump:
+		open(xmlCmpPath, "wb").write(xmlDump)
+		print "  FAIL!", xmlCmpPath
+	elif os.path.isfile(xmlCmpPath):
+		os.remove(xmlCmpPath)
+	
+	# compare all bitmap renderings (and create diff bitmaps where needed)
+	for file in fnmatch.filter(os.listdir(refdir), base + "-[0-9]*.ref.tga"):
+		tgaRefPath = pjoin(refdir, file)
+		tgaCmpPath, tgaDiffPath = tgaRefPath[:-8] + ".cmp.tga", tgaRefPath[:-8] + ".diff.tga"
+		if BitmapDiff(tgaRefPath, tgaCmpPath, tgaDiffPath):
+			print "  FAIL!", tgaCmpPath
+		else:
+			os.remove(tgaCmpPath)
+			if os.path.isfile(tgaDiffPath):
+				os.remove(tgaDiffPath)
+	for file in fnmatch.filter(os.listdir(refdir), base + "-[0-9]*.cmp.tga"):
+		tgaCmpPath = pjoin(refdir, file)
+		tgaRefPath = tgaCmpPath[:-8] + ".ref.tga"
+		if not os.path.isfile(tgaRefPath):
+			os.rename(tgaCmpPath, tgaRefPath)
 
 def RefTestDir(EngineDumpExe, dir, refdir):
-    # create reference directory, if it doesn't exists yet
-    if not os.path.isdir(refdir):
-        os.makedirs(refdir)
-
-    # test all files in the directory to test
-    for file in os.listdir(dir):
-        file = pjoin(dir, file)
-        if os.path.isfile(file):
-            print "Testing", file
-            RefTestFile(EngineDumpExe, file, refdir)
-
-    # list all differences (again)
-    diffs = fnmatch.filter(os.listdir(refdir), "*.cmp.*")
-    if diffs:
-        print "\n" + "=" * 25 + " RENDERING DIFFERENCES " + "=" * 25
-        for file in diffs:
-            print pjoin(refdir, file)
-        print
-
-    return len(diffs)
-
+	# create reference directory, if it doesn't exists yet
+	if not os.path.isdir(refdir):
+		os.makedirs(refdir)
+	
+	# test all files in the directory to test
+	for file in os.listdir(dir):
+		file = pjoin(dir, file)
+		if os.path.isfile(file):
+			print "Testing", file
+			RefTestFile(EngineDumpExe, file, refdir)
+	
+	# list all differences (again)
+	diffs = fnmatch.filter(os.listdir(refdir), "*.cmp.*")
+	if diffs:
+		print "\n" + "=" * 25 + " RENDERING DIFFERENCES " + "=" * 25
+		for file in diffs:
+			print pjoin(refdir, file)
+		print
+	
+	return len(diffs)
 
 def main(args):
-    # find a path to EngineDump.exe (defaults to ../obj-dbg/EngineDump.exe)
-    if len(args) > 1 and args[1].lower().endswith(".exe"):
-        EngineDumpExe = args.pop(1)
-    else:
-        EngineDumpExe = pjoin(os.path.dirname(__file__),
-                              "..", "obj-dbg", "EngineDump.exe")
-
-    # minimal sanity check of arguments
-    if not args[1:] or not os.path.isdir(args[1]):
-        print "Usage: %s [EngineDump.exe] <dir> [-refdir <dir>] [<dir> ...]" % (os.path.split(args[0])[1])
-        return
-
-    # collect all directories to test (and the corresonding reference
-    # directories)
-    dirs, ix = [], 1
-    while ix < len(args):
-        if ix + 2 < len(args) and args[ix + 1] == "-refdir":
-            dirs.append((args[ix], args[ix + 2]))
-            ix += 3
-        else:
-            dirs.append((args[ix], pjoin(args[ix], "references")))
-            ix += 1
-
-    # run the test
-    fails = 0
-    for (dir, refdir) in dirs:
-        fails += RefTestDir(EngineDumpExe, dir, refdir)
-    sys.exit(fails)
+	# find a path to EngineDump.exe (defaults to ../obj-dbg/EngineDump.exe)
+	if len(args) > 1 and args[1].lower().endswith(".exe"):
+		EngineDumpExe = args.pop(1)
+	else:
+		EngineDumpExe = pjoin(os.path.dirname(__file__), "..", "obj-dbg", "EngineDump.exe")
+	
+	# minimal sanity check of arguments
+	if not args[1:] or not os.path.isdir(args[1]):
+		print "Usage: %s [EngineDump.exe] <dir> [-refdir <dir>] [<dir> ...]" % (os.path.split(args[0])[1])
+		return
+	
+	# collect all directories to test (and the corresonding reference directories)
+	dirs, ix = [], 1
+	while ix < len(args):
+		if ix + 2 < len(args) and args[ix + 1] == "-refdir":
+			dirs.append((args[ix], args[ix + 2]))
+			ix += 3
+		else:
+			dirs.append((args[ix], pjoin(args[ix], "references")))
+			ix += 1
+	
+	# run the test
+	fails = 0
+	for (dir, refdir) in dirs:
+		fails += RefTestDir(EngineDumpExe, dir, refdir)
+	sys.exit(fails)
 
 if __name__ == "__main__":
-    main(sys.argv[:])
+	main(sys.argv[:])
--- scripts/update_dependencies.py
+++ scripts/update_dependencies.py
@@ -6,112 +6,92 @@ from sources in src and subfolders, so that changing a header file always
 leads to the recompilation of all the files depending on this header.
 """
 
-import os
-import re
-import fnmatch
-import util2
-import update_vs
-import update_vs2008
+import os, re, fnmatch, util2, update_vs, update_vs2008
 pjoin = os.path.join
 
-DIRS = ["src", pjoin("src", "utils"), pjoin("src", "installer"), pjoin("src", "ifilter"), pjoin("src", "browserplugin"), pjoin(
-    "src", "previewer"), pjoin("src", "mui"), pjoin("src", "memtrace"), pjoin("src", "regress"), pjoin("src", "uia")]
+DIRS = ["src", pjoin("src", "utils"), pjoin("src", "installer"), pjoin("src", "ifilter"), pjoin("src", "browserplugin"), pjoin("src", "previewer"), pjoin("src", "mui"), pjoin("src", "memtrace"), pjoin("src", "regress"), pjoin("src", "uia")]
 INCLUDE_DIRS = DIRS + [pjoin("mupdf", "include")]
-OBJECT_DIRS = {"src\\utils": "$(OU)", "src\\browserplugin": "$(ODLL)", "src\\ifilter": "$(ODLL)", "src\\previewer":
-               "$(ODLL)", "src\\mui": "$(OMUI)", "src\\memtrace": "$(OM)", "src\\uia": "$(OUIA)"}  # default: "$(OS)"
+OBJECT_DIRS = { "src\\utils": "$(OU)", "src\\browserplugin": "$(ODLL)", "src\\ifilter": "$(ODLL)", "src\\previewer": "$(ODLL)", "src\\mui": "$(OMUI)", "src\\memtrace": "$(OM)", "src\\uia": "$(OUIA)" } # default: "$(OS)"
 MAKEFILE = "makefile.deps"
 DEPENDENCIES_PER_LINE = 3
 
-
 def prependPath(files, basefile=None):
-    result = []
-    include_dirs = INCLUDE_DIRS
-    if basefile:
-        include_dirs = [os.path.dirname(basefile)] + include_dirs
+	result = []
+	include_dirs = INCLUDE_DIRS
+	if basefile:
+		include_dirs = [os.path.dirname(basefile)] + include_dirs
 
-    for file in files:
-        if file in ["string.h", "math.h"]:
-            # skip names of system headers which also exist in
-            # mupdf/include/mupdf/fitz
-            continue
-        for dir in include_dirs:
-            path = pjoin(dir, file)
-            if os.path.exists(path):
-                result.append(path)
-                break
-    return result
-
+	for file in files:
+		if file in ["string.h", "math.h"]:
+			continue # skip names of system headers which also exist in mupdf/include/mupdf/fitz
+		for dir in include_dirs:
+			path = pjoin(dir, file)
+			if os.path.exists(path):
+				result.append(path)
+				break
+	return result
 
 def getObjectPath(file):
-    file = file.replace("/", "\\")
-    for (path, odir) in OBJECT_DIRS.items():
-        if file.startswith(path + "\\"):
-            return odir
-    return "$(OS)"
-
+	file = file.replace("/", "\\")
+	for (path, odir) in OBJECT_DIRS.items():
+		if file.startswith(path + "\\"):
+			return odir
+	return "$(OS)"
 
 @util2.memoize
 def extractIncludes(file):
-    content = open(file, "r").read()
-    content = content.replace("\r\n", "\n")
-    # filter out multi-line comments (could contain #include lines as examples)
-    content = re.sub(r'(?s)/\*.*?\*/', '/* */', content)
-    # try to filter out "#if 0 ... #endif" sections (hacky)
-    content = re.sub(r'(?sm)^#if 0$.*?^#endif$', '', content)
-    includes = re.findall(r'(?m)^#include ["<]([^">]+)[">]', content)
-    includes = prependPath(includes, file)
+	content = open(file, "r").read()
+	content = content.replace("\r\n", "\n")
+	# filter out multi-line comments (could contain #include lines as examples)
+	content = re.sub(r'(?s)/\*.*?\*/', '/* */', content)
+	# try to filter out "#if 0 ... #endif" sections (hacky)
+	content = re.sub(r'(?sm)^#if 0$.*?^#endif$', '', content)
+	includes = re.findall(r'(?m)^#include ["<]([^">]+)[">]', content)
+	includes = prependPath(includes, file)
 
-    for inc in includes:
-        includes += extractIncludes(inc)
-    return util2.uniquify(includes)
-
+	for inc in includes:
+		includes += extractIncludes(inc)
+	return util2.uniquify(includes)
 
 def createDependencyList():
-    dependencies = {}
-    for dir in DIRS:
-        all_c_files = fnmatch.filter(os.listdir(dir), "*.c*")
-        for file in all_c_files:
-            file = pjoin(dir, file)
-            dependencies[file] = extractIncludes(file)
-    return dependencies
-
+	dependencies = {}
+	for dir in DIRS:
+		all_c_files = fnmatch.filter(os.listdir(dir), "*.c*")
+		for file in all_c_files:
+			file = pjoin(dir, file)
+			dependencies[file] = extractIncludes(file)
+	return dependencies
 
 def flattenDependencyList(dependencies):
-    flatlist = []
-    for file in dependencies.keys():
-        if dependencies[file]:
-            opath = getObjectPath(file)
-            filename = os.path.splitext(os.path.split(file)[1])[0]
-            # TODO: normalizing paths already in prependPath makes
-            # getObjectPath fail under cygwin
-            deplist = sorted(
-                dependencies[file], key=lambda s: str.lower(s.replace("/", "\\")))
-            for depgroup in util2.group(deplist, DEPENDENCIES_PER_LINE):
-                flatlist.append("%s\\%s.obj: $B\\%s" %
-                                (opath, filename, " $B\\".join(depgroup)))
-    return flatlist
-
+	flatlist = []
+	for file in dependencies.keys():
+		if dependencies[file]:
+			opath = getObjectPath(file)
+			filename = os.path.splitext(os.path.split(file)[1])[0]
+			# TODO: normalizing paths already in prependPath makes getObjectPath fail under cygwin
+			deplist = sorted(dependencies[file], key=lambda s: str.lower(s.replace("/", "\\")))
+			for depgroup in util2.group(deplist, DEPENDENCIES_PER_LINE):
+				flatlist.append("%s\\%s.obj: $B\\%s" % (opath, filename, " $B\\".join(depgroup)))
+	return flatlist
 
 def normalizePaths(paths):
-    return re.sub(r"( |\\)[^.\\\s]+\\..\\", r"\1", paths.replace("/", "\\"))
-
+	return re.sub(r"( |\\)[^.\\\s]+\\..\\", r"\1", paths.replace("/", "\\"))
 
 def injectDependencyList(flatlist):
-    flatlist = "\n".join(sorted(flatlist, key=str.lower))
-    flatlist = normalizePaths(flatlist)
-    content = "## Header-dependencies for src\* and src\*\*\n"
-    content += "### the list below is auto-generated by update_dependencies.py\n"
-    content += "B=$(BASEDIR)\n"
-    content += flatlist + "\n"
+	flatlist = "\n".join(sorted(flatlist, key=str.lower))
+	flatlist = normalizePaths(flatlist)
+	content  = "## Header-dependencies for src\* and src\*\*\n"
+	content += "### the list below is auto-generated by update_dependencies.py\n"
+	content += "B=$(BASEDIR)\n"
+	content += flatlist + "\n"
 
-    open(MAKEFILE, "wb").write(content.replace("\n", "\r\n"))
-
+	open(MAKEFILE, "wb").write(content.replace("\n", "\r\n"))
 
 def main():
-    util2.chdir_top()
-    injectDependencyList(flattenDependencyList(createDependencyList()))
+	util2.chdir_top()
+	injectDependencyList(flattenDependencyList(createDependencyList()))
 
 if __name__ == "__main__":
-    main()
-    update_vs.main()
-    update_vs2008.main()
+	main()
+	update_vs.main()
+	update_vs2008.main()
