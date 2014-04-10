762bc5a5401e02a70766df1bfb97b74ebf90b9fb
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-04-10 02:28:49 +0000

    add more sample text

--- tools/render-speed-test/render-speed-test/render-speed-test.h
+++ tools/render-speed-test/render-speed-test/render-speed-test.h
@@ -6,20 +6,23 @@
 // which is why it's not in the .cpp file
 #define SAMPLE_TEXT "This program tests various ways of measuring/drawing text.\n" \
     "\n" \
-    "The GDI+ method we currently use in ebook mode in Sumatra seems slow, which makes" \
-    "breaking up into pages quite slow." \
+    "The GDI+ method we currently use in ebook mode in Sumatra seems slow, which makes\n" \
+    "breaking up into pages quite slow.\n" \
     "\n" \
-    "According to http://theartofdev.wordpress.com/2013/08/12/the-wonders-of-text-rendering-and-gdi/" \
-    "GDI can be substantially faster." \
+    "According to http://theartofdev.wordpress.com/2013/08/12/the-wonders-of-text-rendering-and-gdi/\n" \
+    "GDI can be substantially faster.\n" \
     "\n" \
-    "Another option available under Vista (and later) is to use Direct Draw 2D." \
+    "Another option available under Vista (and later) is to use Direct Draw 2D.\n" \
     "\n" \
-    "This program uses latest C++ features available in Visual Studio 2013, because I'm curious" \
-    "if they provide substantial benefits." \
+    "This program uses latest C++ features available in Visual Studio 2013, because I'm curious\n" \
+    "if they provide substantial benefits.\n" \
     "\n" \
-    "As a result, this code might not compile under earlier version of Visual Studio (I'm using" \
-    "Visual Studio 2013 Update 1 to be exact)." \
+    "As a result, this code might not compile under earlier version of Visual Studio (I'm using\n" \
+    "Visual Studio 2013 Update 1 to be exact).\n" \
     "\n" \
-    "This program is also a simple text viewer, because it can only display text. For simplicity" \
-    "it doesn't handle any markup."
-
+    "This program is also a simple text viewer, because it can only display text. For simplicity\n" \
+    "it doesn't handle any markup.\n" \
+    "\n" \
+    "I have a need to convert Pixels to Points in C#. I've seen some complicated explanations about the topic, but can't seem to locate a simple formula. Let's assume a standard 96dpi, how do I calulate this conversion?\n" \
+    "\n" \
+    "System.Drawing.Graphics has DpiX and DpiY properties.DpiX is pixels per inch horizontally. DpiY is pixels per inch vertically. Use those to convert from points(72 points per inch) to pixels.\n"
