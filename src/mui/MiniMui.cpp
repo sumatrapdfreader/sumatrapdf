0d7da5ed2adc57f36d669771dc5545d16296f8a4
Simon Bunzli <zeniko@gmail.com>
2014-04-15 09:19:54 +0000

    fix MiniMui

--- src/mui/MiniMui.cpp
+++ src/mui/MiniMui.cpp
@@ -5,6 +5,7 @@
 
 #include "BaseUtil.h"
 #include "MiniMui.h"
+#include "WinUtil.h"
 
 namespace mui {
 
@@ -19,17 +20,16 @@ void InitGraphicsMode(Graphics *g)
     g->SetPageUnit(UnitPixel);
 }
 
-class FontCache {
-    struct Entry {
-        WCHAR *     name;
-        float       size;
-        FontStyle   style;
-        Font *      font;
+enum FontRenderType { FRT_Gdiplus, FRT_Gdi };
 
-        Entry(WCHAR *name=NULL, float size=0.0f, FontStyle style=FontStyleRegular, Font *font=NULL) :
-            name(name), size(size), style(style), font(font) { }
+class FontCache {
+    struct Entry : public CachedFont {
+        Entry(WCHAR *name=NULL, float sizePt=0.0f, FontStyle style=FontStyleRegular, Font *font=NULL, HFONT hdcFont=NULL) {
+            this->name = name; this->sizePt = sizePt; this->style = style; this->font = font; this->hdcFont = hdcFont;
+        }
         bool operator==(const Entry& other) const {
-            return size == other.size && style == other.style && str::Eq(name, other.name);
+            return SameAs(other.name, other.sizePt, other.style) &&
+                   !font == !other.font && !hdcFont == !other.hdcFont;
         }
     };
 
@@ -42,32 +42,55 @@ public:
         for (Entry *e = cache.IterStart(); e; e = cache.IterNext()) {
             free(e->name);
             ::delete e->font;
+            DeleteObject(e->hdcFont);
         }
     }
 
-    Font *GetFont(const WCHAR *name, float size, FontStyle style) {
-        int idx = cache.Find(Entry((WCHAR *)name, size, style));
+    CachedFont *GetFont(const WCHAR *name, float size, FontStyle style, FontRenderType type) {
+        int idx = cache.Find(Entry((WCHAR *)name, size, style, FRT_Gdiplus == type ? (Font *)-1 : NULL, FRT_Gdi == type ? (HFONT)-1 : NULL));
         if (idx != -1)
-            return cache.At(idx).font;
+            return &cache.At(idx);
 
-        Font *font = ::new Font(name, size, style);
-        if (!font) {
-            // fall back to the default font, if a desired font can't be created
-            font = ::new Font(L"Times New Roman", size, style);
+        if (FRT_Gdiplus == type) {
+            Font *font = ::new Font(name, size, style);
+            CrashIf(!font);
             if (!font) {
-                return cache.Count() > 0 ? cache.At(0).font : NULL;
+                // fall back to the default font, if a desired font can't be created
+                font = ::new Font(L"Times New Roman", size, style);
+                if (!font) {
+                    return cache.Count() > 0 ? &cache.At(0) : NULL;
+                }
             }
+            cache.Append(Entry(str::Dup(name), size, style, font, NULL));
+            return &cache.Last();
         }
-        cache.Append(Entry(str::Dup(name), size, style, font));
-        return font;
+        else if (FRT_Gdi == type) {
+            // TODO: DPI scaling doesn't belong that deep in the model
+            int sizePx = (int)(size * 72 / 96);
+            // TODO: take FontStyle into account as well
+            HDC hdc = GetDC(NULL);
+            HFONT font = CreateSimpleFont(hdc, name, sizePx);
+            ReleaseDC(NULL, hdc);
+            cache.Append(Entry(str::Dup(name), size, style, NULL, font));
+            return &cache.Last();
+        }
+        else {
+            CrashIf(true);
+            return NULL;
+        }
     }
 };
 
 static FontCache gFontCache;
 
-Font *GetCachedFont(const WCHAR *name, float size, FontStyle style)
+CachedFont *GetCachedFontGdi(const WCHAR *name, float size, FontStyle style)
 {
-    return gFontCache.GetFont(name, size, style);
+    return gFontCache.GetFont(name, size, style, FRT_Gdi);
+}
+
+CachedFont *GetCachedFontGdiplus(const WCHAR *name, float size, FontStyle style)
+{
+    return gFontCache.GetFont(name, size, style, FRT_Gdiplus);
 }
 
 class GlobalGraphicsHack {
--- src/mui/MiniMui.h
+++ src/mui/MiniMui.h
@@ -5,13 +5,34 @@
 #define Mui_h
 
 // as little of mui as necessary to make ../EngineDump.cpp compile
+// TODO: extract from UI toolkit so that it can be used in model independent of view
 
 using namespace Gdiplus;
 
 namespace mui {
 
 void InitGraphicsMode(Graphics *g);
-Font *GetCachedFont(const WCHAR *name, float size, FontStyle style);
+
+struct CachedFont {
+    WCHAR *             name;
+    float               sizePt;
+    Gdiplus::FontStyle  style;
+
+    // only one of font or hdcFont can be set
+    Gdiplus::Font *     font;
+    HFONT               hdcFont;
+
+    Gdiplus::FontStyle  GetStyle() const { return style; }
+    float               GetSize() const { return sizePt; }
+    const WCHAR *       GetName() const { return name; }
+    bool                SameAs(const WCHAR *name, float sizePt, FontStyle style) const {
+        return this->sizePt == sizePt && this->style == style && str::Eq(this->name, name);
+    }
+};
+
+CachedFont *GetCachedFontGdi(const WCHAR *name, float sizePt, FontStyle style);
+CachedFont *GetCachedFontGdiplus(const WCHAR *name, float sizePt, FontStyle style);
+
 Graphics *AllocGraphicsForMeasureText();
 void FreeGraphicsForMeasureText(Graphics *g);
 
