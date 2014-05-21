d91ada94f98c22920deea8615fbe135f4d42593f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-05-21 03:53:05 +0000

    working version of coverity.bat; add support for building with VS 2013 and (untested) fix to make VS 2012 build support Windows XP (thanks to XhmikosR)

--- scripts/vc.bat
+++ scripts/vc.bat
@@ -4,6 +4,7 @@ REM append ..\bin to PATH to make nasm.exe available
 FOR %%p IN (nasm.exe) DO IF "%%~$PATH:p" == "" SET PATH=%PATH%;%~dp0..\bin
 
 REM Allow to explicitly specify the desired Visual Studio version
+IF /I "%1" == "vc13" GOTO TRY_VS13
 IF /I "%1" == "vc12" GOTO TRY_VS12
 IF /I "%1" == "vc10" GOTO TRY_VS10
 IF /I "%1" == "vc9" GOTO TRY_VS9
@@ -25,6 +26,18 @@ REM vs12 is VS 2012
 :TRY_VS12
 CALL "%VS110COMNTOOLS%\vsvars32.bat" 2>NUL
 IF NOT ERRORLEVEL 1 EXIT /B
+REM xp support http://blogs.msdn.com/b/vcblog/archive/2012/10/08/10357555.aspx
+SET "INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;%INCLUDE%"
+SET "PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%"
+SET "LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%"
+REM TODO: for 64bit it should be:
+REM set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib\x64;%LIB%
+set CL=/D_USING_V110_SDK71_;%CL%
 
-ECHO Visual Studio 2012, 2010, or 2008 doesn't seem to be installed
+REM vs13 is VS 2013
+:TRY_VS13
+CALL "%VS120COMNTOOLS%\vsvars32.bat" 2>NUL
+IF NOT ERRORLEVEL 1 EXIT /B
+
+ECHO Visual Studio 2013, 2012, 2010, or 2008 doesn't seem to be installed
 EXIT /B 1
