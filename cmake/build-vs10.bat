7242c290955d55e194cfb033e3fc28cfe9f50446
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-06-17 07:06:48 +0000

    add cmake/build-vs10.bat and cmake/build-vs10-64.bat; set /machine linker option for 64-bit builds properly

---
+++ cmake/build-vs10.bat
@@ -0,0 +1,22 @@
+@echo off
+
+@rem setup for Visual Studio 2010 or bust
+CALL "%VS100COMNTOOLS%\vsvars32.bat" 2>NUL
+IF NOT ERRORLEVEL 1 GOTO VSFOUND
+ECHO Visual Studio 2010 doesn't seem to be installed
+EXIT /B 1
+
+:VSFOUND
+
+if not exist "cmake\vs10" mkdir "cmake\vs10"
+cd cmake\vs10
+
+rem this is a work-around cl.exe failing if both TMP and tmp env variables are
+rem defined, which might happen due to cygwin. This in turns braks cmake
+set TMP=
+set TEMP=
+
+echo running cmake
+cmake -G "Visual Studio 10" ..\..
+echo runing msbuild
+msbuild all.sln /t:Rebuild /p:Configuration=Release
