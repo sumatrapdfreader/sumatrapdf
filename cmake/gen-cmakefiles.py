e34ba2f71b2e37f03b00d19b1f011bf54a7b189a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-06-18 03:45:13 +0000

    partial jpeg-turbo support

--- cmake/gen-cmakefiles.py
+++ cmake/gen-cmakefiles.py
@@ -8,6 +8,24 @@
 # It's not perfect and re
 import os
 
+s_objs = """
+	$(OT)\jfsseflt-64.obj $(OT)\jccolss2-64.obj $(OT)\jdcolss2-64.obj $(OT)\jcgrass2-64.obj \
+	$(OT)\jcsamss2-64.obj $(OT)\jdsamss2-64.obj $(OT)\jdmerss2-64.obj $(OT)\jcqnts2i-64.obj  \
+	$(OT)\jfss2fst-64.obj $(OT)\jfss2int-64.obj $(OT)\jiss2red-64.obj $(OT)\jiss2int-64.obj \
+	$(OT)\jiss2fst-64.obj $(OT)\jcqnts2f-64.obj $(OT)\jiss2flt-64.obj $(OT)\jsimd_x86_64.obj
+"""
+
+s_objs2 = """
+	$(OT)\jsimdcpu.obj $(OT)\jccolmmx.obj $(OT)\jcgrammx.obj $(OT)\jdcolmmx.obj \
+	$(OT)\jcsammmx.obj $(OT)\jdsammmx.obj $(OT)\jdmermmx.obj $(OT)\jcqntmmx.obj \
+	$(OT)\jfmmxfst.obj $(OT)\jfmmxint.obj $(OT)\jimmxred.obj $(OT)\jimmxint.obj \
+	$(OT)\jimmxfst.obj $(OT)\jcqnt3dn.obj $(OT)\jf3dnflt.obj $(OT)\ji3dnflt.obj \
+	$(OT)\jcqntsse.obj $(OT)\jfsseflt.obj $(OT)\jisseflt.obj $(OT)\jccolss2.obj \
+	$(OT)\jcgrass2.obj $(OT)\jdcolss2.obj $(OT)\jcsamss2.obj $(OT)\jdsamss2.obj \
+	$(OT)\jdmerss2.obj $(OT)\jcqnts2i.obj $(OT)\jfss2fst.obj $(OT)\jfss2int.obj \
+	$(OT)\jiss2red.obj $(OT)\jiss2int.obj $(OT)\jiss2fst.obj $(OT)\jcqnts2f.obj \
+	$(OT)\jiss2flt.obj $(OT)\jsimd_i386.obj
+"""
 
 # if true, paths generated to be relative to cmake directory, so that we
 # can place CMakeLists*.txt inside cmake directory
@@ -27,7 +45,7 @@ def strip_ext(file_name):
 # returns dict mapping file name without extension to DirInfo
 def build_files_map():
 	res = {}
-	for dir, dirnames, filenames in os.walk("."):
+	for dir, dirnames, filenames in os.walk("ext"):
 		#print("%s, %s, %s\n" % (dir, dirnames, filenames))
 		di = DirInfo(dir, filenames)
 		for f in filenames:
@@ -38,59 +56,31 @@ def build_files_map():
 # given $(OWP)\alpha.obj return alpha
 def clean_obj_name(s):
 	parts = s.split("\\")
-	if len(parts) == 1:
-		return None
 	assert len(parts) == 2, "len(parts) != 2, parts: '%s'" % parts
 	s = parts[-1]
 	if s.endswith(".obj"):
 		s = s[:-4]
-	if s.endswith(".exe"): # mudraw.exe
-		s = s[:-4]
 	return s
 
 
-# returns dict mapping string defining a target (e.g. UNRAR_OBJS) to a list
-# of source (.c, .cpp, .asm) files (full paths)
-def parse_makefile(file_path, res):
-	in_obj_list = False
-	curr_name = None
-	curr_objs = None
-	for l in open(file_path).readlines():
+def parse_objs(s):
+	res = []
+	for l in s.split("\n"):
 		l = l.strip()
-		if in_obj_list:
-			parts = l.split()
-			for p in parts:
-				 if p != "\\":
-				 	p = clean_obj_name(p)
-				 	if p != None:
-					 	curr_objs.append(p)
-				 	#print(p)
-			if len(parts) > 0 and parts[-1] != "\\":
-				in_obj_list = False
-				res[curr_name] = curr_objs
-				curr_name = None
-				curr_objs = None
-				#print("")
-		elif "_OBJS =" in l:
-			parts = l.split()
-			name = parts[0]
-			#print("'%s'" % name)
-			in_obj_list = True
-			curr_name = name
-			curr_objs = []
+		parts = l.split()
+		for p in parts:
+			if p == "\\":
+				continue
+			p = clean_obj_name(p)
+			res.append(p)
+			#print(p)
 	return res
 
 
-def parse_makefile_in_dir(dir, defs):
-	return parse_makefile(os.path.join(dir, "makefile.msvc"), defs)
-
-
 def dump_defs(defs):
-	for name in defs.keys():
-		print("\n%s" % name)
-		for v in defs[name]:
-			v = clean_obj_name(v)
-			print("  %s" % v)
+	for v in defs:
+		#v = clean_obj_name(v)
+		print("  %s" % v)
 
 
 def get_dir_for_files(files, files_map):
@@ -98,6 +88,10 @@ def get_dir_for_files(files, files_map):
 		return None
 	f = files[0] # TODO: find the one with the most matches
 	return files_map[f]
+
+
+def get_dir_for_file(file, files_map):
+	return files_map[file]
 
 
 # file_name is name without extenstion. files is a list of files in a directory
@@ -122,37 +116,27 @@ def fixup_dirname(s):
 	return s
 
 
-def gen_cmake(defs, files_map):
+def gen_cmake(obj_names, files_map):
 	lines = []
-	for name in defs.keys():
-		if name in ["UNINSTALLER_OBJS"]:
-			continue
-		files = defs[name]
-		name = name.replace("_OBJS", "_SRC")
-		lines.append("file (GLOB %s" % name)
-		di = get_dir_for_files(files, files_map)
-		if di == None:
-			print("no dir for %s" % name)
-			continue
-		# add all .h files by default, using a single line, for shorter files
+	for file_no_ext in obj_names:
+		di = get_dir_for_file(file_no_ext, files_map)
+		assert di is not None, "no dir for %s" % file_no_ext
 		dir = fixup_dirname(di.dir)
-		s = "%s/*.h" % dir
+		# add all .h files by default, using a single line, for shorter files
+		#s = "%s/*.h" % dir
+		#lines.append("\t" + quote(s))
+		name_with_ext = find_compilable_for_name(file_no_ext, di.files)
+		s = "%s/%s" % (dir, name_with_ext)
 		lines.append("\t" + quote(s))
-		for f in files:
-			name_with_ext = find_compilable_for_name(f, di.files)
-			s = "%s/%s" % (dir, name_with_ext)
-			lines.append("\t" + quote(s))
-		lines.append("\t)\n")
+	lines.append("\t)\n")
 	return "\n".join(lines)
 
 
 def main():
 	files_map = build_files_map()
-	defs = parse_makefile_in_dir("mupdf", {})
-	defs = parse_makefile_in_dir("ext", defs)
-	defs = parse_makefile_in_dir(".", defs)
-	#dump_defs(in_ext)
-	s = gen_cmake(defs, files_map)
+	obj_names = parse_objs(s_objs2)
+	dump_defs(obj_names)
+	s = gen_cmake(obj_names, files_map)
 	print(s)
 
 
