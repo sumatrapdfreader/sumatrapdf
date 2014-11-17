b56fdf56624e438fde97cb827966ab1e37c3f274
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-11-17 06:09:32 +0000

    actually, it seems to be _USING_V110_SDK71_; move setting it from vc.bat to makefile

--- scripts/vc.bat
+++ scripts/vc.bat
@@ -3,8 +3,6 @@
 REM append ..\bin to PATH to make nasm.exe available
 FOR %%p IN (nasm.exe) DO IF "%%~$PATH:p" == "" SET PATH=%PATH%;%~dp0..\bin
 
-REM vs13 is VS 2013
-:TRY_VS13
 CALL "%VS120COMNTOOLS%\vsvars32.bat" 2>NUL
 IF ERRORLEVEL 1 GOTO NO_VS
 
@@ -19,9 +17,6 @@ SET "PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%"
 SET "LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%"
 @REM REM TODO: for 64bit it should be:
 @REM REM set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib\x64;%LIB%
-@REM https://github.com/ssendeavour/SchoolPersonnelMgmt/blob/master/set_env_var.bat
-REM use toolchain for WinXP 32bit, change 5.01 to 5.02 for 64bit
-set CL=/D_USING_V120_SDK71_;%CL%
 GOTO OK
 
 :XP_WIN32
@@ -31,9 +26,6 @@ SET "PATH=%PROGRAMFILES%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%"
 SET "LIB=%PROGRAMFILES%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%"
 @REM REM TODO: for 64bit it should be:
 @REM REM set LIB=%PROGRAMFILES%\Microsoft SDKs\Windows\7.1A\Lib\x64;%LIB%
-@REM https://github.com/ssendeavour/SchoolPersonnelMgmt/blob/master/set_env_var.bat
-REM use toolchain for WinXP 32bit, change 5.01 to 5.02 for 64bit
-set CL=/D_USING_V120_SDK71_;%CL%
 GOTO OK
 
 :NO_VS
