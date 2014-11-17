c4f0115177ed4ced19e9dbe9738ffa7b11441153
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-11-17 17:44:33 +0000

    really update buildbot.bat

--- scripts/buildbot.bat
+++ scripts/buildbot.bat
@@ -1,15 +1,8 @@
 @ECHO OFF
 
-@REM :TRY_VS12
-@REM CALL "%VS110COMNTOOLS%\vsvars32.bat" 2>NUL
-@REM IF NOT ERRORLEVEL 1 GOTO VSFOUND
-
-:TRY_VS10
-CALL "%VS100COMNTOOLS%\vsvars32.bat" 2>NUL
+CALL scripts\vc.bat
 IF NOT ERRORLEVEL 1 GOTO VSFOUND
-
-REM Fail, if no Visual Studio installation has been found
-ECHO Visual Studio 2010 doesn't seem to be installed
+ECHO Visual Studio 2013 doesn't seem to be installed
 EXIT /B 1
 
 :VSFOUND
--- scripts/efi_cmp.bat
+++ scripts/efi_cmp.bat
@@ -1,14 +1,8 @@
 @ECHO OFF
 
-:TRY_VS10
-CALL "%VS100COMNTOOLS%\vsvars32.bat" 2>NUL
+CALL scripts\vc.bat
 IF NOT ERRORLEVEL 1 GOTO VSFOUND
-
-:TRY_VS12
-CALL "%VS110COMNTOOLS%\vsvars32.bat" 2>NUL
-IF NOT ERRORLEVEL 1 GOTO VSFOUND
-
-ECHO Visual Studio 2010 or 2012 doesn't seem to be installed
+ECHO Visual Studio 2013 doesn't seem to be installed
 EXIT /B 1
 
 :VSFOUND
