bade917f0b51b16b6f0b3b19978fa42b3fd632d6
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-11-29 21:31:14 -0800

    add appveyor build

---
+++ scripts/appveyor-build.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SETLOCAL
+
+REM assumes we're being run from top-level directory as:
+REM scripts\appveyor-build.bat
+
+@rem TODO: don't call when inappveyor env variable is set
+@rem because Visual Studio is already in %PATH%
+@rem also it seems that python is installed, so we could
+@rem write build script in python
+@rem alternatively, the script could be written in PowerShell
+CALL scripts\vc.bat
+IF ERRORLEVEL 1 EXIT /B 1
+
+REM add our nasm.exe to the path
+SET PATH=%CD%\bin;%PATH%
+
+nmake -f makefile.msvc CFG=rel PLATFORM=X86 SumatraPDF Uninstaller
+
+@rem TODO: build with more platforms, build and run tests etc.
