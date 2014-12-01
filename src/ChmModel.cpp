sumatrapdfreader/sumatrapdf

commit 2a0aec58a4c2cfeb350a9bf951a0ea2dab69659d

that didn't seem necessary

src/ChmModel.cpp
@@ -509,7 +509,7 @@ void ChmModel::UpdateDisplayState(DisplayState *ds)
     ds->scrollPos = PointI();
 }
 
-class ChmThumbnailTask : public HtmlWindowCallback, public UITask
+class ChmThumbnailTask : public HtmlWindowCallback
 {
     ChmDoc *doc;
     HWND hwnd;
@@ -557,7 +557,6 @@ public:
                 tnCb->SaveThumbnail(bmp);
                 tnCb = NULL;
             }
-            uitask::Post(this);
         }
     }
     virtual void OnLButtonDown() { }
@@ -569,8 +568,6 @@ public:
         return data.Last();
     }
     virtual void DownloadData(const WCHAR *url, const unsigned char *data, size_t len) { }
-
-    virtual void Execute() { }
 };
 
 // Create a thumbnail of chm document by loading it again and rendering

commit f53d54c6ccb2407b4ccab37368b21b256304cde7

fix memory leak introduced with commit 2a0aec58a4c2cfeb350a9bf951a0ea2dab69659d

src/ChmModel.cpp
@@ -555,6 +555,8 @@ public:
                 RenderedBitmap *bmp = new RenderedBitmap(hbmp, size);
                 saveThumbnail(bmp);
             }
+            // TODO: why is destruction on the UI thread necessary?
+            uitask::Post([=] { delete this; });
         }
     }
     virtual void OnLButtonDown() { }