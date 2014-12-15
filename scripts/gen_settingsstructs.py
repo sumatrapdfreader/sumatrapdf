sumatrapdfreader/sumatrapdf

commit 03941ec8cb665dadfdb91e0ca01b8c1c64f3a472

fix policy inconsistencies: make -restrict override sumatrapdfrestrict.ini, add webpage to DEFAULT_FILE_PERCEIVED_TYPES as documented

src/SumatraPDF.cpp
@@ -103,7 +103,7 @@ bool             gUseGdiRenderer = false;
 // in plugin mode, the window's frame isn't drawn and closing and
 // fullscreen are disabled, so that SumatraPDF can be displayed
 // embedded (e.g. in a web browser)
-WCHAR *          gPluginURL = nullptr; // owned by CommandLineInfo in WinMain
+const WCHAR *    gPluginURL = nullptr; // owned by CommandLineInfo in WinMain
 
 #define SPLITTER_DX         5
 #define SIDEBAR_MIN_WIDTH   150
@@ -131,24 +131,19 @@ bool                         gCrashOnOpen = false;
 // in restricted mode, some features can be disabled (such as
 // opening files, printing, following URLs), so that SumatraPDF
 // can be used as a PDF reader on locked down systems
-int                          gPolicyRestrictions = Perm_All;
+static int                   gPolicyRestrictions = Perm_RestrictedUse;
 // only the listed protocols will be passed to the OS for
 // opening in e.g. a browser or an email client (ignored,
 // if gPolicyRestrictions doesn't contain Perm_DiskAccess)
-WStrVec                      gAllowedLinkProtocols;
+static WStrVec               gAllowedLinkProtocols;
 // only files of the listed perceived types will be opened
 // externally by LinkHandler::LaunchFile (i.e. when clicking
 // on an in-document link); examples: "audio", "video", ...
-WStrVec                      gAllowedFileTypes;
+static WStrVec               gAllowedFileTypes;
 
 static void CloseDocumentInTab(WindowInfo *win, bool keepUIEnabled=false, bool deleteModel=false);
 static bool SidebarSplitterCb(void *ctx, bool done);
 static bool FavSplitterCb(void *ctx, bool done);
-
-bool HasPermission(int permission)
-{
-    return (permission & gPolicyRestrictions) == permission;
-}
 
 void SetCurrentLang(const char *langCode)
 {
@@ -176,6 +171,87 @@ void SetCurrentLang(const char *langCode)
 #endif
 
 #define SECS_IN_DAY 60*60*24
+
+#define RESTRICTIONS_FILE_NAME       L"sumatrapdfrestrict.ini"
+
+#define DEFAULT_LINK_PROTOCOLS       L"http,https,mailto"
+#define DEFAULT_FILE_PERCEIVED_TYPES L"audio,video,webpage"
+
+void InitializedPolicies(bool restrict)
+{
+    // default configuration should be to restrict everything
+    CrashIf(gPolicyRestrictions != Perm_RestrictedUse);
+    CrashIf(gAllowedLinkProtocols.Count() != 0 || gAllowedFileTypes.Count() != 0);
+
+    // the -restrict command line flag overrides any sumatrapdfrestrict.ini configuration
+    if (restrict)
+        return;
+
+    // allow to restrict SumatraPDF's functionality from an INI file in the
+    // same directory as SumatraPDF.exe (cf. ../docs/sumatrapdfrestrict.ini)
+    // (if the file isn't there, everything is allowed)
+    ScopedMem<WCHAR> restrictPath(path::GetAppPath(RESTRICTIONS_FILE_NAME));
+    if (!file::Exists(restrictPath)) {
+        gPolicyRestrictions = Perm_All;
+        gAllowedLinkProtocols.Split(DEFAULT_LINK_PROTOCOLS, L",");
+        gAllowedFileTypes.Split(DEFAULT_FILE_PERCEIVED_TYPES, L",");
+        return;
+    }
+
+    ScopedMem<char> restrictData(file::ReadAll(restrictPath, nullptr));
+    SquareTree sqt(restrictData);
+    SquareTreeNode *polsec = sqt.root ? sqt.root->GetChild("Policies") : nullptr;
+    // if the restriction file is broken, err on the side of full restriction
+    if (!polsec)
+        return;
+
+    static struct {
+        const char *name;
+        int perm;
+    } policies[] = {
+        { "InternetAccess",     Perm_InternetAccess     },
+        { "DiskAccess",         Perm_DiskAccess         },
+        { "SavePreferences",    Perm_SavePreferences    },
+        { "RegistryAccess",     Perm_RegistryAccess     },
+        { "PrinterAccess",      Perm_PrinterAccess      },
+        { "CopySelection",      Perm_CopySelection      },
+        { "FullscreenAccess",   Perm_FullscreenAccess   },
+    };
+
+    // enable policies as indicated in sumatrapdfrestrict.ini
+    for (size_t i = 0; i < dimof(policies); i++) {
+        const char *value = polsec->GetValue(policies[i].name);
+        if (value && atoi(value) != 0)
+            gPolicyRestrictions = gPolicyRestrictions | policies[i].perm;
+    }
+
+    // determine the list of allowed link protocols and perceived file types
+    if ((gPolicyRestrictions & Perm_DiskAccess)) {
+        const char *value;
+        if ((value = polsec->GetValue("LinkProtocols")) != nullptr) {
+            ScopedMem<WCHAR> protocols(str::conv::FromUtf8(value));
+            str::ToLower(protocols);
+            str::TransChars(protocols, L":; ", L",,,");
+            gAllowedLinkProtocols.Split(protocols, L",", true);
+        }
+        if ((value = polsec->GetValue("SafeFileTypes")) != nullptr) {
+            ScopedMem<WCHAR> protocols(str::conv::FromUtf8(value));
+            str::ToLower(protocols);
+            str::TransChars(protocols, L":; ", L",,,");
+            gAllowedFileTypes.Split(protocols, L",", true);
+        }
+    }
+}
+
+void RestrictPolicies(int revokePermission)
+{
+    gPolicyRestrictions = (gPolicyRestrictions | Perm_RestrictedUse) & ~revokePermission;
+}
+
+bool HasPermission(int permission)
+{
+    return (permission & gPolicyRestrictions) == permission;
+}
 
 // lets the shell open a URI for any supported scheme in
 // the appropriate application (web browser, mail client, etc.)
@@ -610,12 +686,6 @@ void ControllerCallbackHandler::RenderThumbnail(DisplayModel *dm, SizeI size, co
     gRenderCache.Render(dm, 1, 0, zoom, pageRect, *callback);
 }
 
-static void SetFileThumbnail(WCHAR *filePath, RenderedBitmap *bmp) {
-    if (bmp)
-        SetThumbnail(gFileHistory.Find(filePath), bmp);
-    free(filePath);
-}
-
 static void CreateThumbnailForFile(WindowInfo& win, DisplayState& ds)
 {
     if (!ShouldSaveThumbnail(ds))
@@ -632,11 +702,12 @@ static void CreateThumbnailForFile(WindowInfo& win, DisplayState& ds)
         return;
     }
 
-    // filePath will be freed in SetFileThumbnail()
     WCHAR *filePath = str::Dup(win.ctrl->FilePath());
     win.ctrl->CreateThumbnail(SizeI(THUMBNAIL_DX, THUMBNAIL_DY),[=] (RenderedBitmap*bmp) {
         uitask::Post([=] {
-            SetFileThumbnail(filePath, bmp);
+            if (bmp)
+                SetThumbnail(gFileHistory.Find(filePath), bmp);
+            free(filePath);
         });
     });
 }

src/SumatraPDF.h
@@ -44,7 +44,7 @@
 
 #define EBOOK_LAYOUT_TIMER_ID       7
 
-// permissions that can be revoked (or explicitly set) through Group Policies
+// permissions that can be revoked through sumatrapdfrestrict.ini or the -restrict command line flag
 enum {
     // enables Update checks, crash report submitting and hyperlinks
     Perm_InternetAccess     = 1 << 0,
@@ -62,7 +62,7 @@ enum {
     Perm_FullscreenAccess   = 1 << 6,
     // enables all of the above
     Perm_All                = 0x0FFFFFF,
-    // set through the command line (Policies might only apply when in restricted use mode)
+    // set if either sumatrapdfrestrict.ini or the -restrict command line flag is present
     Perm_RestrictedUse      = 0x1000000,
 };
 
@@ -102,11 +102,8 @@ struct LabelWithCloseWnd;
 extern bool                     gDebugShowLinks;
 extern bool                     gShowFrameRate;
 extern bool                     gUseGdiRenderer;
-extern int                      gPolicyRestrictions;
-extern WStrVec                  gAllowedLinkProtocols;
-extern WStrVec                  gAllowedFileTypes;
 
-extern WCHAR *                  gPluginURL;
+extern const WCHAR *            gPluginURL;
 extern Vec<WindowInfo*>         gWindows;
 extern Favorites                gFavorites;
 extern FileHistory              gFileHistory;
@@ -120,6 +117,8 @@ extern bool                     gCrashOnOpen;
 
 #define gPluginMode             (gPluginURL != nullptr)
 
+void  InitializedPolicies(bool restrict);
+void  RestrictPolicies(int revokePermission);
 bool  HasPermission(int permission);
 bool  IsUIRightToLeft();
 bool  LaunchBrowser(const WCHAR *url);
@@ -185,3 +184,5 @@ void GetEbookUiColors(COLORREF& text, COLORREF& bg);
 void RebuildMenuBarForWindow(WindowInfo *win);
 void UpdateCheckAsync(WindowInfo *win, bool autoCheck);
 void DeleteWindowInfo(WindowInfo *win);
+
+LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);

src/SumatraStartup.cpp
@@ -67,13 +67,6 @@
 #define ABOUT_BG_GRAY_COLOR     RGB(0xF2, 0xF2, 0xF2)
 
 #define CRASH_DUMP_FILE_NAME         L"sumatrapdfcrash.dmp"
-#define RESTRICTIONS_FILE_NAME       L"sumatrapdfrestrict.ini"
-
-#define DEFAULT_LINK_PROTOCOLS       L"http,https,mailto"
-#define DEFAULT_FILE_PERCEIVED_TYPES L"audio,video"
-
-// in SumatraPDF.cpp
-extern LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 #ifdef DEBUG
 static bool TryLoadMemTrace()
@@ -348,7 +341,7 @@ static bool SetupPluginMode(CommandLineInfo& i)
     // TODO: Perm_DiskAccess is required for saving viewed files and printing and
     //       Perm_InternetAccess is required for crash reports
     // (they can still be disabled through sumatrapdfrestrict.ini or -restrict)
-    gPolicyRestrictions = (gPolicyRestrictions | Perm_RestrictedUse) & ~(Perm_SavePreferences | Perm_FullscreenAccess);
+    RestrictPolicies(Perm_SavePreferences | Perm_FullscreenAccess);
 
     i.reuseDdeInstance = i.exitWhenDone = false;
     gGlobalPrefs->reuseInstance = false;
@@ -456,68 +449,6 @@ Error:
 }
 
 extern void RedirectDllIOToConsole();
-
-static int GetPolicies(bool isRestricted)
-{
-    static struct {
-        const char *name;
-        int perm;
-    } policies[] = {
-        { "InternetAccess", Perm_InternetAccess },
-        { "DiskAccess",     Perm_DiskAccess },
-        { "SavePreferences",Perm_SavePreferences },
-        { "RegistryAccess", Perm_RegistryAccess },
-        { "PrinterAccess",  Perm_PrinterAccess },
-        { "CopySelection",  Perm_CopySelection },
-        { "FullscreenAccess",Perm_FullscreenAccess },
-    };
-
-    gAllowedLinkProtocols.Reset();
-    gAllowedFileTypes.Reset();
-
-    // allow to restrict SumatraPDF's functionality from an INI file in the
-    // same directory as SumatraPDF.exe (cf. ../docs/sumatrapdfrestrict.ini)
-    ScopedMem<WCHAR> restrictPath(path::GetAppPath(RESTRICTIONS_FILE_NAME));
-    if (file::Exists(restrictPath)) {
-        ScopedMem<char> restrictData(file::ReadAll(restrictPath, nullptr));
-        SquareTree sqt(restrictData);
-        SquareTreeNode *polsec = sqt.root ? sqt.root->GetChild("Policies") : nullptr;
-        if (!polsec)
-            return Perm_RestrictedUse;
-
-        int policy = Perm_RestrictedUse;
-        for (size_t i = 0; i < dimof(policies); i++) {
-            const char *value = polsec->GetValue(policies[i].name);
-            if (value && atoi(value) != 0)
-                policy = policy | policies[i].perm;
-        }
-        // determine the list of allowed link protocols and perceived file types
-        if ((policy & Perm_DiskAccess)) {
-            const char *value;
-            if ((value = polsec->GetValue("LinkProtocols")) != nullptr) {
-                ScopedMem<WCHAR> protocols(str::conv::FromUtf8(value));
-                str::ToLower(protocols);
-                str::TransChars(protocols, L":; ", L",,,");
-                gAllowedLinkProtocols.Split(protocols, L",", true);
-            }
-            if ((value = polsec->GetValue("SafeFileTypes")) != nullptr) {
-                ScopedMem<WCHAR> protocols(str::conv::FromUtf8(value));
-                str::ToLower(protocols);
-                str::TransChars(protocols, L":; ", L",,,");
-                gAllowedFileTypes.Split(protocols, L",", true);
-            }
-        }
-
-        return policy;
-    }
-
-    if (isRestricted)
-        return Perm_RestrictedUse;
-
-    gAllowedLinkProtocols.Split(DEFAULT_LINK_PROTOCOLS, L",");
-    gAllowedFileTypes.Split(DEFAULT_FILE_PERCEIVED_TYPES, L",");
-    return Perm_All;
-}
 
 // Registering happens either through the Installer or the Options dialog;
 // here we just make sure that we're still registered
@@ -702,7 +633,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         goto Exit;
     gCrashOnOpen = i.crashOnOpen;
 
-    gPolicyRestrictions = GetPolicies(i.restrictedUse);
+    InitializedPolicies(i.restrictedUse);
     GetFixedPageUiColors(gRenderCache.textColor, gRenderCache.backgroundColor);
     DebugGdiPlusDevice(gUseGdiRenderer);
 
@@ -792,7 +723,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
     if (i.stressTestPath) {
         // don't save file history and preference changes
-        gPolicyRestrictions = (gPolicyRestrictions | Perm_RestrictedUse) & ~Perm_SavePreferences;
+        RestrictPolicies(Perm_SavePreferences);
         RebuildMenuBarForWindow(win);
         StartStressTest(&i, win);
     }

makefile.deps
@@ -516,16 +516,15 @@ $(OS)\SumatraAbout.obj: $B\src\utils\BaseUtil.h $B\src\utils\Dpi.h $B\src\utils\
 $(OS)\SumatraAbout.obj: $B\src\utils\GeomUtil.h $B\src\utils\mingw_compat.h $B\src\utils\Scoped.h
 $(OS)\SumatraAbout.obj: $B\src\utils\StrUtil.h $B\src\utils\Vec.h $B\src\utils\WinUtil.h
 $(OS)\SumatraAbout.obj: $B\src\Version.h $B\src\WindowInfo.h
-$(OS)\SumatraAbout2.obj: $B\src\BaseEngine.h $B\src\mui\Mui.h $B\src\mui\MuiBase.h
-$(OS)\SumatraAbout2.obj: $B\src\mui\MuiButton.h $B\src\mui\MuiControl.h $B\src\mui\MuiCss.h
-$(OS)\SumatraAbout2.obj: $B\src\mui\MuiEventMgr.h $B\src\mui\MuiFromText.h $B\src\mui\MuiGrid.h
-$(OS)\SumatraAbout2.obj: $B\src\mui\MuiHwndWrapper.h $B\src\mui\MuiLayout.h $B\src\mui\MuiPainter.h
-$(OS)\SumatraAbout2.obj: $B\src\mui\MuiScrollBar.h $B\src\mui\TextRender.h $B\src\resource.h
-$(OS)\SumatraAbout2.obj: $B\src\SettingsStructs.h $B\src\SumatraAbout2.h $B\src\SumatraPDF.h
-$(OS)\SumatraAbout2.obj: $B\src\Translations.h $B\src\utils\BaseUtil.h $B\src\utils\GdiPlusUtil.h
-$(OS)\SumatraAbout2.obj: $B\src\utils\GeomUtil.h $B\src\utils\HtmlParserLookup.h $B\src\utils\mingw_compat.h
-$(OS)\SumatraAbout2.obj: $B\src\utils\Scoped.h $B\src\utils\StrUtil.h $B\src\utils\Vec.h
-$(OS)\SumatraAbout2.obj: $B\src\utils\WinUtil.h $B\src\Version.h
+$(OS)\SumatraAbout2.obj: $B\src\mui\Mui.h $B\src\mui\MuiBase.h $B\src\mui\MuiButton.h
+$(OS)\SumatraAbout2.obj: $B\src\mui\MuiControl.h $B\src\mui\MuiCss.h $B\src\mui\MuiEventMgr.h
+$(OS)\SumatraAbout2.obj: $B\src\mui\MuiFromText.h $B\src\mui\MuiGrid.h $B\src\mui\MuiHwndWrapper.h
+$(OS)\SumatraAbout2.obj: $B\src\mui\MuiLayout.h $B\src\mui\MuiPainter.h $B\src\mui\MuiScrollBar.h
+$(OS)\SumatraAbout2.obj: $B\src\mui\TextRender.h $B\src\resource.h $B\src\SumatraAbout2.h
+$(OS)\SumatraAbout2.obj: $B\src\SumatraPDF.h $B\src\Translations.h $B\src\utils\BaseUtil.h
+$(OS)\SumatraAbout2.obj: $B\src\utils\GdiPlusUtil.h $B\src\utils\GeomUtil.h $B\src\utils\HtmlParserLookup.h
+$(OS)\SumatraAbout2.obj: $B\src\utils\mingw_compat.h $B\src\utils\Scoped.h $B\src\utils\StrUtil.h
+$(OS)\SumatraAbout2.obj: $B\src\utils\Vec.h $B\src\utils\WinUtil.h $B\src\Version.h
 $(OS)\SumatraDialogs.obj: $B\src\AppTools.h $B\src\GlobalPrefs.h $B\src\resource.h
 $(OS)\SumatraDialogs.obj: $B\src\SettingsStructs.h $B\src\SumatraDialogs.h $B\src\SumatraPDF.h
 $(OS)\SumatraDialogs.obj: $B\src\Translations.h $B\src\utils\BaseUtil.h $B\src\utils\GeomUtil.h

src/SumatraAbout2.cpp
@@ -1,18 +1,18 @@
 /* Copyright 2014 the SumatraPDF project authors (see AUTHORS file).
    License: GPLv3 */
 
+// utils
 #include "BaseUtil.h"
+#include "GdiPlusUtil.h"
 #include "HtmlParserLookup.h"
 #include "Mui.h"
-#include "BaseEngine.h"
-#include "SettingsStructs.h"
-#include "SumatraAbout2.h"
-#include "GdiPlusUtil.h"
-#include "resource.h"
+#include "WinUtil.h"
+// ui
 #include "SumatraPDF.h"
+#include "resource.h"
+#include "SumatraAbout2.h"
 #include "Translations.h"
 #include "Version.h"
-#include "WinUtil.h"
 
 using namespace mui;
 

commit 29b293b41c965080f74bb63e8520424cae8cd29d

unbreak -register-for-pdf (regression from commit 03941ec8cb665dadfdb91e0ca01b8c1c64f3a472)

scripts/gen_settingsstructs.py
@@ -504,7 +504,7 @@ GlobalPrefs = [
 		"value of DefaultZoom for internal usage",
 		internal=True),
 	EmptyLine(),
-	Comment("Settings after this line have not been recognised by the current version"),
+	Comment("Settings after this line have not been recognized by the current version"),
 ]
 
 GlobalPrefs = Struct("GlobalPrefs", GlobalPrefs,

src/SettingsStructs.h
@@ -589,7 +589,7 @@ static const FieldInfo gGlobalPrefsFields[] = {
     { offsetof(GlobalPrefs, timeOfLastUpdateCheck),    Type_Compact,     (intptr_t)&gFILETIMEInfo                                                                                              },
     { offsetof(GlobalPrefs, openCountWeek),            Type_Int,         0                                                                                                                     },
     { (size_t)-1,                                      Type_Comment,     0                                                                                                                     },
-    { (size_t)-1,                                      Type_Comment,     (intptr_t)"Settings after this line have not been recognised by the current version"                                  },
+    { (size_t)-1,                                      Type_Comment,     (intptr_t)"Settings after this line have not been recognized by the current version"                                  },
 };
 static const StructInfo gGlobalPrefsInfo = { sizeof(GlobalPrefs), 53, gGlobalPrefsFields, "\0\0MainWindowBackground\0EscToExit\0ReuseInstance\0UseSysColors\0\0FixedPageUI\0EbookUI\0ComicBookUI\0ChmUI\0ExternalViewers\0PrereleaseSettings\0ShowMenubar\0ReloadModifiedDocuments\0FullPathInTitle\0ZoomLevels\0ZoomIncrement\0\0PrinterDefaults\0ForwardSearch\0AnnotationDefaults\0DefaultPasswords\0CustomScreenDPI\0\0RememberStatePerDocument\0UiLanguage\0ShowToolbar\0ShowFavorites\0AssociatedExtensions\0AssociateSilently\0CheckForUpdates\0VersionToSkip\0RememberOpenedFiles\0InverseSearchCmdLine\0EnableTeXEnhancements\0DefaultDisplayMode\0DefaultZoom\0WindowState\0WindowPos\0ShowToc\0SidebarDx\0TocDy\0ShowStartPage\0UseTabs\0\0FileStates\0WindowTabsInfo\0ReopenOnce\0TimeOfLastUpdateCheck\0OpenCountWeek\0\0" };
 

src/SumatraPDF.cpp
@@ -178,7 +178,7 @@ void SetCurrentLang(const char *langCode)
 #define DEFAULT_LINK_PROTOCOLS       L"http,https,mailto"
 #define DEFAULT_FILE_PERCEIVED_TYPES L"audio,video,webpage"
 
-void InitializedPolicies(bool restrict)
+void InitializePolicies(bool restrict)
 {
     // default configuration should be to restrict everything
     CrashIf(gPolicyRestrictions != Perm_RestrictedUse);

src/SumatraPDF.h
@@ -117,7 +117,7 @@ extern bool                     gCrashOnOpen;
 
 #define gPluginMode             (gPluginURL != nullptr)
 
-void  InitializedPolicies(bool restrict);
+void  InitializePolicies(bool restrict);
 void  RestrictPolicies(int revokePermission);
 bool  HasPermission(int permission);
 bool  IsUIRightToLeft();

src/SumatraStartup.cpp
@@ -608,6 +608,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     CommandLineInfo i(GetCommandLine());
 
     SetCurrentLang(i.lang ? i.lang : gGlobalPrefs->uiLanguage);
+    InitializePolicies(i.restrictedUse);
 
     // This allows ad-hoc comparison of gdi, gdi+ and gdi+ quick when used
     // in layout
@@ -633,7 +634,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         goto Exit;
     gCrashOnOpen = i.crashOnOpen;
 
-    InitializedPolicies(i.restrictedUse);
     GetFixedPageUiColors(gRenderCache.textColor, gRenderCache.backgroundColor);
     DebugGdiPlusDevice(gUseGdiRenderer);
 