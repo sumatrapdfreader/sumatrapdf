9156dd758a9ddcec08c86ab0b7af19278f368827
Simon Bünzli <zeniko@gmail.com>
2014-12-20 10:21:51 +0100

    pdfextract: prevent warning

--- mupdf/source/tools/pdfextract.c
+++ mupdf/source/tools/pdfextract.c
@@ -219,7 +219,8 @@ int pdfextract_main(int argc, char **argv)
 	if (fz_optind == argc)
 	{
 		int len = pdf_count_objects(doc);
-		for (o = 0; o < len; o++)
+		/* SumatraPDF: object number 0 is always free */
+		for (o = 1; o < len; o++)
 			showobject(o);
 	}
 	else
