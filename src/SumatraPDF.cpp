sumatrapdfreader/sumatrapdf

commit d3f63900b4221069382793348b916bc6b7e547e3

make SetCurrentLanguageAndRefreshUI apply all changes to a window before changing the next (and related changes)

src/SumatraPDF.cpp
@@ -548,7 +548,8 @@ static void UpdateWindowRtlLayout(WindowInfo *win)
         return;
 
     bool tocVisible = win->tocVisible;
-    if (tocVisible || gGlobalPrefs->showFavorites)
+    bool favVisible = gGlobalPrefs->showFavorites;
+    if (tocVisible || favVisible)
         SetSidebarVisibility(win, false, false);
 
     // cf. http://www.microsoft.com/middleeast/msdn/mirror.aspx
@@ -584,19 +585,12 @@ static void UpdateWindowRtlLayout(WindowInfo *win)
 
     // ensure that the ToC sidebar is on the correct side and that its
     // title and close button are also correctly laid out
-    if (tocVisible || gGlobalPrefs->showFavorites) {
-        SetSidebarVisibility(win, tocVisible, gGlobalPrefs->showFavorites);
+    if (tocVisible || favVisible) {
+        SetSidebarVisibility(win, tocVisible, favVisible);
         if (tocVisible)
             SendMessage(win->hwndTocBox, WM_SIZE, 0, 0);
-        if (gGlobalPrefs->showFavorites)
+        if (favVisible)
             SendMessage(win->hwndFavBox, WM_SIZE, 0, 0);
-    }
-}
-
-void UpdateRtlLayoutForAllWindows()
-{
-    for (size_t i = 0; i < gWindows.Count(); i++) {
-        UpdateWindowRtlLayout(gWindows.At(i));
     }
 }
 
@@ -607,13 +601,6 @@ void RebuildMenuBarForWindow(WindowInfo *win)
     if (!win->presentation && !win->isFullScreen && !win->isMenuHidden)
         SetMenu(win->hwndFrame, win->menu);
     DestroyMenu(oldMenu);
-}
-
-static void RebuildMenuBarForAllWindows()
-{
-    for (size_t i = 0; i < gWindows.Count(); i++) {
-        RebuildMenuBarForWindow(gWindows.At(i));
-    }
 }
 
 static bool ShouldSaveThumbnail(DisplayState& ds)
@@ -1008,7 +995,7 @@ static bool LoadDocIntoCurrentTab(LoadArgs& args, PasswordUI *pwdUI, DisplayStat
     // menu for chm and ebook docs is different, so we have to re-create it
     RebuildMenuBarForWindow(win);
     // the toolbar isn't supported for ebook docs (yet)
-    ShowOrHideToolbarForWindow(win);
+    ShowOrHideToolbar(win);
     ToolbarUpdateStateForWindow(win, true);
 
     if (!args.isNewWindow && win->IsDocLoaded()) {
@@ -1272,6 +1259,16 @@ static WindowInfo* CreateWindowInfo()
     return win;
 }
 
+static void UpdateToolbarSidebarText(WindowInfo *win)
+{
+    UpdateToolbarPageText(win, -1);
+    UpdateToolbarFindText(win);
+    UpdateToolbarButtonsToolTipsForWindow(win);
+
+    SetLabel(win->tocLabelWithClose, _TR("Bookmarks"));
+    SetLabel(win->favLabelWithClose, _TR("Favorites"));
+}
+
 WindowInfo *CreateAndShowWindowInfo()
 {
     // CreateWindowInfo shouldn't change the windowState value
@@ -1280,6 +1277,9 @@ WindowInfo *CreateAndShowWindowInfo()
     if (!win)
         return nullptr;
     CrashIf(windowState != gGlobalPrefs->windowState);
+
+    // needed for RTL languages
+    UpdateToolbarSidebarText(win);
 
     if (WIN_STATE_FULLSCREEN == windowState || WIN_STATE_MAXIMIZED == windowState)
         ShowWindow(win->hwndFrame, SW_MAXIMIZE);
@@ -1573,7 +1573,7 @@ void LoadModelIntoTab(WindowInfo *win, TabInfo *tdata)
     if (!win->AsFixed())
         ShowScrollBar(win->hwndCanvas, SB_BOTH, FALSE);
     // the toolbar isn't supported for ebook docs (yet)
-    ShowOrHideToolbarForWindow(win);
+    ShowOrHideToolbar(win);
     UpdateCurrentTabBgColor(win);
 
     int pageCount = win->ctrl ? win->ctrl->PageCount() : 0;
@@ -2007,8 +2007,7 @@ void GetEbookUiColors(COLORREF& text, COLORREF& bg)
 void UpdateDocumentColors()
 {
     // TODO: only do this if colors have actually changed?
-    for (size_t i = 0; i < gWindows.Count(); i++) {
-        WindowInfo *win = gWindows.At(i);
+    for (WindowInfo *win : gWindows) {
         if (win->AsEbook()) {
             win->AsEbook()->UpdateDocumentColors();
             UpdateTocColors(win);
@@ -2100,7 +2099,7 @@ static void CloseDocumentInTab(WindowInfo *win, bool keepUIEnabled, bool deleteM
         if (wasntFixed) {
             // restore the full menu and toolbar
             RebuildMenuBarForWindow(win);
-            ShowOrHideToolbarForWindow(win);
+            ShowOrHideToolbar(win);
         }
         ShowScrollBar(win->hwndCanvas, SB_BOTH, FALSE);
         win->RedrawAll();
@@ -2865,32 +2864,41 @@ static void FrameOnSize(WindowInfo* win, int dx, int dy)
     }
 }
 
-void SetCurrentLanguageAndRefreshUi(const char *langCode)
+void RelayoutWindow(WindowInfo *win)
+{
+    RelayoutFrame(win);
+}
+
+void SetCurrentLanguageAndRefreshUI(const char *langCode)
 {
     if (!langCode || str::Eq(langCode, trans::GetCurrentLangCode()))
         return;
     SetCurrentLang(langCode);
-    UpdateRtlLayoutForAllWindows();
-    RebuildMenuBarForAllWindows();
-    UpdateUITextForLanguage();
-    if (gWindows.Count() > 0 && gWindows.At(0)->IsAboutWindow())
-        gWindows.At(0)->RedrawAll(true);
+
+    for (WindowInfo *win : gWindows) {
+        UpdateWindowRtlLayout(win);
+        RebuildMenuBarForWindow(win);
+        UpdateToolbarSidebarText(win);
+        // About page text is translated during (re)drawing
+        if (win->IsAboutWindow())
+            win->RedrawAll(true);
+    }
+
     prefs::Save();
 }
 
 static void OnMenuChangeLanguage(HWND hwnd)
 {
     const char *newLangCode = Dialog_ChangeLanguge(hwnd, trans::GetCurrentLangCode());
-    SetCurrentLanguageAndRefreshUi(newLangCode);
+    SetCurrentLanguageAndRefreshUI(newLangCode);
 }
 
 static void OnMenuViewShowHideToolbar(WindowInfo *win)
 {
-    if (win->presentation || win->isFullScreen)
-        return;
-
     gGlobalPrefs->showToolbar = !gGlobalPrefs->showToolbar;
-    ShowOrHideToolbarGlobally();
+    for (WindowInfo *win : gWindows) {
+        ShowOrHideToolbar(win);
+    }
 }
 
 static void OnMenuAdvancedOptions()
@@ -3518,24 +3526,6 @@ static bool FrameOnSysChar(WindowInfo& win, WPARAM key)
     }
 
     return false;
-}
-
-static void UpdateSidebarTitles(WindowInfo* win)
-{
-    SetLabel(win->tocLabelWithClose, _TR("Bookmarks"));
-    SetLabel(win->favLabelWithClose, _TR("Favorites"));
-}
-
-void UpdateUITextForLanguage()
-{
-    for (size_t i = 0; i < gWindows.Count(); i++) {
-        WindowInfo *win = gWindows.At(i);
-        UpdateToolbarPageText(win, -1);
-        UpdateToolbarFindText(win);
-        UpdateToolbarButtonsToolTipsForWindow(win);
-        // also update the sidebar title at this point
-        UpdateSidebarTitles(win);
-    }
 }
 
 static bool SidebarSplitterCb(void *ctx, bool done)

makefile.deps
@@ -145,10 +145,11 @@ $(OMUI)\TextRender.obj: $B\src\utils\WinUtil.h
 $(OS)\AppPrefs.obj: $B\src\AppPrefs.h $B\src\AppTools.h $B\src\BaseEngine.h
 $(OS)\AppPrefs.obj: $B\src\EbookEngine.h $B\src\Favorites.h $B\src\FileHistory.h
 $(OS)\AppPrefs.obj: $B\src\GlobalPrefs.h $B\src\SettingsStructs.h $B\src\SumatraPDF.h
-$(OS)\AppPrefs.obj: $B\src\Translations.h $B\src\utils\BaseUtil.h $B\src\utils\FileTransactions.h
-$(OS)\AppPrefs.obj: $B\src\utils\FileUtil.h $B\src\utils\FileWatcher.h $B\src\utils\GeomUtil.h
-$(OS)\AppPrefs.obj: $B\src\utils\mingw_compat.h $B\src\utils\Scoped.h $B\src\utils\StrUtil.h
-$(OS)\AppPrefs.obj: $B\src\utils\UITask.h $B\src\utils\Vec.h $B\src\WindowInfo.h
+$(OS)\AppPrefs.obj: $B\src\Toolbar.h $B\src\Translations.h $B\src\utils\BaseUtil.h
+$(OS)\AppPrefs.obj: $B\src\utils\FileTransactions.h $B\src\utils\FileUtil.h $B\src\utils\FileWatcher.h
+$(OS)\AppPrefs.obj: $B\src\utils\GeomUtil.h $B\src\utils\mingw_compat.h $B\src\utils\Scoped.h
+$(OS)\AppPrefs.obj: $B\src\utils\StrUtil.h $B\src\utils\UITask.h $B\src\utils\Vec.h
+$(OS)\AppPrefs.obj: $B\src\WindowInfo.h
 $(OS)\AppTools.obj: $B\src\AppTools.h $B\src\Translations.h $B\src\utils\BaseUtil.h
 $(OS)\AppTools.obj: $B\src\utils\CmdLineParser.h $B\src\utils\DbgHelpDyn.h $B\src\utils\FileUtil.h
 $(OS)\AppTools.obj: $B\src\utils\GeomUtil.h $B\src\utils\mingw_compat.h $B\src\utils\Scoped.h

src/AppPrefs.cpp
@@ -20,6 +20,7 @@
 #include "AppPrefs.h"
 #include "AppTools.h"
 #include "Favorites.h"
+#include "Toolbar.h"
 #include "Translations.h"
 
 #define PREFS_FILE_NAME     L"SumatraPDF-settings.txt"
@@ -202,13 +203,15 @@ bool Reload()
     }
 
     if (!str::Eq(uiLanguage, gGlobalPrefs->uiLanguage))
-        SetCurrentLanguageAndRefreshUi(gGlobalPrefs->uiLanguage);
+        SetCurrentLanguageAndRefreshUI(gGlobalPrefs->uiLanguage);
 
-    if (gGlobalPrefs->showToolbar != showToolbar)
-        ShowOrHideToolbarGlobally();
+    for (WindowInfo *win : gWindows) {
+        if (gGlobalPrefs->showToolbar != showToolbar)
+            ShowOrHideToolbar(win);
+        UpdateFavoritesTree(win);
+    }
 
     UpdateDocumentColors();
-    UpdateFavoritesTreeForAllWindows();
 
     return true;
 }

src/Favorites.cpp
@@ -486,30 +486,27 @@ void PopulateFavTreeIfNeeded(WindowInfo *win)
     RedrawWindow(hwndTree, nullptr, nullptr, fl);
 }
 
-static void UpdateFavoritesTreeIfNecessary(WindowInfo *win)
+void UpdateFavoritesTree(WindowInfo *win)
 {
     HWND hwndTree = win->hwndFavTree;
-    if (0 == TreeView_GetCount(hwndTree))
-        return;
 
-    SendMessage(hwndTree, WM_SETREDRAW, FALSE, 0);
-    TreeView_DeleteAllItems(hwndTree);
-    PopulateFavTreeIfNeeded(win);
+    if (TreeView_GetCount(hwndTree) > 0) {
+        // PopulateFavTreeIfNeeded will re-enable WM_SETREDRAW
+        SendMessage(hwndTree, WM_SETREDRAW, FALSE, 0);
+        TreeView_DeleteAllItems(hwndTree);
+        PopulateFavTreeIfNeeded(win);
+    }
+
+    // hide the favorites tree if we've removed the last favorite
+    if (0 == TreeView_GetCount(hwndTree)) {
+        SetSidebarVisibility(win, win->tocVisible, false);
+    }
 }
 
 void UpdateFavoritesTreeForAllWindows()
 {
-    for (size_t i = 0; i < gWindows.Count(); i++) {
-        UpdateFavoritesTreeIfNecessary(gWindows.At(i));
-    }
-
-    // hide the favorites tree if we removed the last favorite
-    if (!HasFavorites()) {
-        gGlobalPrefs->showFavorites = false;
-        for (size_t i = 0; i < gWindows.Count(); i++) {
-            WindowInfo *win = gWindows.At(i);
-            SetSidebarVisibility(win, win->tocVisible, gGlobalPrefs->showFavorites);
-        }
+    for (WindowInfo *win : gWindows) {
+        UpdateFavoritesTree(win);
     }
 }
 

src/Favorites.h
@@ -44,4 +44,5 @@ void ToggleFavorites(WindowInfo *win);
 void PopulateFavTreeIfNeeded(WindowInfo *win);
 void RememberFavTreeExpansionStateForAllWindows();
 void GoToFavoriteByMenuId(WindowInfo *win, int wmId);
+void UpdateFavoritesTree(WindowInfo *win);
 void UpdateFavoritesTreeForAllWindows();

src/Search.cpp
@@ -200,7 +200,7 @@ struct FindThreadData : public ProgressUpdateUI {
 
     FindThreadData(WindowInfo *win, TextSearchDirection direction, HWND findBox) :
         win(win), direction(direction), text(win::GetText(findBox)),
-        wasModified(Edit_GetModify(findBox)), wnd(nullptr) { }
+        wasModified(Edit_GetModify(findBox)), wnd(nullptr), thread(nullptr) { }
     ~FindThreadData() { CloseHandle(thread); }
 
     void ShowUI(bool showProgress) {

src/SumatraPDF.h
@@ -132,14 +132,13 @@ void  RememberFavTreeExpansionState(WindowInfo *win);
 void  LayoutTreeContainer(LabelWithCloseWnd *l, HWND hwndTree);
 void  AdvanceFocus(WindowInfo* win);
 bool  WindowInfoStillValid(WindowInfo *win);
-void  SetCurrentLanguageAndRefreshUi(const char *langCode);
-void  ShowOrHideToolbarGlobally();
-void  ShowOrHideToolbarForWindow(WindowInfo *win);
+void  SetCurrentLanguageAndRefreshUI(const char *langCode);
 void  UpdateDocumentColors();
 void  UpdateTabFileDisplayStateForWin(WindowInfo *win, TabInfo *td);
 bool  FrameOnKeydown(WindowInfo* win, WPARAM key, LPARAM lparam, bool inTextfield=false);
 void  ReloadDocument(WindowInfo *win, bool autorefresh=false);
 void  OnMenuViewFullscreen(WindowInfo* win, bool presentation=false);
+void  RelayoutWindow(WindowInfo *win);
 
 COLORREF GetLogoBgColor();
 COLORREF GetAboutBgColor();
@@ -178,7 +177,6 @@ bool DocumentPathExists(const WCHAR *path);
 void EnterFullScreen(WindowInfo* win, bool presentation=false);
 void ExitFullScreen(WindowInfo* win);
 void SetCurrentLang(const char *langCode);
-void UpdateUITextForLanguage();
 void GetFixedPageUiColors(COLORREF& text, COLORREF& bg);
 void GetEbookUiColors(COLORREF& text, COLORREF& bg);
 void RebuildMenuBarForWindow(WindowInfo *win);

src/SumatraStartup.cpp
@@ -714,8 +714,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
             goto Exit;
     }
 
-    UpdateUITextForLanguage(); // needed for RTL languages
-
     // Make sure that we're still registered as default,
     // if the user has explicitly told us to be
     if (gGlobalPrefs->associatedExtensions)

commit c2d186c87704d0fa6f285e2a04620187dca584d9

fix empty sidebar title (regression from commit d3f63900b4221069382793348b916bc6b7e547e3)

src/SumatraPDF.cpp
@@ -1220,6 +1220,16 @@ static void CreateSidebar(WindowInfo* win)
    }
 }
 
+static void UpdateToolbarSidebarText(WindowInfo *win)
+{
+    UpdateToolbarPageText(win, -1);
+    UpdateToolbarFindText(win);
+    UpdateToolbarButtonsToolTipsForWindow(win);
+
+    SetLabel(win->tocLabelWithClose, _TR("Bookmarks"));
+    SetLabel(win->favLabelWithClose, _TR("Favorites"));
+}
+
 static WindowInfo* CreateWindowInfo()
 {
     RectI windowPos = gGlobalPrefs->windowPos;
@@ -1285,7 +1295,9 @@ static WindowInfo* CreateWindowInfo()
         DragAcceptFiles(win->hwndCanvas, TRUE);
 
     gWindows.Append(win);
+    // needed for RTL languages
     UpdateWindowRtlLayout(win);
+    UpdateToolbarSidebarText(win);
 
     if (Touch::SupportsGestures()) {
         GESTURECONFIG gc = { 0, GC_ALLGESTURES, 0 };
@@ -1297,16 +1309,6 @@ static WindowInfo* CreateWindowInfo()
     return win;
 }
 
-static void UpdateToolbarSidebarText(WindowInfo *win)
-{
-    UpdateToolbarPageText(win, -1);
-    UpdateToolbarFindText(win);
-    UpdateToolbarButtonsToolTipsForWindow(win);
-
-    SetLabel(win->tocLabelWithClose, _TR("Bookmarks"));
-    SetLabel(win->favLabelWithClose, _TR("Favorites"));
-}
-
 WindowInfo *CreateAndShowWindowInfo()
 {
     // CreateWindowInfo shouldn't change the windowState value
@@ -1315,9 +1317,6 @@ WindowInfo *CreateAndShowWindowInfo()
     if (!win)
         return nullptr;
     CrashIf(windowState != gGlobalPrefs->windowState);
-
-    // needed for RTL languages
-    UpdateToolbarSidebarText(win);
 
     if (WIN_STATE_FULLSCREEN == windowState || WIN_STATE_MAXIMIZED == windowState)
         ShowWindow(win->hwndFrame, SW_MAXIMIZE);