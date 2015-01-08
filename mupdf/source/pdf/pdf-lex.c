c9a51a930983cd9cd9da1201345fc1ae95553bde
Simon Bünzli <zeniko@gmail.com>
2015-01-08 21:38:34 +0100

    PDF: improve handling of octal escapes (fixes issue #66)

--- mupdf/source/pdf/pdf-lex.c
+++ mupdf/source/pdf/pdf-lex.c
@@ -296,14 +296,15 @@ lex_string(fz_stream *f, pdf_lexbuf *lb)
 			case '\\':
 				*s++ = '\\';
 				break;
-			case RANGE_0_9:
+			/* cf. https://github.com/sumatrapdfreader/sumatrapdf/issues/66 */
+			case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7':
 				oct = c - '0';
 				c = fz_read_byte(f);
-				if (c >= '0' && c <= '9')
+				if (c >= '0' && c <= '7')
 				{
 					oct = oct * 8 + (c - '0');
 					c = fz_read_byte(f);
-					if (c >= '0' && c <= '9')
+					if (c >= '0' && c <= '7')
 						oct = oct * 8 + (c - '0');
 					else if (c != EOF)
 						fz_unread_byte(f);
