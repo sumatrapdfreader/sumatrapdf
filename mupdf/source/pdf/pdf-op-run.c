7dca2c0eebb71b766419855274fb6a80b6a9b5dd
Simon Bünzli <zeniko@gmail.com>
2015-01-18 16:31:25 +0100

    PDF: don't omit patterns with huge xstep/ystep values

--- mupdf/source/pdf/pdf-op-run.c
+++ mupdf/source/pdf/pdf-op-run.c
@@ -765,6 +765,11 @@ pdf_show_pattern(pdf_csi *csi, pdf_run_state *pr, pdf_pattern *pat, pdf_gstate *
 			y0 = floorf(fy0 + 0.001);
 			x1 = ceilf(fx1 - 0.001);
 			y1 = ceilf(fy1 - 0.001);
+			/* SumatraPDF: make sure that patterns with extreme step values are still rendered */
+			if (fx1 > fx0 && x1 == x0)
+				x1 = x0 + 1;
+			if (fy1 > fy0 && y1 == y0)
+				y1 = y0 + 1;
 
 			for (y = y0; y < y1; y++)
 			{
