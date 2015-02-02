ea3081b8e63ad2ef71cc4ef8d1b391dbfcedbc0a
Simon Bünzli <zeniko@gmail.com>
2015-02-02 17:56:34 +0100

    try to make appveyor build a 64-bit installer

--- scripts/appveyor-build.bat
+++ scripts/appveyor-build.bat
@@ -4,12 +4,18 @@ SETLOCAL
 REM assumes we're being run from top-level directory as:
 REM scripts\appveyor-build.bat
 
+REM temporarily build 64-bit releases
+GOTO build64
+
 CALL scripts\vc.bat
 IF ERRORLEVEL 1 EXIT /B 1
-
-REM add our nasm.exe to the path
-SET PATH=%CD%\bin;%PATH%
-
 nmake -f makefile.msvc CFG=rel PLATFORM=X86 SumatraPDF Uninstaller
+
+EXIT /B
+
+:build64
+CALL "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+IF ERRORLEVEL 1 EXIT /B 1
+nmake -f makefile.msvc CFG=rel PLATFORM=X64 all_sumatrapdf
 
 @rem TODO: build with more platforms, build and run tests etc.
