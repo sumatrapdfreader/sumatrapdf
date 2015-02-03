5d868c5dca90d3126c6a841ed20f99f4346b9c68
Simon Bünzli <zeniko@gmail.com>
2015-02-03 09:02:42 +0100

    run unit tests from appveyor-build.bat

--- scripts/appveyor-build.bat
+++ scripts/appveyor-build.bat
@@ -5,17 +5,25 @@ REM assumes we're being run from top-level directory as:
 REM scripts\appveyor-build.bat
 
 REM temporarily build 64-bit releases
-GOTO build64
+GOTO Build64
 
 CALL scripts\vc.bat
 IF ERRORLEVEL 1 EXIT /B 1
 nmake -f makefile.msvc CFG=rel PLATFORM=X86 SumatraPDF Installer AllSymbols
+IF ERRORLEVEL 1 EXIT /B 1
 
-EXIT /B
+GOTO RunTests
 
-:build64
+:Build64
 CALL "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 IF ERRORLEVEL 1 EXIT /B 1
 nmake -f makefile.msvc CFG=rel PLATFORM=X64 EXTCFLAGS="/D VER_QUALIFIER=x64" STRICT_X64=1 SumatraPDF Installer AllSymbols
+IF ERRORLEVEL 1 EXIT /B 1
+
+GOTO RunTests
+
+:RunTests
+devenv /NOLOGO vs\all_tests.sln /build Release && obj-rel\test_util.exe
+IF ERRORLEVEL 1 EXIT /B 1
 
 @rem TODO: build with more platforms, build and run tests etc.
