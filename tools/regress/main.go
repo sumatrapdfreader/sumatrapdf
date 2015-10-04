7633619fee1334425b0802492bbce4fe0f23cfa3
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-10-04 13:59:10 -0700

    finish regress tool

--- tools/regress/main.go
+++ tools/regress/main.go
@@ -22,6 +22,7 @@ for simplicity we'll probably just add more functionality to SumatraPDF.exe.
 Test succeeds if the output is same as expected.
 */
 
+// Test describes a single test
 type Test struct {
 	// values from parsing test file
 	CmdUnparsed    string
@@ -30,26 +31,49 @@ type Test struct {
 	ExpectedOutput string
 
 	// computed values
-	CmdPath  string
+	CmdName  string // e.g. SumatraPDF.exe
+	CmdPath  string // e.g. rel64\SumatraPDF.exe
 	CmdArgs  []string
 	FilePath string
 	Error    error
 	Output   string
 }
 
+// TestFile describes as test file
 type TestFile struct {
 	Path    string
 	Sha1Hex string
 }
 
 var (
-	inFatal     bool
-	failedTests []*Test
-	testFiles   map[string]*TestFile
+	inFatal         bool
+	testFilesBySha1 map[string]*TestFile
 )
 
 func init() {
-	testFiles = make(map[string]*TestFile)
+	testFilesBySha1 = make(map[string]*TestFile)
+}
+
+func errStr(err error) string {
+	if err == nil {
+		return ""
+	}
+	return err.Error()
+}
+
+func dumpTest(t *Test) {
+	fmt.Printf(`CmdUnparsed: '%s'
+FileSha1Hex: %s
+FileURL: '%s'
+ExpectedOutput: '%s'
+CmdName: '%s'
+CmdPath: '%s'
+CmdArgs: %v
+FilePath: '%s'
+Error: '%s'
+Output: '%s'
+
+`, t.CmdUnparsed, t.FileSha1Hex, t.FileURL, t.ExpectedOutput, t.CmdName, t.CmdPath, t.CmdArgs, t.FilePath, errStr(t.Error), t.Output)
 }
 
 func printStack() {
@@ -157,8 +181,10 @@ func parseTest(lines []string) (*Test, []string) {
 	fatalif(t.FileSha1Hex == "", "Sha1: field missing")
 	fatalif(t.CmdUnparsed == "", "Cmd: field missing")
 	fatalif(t.ExpectedOutput == "", "Out: field missing")
-	// TODO: parse t.CmdUnparsed int t.CmdPath and t.CmdArgs
-	// TODO: replace $file in t.ExpectedOutput with t.File
+
+	parts := strings.Split(t.CmdUnparsed, " ")
+	t.CmdName = parts[0]
+	t.CmdArgs = parts[1:]
 	return t, lines
 }
 
@@ -191,8 +217,10 @@ func cmdToStrLong(cmd *exec.Cmd) string {
 }
 
 func isOutputEqual(s1, s2 string) bool {
-	// TODO: normalize whitespace
-	return s1 != s2
+	// TODO: normalize newlines
+	s1 = strings.TrimSpace(s1)
+	s2 = strings.TrimSpace(s2)
+	return s1 == s2
 }
 
 func runTest(t *Test) {
@@ -204,23 +232,32 @@ func runTest(t *Test) {
 	cmd := exec.Command(t.CmdPath, t.CmdArgs...)
 	fmt.Printf("Running: %s\n", cmdToStrLong(cmd))
 	res, err := cmd.Output()
-	t.Output = string(res)
+	t.Output = strings.TrimSpace(string(res))
 	if err != nil {
 		t.Error = err
-		failedTests = append(failedTests, t)
+		fmt.Printf("Failed test:\n")
+		dumpTest(t)
 		return
 	}
 	if !isOutputEqual(t.Output, t.ExpectedOutput) {
-		failedTests = append(failedTests, t)
+		fmt.Printf("Failed test:\n")
+		dumpTest(t)
 		return
 	}
 	fmt.Printf("test passed, output: %s\n", res)
 }
 
+func isFailedTest(t *Test) bool {
+	if t.Error != nil {
+		return true
+	}
+	return !isOutputEqual(t.Output, t.ExpectedOutput)
+}
+
 func dumpFailedTest(t *Test) {
-	// TODO: write me
 	args := strings.Join(t.CmdArgs, " ")
 	fmt.Printf("Test %s %s failed\n", t.CmdPath, args)
+	dumpTest(t)
 	if t.Error != nil {
 		fmt.Printf("Reason: process exited with error '%s'\n", t.Error)
 		return
@@ -238,47 +275,47 @@ expected:
 `, t.Output, t.ExpectedOutput)
 		return
 	}
-	fmt.Printf("Internal rror: unknown reason\n")
+	fmt.Printf("Internal error: unknown reason\n")
 }
 
-func dumpFailedTests() int {
-	if len(failedTests) == 0 {
+func dumpFailedTests(tests []*Test) int {
+	nFailed := 0
+	for _, test := range tests {
+		if !isFailedTest(test) {
+			continue
+		}
+		nFailed++
+		dumpFailedTest(test)
+	}
+	if nFailed == 0 {
 		fmt.Printf("All tests passed!\n")
-		return 0
+	} else {
+		fmt.Printf("Failed %d out of %d tests\n", nFailed, len(tests))
 	}
-	for _, t := range failedTests {
-		dumpFailedTest(t)
-	}
-	return len(failedTests)
+	return nFailed
 }
 
-func Sha1OfBytes(data []byte) []byte {
+func sha1OfBytes(data []byte) []byte {
 	res := sha1.Sum(data)
 	return res[:]
 }
 
-func Sha1HexOfBytes(data []byte) string {
-	return fmt.Sprintf("%x", Sha1OfBytes(data))
+func sha1HexOfBytes(data []byte) string {
+	return fmt.Sprintf("%x", sha1OfBytes(data))
 }
 
-func Sha1OfFile(path string) ([]byte, error) {
+func sha1OfFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
-	if err != nil {
-		//fmt.Printf("os.Open(%s) failed with %s\n", path, err.Error())
-		return nil, err
-	}
+	fataliferr(err)
 	defer f.Close()
 	h := sha1.New()
 	_, err = io.Copy(h, f)
-	if err != nil {
-		//fmt.Printf("io.Copy() failed with %s\n", err.Error())
-		return nil, err
-	}
+	fataliferr(err)
 	return h.Sum(nil), nil
 }
 
-func Sha1HexOfFile(path string) (string, error) {
-	sha1, err := Sha1OfFile(path)
+func sha1HexOfFile(path string) (string, error) {
+	sha1, err := sha1OfFile(path)
 	if err != nil {
 		return "", err
 	}
@@ -295,7 +332,7 @@ func httpDlMust(uri string) []byte {
 }
 
 func testFileExists(sha1Hex string) bool {
-	return nil != testFiles[sha1Hex]
+	return nil != testFilesBySha1[sha1Hex]
 }
 
 func dlIfNotExistsMust(uri, sha1Hex string) {
@@ -304,7 +341,7 @@ func dlIfNotExistsMust(uri, sha1Hex string) {
 	}
 	fmt.Printf("downloading '%s'...", uri)
 	d := httpDlMust(uri)
-	realSha1Hex := Sha1HexOfBytes(d)
+	realSha1Hex := sha1HexOfBytes(d)
 	fatalif(sha1Hex != realSha1Hex, "sha1Hex != realSha1Hex (%s != %s)", sha1Hex, realSha1Hex)
 	ext := filepath.Ext(uri)
 	fileName := sha1Hex + ext
@@ -312,6 +349,10 @@ func dlIfNotExistsMust(uri, sha1Hex string) {
 	err := ioutil.WriteFile(path, d, 0644)
 	fataliferr(err)
 	fmt.Printf(" saved to '%s'\n", path)
+	testFilesBySha1[sha1Hex] = &TestFile{
+		Path:    path,
+		Sha1Hex: sha1Hex,
+	}
 }
 
 func downloadTestFilesMust(tests []*Test) {
@@ -342,34 +383,108 @@ func verifyTestFiles() {
 		path := filepath.Join(d, fi.Name())
 		sha1HexFromName := removeExt(fi.Name())
 		fatalif(len(sha1HexFromName) != 40, "len(sha1HexFromName) != 40 (%d)", len(sha1HexFromName))
-		sha1Hex, err := Sha1HexOfFile(path)
+		sha1Hex, err := sha1HexOfFile(path)
 		fataliferr(err)
 		fatalif(sha1Hex != sha1HexFromName, "sha1Hex != sha1HexFromName (%s != %s)", sha1Hex, sha1HexFromName)
-		testFiles[sha1Hex] = &TestFile{
+		testFilesBySha1[sha1Hex] = &TestFile{
 			Path:    path,
 			Sha1Hex: sha1Hex,
 		}
 	}
-	fmt.Printf("%d test files locally\n", len(testFiles))
+	fmt.Printf("%d test files locally\n", len(testFilesBySha1))
+}
+
+func isOS64Bit() bool {
+	return runtime.GOARCH == "amd64"
+}
+
+func dirExists(path string) bool {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return fi.IsDir()
+}
+
+func fileExists(path string) bool {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return fi.Mode().IsRegular()
+}
+
+func verifyCommandsMust(tests []*Test) {
+	var dirsToCheck []string
+	cmds := make(map[string]bool)
+	if isOS64Bit() && dirExists("rel64") {
+		dirsToCheck = append(dirsToCheck, "rel64")
+	}
+	if dirExists("rel") {
+		dirsToCheck = append(dirsToCheck, "rel")
+	}
+	// TODO: also check dbg64 and dbg?
+	fatalif(len(dirsToCheck) == 0, "there is no rel or rel64 directory with executables")
+	for _, test := range tests {
+		cmds[test.CmdName] = true
+	}
+	var dirWithCommands string
+	for _, dir := range dirsToCheck {
+		var cmdsFound []string
+		for cmd := range cmds {
+			path := filepath.Join(dir, cmd)
+			if fileExists(path) {
+				cmdsFound = append(cmdsFound, cmd)
+			}
+		}
+		if len(cmdsFound) == len(cmds) {
+			fmt.Printf("found all test commands in '%s'\n", dir)
+			dirWithCommands = dir
+			break
+		} else {
+			fmt.Printf("dir '%s' has only %d out of %d commands\n", dir, len(cmdsFound), len(cmds))
+		}
+	}
+	fatalif(dirWithCommands == "", "didn't find a directory with all tests commands %v\n", cmds)
+	for _, test := range tests {
+		test.CmdPath = filepath.Join(dirWithCommands, test.CmdName)
+	}
+}
+
+func dumpTests(tests []*Test) {
+	for _, test := range tests {
+		dumpTest(test)
+	}
+}
+
+func substFileVar(s, filePath string) string {
+	return strings.Replace(s, "$file", filePath, -1)
+}
+
+func substFileVarAll(tests []*Test) {
+	for _, test := range tests {
+		sha1Hex := test.FileSha1Hex
+		tf := testFilesBySha1[sha1Hex]
+		fatalif(tf == nil, "no test file for '%s'\n", sha1Hex)
+		test.FilePath = tf.Path
+		test.ExpectedOutput = substFileVar(test.ExpectedOutput, tf.Path)
+		for i, arg := range test.CmdArgs {
+			test.CmdArgs[i] = substFileVar(arg, tf.Path)
+		}
+	}
 }
 
 func main() {
-	fmt.Printf("regress\n")
+	fmt.Printf("regress, 64-bit os: %v\n", isOS64Bit())
+
 	verifyTestFiles()
 	p := filepath.Join("tools", "regress", "tests.txt")
 	tests := parseTestsMust(p)
-	t := &Test{
-		FileURL:        "https://kjkpub.s3.amazonaws.com/testfiles/6f/d3/89a36816f1ab490d46c0c7a6b34b678f72bf.pdf",
-		FileSha1Hex:    "6fd389a36816f1ab490d46c0c7a6b34b678f72bf",
-		CmdPath:        "dbg/SumatraPDF.exe",
-		CmdArgs:        []string{"-render", "2", "-zoom", "5", "$file"},
-		FilePath:       "89a36816f1ab490d46c0c7a6b34b678f72bf.pdf",
-		ExpectedOutput: "rendering page 1 for '89a36816f1ab490d46c0c7a6b34b678f72bf.pdf', zoom: 5.00",
-	}
-	//runTest(t)
-	tests = []*Test{t}
+	verifyCommandsMust(tests)
 	downloadTestFilesMust(tests)
+	substFileVarAll(tests)
+	//dumpTests(tests)
 
-	//runTests(tests)
-	os.Exit(dumpFailedTests())
+	runTests(tests)
+	os.Exit(dumpFailedTests(tests))
 }
