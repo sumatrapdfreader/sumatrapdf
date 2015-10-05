sumatrapdfreader/sumatrapdf

commit f95036cfdba216dcdf1fead096473712c35400f0

drmemory: supress more

src/SumatraProperties.cpp
@@ -127,8 +127,9 @@ static bool IsoDateParse(const WCHAR *isoDate, SYSTEMTIME *timeOut)
 
 static WCHAR *FormatSystemTime(SYSTEMTIME& date)
 {
-    WCHAR buf[512];
+    WCHAR buf[512] = { 0 };
     int cchBufLen = dimof(buf);
+    ZeroMemory(&date, sizeof(SYSTEMTIME));
     int ret = GetDateFormat(LOCALE_USER_DEFAULT, DATE_SHORTDATE, &date, nullptr, buf, cchBufLen);
     if (ret < 2) // GetDateFormat() failed or returned an empty result
         return nullptr;
@@ -240,9 +241,7 @@ static WCHAR *FormatPageSize(BaseEngine *engine, int pageNo, int rotation)
     case Paper_Tabloid: formatName = L" (Tabloid)"; break;
     }
 
-    WCHAR unitSystem[2];
-    GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_IMEASURE, unitSystem, dimof(unitSystem));
-    bool isMetric = unitSystem[0] == '0';
+    bool isMetric = GetMeasurementSystem() == 0;
     double unitsPerInch = isMetric ? 2.54 : 1.0;
     const WCHAR *unit = isMetric ? L"cm" : L"in";
 

src/DisplayModel.cpp
@@ -56,6 +56,7 @@
 #include "PdfSync.h"
 #include "TextSelection.h"
 #include "TextSearch.h"
+#include "WinUtil.h"
 
 // Note: adding chm handling to DisplayModel is a hack, because DisplayModel
 // doesn't map to chm features well.
@@ -224,19 +225,6 @@ void DisplayModel::SetInitialViewSettings(DisplayMode newDisplayMode, int newSta
     }
     displayR2L = (layout & Layout_R2L) != 0;
     BuildPagesInfo();
-}
-
-// 0 - metric (centimeters etc.)
-// 1 - imperial (inches etc.)
-// this triggers drmemory. Force no inlining so that it's easy to write a
-// localized suppression
-static __declspec(noinline) int GetMeasurementSystem() {
-    WCHAR unitSystem[2] = { 0 };
-    GetLocaleInfoW(LOCALE_USER_DEFAULT, LOCALE_IMEASURE, unitSystem, dimof(unitSystem));
-    if (unitSystem[0] == '0') {
-        return 0;
-    }
-    return 1;
 }
 
 void DisplayModel::BuildPagesInfo()

src/SumatraStartup.cpp
@@ -811,6 +811,7 @@ Exit:
         DeleteWindowInfo(gWindows.At(0));
     }
 
+    DeleteCachedCursors();
 #ifndef DEBUG
 
     // leave all the remaining clean-up to the OS

src/utils/StrUtil.cpp
@@ -836,7 +836,7 @@ bool HexToMem(const char *s, unsigned char *buf, size_t bufLen)
 // Caller needs to free() the result.
 WCHAR *FormatNumWithThousandSep(size_t num, LCID locale)
 {
-    WCHAR thousandSep[4];
+    WCHAR thousandSep[4] = { 0 };
     if (!GetLocaleInfo(locale, LOCALE_STHOUSAND, thousandSep, dimof(thousandSep)))
         str::BufSet(thousandSep, dimof(thousandSep), L",");
     ScopedMem<WCHAR> buf(str::Format(L"%Iu", num));

src/utils/WinUtil.cpp
@@ -1266,3 +1266,28 @@ HCURSOR GetCursor(LPWSTR id) {
 }
 
 void SetCursor(LPWSTR id) { SetCursor(GetCursor(id)); }
+
+void DeleteCachedCursors() {
+    for (int i = 0; i < dimof(knownCursorIds); i++) {
+        HCURSOR cur = cachedCursors[i];
+        if (cur) {
+            DestroyCursor(cur);
+            cachedCursors[i] = NULL;
+        }
+    }
+}
+
+
+// 0 - metric (centimeters etc.)
+// 1 - imperial (inches etc.)
+// this triggers drmemory. Force no inlining so that it's easy to write a
+// localized suppression
+__declspec(noinline) int GetMeasurementSystem() {
+    WCHAR unitSystem[2] = { 0 };
+    GetLocaleInfoW(LOCALE_USER_DEFAULT, LOCALE_IMEASURE, unitSystem, dimof(unitSystem));
+    if (unitSystem[0] == '0') {
+        return 0;
+    }
+    return 1;
+}
+

src/utils/WinUtil.h
@@ -193,3 +193,6 @@ void DivideRectV(const RECT &r, int x, int dx, RECT &r1, RECT &r2, RECT &r3);
 
 HCURSOR GetCursor(LPWSTR id);
 void SetCursor(LPWSTR id);
+void DeleteCachedCursors();
+
+int GetMeasurementSystem();

commit 773f8c60ce02e6d86730174d0d6216f27cac6b81

unbreak FormatSystemTime (broken with commit f95036c)

src/SumatraProperties.cpp
@@ -129,7 +129,6 @@ static WCHAR *FormatSystemTime(SYSTEMTIME& date)
 {
     WCHAR buf[512] = { 0 };
     int cchBufLen = dimof(buf);
-    ZeroMemory(&date, sizeof(SYSTEMTIME));
     int ret = GetDateFormat(LOCALE_USER_DEFAULT, DATE_SHORTDATE, &date, nullptr, buf, cchBufLen);
     if (ret < 2) // GetDateFormat() failed or returned an empty result
         return nullptr;
@@ -156,7 +155,7 @@ static void ConvDateToDisplay(WCHAR **s, bool (* DateParse)(const WCHAR *date, S
     if (!s || !*s || !DateParse)
         return;
 
-    SYSTEMTIME date;
+    SYSTEMTIME date = { 0 };
     bool ok = DateParse(*s, &date);
     if (!ok)
         return;