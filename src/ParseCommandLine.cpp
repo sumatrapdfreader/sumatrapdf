12e7fe77a710371a855887ea62373adad118e84d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-10-04 21:29:47 -0700

    cmd-line arg must start with '-' or '/'

--- src/ParseCommandLine.cpp
+++ src/ParseCommandLine.cpp
@@ -265,6 +265,8 @@ enum {
 static int GetArgNo(const WCHAR *argName) {
     if (*argName == '-' || *argName == '/') {
         argName++;
+    } else {
+        return -1;
     }
     return seqstrings::StrToIdx(argNames, argName);
 }
