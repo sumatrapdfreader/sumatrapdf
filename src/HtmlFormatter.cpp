sumatrapdfreader/sumatrapdf

commit 52b73aacf610fafe5c72953808624d7fc01e66ad

Do not display soft hyphens
Most standards agree soft hyphens should not be displayed when not used
to break words at the end of a line.
Fixes #241

src/HtmlFormatter.cpp
@@ -741,6 +741,8 @@ void HtmlFormatter::EmitTextRun(const char *s, const char *end)
             currReparseIdx = s - htmlParser->Start();
 
         size_t strLen = str::Utf8ToWcharBuf(s, end - s, buf, dimof(buf));
+        // soft hyphens should not be displayed
+        strLen -= str::RemoveChars(buf, L"\xad");
         textMeasure->SetFont(CurrFont());
         RectF bbox = textMeasure->Measure(buf, strLen);
         EnsureDx(bbox.Width);
@@ -1374,6 +1376,8 @@ void DrawHtmlPage(Graphics *g, mui::ITextRender *textDraw, Vec<DrawInstr> *drawI
         bbox.Y += offY;
         if (InstrString == i.type || InstrRtlString == i.type) {
             size_t strLen = str::Utf8ToWcharBuf(i.str.s, i.str.len, buf, dimof(buf));
+            // soft hyphens should not be displayed
+            strLen -= str::RemoveChars(buf, L"\xad");
             textDraw->Draw(buf, strLen, bbox, InstrRtlString == i.type);
         } else if (InstrSetFont == i.type) {
             textDraw->SetFont(i.font);

commit 65c754da3774cd20ae4a0a95cf3f7e58341431f4

ebook: don't emit empty text runs (fixes crash 134826, regression from commit 52b73aa)

src/HtmlFormatter.cpp
@@ -743,6 +743,8 @@ void HtmlFormatter::EmitTextRun(const char *s, const char *end)
         size_t strLen = str::Utf8ToWcharBuf(s, end - s, buf, dimof(buf));
         // soft hyphens should not be displayed
         strLen -= str::RemoveChars(buf, L"\xad");
+        if (0 == strLen)
+            break;
         textMeasure->SetFont(CurrFont());
         RectF bbox = textMeasure->Measure(buf, strLen);
         EnsureDx(bbox.Width);