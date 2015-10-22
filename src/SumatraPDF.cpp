37a130ec3c0684a0489012a229be3b8a1b392821
Simon Bünzli <zeniko@gmail.com>
2015-10-22 23:45:34 +0200

    CHM: prevent read access violation (fixes crash 134868)

--- src/SumatraPDF.cpp
+++ src/SumatraPDF.cpp
@@ -1034,6 +1034,7 @@ static void LoadDocIntoCurrentTab(LoadArgs& args, Controller *ctrl, DisplayState
         else if (win->AsChm()) {
             win->AsChm()->SetParentHwnd(win->hwndCanvas);
             win->ctrl->SetDisplayMode(displayMode);
+            ss.page = limitValue(ss.page, 1, win->ctrl->PageCount());
             win->ctrl->GoToPage(ss.page, false);
         }
         else if (win->AsEbook()) {
