9798e093d71f926ddee2525a3f22981ca19305a3
Rajas S <rajas.sambhare@gmail.com>
2016-05-20 14:26:05 -0400

    Added vertical space before and after paragraphs, changed default monospace font from Courier New to Lucida Console, changed default heading font to Arial

--- src/HtmlFormatter.cpp
+++ src/HtmlFormatter.cpp
@@ -848,9 +848,11 @@ void HtmlFormatter::HandleTagP(HtmlToken *t, bool isDiv)
 
         SetAlignment(align);
         EmitParagraph(indent);
+        EmitEmptyLine(0.4 * CurrFont()->GetSize());
     } else {
         FlushCurrLine(true);
         RevertStyleChange();
+        EmitEmptyLine(0.4 * CurrFont()->GetSize());
     }
 }
 
@@ -933,7 +935,7 @@ void HtmlFormatter::HandleTagHx(HtmlToken *t)
         float fontSize = defaultFontSize * pow(1.1f, '5' - t->s[1]);
         if (currY > 0)
             currY += fontSize / 2;
-        SetFontBasedOn(CurrFont(), FontStyleBold, fontSize);
+        SetFont(L"Arial", FontStyleBold, fontSize);
 
         StyleRule rule = ComputeStyleRule(t);
         if (Align_NotFound == rule.textAlign)
@@ -956,7 +958,7 @@ void HtmlFormatter::HandleTagPre(HtmlToken *t)
 {
     FlushCurrLine(true);
     if (t->IsStartTag()) {
-        SetFont(L"Courier New", (FontStyle)CurrFont()->GetStyle());
+        SetFont(L"Lucida Console", (FontStyle)CurrFont()->GetStyle());
         CurrStyle()->align = Align_Left;
         preFormatted = true;
     }
@@ -1194,7 +1196,7 @@ void HtmlFormatter::HandleHtmlTag(HtmlToken *t)
             RevertStyleChange();
     } else if (Tag_Code == tag || Tag_Tt == tag) {
         if (t->IsStartTag())
-            SetFont(L"Courier New", (FontStyle)CurrFont()->GetStyle());
+            SetFont(L"Lucida Console", (FontStyle)CurrFont()->GetStyle());
         else if (t->IsEndTag())
             RevertStyleChange();
     } else if (Tag_Pre == tag) {
