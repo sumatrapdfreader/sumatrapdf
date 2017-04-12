3e45a7dc54f959c62d7a82747ad45dc7f35df0ed
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-04-11 21:23:20 -0700

    disable libdjvu debug messages in debug build

--- premake5.lua
+++ premake5.lua
@@ -153,7 +153,7 @@ workspace "SumatraPDF"
     language "C++"
     -- TODO: try /D USE_EXCEPTION_EMULATION to see if it reduces the size
     -- and disables the exceptions warnings
-    defines { "NEED_JPEG_DECODER", "THREADMODEL=0", "DDJVUAPI=/**/",  "MINILISPAPI=/**/", "DO_CHANGELOCALE=0" }
+    defines { "NEED_JPEG_DECODER", "THREADMODEL=0", "DDJVUAPI=/**/",  "MINILISPAPI=/**/", "DO_CHANGELOCALE=0", "DEBUGLVL=0" }
     disablewarnings { "4100", "4189", "4244", "4267", "4302", "4311", "4312" }
     disablewarnings { "4456", "4457", "4459", "4530", "4611", "4701", "4702", "4703", "4706" }
     includedirs { "ext/libjpeg-turbo" }
