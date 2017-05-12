0b59d225f749a6530b1dca0fee7e5eafebb2702a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-05-12 13:08:53 -0700

    update links to forum

--- src/SumatraAbout2.cpp
+++ src/SumatraAbout2.cpp
@@ -121,7 +121,7 @@ struct AboutLayoutInfoEl {
 static AboutLayoutInfoEl gAboutLayoutInfo[] = {
     { L"website",        L"SumatraPDF website",   WEBSITE_MAIN_URL},
     { L"manual",         L"SumatraPDF manual",    WEBSITE_MANUAL_URL },
-    { L"forums",         L"SumatraPDF forums",    L"https://www.sumatrapdfreader.org/forum.html" },
+    { L"forums",         L"SumatraPDF forums",    L"https://forum.sumatrapdfreader.org/" },
     { L"programming",    L"The Programmers",      URL_AUTHORS },
     { L"translations",   L"The Translators",      URL_TRANSLATORS },
     { L"licenses",       L"Various Open Source",  URL_LICENSE },
