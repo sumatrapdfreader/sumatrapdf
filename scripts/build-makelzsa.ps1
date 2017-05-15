4c4dc6320f6819078571153c8a984beea4e4ba99
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-05-14 20:22:28 -0700

    rename some scripts from .bat => .ps1

--- scripts/build-makelzsa.ps1
+++ scripts/build-makelzsa.ps1
@@ -1,7 +1,7 @@
 
-@rem builds and sings 32-bit release version of MakeLZSA.exe so that
-@rem we can add it to bin\
+# builds and sings 32-bit release version of MakeLZSA.exe so that
+# we can add it to bin\
 
 go run .\tools\build\analyze.go .\tools\build\cmd.go .\tools\build\main.go .\tools\build\s3.go .\tools\build\util.go -build-makelzsa
 
-dir rel\MakeLZSA.exe
+Get-ChildItem rel\MakeLZSA.exe
