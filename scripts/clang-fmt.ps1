05ae8fa1aeec232b749102a54e85273574b304f4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-11-21 12:29:07 -0800

    shorten clang-fmt.ps1

--- scripts/clang-fmt.ps1
+++ scripts/clang-fmt.ps1
@@ -15,18 +15,37 @@ $files =
 "src\utils\tests\*.h",
 "src\wingui\*.cpp",
 "src\wingui\*.h",
-"src\installer\Install.cpp",
-"src\installer\Installer.cpp",
-"src\installer\Installer.h",
-"src\installer\Uninstall.cpp",
+"src\installer\*.cpp",
+"src\installer\*.h",
 "src\tools\*.cpp"
 "src\tools\*.h"
+
+$whiteList =
+"resource.h",
+"Version.h",
+"Trans_sumatra_txt.cpp",
+"Trans_installer_txt.cpp"
+
+function isWhiteListed($name) {
+  $name = $name.ToLower()
+	foreach ($item in $whiteList) {
+		$inList = $item.ToLower().EndsWith($name)
+		if ($inList) {
+			return $inList
+		}
+	}
+	return $false
+}
 
 foreach ($file in $files) {
   $files2 = Get-ChildItem $file
   foreach ($file2 in $files2) {
-    Write-Host $file2
-    clang-format.exe -i -style=file $file2
+    if (isWhiteListed($file2.Name)) {
+      Write-Host "Skipping $file2"
+    } else {
+      Write-Host $file2
+      clang-format.exe -i -style=file $file2
+    }
   }
 }
 
