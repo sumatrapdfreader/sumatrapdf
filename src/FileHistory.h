f176cf948f3c3b0f36f5a41e7929e063c67b02a0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-11-21 10:17:02 -0800

    clang-format FileHistory.h

--- src/FileHistory.h
+++ src/FileHistory.h
@@ -4,28 +4,28 @@
 // number of most recently used files that will be shown in the menu
 // (and remembered in the preferences file, if just filenames are
 //  to be remembered and not individual view settings per document)
-#define FILE_HISTORY_MAX_RECENT     10
+#define FILE_HISTORY_MAX_RECENT 10
 
 // maximum number of most frequently used files that will be shown on the
 // Frequent Read list (space permitting)
-#define FILE_HISTORY_MAX_FREQUENT   10
+#define FILE_HISTORY_MAX_FREQUENT 10
 
 class FileHistory {
     // owned by gGlobalPrefs->fileStates
-    Vec<DisplayState *> *states;
+    Vec<DisplayState*>* states;
 
-public:
-    FileHistory() : states(nullptr) { }
-    ~FileHistory() { }
+  public:
+    FileHistory() : states(nullptr) {}
+    ~FileHistory() {}
 
     void Clear(bool keepFavorites);
-    void Append(DisplayState *state) { states->Append(state); }
-    void Remove(DisplayState *state) { states->Remove(state); }
-    DisplayState *Get(size_t index) const;
-    DisplayState *Find(const WCHAR *filePath, size_t *idxOut = nullptr) const;
-    DisplayState *MarkFileLoaded(const WCHAR *filePath);
-    bool MarkFileInexistent(const WCHAR *filePath, bool hide = false);
-    void GetFrequencyOrder(Vec<DisplayState *>& list);
+    void Append(DisplayState* state) { states->Append(state); }
+    void Remove(DisplayState* state) { states->Remove(state); }
+    DisplayState* Get(size_t index) const;
+    DisplayState* Find(const WCHAR* filePath, size_t* idxOut = nullptr) const;
+    DisplayState* MarkFileLoaded(const WCHAR* filePath);
+    bool MarkFileInexistent(const WCHAR* filePath, bool hide = false);
+    void GetFrequencyOrder(Vec<DisplayState*>& list);
     void Purge(bool alwaysUseDefaultState = false);
-    void UpdateStatesSource(Vec<DisplayState *> *states) { this->states = states; }
+    void UpdateStatesSource(Vec<DisplayState*>* states) { this->states = states; }
 };
