8fe12529f0573d8044b2a8b368a400b4a6d2ad61
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-11-21 08:10:25 -0800

    clang-format ByteOrderDecoder.[h|cpp]

--- src/utils/ByteOrderDecoder.cpp
+++ src/utils/ByteOrderDecoder.cpp
@@ -4,42 +4,36 @@
 #include "BaseUtil.h"
 #include "ByteOrderDecoder.h"
 
-uint16_t UInt16BE(const uint8_t* d)
-{
+uint16_t UInt16BE(const uint8_t* d) {
     return d[1] | (d[0] << 8);
 }
 
-uint16_t UInt16LE(const uint8_t* d)
-{
+uint16_t UInt16LE(const uint8_t* d) {
     return d[0] | (d[1] << 8);
 }
 
-uint32_t UInt32BE(const uint8_t* d)
-{
+uint32_t UInt32BE(const uint8_t* d) {
     return d[3] | (d[2] << 8) | (d[1] << 16) | (d[0] << 24);
 }
 
-uint32_t UInt32LE(const uint8_t* d)
-{
+uint32_t UInt32LE(const uint8_t* d) {
     return d[0] | (d[1] << 8) | (d[2] << 16) | (d[3] << 24);
 }
 
-ByteOrderDecoder::ByteOrderDecoder(const char *d, size_t len, ByteOrder order)
-    : data((const uint8_t *)d), left(len), byteOrder(order), curr(data) { }
+ByteOrderDecoder::ByteOrderDecoder(const char* d, size_t len, ByteOrder order)
+    : data((const uint8_t*)d), left(len), byteOrder(order), curr(data) {}
 
-ByteOrderDecoder::ByteOrderDecoder(const uint8_t *d, size_t len, ByteOrder order)
-    : data(d), left(len), byteOrder(order), curr(data) { }
+ByteOrderDecoder::ByteOrderDecoder(const uint8_t* d, size_t len, ByteOrder order)
+    : data(d), left(len), byteOrder(order), curr(data) {}
 
-uint8_t ByteOrderDecoder::UInt8()
-{
+uint8_t ByteOrderDecoder::UInt8() {
     CrashIf(left < 1);
 
     left--;
     return *curr++;
 }
 
-uint16_t ByteOrderDecoder::UInt16()
-{
+uint16_t ByteOrderDecoder::UInt16() {
     uint16_t v;
     CrashIf(left < sizeof(v));
 
@@ -52,8 +46,7 @@ uint16_t ByteOrderDecoder::UInt16()
     return v;
 }
 
-uint32_t ByteOrderDecoder::UInt32()
-{
+uint32_t ByteOrderDecoder::UInt32() {
     uint32_t v;
     CrashIf(left < sizeof(v));
 
@@ -66,31 +59,27 @@ uint32_t ByteOrderDecoder::UInt32()
     return v;
 }
 
-uint64_t ByteOrderDecoder::UInt64()
-{
+uint64_t ByteOrderDecoder::UInt64() {
     uint64_t first = UInt32();
     if (LittleEndian == byteOrder)
         return first | ((uint64_t)UInt32() << 32);
     return (first << 32) | UInt32();
 }
 
-void ByteOrderDecoder::Bytes(char *dest, size_t len)
-{
+void ByteOrderDecoder::Bytes(char* dest, size_t len) {
     CrashIf(left < len);
     memcpy(dest, curr, len);
     left -= len;
     curr += len;
 }
 
-void ByteOrderDecoder::Skip(size_t len)
-{
+void ByteOrderDecoder::Skip(size_t len) {
     CrashIf(left < len);
     left -= len;
     curr += len;
 }
 
-void ByteOrderDecoder::Unskip(size_t len)
-{
+void ByteOrderDecoder::Unskip(size_t len) {
     CrashIf(curr < data + len);
     left += len;
     curr -= len;
--- src/utils/ByteOrderDecoder.h
+++ src/utils/ByteOrderDecoder.h
@@ -2,37 +2,39 @@
    License: Simplified BSD (see COPYING.BSD) */
 
 // class for decoding of byte buffer as a sequence of numbers
-class ByteOrderDecoder
-{
-public:
+class ByteOrderDecoder {
+  public:
     enum ByteOrder { LittleEndian, BigEndian };
 
-    ByteOrderDecoder(const char *d, size_t len, ByteOrder order);
-    ByteOrderDecoder(const uint8_t *d, size_t len, ByteOrder order);
+    ByteOrderDecoder(const char* d, size_t len, ByteOrder order);
+    ByteOrderDecoder(const uint8_t* d, size_t len, ByteOrder order);
 
-    void ChangeOrder(ByteOrder newOrder) { CrashIf(byteOrder == newOrder); byteOrder = newOrder; }
+    void ChangeOrder(ByteOrder newOrder) {
+        CrashIf(byteOrder == newOrder);
+        byteOrder = newOrder;
+    }
 
-    uint8_t  UInt8();
-    char   Char() { return (char)UInt8(); }
+    uint8_t UInt8();
+    char Char() { return (char)UInt8(); }
     uint16_t UInt16();
-    int16_t  Int16() { return (int16_t)UInt16(); }
+    int16_t Int16() { return (int16_t)UInt16(); }
     uint32_t UInt32();
-    int32_t  Int32() { return (int32_t)UInt32(); }
+    int32_t Int32() { return (int32_t)UInt32(); }
     uint64_t UInt64();
-    int64_t  Int64() { return (int64_t)UInt64(); }
+    int64_t Int64() { return (int64_t)UInt64(); }
 
-    void   Bytes(char *dest, size_t len);
+    void Bytes(char* dest, size_t len);
 
-    void   Skip(size_t len);
-    void   Unskip(size_t len);
+    void Skip(size_t len);
+    void Unskip(size_t len);
 
     size_t Offset() const { return curr - data; }
 
-protected:
+  protected:
     ByteOrder byteOrder;
-    const uint8_t *data;
+    const uint8_t* data;
 
-    const uint8_t *curr;
+    const uint8_t* curr;
     size_t left;
 };
 
