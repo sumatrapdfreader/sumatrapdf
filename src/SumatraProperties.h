a5ec11859fd6671c423897fcb70e8f65e513084f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-11-21 10:15:09 -0800

    clang-format SumatraProperties.h

--- src/SumatraProperties.h
+++ src/SumatraProperties.h
@@ -1,11 +1,20 @@
 /* Copyright 2015 the SumatraPDF project authors (see AUTHORS file).
    License: GPLv3 */
 
-#define PROPERTIES_CLASS_NAME   L"SUMATRA_PDF_PROPERTIES"
+#define PROPERTIES_CLASS_NAME L"SUMATRA_PDF_PROPERTIES"
 
-enum PaperFormat { Paper_Other, Paper_A4, Paper_A3, Paper_A5, Paper_Letter, Paper_Legal, Paper_Tabloid, Paper_Statement };
+enum PaperFormat {
+    Paper_Other,
+    Paper_A4,
+    Paper_A3,
+    Paper_A5,
+    Paper_Letter,
+    Paper_Legal,
+    Paper_Tabloid,
+    Paper_Statement
+};
 PaperFormat GetPaperFormat(SizeD size);
 
-void OnMenuProperties(WindowInfo *win);
+void OnMenuProperties(WindowInfo* win);
 void DeletePropertiesWindow(HWND hwndParent);
 LRESULT CALLBACK WndProcProperties(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
