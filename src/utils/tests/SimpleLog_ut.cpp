96c1565750b3cb5e65cac07f5d25f1bda799af44
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-11-21 10:49:53 -0800

    fix build

--- src/utils/tests/SimpleLog_ut.cpp
+++ src/utils/tests/SimpleLog_ut.cpp
@@ -9,27 +9,12 @@
 #include "UtAssert.h"
 
 void SimpleLogTest() {
-    slog::MultiLogger log;
-    log.LogAndFree(str::Dup(L"Don't leak me!"));
-
-    slog::MemoryLogger logAll;
-    log.AddLogger(&logAll);
-
     {
-        slog::MemoryLogger ml;
-        log.AddLogger(&ml);
+        slog::MemoryLogger log;
         log.Log(L"Test1");
-        ml.Log(L"ML");
-        ml.LogFmt(L"%s : %d", L"filen\xE4me.pdf", 25);
-        log.RemoveLogger(&ml);
+        log.Log(L"ML");
+        log.LogFmt(L"%s : %d", L"filen\xE4me.pdf", 25);
 
-        utassert(str::Eq(ml.GetData(), L"Test1\r\nML\r\nfilen\xE4me.pdf : 25\r\n"));
+        utassert(str::Eq(log.GetData(), L"Test1\r\nML\r\nfilen\xE4me.pdf : 25\r\n"));
     }
-
-    utassert(str::Eq(logAll.GetData(), L"Test1\r\nTest2\r\nfilen\xE4me.pdf : 25\r\n"));
-    log.RemoveLogger(&logAll);
-
-    // don't leak the logger, don't crash on logging nullptr
-    log.AddLogger(new slog::DebugLogger());
-    log.Log(nullptr);
 }
