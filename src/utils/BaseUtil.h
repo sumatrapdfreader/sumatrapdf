23f25dbee560a7e8ae520ccb1116e4ca3dc3d655
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-09-01 14:36:10 -0700

    add defer

--- src/utils/BaseUtil.h
+++ src/utils/BaseUtil.h
@@ -562,6 +562,37 @@ class MaybeOwnedData {
     OwnedData StealData();
 };
 
+// from https://pastebin.com/3YvWQa5c
+#define CONCAT_INTERNAL(x, y) x##y
+#define CONCAT(x, y) CONCAT_INTERNAL(x, y)
+
+template <typename T>
+struct ExitScope {
+    T lambda;
+    ExitScope(T lambda) : lambda(lambda) {}
+    ~ExitScope() { lambda(); }
+    ExitScope(const ExitScope&);
+
+  private:
+    ExitScope& operator=(const ExitScope&);
+};
+
+class ExitScopeHelp {
+  public:
+    template <typename T>
+    ExitScope<T> operator+(T t) {
+        return t;
+    }
+};
+
+#define defer const auto& CONCAT(defer__, __LINE__) = ExitScopeHelp() + [&]()
+
+/* How to use:
+defer { free(tools_filename); };
+defer { fclose(f); };
+defer { instance->Release(); };
+*/
+
 #include "GeomUtil.h"
 #include "StrUtil.h"
 #include "Scoped.h"
