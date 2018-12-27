2e2cca11f64ca5ef712c6db3c1b68be61052f7f2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-27 02:39:39 -0800

    use unrar.h instead of replicating it

--- src/utils/Archive.h
+++ src/utils/Archive.h
@@ -1,14 +1,6 @@
 /* Copyright 2018 the SumatraPDF project authors (see AUTHORS file).
    License: Simplified BSD (see COPYING.BSD) */
 
-// if ENABLE_UNRARDLL_FALLBACK is 1, SumatraPDF will look for unrar.dll/unrar64.dll
-// in the same directory as SumatraPDF.exe whenever a RAR archive
-// fails to open or extract and uses that as a fallback.
-// If not set externally, we enable unarr fallback on Windows
-#if !defined(ENABLE_UNRARDLL_FALLBACK)
-#define ENABLE_UNRARDLL_FALLBACK OS_WIN
-#endif
-
 extern "C" {
 typedef struct ar_stream_s ar_stream;
 typedef struct ar_archive_s ar_archive;
@@ -63,14 +55,12 @@ class Archive {
     ar_stream* data_ = nullptr;
     ar_archive* ar_ = nullptr;
 
-#if ENABLE_UNRARDLL_FALLBACK
     // only set when we loaded file infos using unrar.dll fallback
     const char* rarFilePath_ = nullptr;
 
     bool OpenUnrarDllFallback(const char* rarPathUtf);
     OwnedData GetFileDataByIdUnarrDll(size_t fileId);
     bool LoadedUsingUnrarDll() const { return rarFilePath_ != nullptr; }
-#endif
 };
 
 Archive* OpenZipArchive(const char* path, bool deflatedOnly);
