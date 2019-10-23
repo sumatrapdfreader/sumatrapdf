4207ab7abad787b809d985c4d49fc426dac5f9b1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-23 02:09:08 -0700

    remove obsolete scripts

--- scripts/premake-regenerate.ps1
+++
@@ -1,6 +0,0 @@
-if (!(Test-Path ".\\bin\\premake5.exe")) {
-  Write-Output "premake5.exe is not in bin directory. Download from https://premake.github.io/download.html and put in bin directory."
-  exit
-}
-
-bin\premake5.exe vs2017
--- scripts/valgrind_rel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -u -e -o pipefail -o verbose
-
-go run -race tools/build/build_unix.go -release $@
-valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./out/rel64/test_unix
