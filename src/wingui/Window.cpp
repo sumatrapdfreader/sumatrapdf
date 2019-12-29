dae4569f6639aa45f10e114428bf797cedaf8722
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-12-28 21:15:04 -0800

    don't sent PostQuitMessage() on NC_DESTROY in Window.cpp WndProc

--- src/wingui/Window.cpp
+++ src/wingui/Window.cpp
@@ -307,8 +307,7 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
             args.window = w;
             w->onDestroyed(&args);
         } else {
-            logf("Calling PostQuitMessage() in Window.cpp WindProc\n");
-            PostQuitMessage(0);
+            // TODO: maybe for top-level windows call PostQuitMessage();
         }
         return 0;
     }
