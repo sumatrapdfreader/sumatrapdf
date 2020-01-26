e8a22acaa991a03eedf2f0cdbd401a97f7282cfc
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-01-26 03:30:00 -0800

    try fix ci upload #3

--- do/upload_spaces.go
+++ do/upload_spaces.go
@@ -164,7 +164,7 @@ func spacesUploadPreReleaseMust(ver string, buildType string) {
 	manifestRemotePath := remoteDir + prefix + "-manifest.txt"
 
 	// ra-micro only needs 64-bit builds
-	if buildType != buildTypeRaMicro || isDaily {
+	if buildType == buildTypePreRel {
 		files := getFileNamesWithPrefix(prefix)
 		err := minioUploadFiles(c, remoteDir, rel32Dir, files)
 		fatalIfErr(err)
