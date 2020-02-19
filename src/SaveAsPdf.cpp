27cd5f03877843d5e64861a45bf696bac417b60f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-02-19 12:45:36 -0800

    fix compilation

--- src/SaveAsPdf.cpp
+++ src/SaveAsPdf.cpp
@@ -1,6 +1,24 @@
 /* Copyright 2020 the SumatraPDF project authors (see AUTHORS file).
    License: Simplified BSD (see COPYING.BSD) */
 
+#include "utils/BaseUtil.h"
+#include "utils/BitManip.h"
+#include "utils/Log.h"
+#include "utils/FileUtil.h"
+#include "utils/ScopedWin.h"
+#include "utils/WinUtil.h"
+#include "utils/Dpi.h"
+
+#include "wingui/TreeModel.h"
+
+#include "resource.h"
+#include "Translations.h"
+
+#include "EngineBase.h"
+
+#include "SaveAsPdf.h"
+
+
 void SaveVirutalAsPdf(TocItem* root, char* filePath) {
     // TODO: implement me
 }
