c934bd95062218e37aa0acdb2dd30ed96535d598
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-03-17 01:27:26 -0700

    tweak testcode

--- src/testcode/test-app.h
+++ src/testcode/test-app.h
@@ -6,4 +6,12 @@
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
 #include <SDKDDKVer.h>
-#include "resource.h"
+
+#define IDR_MAINFRAME               128
+#define IDD_RENDERSPEEDTEST_DIALOG  102
+#define IDD_ABOUTBOX                103
+#define IDM_ABOUT                   104
+#define IDM_EXIT                    105
+#define IDI_TESTWIN                 107
+#define IDI_SMALL                   108
+#define IDC_TESTWIN                 109
