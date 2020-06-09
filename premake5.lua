eff349e7e43a0af83b4a898a312d80f1927a4c95
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-06-09 16:18:21 -0700

    un-disable some mupdf warnings

--- premake5.lua
+++ premake5.lua
@@ -367,10 +367,10 @@ workspace "SumatraPDF"
     -- this defines which fonts are to be excluded from being included directly
     -- we exclude the very big cjk fonts
     defines { "TOFU", "TOFU_CJK_LANG" }
-    disablewarnings { 
-      "4005", "4018", "4028", "4100", "4115", "4130", "4204", "4206", "4244",
-      "4245", "4267", "4295", "4305", "4389", "4456", "4457", "4459",
-      "4702", "4703", "4706",
+
+    disablewarnings {
+      "4005", "4018", "4100", "4115", "4130", "4204", "4206", "4245", "4267", "4295",
+      "4305", "4389", "4456", "4703", "4706"
     }
     -- force including mupdf/scripts/openjpeg/opj_config_private.h
     -- with our build over-rides
