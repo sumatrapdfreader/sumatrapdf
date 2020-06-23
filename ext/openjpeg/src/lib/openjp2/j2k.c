347f6dc3d612cc9c45c11a9aa23ceff90f109668
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-06-23 16:41:42 -0700

    update openjpeg

--- ext/openjpeg/src/lib/openjp2/j2k.c
+++ ext/openjpeg/src/lib/openjp2/j2k.c
@@ -8853,8 +8853,8 @@ OPJ_BOOL opj_j2k_read_tile_header(opj_j2k_t * p_j2k,
     if (l_current_marker == J2K_MS_EOC) {
         if (p_j2k->m_specific_param.m_decoder.m_state != J2K_STATE_EOC) {
             p_j2k->m_current_tile_number = 0;
-        p_j2k->m_specific_param.m_decoder.m_state = J2K_STATE_EOC;
-    }
+            p_j2k->m_specific_param.m_decoder.m_state = J2K_STATE_EOC;
+        }
     }
 
     /* FIXME DOC ???*/
