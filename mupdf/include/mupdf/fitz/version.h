63509447ab7d09b76d96f8b47e737810ad10f071
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-09-29 19:38:53 -0700

    update mupdf

--- mupdf/include/mupdf/fitz/version.h
+++ mupdf/include/mupdf/fitz/version.h
@@ -1,9 +1,9 @@
 #ifndef MUPDF_FITZ_VERSION_H
 #define MUPDF_FITZ_VERSION_H
 #ifndef FZ_VERSION
-#define FZ_VERSION "1.17.0"
+#define FZ_VERSION "1.18.0"
 #define FZ_VERSION_MAJOR 1
-#define FZ_VERSION_MINOR 17
+#define FZ_VERSION_MINOR 18
 #define FZ_VERSION_PATCH 0
 #endif
 #endif
