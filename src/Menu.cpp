b09dcd8754fbd0b741880a545dd4dc8a3a2b6c50
Rohit D Borkar <rohitborkar00@gmail.com>
2021-05-09 17:43:42 +0530

    Fixed &Close conflict

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -94,7 +94,7 @@ static MenuDef menuDefFile[] = {
     { _TRN("&Open...\tCtrl+O"),             CmdOpen,                   MF_REQ_DISK_ACCESS },
     { "Open Folder",                        CmdOpenFolder,             MF_REQ_DISK_ACCESS | MF_RAMICRO_ONLY },
     { _TRN("&Close\tCtrl+W"),               CmdClose,                  MF_REQ_DISK_ACCESS },
-    { _TRN("&Close All Files\tCtrl+Shift+W"),  CmdCloseAll,             MF_REQ_DISK_ACCESS },
+    { _TRN("Close All &Tabs\tCtrl+Shift+W"),  CmdCloseAll,             MF_REQ_DISK_ACCESS },
     { _TRN("Show in &folder"),              CmdShowInFolder,           MF_REQ_DISK_ACCESS },
     { _TRN("&Save As...\tCtrl+S"),          CmdSaveAs,                 MF_REQ_DISK_ACCESS },
     { _TRN("Save Annotations"),             CmdSaveAnnotations,        MF_REQ_DISK_ACCESS },
