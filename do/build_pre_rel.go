8e4129d08b3c44c872e6fa594d6fca91d7db1ccd
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-06-09 11:18:50 -0700

    fix buildPreRelease() for ci

--- do/build_pre_rel.go
+++ do/build_pre_rel.go
@@ -28,21 +28,11 @@ func buildPreRelease() {
 	nameInZip = fmt.Sprintf("SumatraPDF-prerel-%s-64.exe", ver)
 	createExeZipWithGoWithNameMust(rel64Dir, nameInZip)
 
-	build(rel64RaDir, "Release", "x64_ramicro")
-	nameInZip = fmt.Sprintf("RAMicroPDFViewer-prerel-%s.exe", ver)
-	createExeZipWithGoWithNameMust(rel64RaDir, nameInZip)
-
 	createManifestMust()
 
 	dstDir := filepath.Join("out", "final-prerel")
 	prefix := fmt.Sprintf("SumatraPDF-prerel-%s", ver)
 	copyBuiltFiles(dstDir, rel32Dir, prefix)
 	copyBuiltFiles(dstDir, rel64Dir, prefix+"-64")
-	copyBuiltManifest(dstDir, prefix)
-
-	// note: manifest won't be for the right files but we don't care
-	dstDir = filepath.Join("out", "final-ramicro")
-	prefix = fmt.Sprintf("RAMicroPDFViewer-prerel-%s", ver)
-	copyBuiltFiles(dstDir, rel64RaDir, prefix+"-64")
 	copyBuiltManifest(dstDir, prefix)
 }
