0560454aca00598fbccbdac0594958793d4cd2f4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-08-17 20:14:11 -0700

    cleanup GdiPlusUtil.cpp

--- src/utils/GdiPlusUtil.cpp
+++ src/utils/GdiPlusUtil.cpp
@@ -20,52 +20,18 @@
 
 #include "utils/Log.h"
 
-// using namespace Gdiplus;
-
-using Gdiplus::ARGB;
 using Gdiplus::Bitmap;
 using Gdiplus::BitmapData;
-using Gdiplus::Brush;
 using Gdiplus::CharacterRange;
-using Gdiplus::Color;
-using Gdiplus::CombineModeReplace;
-using Gdiplus::CompositingQualityHighQuality;
 using Gdiplus::Font;
-using Gdiplus::FontFamily;
-using Gdiplus::FontStyle;
-using Gdiplus::FontStyleBold;
-using Gdiplus::FontStyleItalic;
-using Gdiplus::FontStyleRegular;
-using Gdiplus::FontStyleStrikeout;
-using Gdiplus::FontStyleUnderline;
-using Gdiplus::FrameDimensionPage;
-using Gdiplus::FrameDimensionTime;
 using Gdiplus::Graphics;
-using Gdiplus::GraphicsPath;
-using Gdiplus::Image;
-using Gdiplus::ImageAttributes;
-using Gdiplus::InterpolationModeHighQualityBicubic;
-using Gdiplus::LinearGradientBrush;
-using Gdiplus::LinearGradientMode;
-using Gdiplus::LinearGradientModeVertical;
 using Gdiplus::Matrix;
 using Gdiplus::MatrixOrderAppend;
 using Gdiplus::Ok;
-using Gdiplus::OutOfMemory;
-using Gdiplus::Pen;
-using Gdiplus::PenAlignmentInset;
-using Gdiplus::PropertyItem;
 using Gdiplus::Region;
-using Gdiplus::SmoothingModeAntiAlias;
-using Gdiplus::SolidBrush;
 using Gdiplus::Status;
-using Gdiplus::StringAlignmentCenter;
 using Gdiplus::StringFormat;
-using Gdiplus::StringFormatFlagsDirectionRightToLeft;
 using Gdiplus::StringFormatFlagsMeasureTrailingSpaces;
-using Gdiplus::TextRenderingHintClearTypeGridFit;
-using Gdiplus::UnitPixel;
-using Gdiplus::Win32Error;
 
 Gdiplus::RectF RectToRectF(const Gdiplus::Rect r) {
     return Gdiplus::RectF((float)r.X, (float)r.Y, (float)r.Width, (float)r.Height);
