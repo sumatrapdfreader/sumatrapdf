11083af9f5e69a0fcdfab7252c8f14deaece02f7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-08-17 19:52:00 -0700

    simplify Calc[MD5|Sha1|Sha2]DigestWin by re-using common code

--- src/utils/CryptoUtil.cpp
+++ src/utils/CryptoUtil.cpp
@@ -57,128 +57,49 @@ void CalcSHA1Digest(const u8* data, size_t byteCount, u8 digest[20]) {
     CalcSha1DigestWin(data, byteCount, digest);
 }
 
-// Note: MS_ENH_RSA_AES_PROV, etc. aren't defined in the SDK shipping with VS2008
-#ifndef MS_ENH_RSA_AES_PROV
-#define MS_ENH_RSA_AES_PROV L"Microsoft Enhanced RSA and AES Cryptographic Provider"
-#endif
-#ifndef MS_ENH_RSA_AES_PROV_XP
-#define MS_ENH_RSA_AES_PROV_XP L"Microsoft Enhanced RSA and AES Cryptographic Provider (Prototype)"
-#endif
-#ifndef PROV_RSA_AES
-#define PROV_RSA_AES 24
-#endif
-// Note: CALG_SHA_256 isn't available for Windows XP SP2 and below
-#ifndef CALG_SHA_256
-#define ALG_SID_SHA_256 12
-#define CALG_SHA_256 (ALG_CLASS_HASH | ALG_TYPE_ANY | ALG_SID_SHA_256)
-#endif
-
-// MD5 digest that uses Windows' CryptoAPI. It's good for code that doesn't already
-// have MD5 code (smaller code) and it's probably faster than most other implementations
-// TODO: could try to use CryptoNG available starting in Vista. But then again, would that be worth it?
-void CalcMD5DigestWin(const void* data, size_t byteCount, u8 digest[16]) {
+// TODO: could use CryptoNG available starting in Vista
+static void CalcDigestWin(const void* data, size_t byteCount, u8* digest, DWORD digestSize, const WCHAR* provider,
+                          DWORD type, ALG_ID alg) {
     HCRYPTPROV hProv = 0;
     HCRYPTHASH hHash = 0;
+    BOOL ok = CryptAcquireContextW(&hProv, nullptr, provider, type, CRYPT_VERIFYCONTEXT);
+    CrashIf(!ok);
+    ok = CryptCreateHash(hProv, alg, 0, 0, &hHash);
+    CrashIf(!ok);
 
-    // http://stackoverflow.com/questions/9794745/ms-cryptoapi-doesnt-work-on-windows-xp-with-cryptacquirecontext
-    BOOL ok = CryptAcquireContext(&hProv, nullptr, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
-    if (!ok) {
-        ok = CryptAcquireContext(&hProv, nullptr, MS_ENH_RSA_AES_PROV_XP, PROV_RSA_AES, CRYPT_VERIFYCONTEXT);
-    }
-
-    CrashAlwaysIf(!ok);
-    ok = CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash);
-    CrashAlwaysIf(!ok);
 #ifdef _WIN64
     for (; byteCount > DWORD_MAX; data = (const BYTE*)data + DWORD_MAX, byteCount -= DWORD_MAX) {
         ok = CryptHashData(hHash, (const BYTE*)data, DWORD_MAX, 0);
-        CrashAlwaysIf(!ok);
+        CrashIf(!ok);
     }
 #endif
-    CrashAlwaysIf(byteCount > DWORD_MAX);
+    CrashIf(byteCount > DWORD_MAX);
     ok = CryptHashData(hHash, (const BYTE*)data, (DWORD)byteCount, 0);
-    CrashAlwaysIf(!ok);
+    CrashIf(!ok);
 
-    DWORD hashLen;
+    DWORD hashLen = 0;
     DWORD argSize = sizeof(DWORD);
     ok = CryptGetHashParam(hHash, HP_HASHSIZE, (BYTE*)&hashLen, &argSize, 0);
     CrashIf(sizeof(DWORD) != argSize);
-    CrashAlwaysIf(!ok);
-    CrashAlwaysIf(16 != hashLen);
+    CrashIf(!ok);
+    CrashIf(digestSize != hashLen);
     ok = CryptGetHashParam(hHash, HP_HASHVAL, digest, &hashLen, 0);
-    CrashAlwaysIf(!ok);
-    CrashAlwaysIf(16 != hashLen);
+    CrashIf(!ok);
+    CrashIf(digestSize != hashLen);
     CryptDestroyHash(hHash);
     CryptReleaseContext(hProv, 0);
 }
 
-// SHA1 digest that uses Windows' CryptoAPI. It's good for code that doesn't already
-// have SHA1 code (smaller code) and it's probably faster than most other implementations
-// TODO: hasn't been tested for corectness
+void CalcMD5DigestWin(const void* data, size_t byteCount, u8 digest[16]) {
+    CalcDigestWin(data, byteCount, digest, 16, MS_DEF_PROV, PROV_RSA_FULL, CALG_MD5);
+}
+
 void CalcSha1DigestWin(const void* data, size_t byteCount, u8 digest[20]) {
-    HCRYPTPROV hProv = 0;
-    HCRYPTHASH hHash = 0;
-
-    BOOL ok = CryptAcquireContext(&hProv, nullptr, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
-    CrashAlwaysIf(!ok);
-    ok = CryptCreateHash(hProv, CALG_SHA1, 0, 0, &hHash);
-    CrashAlwaysIf(!ok);
-#ifdef _WIN64
-    for (; byteCount > DWORD_MAX; data = (const BYTE*)data + DWORD_MAX, byteCount -= DWORD_MAX) {
-        ok = CryptHashData(hHash, (const BYTE*)data, DWORD_MAX, 0);
-        CrashAlwaysIf(!ok);
-    }
-#endif
-    CrashAlwaysIf(byteCount > DWORD_MAX);
-    ok = CryptHashData(hHash, (const BYTE*)data, (DWORD)byteCount, 0);
-    CrashAlwaysIf(!ok);
-
-    DWORD hashLen;
-    DWORD argSize = sizeof(DWORD);
-    ok = CryptGetHashParam(hHash, HP_HASHSIZE, (BYTE*)&hashLen, &argSize, 0);
-    CrashIf(sizeof(DWORD) != argSize);
-    CrashAlwaysIf(!ok);
-    CrashAlwaysIf(20 != hashLen);
-    ok = CryptGetHashParam(hHash, HP_HASHVAL, digest, &hashLen, 0);
-    CrashAlwaysIf(!ok);
-    CrashAlwaysIf(20 != hashLen);
-    CryptDestroyHash(hHash);
-    CryptReleaseContext(hProv, 0);
+    CalcDigestWin(data, byteCount, digest, 20, MS_DEF_PROV, PROV_RSA_FULL, CALG_SHA1);
 }
 
 void CalcSha2DigestWin(const void* data, size_t byteCount, u8 digest[32]) {
-    HCRYPTPROV hProv = 0;
-    HCRYPTHASH hHash = 0;
-
-    BOOL ok = CryptAcquireContext(&hProv, nullptr, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CRYPT_VERIFYCONTEXT);
-    if (!ok) {
-        // TODO: test this on XP SP3
-        ok = CryptAcquireContext(&hProv, nullptr, MS_ENH_RSA_AES_PROV_XP, PROV_RSA_AES, CRYPT_VERIFYCONTEXT);
-    }
-    CrashAlwaysIf(!ok);
-    ok = CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash);
-    CrashAlwaysIf(!ok);
-#ifdef _WIN64
-    for (; byteCount > DWORD_MAX; data = (const BYTE*)data + DWORD_MAX, byteCount -= DWORD_MAX) {
-        ok = CryptHashData(hHash, (const BYTE*)data, DWORD_MAX, 0);
-        CrashAlwaysIf(!ok);
-    }
-#endif
-    CrashAlwaysIf(byteCount > DWORD_MAX);
-    ok = CryptHashData(hHash, (const BYTE*)data, (DWORD)byteCount, 0);
-    CrashAlwaysIf(!ok);
-
-    DWORD hashLen;
-    DWORD argSize = sizeof(DWORD);
-    ok = CryptGetHashParam(hHash, HP_HASHSIZE, (BYTE*)&hashLen, &argSize, 0);
-    CrashIf(sizeof(DWORD) != argSize);
-    CrashAlwaysIf(!ok);
-    CrashAlwaysIf(32 != hashLen);
-    ok = CryptGetHashParam(hHash, HP_HASHVAL, digest, &hashLen, 0);
-    CrashAlwaysIf(!ok);
-    CrashAlwaysIf(32 != hashLen);
-    CryptDestroyHash(hHash);
-    CryptReleaseContext(hProv, 0);
+    CalcDigestWin(data, byteCount, digest, 32, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CALG_SHA_256);
 }
 
 static bool ExtractSignature(const char* hexSignature, const void* data, size_t& dataLen, ScopedMem<BYTE>& signature,
