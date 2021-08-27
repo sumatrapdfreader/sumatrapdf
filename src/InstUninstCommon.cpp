bfef39ab2da9712d63fb4644476cfa71e54d2d40
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-08-27 01:06:40 -0700

    less using Gdiplus::

--- src/InstUninstCommon.cpp
+++ src/InstUninstCommon.cpp
@@ -49,43 +49,16 @@
 
 #define TEN_SECONDS_IN_MS 10 * 1000
 
-// using namespace Gdiplus;
-
-using Gdiplus::ARGB;
 using Gdiplus::Bitmap;
-using Gdiplus::Brush;
 using Gdiplus::Color;
-using Gdiplus::CombineModeReplace;
 using Gdiplus::CompositingQualityHighQuality;
 using Gdiplus::Font;
-using Gdiplus::FontFamily;
-using Gdiplus::FontStyle;
-using Gdiplus::FontStyleBold;
-using Gdiplus::FontStyleItalic;
 using Gdiplus::FontStyleRegular;
-using Gdiplus::FontStyleStrikeout;
-using Gdiplus::FontStyleUnderline;
-using Gdiplus::FrameDimensionPage;
-using Gdiplus::FrameDimensionTime;
 using Gdiplus::Graphics;
-using Gdiplus::GraphicsPath;
 using Gdiplus::Image;
-using Gdiplus::ImageAttributes;
-using Gdiplus::InterpolationModeHighQualityBicubic;
-using Gdiplus::LinearGradientBrush;
-using Gdiplus::LinearGradientMode;
-using Gdiplus::LinearGradientModeVertical;
-using Gdiplus::Matrix;
 using Gdiplus::MatrixOrderAppend;
-using Gdiplus::Ok;
-using Gdiplus::OutOfMemory;
-using Gdiplus::Pen;
-using Gdiplus::PenAlignmentInset;
-using Gdiplus::PropertyItem;
-using Gdiplus::Region;
 using Gdiplus::SmoothingModeAntiAlias;
 using Gdiplus::SolidBrush;
-using Gdiplus::Status;
 using Gdiplus::StringAlignmentCenter;
 using Gdiplus::StringFormat;
 using Gdiplus::StringFormatFlagsDirectionRightToLeft;
