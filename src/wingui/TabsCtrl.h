d58907dbaf43f2a060ce5eebea940b6d1038bf36
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-05-22 20:59:15 -0400

    remove the old, unused TabsCtrl control

--- src/wingui/TabsCtrl.h
+++ src/wingui/TabsCtrl.h
@@ -1,52 +1,6 @@
 /* Copyright 2022 the SumatraPDF project authors (see AUTHORS file).
    License: Simplified BSD (see COPYING.BSD) */
 
-struct TabsCtrl;
-class TabsCtrlState;
-
-using TabSelectedCb = std::function<void(TabsCtrl*, TabsCtrlState*, int)>;
-using TabClosedCb = std::function<void(TabsCtrl*, TabsCtrlState*, int)>;
-
-class TabItem {
-  public:
-    TabItem(const char* title, const char* toolTip);
-
-    str::Str title;
-    str::Str toolTip;
-};
-
-class TabsCtrlState {
-  public:
-    Vec<TabItem*> tabs;
-    int selectedItem = 0;
-};
-
-class TabsCtrlPrivate;
-
-struct TabsCtrl {
-    // creation parameters. must be set before CreateTabsCtrl
-    HWND parent = nullptr;
-    RECT initialPos{};
-
-    TabSelectedCb onTabSelected = nullptr;
-    TabClosedCb onTabClosed = nullptr;
-
-    TabsCtrlPrivate* priv;
-};
-
-/* Creation sequence:
-- AllocTabsCtrl()
-- set creation parameters
-- CreateTabsCtrl()
-*/
-
-TabsCtrl* AllocTabsCtrl(HWND parent, RECT initialPosition);
-bool CreateTabsCtrl(TabsCtrl*);
-void DeleteTabsCtrl(TabsCtrl*);
-void SetState(TabsCtrl*, TabsCtrlState*);
-SIZE GetIdealSize(TabsCtrl*);
-void SetPos(TabsCtrl*, RECT&);
-void SetFont(TabsCtrl*, HFONT);
 
 struct TabsCtrl2 : WindowBase {
     str::Str lastTabText;
