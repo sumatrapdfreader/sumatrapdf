47dc159bb9ac8e821bab6300f0f5d4b66f6006d1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-05-30 23:44:15 -0400

    convert heif/avif decoded image into Gdiplus::Bitmap

--- src/utils/AvifReader.cpp
+++ src/utils/AvifReader.cpp
@@ -60,9 +60,11 @@ Gdiplus::Bitmap* AvifImageFromData(const ByteSlice& d) {
     Gdiplus::Bitmap* bmp = nullptr;
     struct heif_image_handle* hdl = nullptr;
     struct heif_image* img = nullptr;
-    int dx, dy, stride;
-    int has_alpha;
+    int dx, dy, srcStride;
+    // int hasAlpha;
     const u8* data = nullptr;
+    heif_chroma chroma;
+    heif_colorspace cs;
 
     heif_context* ctx = heif_context_alloc();
     auto err = heif_context_read_from_memory_without_copy(ctx, (const void*)d.Get(), d.size(), nullptr);
@@ -77,20 +79,55 @@ Gdiplus::Bitmap* AvifImageFromData(const ByteSlice& d) {
     }
 
     dx = heif_image_handle_get_width(hdl);
-    dy = heif_image_handle_get_width(hdl);
+    dy = heif_image_handle_get_height(hdl);
 
-    has_alpha = heif_image_handle_has_alpha_channel(hdl);
-
-    err = heif_decode_image(hdl, &img, heif_colorspace_RGB,
-                            has_alpha ? heif_chroma_interleaved_RGBA : heif_chroma_interleaved_RGB, nullptr);
+    // hasAlpha = heif_image_handle_has_alpha_channel(hdl);
+    // chroma = hasAlpha ? heif_chroma_interleaved_RGBA : heif_chroma_interleaved_RGB;
+    chroma = heif_chroma_interleaved_RGB;
+    cs = heif_colorspace_RGB; // TODO: can I do it or do I have to match alpha?
+    err = heif_decode_image(hdl, &img, cs, chroma, nullptr);
 
     if (err.code != heif_error_Ok) {
         goto Exit;
     }
 
-    data = heif_image_get_plane_readonly(img, heif_channel_interleaved, &stride);
+    data = heif_image_get_plane_readonly(img, heif_channel_interleaved, &srcStride);
+    if (!data) {
+        goto Exit;
+    }
+
+    bmp = new Gdiplus::Bitmap(dx, dy, PixelFormat32bppRGB);
+    {
+        Gdiplus::Rect bmpRect(0, 0, dx, dy);
+        Gdiplus::BitmapData bmpData;
+        Gdiplus::Status ok = bmp->LockBits(&bmpRect, Gdiplus::ImageLockModeWrite, PixelFormat32bppRGB, &bmpData);
+        if (ok != Gdiplus::Ok) {
+            return nullptr;
+        }
+        int dstStride = bmpData.Stride;
+        u8* src = (u8*)data;
+        u8* dst = (u8*)bmpData.Scan0;
+        for (int i = 0; i < dy; i++) {
+            u8* srcTmp = src;
+            u8* dstTmp = dst;
+            // TODO: memcpy?
+            for (int j = 0; j < dx; j++) {
+                *dst++ = src[2];
+                *dst++ = src[1];
+                *dst++ = src[0];
+                dst++;
+                src += 3;
+            }
+            src = srcTmp + srcStride;
+            dst = dstTmp + dstStride;
+        }
+        bmp->UnlockBits(&bmpData);
+    }
 
 Exit:
+    if (img) {
+        heif_image_release(img);
+    }
     if (hdl) {
         heif_image_handle_release(hdl);
     }
