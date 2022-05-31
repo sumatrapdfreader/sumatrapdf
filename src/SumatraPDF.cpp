9c02cb34ce370fa63c13ed0b11abd633f15b373d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-05-31 10:38:02 -0400

    show .heic and .avif images in File Open dialog (for #1520)

--- src/SumatraPDF.cpp
+++ src/SumatraPDF.cpp
@@ -3072,7 +3072,7 @@ static void OnMenuOpen(MainWindow* win) {
         {_TR("Mobi documents"), L"*.mobi", true},
         {_TR("FictionBook documents"), L"*.fb2;*.fb2z;*.zfb2;*.fb2.zip", true},
         {_TR("PalmDoc documents"), L"*.pdb;*.prc", true},
-        {_TR("Images"), L"*.bmp;*.dib;*.gif;*.jpg;*.jpeg;*.jxr;*.png;*.tga;*.tif;*.tiff;*.webp", true},
+        {_TR("Images"), L"*.bmp;*.dib;*.gif;*.jpg;*.jpeg;*.jxr;*.png;*.tga;*.tif;*.tiff;*.webp;*.heic;*.avif", true},
         {_TR("Text documents"), L"*.txt;*.log;*.nfo;file_id.diz;read.me;*.tcr", true},
     };
     // Prepare the file filters (use \1 instead of \0 so that the
