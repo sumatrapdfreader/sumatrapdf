821e7c8515a90b078cbf96d10ff5aa6d52ba7354
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-06-04 02:42:13 -0400

    more TabsCtrl refactor

--- src/Tabs.h
+++ src/Tabs.h
@@ -11,7 +11,6 @@ TabInfo* CreateNewTab(MainWindow* win, const char* filePath);
 void TabsOnCloseDoc(MainWindow* win);
 void TabsOnCloseWindow(MainWindow* win);
 void TabsOnChangedDoc(MainWindow* win);
-LRESULT TabsOnNotify(MainWindow* win, LPARAM lp, int tab1 = -1, int tab2 = -1);
 void TabsSelect(MainWindow* win, int tabIndex);
 void TabsOnCtrlTab(MainWindow* win, bool reverse);
 // also shows/hides the tabbar when necessary
