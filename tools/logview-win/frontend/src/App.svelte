2d689e5b4e727c80d15b221e89abdaeee498e39b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-04-04 03:18:15 -0400

    more logview-win

--- tools/logview-win/frontend/src/App.svelte
+++ tools/logview-win/frontend/src/App.svelte
@@ -1,29 +1,143 @@
 <script>
-  let logs = ["Logview SumatraPDF"];
+  import { afterUpdate } from "svelte";
+
+  let idx = 2;
+  /** @type {[string, number][]} */
+  let logs = [["Logview SumatraPDF", 1]];
+  let filteredLogs = [];
+  let autoScrollPaused = false;
+  let btnText = "pause scrolling";
+  let searchTerm = "";
+  let element;
+
+  /**
+   * @param {string} s
+   * @param {string} searchTerm
+   */
+  function matches(s, searchTerm) {
+    return s.includes(searchTerm);
+  }
+
+  /**
+   * @param {string} s
+   * @param {number} no
+   */
   function plog(s, no) {
-    console.log("plog:", s);
-    logs.push(s);
-    logs = logs;
+    /** @type {[string, number]}*/
+    let el = [s, idx];
+    idx = idx + 1;
+    logs.push(el);
+    if (searchTerm === "") {
+      filteredLogs = logs;
+      return;
+    }
+    if (matches(s, searchTerm)) {
+      filteredLogs.push(el);
+    }
+    filteredLogs = filteredLogs;
+  }
+
+  $: filterLogs(searchTerm);
+
+  /**
+   * @param {string} searchTerm
+   */
+  function filterLogs(searchTerm) {
+    searchTerm = searchTerm.trim();
+    if (searchTerm === "") {
+      filteredLogs = logs;
+      return;
+    }
+    let res = [];
+    for (let el of logs) {
+      if (matches(el[0], searchTerm)) {
+        res.push(el);
+      }
+    }
+    filteredLogs = res;
+  }
+
+  /**
+   * @param {HTMLElement} node
+   */
+  function scrollToBottom(node) {
+    node.scroll({ top: node.scrollHeight, behavior: "smooth" });
+  }
+
+  afterUpdate(() => {
+    if (autoScrollPaused) {
+      return;
+    }
+    scrollToBottom(element);
+  });
+
+  function pauseClicked() {
+    autoScrollPaused = !autoScrollPaused;
+    if (autoScrollPaused) {
+      btnText = "unpause scrolling";
+    } else {
+      btnText = "pause scrolling";
+    }
+  }
+
+  function len(o) {
+    return o ? o.length : 0;
+  }
+  function clearLogs() {
+    logs = [["Logview SumatraPDF", 1]];
+    filteredLogs = logs;
   }
   // @ts-ignore
   window.runtime.EventsOn("plog", plog);
 </script>
 
 <main>
-  {#each logs as log}
-    <span>{log}</span><br />
-  {/each}
+  <div class="top">
+    <input type="text" placeholder="search term..." bind:value={searchTerm} />
+    <button class="btn-pause" on:click={pauseClicked}>{btnText}</button>
+    <button on:click={clearLogs}>clear</button>
+  </div>
+  <div bind:this={element} class="log">
+    {#if len(filteredLogs) == 0}
+      <div class="no-results">No results matching '<b>{searchTerm}</b>'</div>
+    {:else}
+      {#each filteredLogs as log (log[1])}
+        <span>{log[0]}</span><br />
+      {/each}
+    {/if}
+  </div>
 </main>
 
 <style>
   main {
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 1rem);
     padding: 0.5rem 0.5rem;
+    overflow: auto;
+  }
+  .no-results {
+    text-align: center;
+    font-size: 120%;
+    margin-top: 30%; /* from eyeballing */
+    /* background-color: bisque; */
+  }
+  .btn-pause {
+    min-width: 8rem;
+  }
+  .top {
+    display: flex;
+    justify-content: center;
+    column-gap: 0.5rem;
+  }
+  .log {
+    overflow: auto;
+    margin-top: 0.5rem;
+    /* background: rgb(239, 250, 254); */
+    height: 100%;
+    background-color: rgb(255, 255, 255);
   }
   span {
     font-family: monospace;
-  }
-  pre {
-    margin: 0;
-    word-wrap: break-word;
   }
 </style>
