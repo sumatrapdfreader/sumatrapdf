fbfa96208c828ed65260b15075eead8067cbc500
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-04-04 00:58:20 -0400

    port logview to github.com/rodrigocfd/windigo; start on logview-win

--- tools/logview/go.mod
+++ tools/logview/go.mod
@@ -1,3 +1,5 @@
 module logview
 
 go 1.20
+
+require github.com/rodrigocfd/windigo v0.0.0-20230404011343-49bfda936fb0 // indirect
--- tools/logview/logview.go
+++ tools/logview/logview.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"sync"
+
+	"github.com/kjk/common/u"
+	"github.com/rodrigocfd/windigo/win"
+	"github.com/rodrigocfd/windigo/win/co"
 )
 
 const kPipeName = "\\\\.\\pipe\\SumatraPDFLogger"
 const kBufSize = 1024 * 16
+const PIPE_UNLIMITED_INSTANCES = 255
+const INVALID_HANDLE_VALUE = -1
 
 var wc sync.WaitGroup
 
-func handlePipe(hPipe HFILE) {
+func handlePipe(hPipe win.HPIPE) {
 	fmt.Printf("handlePipe\n")
 	var buf [kBufSize]byte
 	var cbBytesRead = 0
@@ -24,29 +30,37 @@ func handlePipe(hPipe HFILE) {
 		d := buf[:int(n)]
 		fmt.Printf("%s", string(d))
 	}
-	DisconnectNamedPipe(hPipe)
+	hPipe.DisconnectNamedPipe()
 	hPipe.CloseHandle()
 	wc.Done()
 }
 
-func main() {
-	fmt.Printf("Hello! This is a PipeView\n")
-	var hPipe HFILE
+func createNamedPipe() (win.HPIPE, error) {
+	const openMode = co.PIPE_ACCESS_INBOUND
+	const mode = co.PIPE_TYPE_MESSAGE | co.PIPE_READMODE_MESSAGE | co.PIPE_WAIT
+	const maxInstances = PIPE_UNLIMITED_INSTANCES
+	return win.CreateNamedPipe(kPipeName, openMode, mode, maxInstances, kBufSize, kBufSize, 0, nil)
 
+}
+
+func IsValidHandle(h win.HANDLE) bool {
+	invalid := h == 0 || int(h) == INVALID_HANDLE_VALUE
+	return !invalid
+}
+
+func main() {
+	fmt.Printf("Logview for SumatraPDF\n")
+	var hPipe win.HPIPE
+	var err error
 	for {
-		const openMode = PIPE_ACCESS_INBOUND
-		const mode = PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT
-		const maxInstances = PIPE_UNLIMITED_INSTANCES
-		hPipe = CreateNamedPipe(kPipeName, openMode, mode, maxInstances, kBufSize, kBufSize, 0)
-		if !IsValidHandle(HANDLE(hPipe)) {
+		hPipe, err = createNamedPipe()
+		u.Must(err)
+		if !IsValidHandle(win.HANDLE(hPipe)) {
 			fmt.Printf("couldn't open pipe\n")
 			return
 		}
-		ok := ConnectNamedPipe(hPipe)
-		// if !ok {
-		// ok = GetLastError() == ERROR_PIPE_CONNECTED
-		// }
-		if !ok {
+		err = hPipe.ConnectNamedPipe()
+		if err != nil {
 			fmt.Printf("client couldn't connect to our pipe\n")
 			hPipe.CloseHandle()
 			continue
