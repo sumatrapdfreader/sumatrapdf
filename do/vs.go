7288b46e7f39f4ff2ac303f488f3baf540e48d9a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-04-25 13:36:37 +0200

    hopefully support building with vs 2019 in github actions

--- do/vs.go
+++ do/vs.go
@@ -15,16 +15,24 @@ var (
 		"10.0.20348.0",
 		"10.0.19041.0",
 		"10.0.18362.0",
+		"10.0.17134.0",
+		"10.0.16299.0",
+		"10.0.15063.0",
 	}
 
 	msBuildName = `MSBuild\Current\Bin\MSBuild.exe`
 
 	vsBasePaths = []string{
-		// https://github.com/actions/virtual-environments/blob/main/images/win/Windows2022-Readme.md
+		// https://github.com/actions/runner-images/blob/main/images/windows/Windows2022-Readme.md
 		`C:\Program Files\Microsoft Visual Studio\2022\Enterprise`,
 		`C:\Program Files\Microsoft Visual Studio\2022\Preview`,
 		`C:\Program Files\Microsoft Visual Studio\2022\Community`,
 		`C:\Program Files\Microsoft Visual Studio\2022\Professional`,
+		// https://github.com/actions/runner-images/blob/main/images/windows/Windows2019-Readme.md
+		`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise`,
+		`C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional`,
+		`C:\Program Files (x86)\Microsoft Visual Studio\2019\Community`,
+		`C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview`,
 	}
 )
 
