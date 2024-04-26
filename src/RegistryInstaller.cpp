632599c5026427f6a67b31467615ead81e7e227f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-04-26 16:01:26 +0200

    register for .webp files

--- src/RegistryInstaller.cpp
+++ src/RegistryInstaller.cpp
@@ -21,7 +21,7 @@ static SeqStrings gSupportedExts =
     ".pdf\0.xps\0.oxps\0.cbz\0.cbr\0.cb7\0.cbt\0" \
     ".djvu\0.chm\0.mobi\0.epub\0.azw\0.azw3\0.azw4\0" \
     ".fb2\0.fb2z\0.prc\0.tif\0.tiff\0.jp2\0.png\0" \
-    ".jpg\0.jpeg\0.tga\0.gif\0.avif\0.heic\0";
+    ".jpg\0.jpeg\0.tga\0.gif\0.avif\0.heic\0.webp\0";
 // clang-format on
 
 // notifies Shell that file associations changed.
