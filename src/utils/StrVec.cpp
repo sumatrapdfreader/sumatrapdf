d0b606e392e1ff80fd7b68905edcbabcfadd19fa
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-05-31 20:51:41 +0200

    optimize StrVec::FindI() by matching size first

--- src/utils/StrVec.cpp
+++ src/utils/StrVec.cpp
@@ -578,11 +578,11 @@ int StrVec::Find(const char* s, int startAt) const {
 }
 
 int StrVec::FindI(const char* s, int startAt) const {
+    int sLen = str::Leni(s);
     auto end = this->end();
     for (auto it = this->begin() + startAt; it != end; it++) {
-        // TODO(perf): check length firsts
-        char* s2 = *it;
-        if (str::EqI(s, s2)) {
+        StrSpan s2 = it.Span();
+        if (s2.Len() == sLen && str::EqI(s, s2.CStr())) {
             return it.idx;
         }
     }
