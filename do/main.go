0407f9db240946e8bada0399b3bc683e708e4e93
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-08-09 15:13:26 +0200

    change -gen-website-docs => -gen-docs-website

--- do/main.go
+++ do/main.go
@@ -296,7 +296,7 @@ func main() {
 		flag.IntVar(&flgBuildNo, "build-no-info", 0, "print build number info for given build number")
 		flag.BoolVar(&flgUpdateGoDeps, "update-go-deps", false, "update go dependencies")
 		flag.BoolVar(&flgGenDocs, "gen-docs", false, "generate html docs in docs/www from markdown in docs/md")
-		flag.BoolVar(&flgGenWebsiteDocs, "gen-website-docs", false, "generate html docs in ../sumatra-website repo and check them in")
+		flag.BoolVar(&flgGenWebsiteDocs, "gen-docs-website", false, "generate html docs in ../sumatra-website repo and check them in")
 		flag.Parse()
 	}
 
