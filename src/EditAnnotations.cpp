22e0bf8f27dea12ff4ef701daa7c39ab84d4420d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-08-13 11:43:00 +0200

    fix crash in ShowEditAnnotationsWindow in response to CmdEditAnnotations

--- src/EditAnnotations.cpp
+++ src/EditAnnotations.cpp
@@ -1428,7 +1428,13 @@ static void CreateMainLayout(EditAnnotationsWindow* ew) {
 }
 
 void ShowEditAnnotationsWindow(WindowTab* tab) {
-    ReportIf(!tab->AsFixed()->GetEngine());
+    auto dm = tab ? tab->AsFixed() : nullptr;
+    auto engine = dm ? dm->GetEngine() : nullptr;
+    auto canAnnotate = EngineSupportsAnnotations(engine);
+    if (!canAnnotate) {
+        ReportDebugIf(true);
+        return;
+    }
     EditAnnotationsWindow* ew = tab->editAnnotsWindow;
     if (ew) {
         HwndMakeVisible(ew->hwnd);
