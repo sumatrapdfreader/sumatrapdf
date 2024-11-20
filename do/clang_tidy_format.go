22c1c422d6b6ecca8b33576facbf0d47285fd37c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-11-20 23:16:19 +0100

    merge clang_format.go into clang_tidy.go

--- do/clang_tidy_format.go
+++ do/clang_tidy_format.go
@@ -4,7 +4,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 /*
@@ -223,4 +225,83 @@ func runClangTidy(fix bool) {
 		}
 	}
 	logf("\nLogged output to '%s'\n", clangTidyLogFile)
+}
+
+var printClangPath bool
+
+func detectClangFormat() string {
+	path := detectPath(vsBasePaths, `VC\Tools\Llvm\bin\clang-format.exe`)
+	panicIf(!fileExists(path), "didn't find clang-format.exe")
+	if !printClangPath {
+		logf("clang-format: %s\n", path)
+		printClangPath = true
+	}
+	return path
+}
+
+func clangFormatFile(path string) {
+	clangFormatPath := detectClangFormat()
+	cmd := exec.Command(clangFormatPath, "-i", "-style=file", path)
+	runCmdLoggedMust(cmd)
+}
+
+func clangFormatFiles() {
+	path := detectClangFormat()
+	logf("using '%s'\n", path)
+	files := []string{
+		`src\*.cpp`,
+		`src\*.h`,
+		`src\mui\*.cpp`,
+		`src\mui\*.h`,
+		`src\utils\*.cpp`,
+		`src\utils\*.h`,
+		`src\utils\tests\*.cpp`,
+		`src\utils\tests\*.h`,
+		`src\wingui\*`,
+		`src\uia\*`,
+		`src\tools\*`,
+		`src\ifilter\*.cpp`,
+		`src\iflter\*.h`,
+		`src\previewer\*.cpp`,
+		`src\previewer\*.h`,
+		`ext\CHMLib\*.c`,
+		`ext\CHMLib\*.h`,
+		`ext\mupdf_load_system_font.c`,
+	}
+	isWhiteListed := func(s string) bool {
+		whitelisted := []string{
+			"resource.h",
+			"Version.h",
+			"TranslationLangs.cpp",
+		}
+		s = strings.ToLower(s)
+		for _, wl := range whitelisted {
+			wl = strings.ToLower(wl)
+			if strings.Contains(s, wl) {
+				logf("Whitelisted '%s'\n", s)
+				return true
+			}
+		}
+		return false
+	}
+	sem := make(chan bool, runtime.NumCPU())
+	var wg sync.WaitGroup
+	for _, globPattern := range files {
+		paths, err := filepath.Glob(globPattern)
+		must(err)
+		for _, path := range paths {
+			if isWhiteListed(path) {
+				continue
+			}
+			sem <- true
+			wg.Add(1)
+			go func(p string) {
+				clangFormatFile(p)
+				wg.Done()
+				<-sem
+			}(path)
+		}
+	}
+	wg.Wait()
+	logf("used '%s'\n", path)
 }
