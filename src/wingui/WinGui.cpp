634c84324768d341b44b7bd220b22440e86931da
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2025-06-07 13:33:31 +0200

    maybe fix crash when dragging tabs (for #4572)

--- src/wingui/WinGui.cpp
+++ src/wingui/WinGui.cpp
@@ -3255,6 +3255,9 @@ void TabsCtrl::Paint(HDC hdc, RECT& rc) {
 
 HBITMAP TabsCtrl::RenderForDragging(int idx) {
     TabInfo* ti = GetTab(idx);
+    if (!ti) {
+        return nullptr;
+    }
     Bitmap bitmap(ti->r.dx, ti->r.dy);
     Graphics* gfx = Graphics::FromImage(&bitmap);
     // DrawString() on a bitmap does not work with CompositingModeSourceCopy - obscure bug.
@@ -3483,6 +3486,10 @@ LRESULT TabsCtrl::WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
                 draggingTab = true;
                 TabInfo* thl = GetTab(hl);
                 HBITMAP hbmp = RenderForDragging(hl);
+                if (!hbmp) {
+                    logfa("TabsCtrl::WndProc: RenderForDragging failed for tab %d\n", hl);
+                    return 0;
+                }
                 HIMAGELIST himl = ImageList_Create(thl->r.dx, thl->r.dy, 0, 1, 0);
                 ImageList_Add(himl, hbmp, NULL);
                 ImageList_BeginDrag(himl, 0, grabLocation.x, grabLocation.y);
