4e772b0cd12169845b16467ab0f2847adbd48cd9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-03-17 18:03:36 +0100

    update version to 3.6.1

--- src/Version.h
+++ src/Version.h
@@ -3,10 +3,10 @@
 
 // CURR_VERSION can be over-written externally
 #ifndef CURR_VERSION
-#define CURR_VERSION 3.6
+#define CURR_VERSION 3.6.1
 #endif
 #ifndef CURR_VERSION_COMMA
-#define CURR_VERSION_COMMA 3,6,0
+#define CURR_VERSION_COMMA 3,6,1
 #endif
 
 // this is sth. like "3.5"
