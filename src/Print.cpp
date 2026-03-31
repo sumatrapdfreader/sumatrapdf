56dfc2f7907b3c2312e22a5fbd2d264ea46576d8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-04-01 01:28:51 +0200

    maybe fix print crash

--- src/Print.cpp
+++ src/Print.cpp
@@ -788,6 +788,7 @@ void PrintCurrentFile(MainWindow* win, bool waitForCompletion) {
         return;
     }
     auto engine = dm->GetEngine();
+    EngineBase* pinnedEngine = nullptr;
     ReportIf(!engine);
     if (!engine) {
         return;
@@ -865,6 +866,8 @@ void PrintCurrentFile(MainWindow* win, bool waitForCompletion) {
     if (!engine) {
         goto Exit;
     }
+    pinnedEngine = engine;
+    pinnedEngine->AddRef();
     rotation = dm->GetRotation();
     nPages = dm->PageCount();
 
@@ -945,7 +948,8 @@ void PrintCurrentFile(MainWindow* win, bool waitForCompletion) {
     }
 
     sel = printSelection ? win->CurrentTab()->selectionOnPage : nullptr;
-    pd = new PrintData(engine, printer, ranges, advanced, rotation, sel);
+    pd = new PrintData(pinnedEngine, printer, ranges, advanced, rotation, sel);
+    SafeEngineRelease(&pinnedEngine);
 
     if (!waitForCompletion && !pd->failedEngineClone) {
         PrintToDeviceOnThread(win, pd);
@@ -955,6 +959,7 @@ void PrintCurrentFile(MainWindow* win, bool waitForCompletion) {
     }
 
 Exit:
+    SafeEngineRelease(&pinnedEngine);
     free(ppr);
     GlobalFree(pdex.hDevNames);
     GlobalFree(pdex.hDevMode);
