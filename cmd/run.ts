3138b98115bf9808942059f5a035e3b09bbb5a3a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-06-17 15:30:46 +0200

    add cmd/run.ts

---
+++ cmd/run.ts
@@ -0,0 +1,35 @@
+import { join } from "node:path";
+import { detectVisualStudio2026, runLogged } from "./util";
+import { clearDirPreserveSettings } from "./clean";
+
+let clean = false;
+
+let t = `/t:SumatraPDF`;
+t = `/t:SumatraPDF-dll`;
+
+async function main() {
+  const timeStart = performance.now();
+
+  console.log("debug build");
+  if (clean) {
+    const dirs = [join("out", "dbg64")];
+    for (const dir of dirs) {
+      clearDirPreserveSettings(dir);
+    }
+  }
+
+  const { msbuildPath } = detectVisualStudio2026();
+  const sln = String.raw`vs2022\SumatraPDF.sln`;
+  // const t = `/t:SumatraPDF;test_util`;
+  const p = `/p:Configuration=Debug;Platform=x64`;
+  await runLogged(msbuildPath, [sln, t, p, `/m`]);
+  const elapsed = ((performance.now() - timeStart) / 1000).toFixed(1);
+  console.log(`build took ${elapsed}s`);
+
+  const path = join("out", "dbg64", "SumatraPDF-dll.exe");
+  Bun.spawn([path], {
+    cwd: ".",
+  });
+}
+
+await main();
