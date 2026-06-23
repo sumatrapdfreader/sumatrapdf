sumatrapdfreader/sumatrapdf

commit a1bcf6249d97d4a2842bd05c732e16c62bdee88a

Replace StrSpan with Str; tighten IsEmpty helpers
Remove StrSpan in favor of Str with direct .s/.len access (AtStr, AsStr). Add templated IsEmpty(v) for len==0 with Str/WStr specializations for null pointers. Keep const char* Find/FindI overloads on StrVec.

prompt: Replace usage of StrSpan with Str. Add templated bool IsEmpty(v) which check v.len or v->len == 0. Add specialization bool IsEmpty(const Str&) which is v.len == 0 || v.s == 0 and use it instead of StrSpan.IsEmpty(). Convert other StrSpan methods to direct data access.

src/utils/StrVec.cpp
@@ -57,7 +57,7 @@ struct StrVecPage {
     // strings (allocated from the end)
 
     char* At(int) const;
-    StrSpan AtSpan(int i) const;
+    Str AtStr(int i) const;
     void* AtDataRaw(int) const;
 
     char* RemoveAt(int);
@@ -66,7 +66,7 @@ struct StrVecPage {
     char* AtHelper(int, int* sLen) const;
     int BytesLeft();
     char* Append(const char* s, int sLen);
-    char* Append(const StrSpan&);
+    char* Append(const Str&);
     char* SetAt(int idxSet, const char* s, int sLen);
     char* InsertAt(int idxSet, const char* s, int sLen);
 };
@@ -198,8 +198,8 @@ char* StrVecPage::InsertAt(int idx, const char* s, int sLen) {
     return (char*)s;
 }
 
-char* StrVecPage::Append(const StrSpan& s) {
-    return InsertAt(nStrings, s.CStr(), s.Len());
+char* StrVecPage::Append(const Str& s) {
+    return InsertAt(nStrings, s.s, s.len);
 }
 
 char* StrVecPage::Append(const char* s, int sLen) {
@@ -227,10 +227,10 @@ char* StrVecPage::At(int idx) const {
     return s;
 }
 
-StrSpan StrVecPage::AtSpan(int idx) const {
+Str StrVecPage::AtStr(int idx) const {
     int sLen = 0;
     char* s = AtHelper(idx, &sLen);
-    return {s, sLen};
+    return Str(s, sLen);
 }
 
 void* StrVecPage::AtDataRaw(int idx) const {
@@ -309,14 +309,14 @@ static StrVecPage* CompactStrVecPages(StrVecPage* first, int extraSize) {
     pageSize = RoundUp(pageSize, 64); // jic
     auto page = AllocStrVecPage(pageSize, dataSize);
     int n;
-    StrSpan s;
+    Str s;
     curr = first;
     int nStr = 0;
     while (curr) {
         n = curr->nStrings;
         // TODO(perf): could optimize slightly
         for (int i = 0; i < n; i++) {
-            s = curr->AtSpan(i);
+            s = curr->AtStr(i);
             page->Append(s);
             if (dataSize > 0) {
                 void* dst = page->AtDataRaw(nStr);
@@ -580,13 +580,13 @@ char* StrVec::At(int idx) const {
     return page->At(idxInPage);
 }
 
-StrSpan StrVec::AtSpan(int idx) const {
+Str StrVec::AtStr(int idx) const {
     if (sortIndexes) {
         idx = sortIndexes[idx];
     }
     int idxInPage;
     auto page = PageForIdx(this, idx, &idxInPage);
-    return page->AtSpan(idxInPage);
+    return page->AtStr(idxInPage);
 }
 
 void* StrVec::AtDataRaw(int idx) const {
@@ -604,24 +604,32 @@ char* StrVec::operator[](int idx) const {
     return At(idx);
 }
 
-int StrVec::Find(const StrSpan& s, int startAt) const {
-    int sLen = s.Len();
+int StrVec::Find(const char* s, int startAt) const {
+    return Find(Str((char*)s), startAt);
+}
+
+int StrVec::FindI(const char* s, int startAt) const {
+    return FindI(Str((char*)s), startAt);
+}
+
+int StrVec::Find(const Str& s, int startAt) const {
+    int sLen = s.len;
     auto end = this->end();
     for (auto it = this->begin() + startAt; it != end; it++) {
-        StrSpan s2 = it.Span();
-        if (s2.Len() == sLen && str::Eq(s.CStr(), s2.CStr())) {
+        Str s2 = it.AsStr();
+        if (s2.len == sLen && str::Eq(s.s, s2.s)) {
             return it.idx;
         }
     }
     return -1;
 }
 
-int StrVec::FindI(const StrSpan& s, int startAt) const {
-    int sLen = s.Len();
+int StrVec::FindI(const Str& s, int startAt) const {
+    int sLen = s.len;
     auto end = this->end();
     for (auto it = this->begin() + startAt; it != end; it++) {
-        StrSpan s2 = it.Span();
-        if (s2.Len() == sLen && str::EqI(s.CStr(), s2.CStr())) {
+        Str s2 = it.AsStr();
+        if (s2.len == sLen && str::EqI(s.s, s2.s)) {
             return it.idx;
         }
     }
@@ -629,7 +637,7 @@ int StrVec::FindI(const StrSpan& s, int startAt) const {
 }
 
 bool StrVec::Contains(const char* s, int sLen) const {
-    StrSpan span(s, sLen);
+    Str span((char*)s, sLen < 0 ? str::Leni(s) : sLen);
     int idx = Find(span);
     return idx != -1;
 }
@@ -661,11 +669,11 @@ char* StrVec::iterator::operator*() const {
     return page->At(idxInPage);
 }
 
-StrSpan StrVec::iterator::Span() const {
+Str StrVec::iterator::AsStr() const {
     if (this->v->sortIndexes) {
-        return v->AtSpan(idx);
+        return v->AtStr(idx);
     }
-    return page->AtSpan(idxInPage);
+    return page->AtStr(idxInPage);
 }
 
 static void AdvanceStrVecIter(StrVec::iterator& it, int n) {
@@ -836,8 +844,8 @@ static int CalcCapForJoin(const StrVec* v, const char* joint) {
     int cap = 0;
     int jointLen = str::Leni(joint);
     for (auto it = v->begin(); it != v->end(); it++) {
-        auto s = it.Span();
-        cap += s.Size() + 1 + jointLen;
+        Str s = it.AsStr();
+        cap += s.len + 1 + jointLen;
     }
     return cap + 32; // +32 arbitrary buffer
 }
@@ -848,8 +856,8 @@ static char* JoinInner(const StrVec* v, const char* joint, StrBuilder& res) {
     int firstForJoint = 0;
     int i = 0;
     for (auto it = v->begin(); it != v->end(); it++) {
-        auto s = it.Span();
-        if (!s.CStr()) {
+        Str s = it.AsStr();
+        if (IsEmpty(s)) {
             firstForJoint++;
             i++;
             continue;
@@ -857,7 +865,7 @@ static char* JoinInner(const StrVec* v, const char* joint, StrBuilder& res) {
         if (i > firstForJoint && jointLen > 0) {
             res.Append(joint, jointLen);
         }
-        res.Append(s.CStr(), s.Len());
+        res.Append(s.s, s.len);
         i++;
     }
     return res.StealData();

src/Translations.cpp
@@ -70,12 +70,12 @@ static void FreeTranslations() {
     gTranslationCache = nullptr;
 }
 
-static void ParseTranslationsTxt(const StrSpan& d, const char* langCode) {
+static void ParseTranslationsTxt(Str d, const char* langCode) {
     langCode = str::JoinTemp(langCode, ":");
     int nLangCode = str::Len(langCode);
 
     StrVec lines;
-    Split(&lines, d.CStr(), "\n", true);
+    Split(&lines, d.s, "\n", true);
     int nStrings = 0;
     for (char* l : lines) {
         if (l[0] == ':') {
@@ -144,8 +144,8 @@ const char* GetTranslation(const char* s) {
     int sLen = str::Leni(s);
     for (int i = 0; i < n; i++) {
         int idx = i * 2;
-        StrSpan s2 = c->AtSpan(idx);
-        if (s2.Len() == sLen && str::Eq(s, s2.CStr())) {
+        Str s2 = c->AtStr(idx);
+        if (s2.len == sLen && str::Eq(s, s2.s)) {
             auto tr = c->At(idx + 1);
             if (!tr) {
                 logf("Didn't find translation for '%s'\n", s);
@@ -225,7 +225,7 @@ void SetCurrentLangByCode(const char* langCode) {
         return;
     }
     int dataSize = (int)(archive.files[fileIdx].uncompressedSize);
-    StrSpan d = {(char*)data, dataSize};
+    Str d((char*)data, dataSize);
     ParseTranslationsTxt(d, langCode);
     free(data);
 }

src/common/common.h
@@ -306,25 +306,25 @@ Str SmartResolveDirectory(Str dir);
 // Works for any struct with int len member (Str, WStr, *Vec, etc.)
 template <typename T>
 inline bool IsEmpty(const T& v) {
-    return v.len <= 0;
+    return v.len == 0;
 }
 template <typename T>
 inline bool IsEmpty(const T* v) {
-    return !v || v->len <= 0;
+    return !v || v->len == 0;
 }
 
 // specialized for Str and WStr
 inline bool IsEmpty(const Str& v) {
-    return !v.s || v.len <= 0;
+    return v.len == 0 || !v.s;
 }
 inline bool IsEmpty(const Str* v) {
-    return !v || !v->s || v->len <= 0;
+    return !v || v->len == 0 || !v->s;
 }
 inline bool IsEmpty(const WStr& v) {
-    return !v.s || v.len <= 0;
+    return v.len == 0 || !v.s;
 }
 inline bool IsEmpty(const WStr* v) {
-    return !v || !v->s || v->len <= 0;
+    return !v || v->len == 0 || !v->s;
 }
 
 // dir_util.cpp

src/utils/StrUtil.cpp
@@ -194,32 +194,6 @@ bool IsEqual(const ByteSlice& d1, const ByteSlice& d2) {
     return res == 0;
 }
 
-StrSpan::StrSpan(const char* s) {
-    d = (char*)s;
-    size = str::Leni(s);
-}
-
-StrSpan::StrSpan(const char* s, int sLen) {
-    d = (char*)s;
-    if (sLen < 0) {
-        size = str::Leni(s);
-    } else {
-        size = sLen;
-    }
-}
-
-bool IsEqual(const StrSpan& d1, const StrSpan& d2) {
-    if (d1.Len() != d2.Len()) {
-        return false;
-    }
-    if (d1.Len() == 0) {
-        return true;
-    }
-    ReportIf(!d1.d || !d2.d);
-    int res = memcmp(d1.d, d2.d, d1.Len());
-    return res == 0;
-}
-
 namespace str {
 
 size_t Len(const char* s) {
@@ -250,8 +224,8 @@ void Free(const WCHAR* s) {
     free((void*)s);
 }
 
-void Free(const StrSpan& s) {
-    free(s.CStr());
+void Free(const Str& s) {
+    free(s.s);
 }
 
 void FreePtr(const char** s) {
@@ -1558,8 +1532,8 @@ bool StrBuilder::AppendChar(char c) {
     return InsertAt(len, c);
 }
 
-bool StrBuilder::Append(const StrSpan& s) {
-    return Append(s.CStr(), (size_t)s.Len());
+bool StrBuilder::Append(const Str& s) {
+    return Append(s.s, (size_t)s.len);
 }
 
 bool StrBuilder::Append(const char* src, size_t count) {

src/utils/StrUtil.h
@@ -12,20 +12,6 @@ bool isLegalUTF8String(const u8** source, const u8* sourceEnd);
 int utf8StrLen(const u8* s);
 int utf8RuneLen(const u8* s);
 
-struct StrSpan {
-    char* d = nullptr;
-    int size = 0;
-
-    StrSpan() = default;
-    StrSpan(const char* s);
-    StrSpan(const char* s, int sLen);
-
-    int Len() const { return size; }
-    int Size() const { return size; }
-    char* CStr() const { return d; }
-    bool IsEmpty() const { return !d || size == 0; }
-};
-
 struct ByteSlice {
     u8* d = nullptr;
     size_t sz = 0;
@@ -100,7 +86,7 @@ int Leni(const char* s);
 
 void Free(const char*);
 void Free(const u8*);
-void Free(const StrSpan& s);
+void Free(const Str& s);
 
 void Free(const WCHAR* s);
 
@@ -289,7 +275,7 @@ struct StrBuilder {
     bool InsertAt(size_t idx, char el);
     bool AppendChar(char c);
     bool Append(const char* src, size_t count = -1);
-    bool Append(const StrSpan&);
+    bool Append(const Str&);
     bool Append(const StrBuilder& s);
     char RemoveAt(size_t idx, size_t count = 1);
     char RemoveLast();

src/utils/StrVec.h
@@ -31,7 +31,7 @@ struct StrVec {
     int Size() const;
     bool IsEmpty() const;
     char* At(int i) const;
-    StrSpan AtSpan(int i) const;
+    Str AtStr(int i) const;
     void* AtDataRaw(int i) const;
     char* operator[](int) const;
 
@@ -42,8 +42,10 @@ struct StrVec {
     char* RemoveAtFast(int);
     bool Remove(const char*);
 
-    int Find(const StrSpan&, int startAt = 0) const;
-    int FindI(const StrSpan&, int startAt = 0) const;
+    int Find(const Str&, int startAt = 0) const;
+    int Find(const char* s, int startAt = 0) const;
+    int FindI(const Str&, int startAt = 0) const;
+    int FindI(const char* s, int startAt = 0) const;
     bool Contains(const char*, int sLen = -1) const;
 
     struct iterator {
@@ -56,7 +58,7 @@ struct StrVec {
 
         iterator(const StrVec* v, int idx);
         char* operator*() const;
-        StrSpan Span() const;
+        Str AsStr() const;
         iterator& operator++();   // ++it
         iterator operator++(int); // it++
         iterator& operator+(int); // it += n
@@ -76,8 +78,8 @@ struct StrVecWithData : StrVec {
         return (T*)(res);
     }
 
-    int Append(const StrSpan& s, const T& data) {
-        StrVec::Append(s.CStr(), s.Size());
+    int Append(const Str& s, const T& data) {
+        StrVec::Append(s.s, s.len);
         int idx = Size() - 1;
         T* d = AtData(idx);
         *d = data;
@@ -85,13 +87,13 @@ struct StrVecWithData : StrVec {
     }
 
     int Append(const char* s, const T& data) {
-        StrSpan sp(s);
+        Str sp((char*)s);
         int idx = this->Append(sp, data);
         return idx;
     }
 
     int AppendFrom(StrVecWithData<T>* src, int srcIdx) {
-        StrSpan s = src->AtSpan(srcIdx);
+        Str s = src->AtStr(srcIdx);
         T* data = src->AtData(srcIdx);
         int idx = this->Append(s, *data);
         return idx;

src/utils/tests/StrVec_ut.cpp
@@ -17,10 +17,10 @@ static void ValidateSize(StrVec* v) {
     utassert(size1 == size2);
 }
 
-static void ValidateAtSpan(StrVec* v, int idx, const char* s) {
-    StrSpan sp = v->AtSpan(idx);
-    ReportIf(!str::Eq(s, sp.d));
-    ReportIf(str::Leni(s) != sp.size);
+static void ValidateAtStr(StrVec* v, int idx, const char* s) {
+    Str sp = v->AtStr(idx);
+    ReportIf(!str::Eq(s, sp.s));
+    ReportIf(str::Leni(s) != sp.len);
 }
 
 static void strEq(const char* s1, const char* s2) {
@@ -585,22 +585,22 @@ static void InsertRandData(StrVecWithData<T>* v) {
 template <typename T>
 static void validateStringMatchesData(StrVecWithData<T>* v) {
     int nStrings = v->Size();
-    StrSpan got;
+    Str got;
     const char* exp;
     T* d;
     int n;
     for (int i = 0; i < nStrings; i++) {
         d = v->AtData(i);
         n = (int)d->n;
-        got = v->AtSpan(i);
+        got = v->AtStr(i);
         exp = StrForN(n);
-        utassert(str::Eq(got.CStr(), exp));
+        utassert(str::Eq(got.s, exp));
     }
 }
 
 template <typename T>
 static void InsertRandData2(StrVecWithData<T>* v) {
-    StrSpan got;
+    Str got;
     for (int i = 0; i < kMaxStringN; i++) {
         int op = rand() % 12;
         if (op <= 5) {
@@ -608,7 +608,7 @@ static void InsertRandData2(StrVecWithData<T>* v) {
             data.n = (decltype(data.n))i;
             const char* s = StrForN(i);
             int idx = v->Append(s, data);
-            ValidateAtSpan(v, idx, s);
+            ValidateAtStr(v, idx, s);
             T* d = v->AtData(idx);
             utassert(d->n == i);
         } else if (op <= 7) {
@@ -616,7 +616,7 @@ static void InsertRandData2(StrVecWithData<T>* v) {
                 int idx = randIdx(v);
                 const char* s = StrForN(idx);
                 v->InsertAt(idx, s);
-                ValidateAtSpan(v, idx, s);
+                ValidateAtStr(v, idx, s);
                 T* d = v->AtData(idx);
                 d->n = (decltype(d->n))idx;
             }
@@ -625,7 +625,7 @@ static void InsertRandData2(StrVecWithData<T>* v) {
                 int idx = randIdx(v);
                 const char* s = StrForN(idx);
                 v->SetAt(idx, s);
-                ValidateAtSpan(v, idx, s);
+                ValidateAtStr(v, idx, s);
                 T* d = v->AtData(idx);
                 d->n = (decltype(d->n))idx;
             }

commit 2bcf10c87e96703a4f9e845a6676a07f38d7afde

Fix StrVec::Join skipping empty strings
JoinInner used IsEmpty(s) after the StrSpan removal, which treats
zero-length strings the same as null slots. Only null entries should
be skipped so Join("a,b,,c,", ";") produces "a;b;;c;" again.

prompt: StrVecTest2_3 fails because joined is "a;b;c;"; possibly broken in a1bcf6249

src/utils/StrVec.cpp
@@ -857,7 +857,7 @@ static char* JoinInner(const StrVec* v, const char* joint, StrBuilder& res) {
     int i = 0;
     for (auto it = v->begin(); it != v->end(); it++) {
         Str s = it.AsStr();
-        if (IsEmpty(s)) {
+        if (!s.s) {
             firstForJoint++;
             i++;
             continue;