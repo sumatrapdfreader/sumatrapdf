675629726f8ca9c90856df1b5a1e4c6728f6a9c8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-08-03 18:25:15 +0200

    use str::CastToWCHAR() to silence codeql

--- src/base/Strconv.cpp
+++ src/base/Strconv.cpp
@@ -185,14 +185,15 @@ TempStr UnknownToUtf8Temp(Str s) {
     }
 
     if (str::TrimPrefix(s, Str(UTF16_BOM))) {
-        int cch = s.len / 2;
-        return ToUtf8Temp(WStr((wchar_t*)s.s, cch));
+        WStr ws = str::CastToWCHAR(s);
+        return ToUtf8Temp(ws);
     }
 
     if (str::TrimPrefix(s, Str(UTF16BE_BOM))) {
         // convert from utf16 big endian to utf16
-        int n = s.len / 2;
-        TempWStr tmpW = str::DupTemp(WStr((wchar_t*)s.s, n));
+        WStr ws = str::CastToWCHAR(s);
+        TempWStr tmpW = str::DupTemp(ws);
+        int n = ws.len;
         u8* bytes = (u8*)tmpW.s;
         for (int i = 0; i < n; i++) {
             int idx = i * (int)sizeof(WCHAR);
