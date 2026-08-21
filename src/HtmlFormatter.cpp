f2569773e87c7c8da40f969af4885af0b520a2cd
andy <andychen200822@gmail.com>
2026-08-21 13:59:09 -0700

    padding left indentation

--- src/HtmlFormatter.cpp
+++ src/HtmlFormatter.cpp
@@ -137,22 +137,11 @@ StyleRule StyleRule::Parse(CssPullParser* parser) {
     StyleRule rule;
     const CssProperty* prop;
     while ((prop = parser->NextProperty()) != nullptr) {
-        switch (prop->type) {
-            case Css_Text_Align:
-                rule.textAlign = FindAlignAttr(prop->s);
-                break;
-                
-            case Css_Text_Indent:
-                ParseSizeWithUnit(prop->s, &rule.textIndent, &rule.textIndentUnit);
-                break;
-
-            case Css_Padding_Left:
-                ParseSizeWithUnit(prop->s, &rule.textIndent, &rule.textIndentUnit);
-                break;
         if (prop->type == Css_Text_Align) {
             rule.textAlign = FindAlignAttr(prop->s);
         } else if (prop->type == Css_Text_Indent) {
-            // TODO: some documents use Css_Padding_Left for indentation
+            ParseSizeWithUnit(prop->s, &rule.textIndent, &rule.textIndentUnit);
+        } else if (prop->type == Css_Padding_Left) {
             ParseSizeWithUnit(prop->s, &rule.textIndent, &rule.textIndentUnit);
         }
     }
