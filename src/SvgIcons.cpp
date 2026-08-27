def43367dab8fb0abe156bb1868c42d067c35879
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-08-28 01:45:01 +0200

    restore transparent paperclip icon backdrop (fixes #6078)
    prompt: fix the paperclip SVG theme rendering issue by restoring its transparent backdrop

--- src/SvgIcons.cpp
+++ src/SvgIcons.cpp
@@ -459,6 +459,7 @@ const char* gIconAnnotCaret =
 // https://github.com/tabler/tabler-icons/blob/main/icons/outline/paperclip.svg
 const char* gIconAnnotFileAttachment =
     R"(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M15 7l-6.5 6.5a1.5 1.5 0 0 0 3 3l6.5 -6.5a3 3 0 0 0 -6 -6l-6.5 6.5a4.5 4.5 0 0 0 9 9l6.5 -6.5" />
 </svg>)";
 
